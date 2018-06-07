d464b79e56e800331797456cbf0f58b6ffd340e2
bakyt <abdrasulov@gmail.com>
2018-06-07 17:03:30 +0600

    Style Show Backup Words screen

--- app/src/main/java/org/grouvi/wallet/lib/WordsAdapter.kt
+++ app/src/main/java/org/grouvi/wallet/lib/WordsAdapter.kt
@@ -2,37 +2,17 @@ package org.grouvi.wallet.lib
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import org.grouvi.wallet.R
 
-class WordsAdapter(private val buttonClickListener: View.OnClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
+class WordsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     var items: List<String> = listOf()
 
-    companion object {
-        const val viewTypeWord = 1
-        const val viewTypeButton = 2
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+            ViewHolderWord(LayoutInflater.from(parent.context).inflate(R.layout.view_holder_word, parent, false) as TextView)
 
-    override fun getItemViewType(position: Int) = when {
-        position < items.size -> viewTypeWord
-        else -> viewTypeButton
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-
-        return if (viewType == viewTypeWord) {
-            ViewHolderWord(inflater.inflate(android.R.layout.simple_list_item_1, parent, false) as TextView)
-        } else {
-            ViewHolderButton(inflater.inflate(R.layout.view_holder_button, parent, false) as Button, buttonClickListener)
-        }
-    }
-
-    override fun getItemCount() = items.size + 1
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
@@ -41,10 +21,5 @@ class WordsAdapter(private val buttonClickListener: View.OnClickListener) : Recy
     }
 
     class ViewHolderWord(val textView: TextView) : RecyclerView.ViewHolder(textView)
-    class ViewHolderButton(button: Button, private val buttonClickListener: View.OnClickListener) : RecyclerView.ViewHolder(button) {
-        init {
-            button.setOnClickListener(buttonClickListener)
-        }
-    }
 
 }
--- app/src/main/java/org/grouvi/wallet/modules/backupWords/BackupWordsShowWordsFragment.kt
+++ app/src/main/java/org/grouvi/wallet/modules/backupWords/BackupWordsShowWordsFragment.kt
@@ -4,14 +4,18 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_backup_words_show_words.*
 import org.grouvi.wallet.R
+import org.grouvi.wallet.lib.WordsAdapter
 
 class BackupWordsShowWordsFragment : Fragment() {
+
     private lateinit var viewModel: BackupWordsViewModel
+    private val wordsAdapter = WordsAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_backup_words_show_words, container, false)
@@ -24,9 +28,13 @@ class BackupWordsShowWordsFragment : Fragment() {
             viewModel = ViewModelProviders.of(it).get(BackupWordsViewModel::class.java)
         }
 
+        recyclerWords.adapter = wordsAdapter
+        recyclerWords.layoutManager = LinearLayoutManager(context)
+
         viewModel.wordsLiveData.observe(this, Observer {
             it?.let {
-                textWords.text = it.joinToString(", ")
+                wordsAdapter.items = it
+                wordsAdapter.notifyDataSetChanged()
             }
         })
 
