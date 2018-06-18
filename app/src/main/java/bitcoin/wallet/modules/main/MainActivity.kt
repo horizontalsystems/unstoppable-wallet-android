da757ca902cba8a399208803efacb5e19b0a7a97
chyngyz <chyngyz.omurov@gmail.com>
2018-06-18 15:27:36 +0600

    Refactor bottom navigation bar.

--- app/src/main/java/bitcoin/wallet/modules/main/MainActivity.kt
+++ app/src/main/java/bitcoin/wallet/modules/main/MainActivity.kt
@@ -1,73 +1,42 @@
 package bitcoin.wallet.modules.main
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import bitcoin.wallet.R
-import bitcoin.wallet.modules.settings.SettingsFragment
-import bitcoin.wallet.modules.transactions.TransactionsFragment
-import bitcoin.wallet.modules.wallet.WalletFragment
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class MainActivity : AppCompatActivity() {
 
-    enum class FragmentTag {
-        WALLET, TRANSACTIONS, SETTINGS
-    }
+    private lateinit var adapter: MainTabsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        setSupportActionBar(toolbar)
+        adapter = MainTabsAdapter(supportFragmentManager)
 
-        // todo move title to fragments
-        supportActionBar?.title = "Balance"
+        viewPager.adapter = adapter
 
-        bottomNavigation.setOnNavigationItemSelectedListener {
-            val fragmentTag = when (it.itemId) {
-                R.id.action_wallet -> {
-                    FragmentTag.WALLET
-                }
-                R.id.action_transactions -> {
-                    FragmentTag.TRANSACTIONS
-                }
-                R.id.action_settings -> {
-                    FragmentTag.SETTINGS
-                }
-                else -> {
-                    FragmentTag.WALLET
-                }
+        bottomNavigation.defaultBackgroundColor = ContextCompat.getColor(this, R.color.appBackground)
+        bottomNavigation.accentColor = ContextCompat.getColor(this, R.color.orange)
+        bottomNavigation.inactiveColor = ContextCompat.getColor(this, R.color.grey)
+
+        bottomNavigation.addItem(AHBottomNavigationItem(R.string.tab_title_wallet, R.drawable.wallet, 0))
+        bottomNavigation.addItem(AHBottomNavigationItem(R.string.tab_title_transactions, R.drawable.transactions, 0))
+        bottomNavigation.addItem(AHBottomNavigationItem(R.string.tab_title_settings, R.drawable.settings, 0))
+
+        bottomNavigation.titleState = AHBottomNavigation.TitleState.ALWAYS_HIDE
+        bottomNavigation.setUseElevation(false)
+
+        bottomNavigation.setOnTabSelectedListener { position, wasSelected ->
+            if (!wasSelected) {
+                viewPager.setCurrentItem(position, false)
             }
-
-            val fragment = getFragmentByTag(fragmentTag)
-            supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.fl_main, fragment, fragmentTag.name)
-                    .commit()
-
             true
         }
-
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fl_main, getFragmentByTag(FragmentTag.WALLET), FragmentTag.WALLET.name)
-                .commit()
-
-
     }
 
-    private fun getFragmentByTag(fragmentTag: FragmentTag): Fragment {
-        return supportFragmentManager.findFragmentByTag(fragmentTag.name) ?: when (fragmentTag) {
-            FragmentTag.WALLET -> {
-                WalletFragment()
-            }
-            FragmentTag.TRANSACTIONS -> {
-                TransactionsFragment()
-            }
-            FragmentTag.SETTINGS -> {
-                SettingsFragment()
-            }
-        }
-    }
 }
