f3c18bb2d47538b403041de09b931e709f0136bd
bakyt <abdrasulov@gmail.com>
2018-06-26 18:01:06 +0600

    Stub LoginManager

--- app/src/main/java/bitcoin/wallet/core/managers/LoginManager.kt
+++ app/src/main/java/bitcoin/wallet/core/managers/LoginManager.kt
@@ -2,11 +2,27 @@ package bitcoin.wallet.core.managers
 
 import bitcoin.wallet.core.INetworkManager
 import io.reactivex.Completable
+import io.realm.ObjectServerError
+import io.realm.SyncCredentials
+import io.realm.SyncUser
 
 class LoginManager(private val networkManager: INetworkManager) {
 
-    fun login(words: List<String>) : Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun login(words: List<String>): Completable = Completable.create { emitter ->
+
+        val credentials = SyncCredentials.usernamePassword("bakyt", "123")
+
+        val authURL = "https://grouvi-wallet.us1a.cloud.realm.io"
+
+        SyncUser.logInAsync(credentials, authURL, object : SyncUser.Callback<SyncUser> {
+            override fun onSuccess(user: SyncUser) {
+                emitter.onComplete()
+            }
+
+            override fun onError(error: ObjectServerError) {
+                emitter.onError(error)
+            }
+        })
     }
 
 }
