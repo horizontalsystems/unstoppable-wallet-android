7822db2f052c14a08dd804039f663951a1a2569b
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2018-07-24 16:26:57 +0600

    separated toolbar, each fragment has its own toolbar, added scroll behaviour for toolbar

--- app/src/main/java/bitcoin/wallet/modules/transactions/TransactionsFragment.kt
+++ app/src/main/java/bitcoin/wallet/modules/transactions/TransactionsFragment.kt
@@ -2,6 +2,7 @@ package bitcoin.wallet.modules.transactions
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
@@ -10,20 +11,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import bitcoin.wallet.R
-import bitcoin.wallet.modules.main.BaseTabFragment
 import bitcoin.wallet.viewHelpers.DateHelper
 import bitcoin.wallet.viewHelpers.NumberFormatHelper
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.fragment_transactions.*
+import kotlinx.android.synthetic.main.view_holder_filter.*
 import kotlinx.android.synthetic.main.view_holder_transaction.*
 
-class TransactionsFragment : BaseTabFragment() {
-
-    override val title: Int
-        get() = R.string.tab_title_transactions
+class TransactionsFragment : android.support.v4.app.Fragment() {
 
     private lateinit var viewModel: TransactionsViewModel
     private val transactionsAdapter = TransactionsAdapter()
+    private val filterAdapter = FilterAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_transactions, container, false)
@@ -47,8 +46,12 @@ class TransactionsFragment : BaseTabFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        toolbar.setTitle(R.string.tab_title_transactions)
         recyclerTransactions.adapter = transactionsAdapter
         recyclerTransactions.layoutManager = LinearLayoutManager(context)
+
+        recyclerTags.adapter = filterAdapter
+        recyclerTags.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
     }
 }
 
@@ -78,5 +81,31 @@ class ViewHolderTransaction(override val containerView: View) : RecyclerView.Vie
         txAmount.text = "$sign ${NumberFormatHelper.cryptoAmountFormat.format(Math.abs(transactionRecord.amount.value))} ${transactionRecord.amount.coin.code}"
         txStatus.text = transactionRecord.status?.name
         txDate.text = DateHelper.getRelativeDateString(itemView.context, transactionRecord.date)
+    }
+}
+
+class FilterAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    var filters = listOf("All", "Bitcon", "Bitcon Cash", "Ethereum", "Litecoin")
+
+    override fun getItemCount() = filters.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+            ViewHolderFilter(LayoutInflater.from(parent.context).inflate(R.layout.view_holder_filter, parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolderFilter -> holder.bind(filters[position], active = position == 0)
+        }
+    }
+
+}
+
+class ViewHolderFilter(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+    fun bind(filterName: String, active: Boolean) {
+        filter_text.text = filterName
+        filter_text.setTextColor(if (active) Color.argb(255, 40, 43, 53) else Color.argb(255, 255, 173, 0))
+        filter_text.isActivated = active
     }
 }
