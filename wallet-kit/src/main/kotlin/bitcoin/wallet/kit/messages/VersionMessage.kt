99f649130042cf86b815eb70bc8656aea86bd8f8
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2018-08-27 13:59:54 +0600

    refactored VersionMessage

--- wallet-kit/src/main/kotlin/bitcoin/wallet/kit/messages/VersionMessage.kt
+++ wallet-kit/src/main/kotlin/bitcoin/wallet/kit/messages/VersionMessage.kt
@@ -61,17 +61,17 @@ class VersionMessage : Message {
     @Throws(IOException::class)
     constructor(payload: ByteArray) : super("version") {
         BitcoinInput(ByteArrayInputStream(payload)).use { input ->
-            this.protocolVersion = input.readInt()
-            this.services = input.readLong()
-            this.timestamp = input.readLong()
-            this.recipientAddress = NetworkAddress.parse(input, true)
-            if (this.protocolVersion >= 106) {
-                this.senderAddress = NetworkAddress.parse(input, true)
-                this.nonce = input.readLong()
-                this.subVersion = input.readString()
-                this.lastBlock = input.readInt()
-                if (this.protocolVersion >= 70001) {
-                    this.relay = input.readByte().toInt() != 0
+            protocolVersion = input.readInt()
+            services = input.readLong()
+            timestamp = input.readLong()
+            recipientAddress = NetworkAddress.parse(input, true)
+            if (protocolVersion >= 106) {
+                senderAddress = NetworkAddress.parse(input, true)
+                nonce = input.readLong()
+                subVersion = input.readString()
+                lastBlock = input.readInt()
+                if (protocolVersion >= 70001) {
+                    relay = input.readByte().toInt() != 0
                 }
             }
         }
@@ -80,29 +80,29 @@ class VersionMessage : Message {
     constructor() : super("version") {}
 
     constructor(lastBlock: Int, recipientAddr: InetAddress) : super("version") {
-        this.protocolVersion = BitcoinConstants.PROTOCOL_VERSION
-        this.services = BitcoinConstants.NETWORK_SERVICES
-        this.timestamp = System.currentTimeMillis() / 1000
-        this.recipientAddress = NetworkAddress(recipientAddr)
-        this.senderAddress = NetworkAddress(NetworkUtils.getLocalInetAddress())
-        this.nonce = BitcoinConstants.NODE_ID
-        this.subVersion = BitcoinConstants.SUB_VERSION
+        protocolVersion = BitcoinConstants.PROTOCOL_VERSION
+        services = BitcoinConstants.NETWORK_SERVICES
+        timestamp = System.currentTimeMillis() / 1000
+        recipientAddress = NetworkAddress(recipientAddr)
+        senderAddress = NetworkAddress(NetworkUtils.getLocalInetAddress())
+        nonce = BitcoinConstants.NODE_ID
+        subVersion = BitcoinConstants.SUB_VERSION
         this.lastBlock = lastBlock
-        this.relay = true
+        relay = true
     }
 
     override fun getPayload(): ByteArray {
         val output = BitcoinOutput()
-        output.writeInt(this.protocolVersion) // protocol
-                .writeLong(this.services) // services
+        output.writeInt(protocolVersion) // protocol
+                .writeLong(services) // services
                 .writeLong(timestamp) // timestamp
-                .write(this.recipientAddress.toByteArray(true)) // recipient-address
-        if (this.protocolVersion >= 106) {
-            output.write(this.senderAddress.toByteArray(true)) // sender-address
-                    .writeLong(this.nonce) // nodeId
-                    .writeString(this.subVersion) // sub-version-string
-                    .writeInt(this.lastBlock) // # of last block
-            if (this.protocolVersion >= 70001) {
+                .write(recipientAddress.toByteArray(true)) // recipient-address
+        if (protocolVersion >= 106) {
+            output.write(senderAddress.toByteArray(true)) // sender-address
+                    .writeLong(nonce) // nodeId
+                    .writeString(subVersion) // sub-version-string
+                    .writeInt(lastBlock) // # of last block
+            if (protocolVersion >= 70001) {
                 output.writeByte(1)
             }
         }
@@ -110,7 +110,6 @@ class VersionMessage : Message {
     }
 
     override fun toString(): String {
-        return ("VersionMessage(lastBlock=" + this.lastBlock + ", protocol=" + this.protocolVersion + ", timestamp="
-                + this.timestamp + ")")
+        return ("VersionMessage(lastBlock=$lastBlock, protocol=$protocolVersion, timestamp=$timestamp)")
     }
 }
