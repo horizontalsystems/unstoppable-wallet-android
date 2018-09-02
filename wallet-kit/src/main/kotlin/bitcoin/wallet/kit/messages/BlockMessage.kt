4390067f30d4f925f67838b93df82fd6ec120089
Talgat Medetbekov <medetbekov.t@gmail.com>
2018-09-02 10:59:28 +0600

    Keep serialized block message fields separately

--- wallet-kit/src/main/kotlin/bitcoin/wallet/kit/messages/BlockMessage.kt
+++ wallet-kit/src/main/kotlin/bitcoin/wallet/kit/messages/BlockMessage.kt
@@ -1,43 +1,51 @@
 package bitcoin.wallet.kit.messages
 
-import bitcoin.wallet.kit.models.Block
+import bitcoin.wallet.kit.models.Header
+import bitcoin.wallet.kit.models.Transaction
 import bitcoin.walllet.kit.io.BitcoinInput
+import bitcoin.walllet.kit.io.BitcoinOutput
 import java.io.ByteArrayInputStream
 import java.io.IOException
-import java.util.*
 
 /**
  * The 'block' message consists of a single serialized block.
  */
 class BlockMessage() : Message("block") {
 
-    lateinit var block: Block
+    lateinit var header: Header
+    lateinit var transactions: Array<Transaction>
 
     @Throws(IOException::class)
     constructor(payload: ByteArray) : this() {
         BitcoinInput(ByteArrayInputStream(payload)).use { input ->
-            block = Block(input)
+            header = Header(input)
+            val txCount = input.readVarInt() // do not store count
+            transactions = Array(txCount.toInt()) {
+                Transaction(input)
+            }
         }
     }
 
     override fun getPayload(): ByteArray {
-        return this.block.toByteArray()
+        val output = BitcoinOutput()
+        output.write(header.toByteArray())
+        output.writeVarInt(transactions.size.toLong())
+        for (transaction in transactions) {
+            output.write(transaction.toByteArray())
+        }
+
+        return output.toByteArray()
     }
 
     /**
      * Validate block hash.
      */
     fun validateHash(): Boolean {
-        val merkleHash = block.calculateMerkleHash()
-        if (!Arrays.equals(merkleHash, block.header.merkleHash)) {
-            return false
-        }
-
         // TODO: validate bits:
         return true
     }
 
     override fun toString(): String {
-        return "BlockMessage(txnCount=${block.txns.size})"
+        return "BlockMessage(txCount=${transactions.size})"
     }
 }
