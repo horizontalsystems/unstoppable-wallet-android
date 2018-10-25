aef8324a66d5cda07ba140866939e55759ae740c
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2018-10-25 16:07:42 +0600

    Add About section to Settings (#196)

--- app/src/main/java/bitcoin/wallet/modules/settings/main/MainSettingsFragment.kt
+++ app/src/main/java/bitcoin/wallet/modules/settings/main/MainSettingsFragment.kt
@@ -13,6 +13,7 @@ import bitcoin.wallet.R
 import bitcoin.wallet.modules.currencyswitcher.CurrencySwitcherModule
 import bitcoin.wallet.modules.main.MainActivity
 import bitcoin.wallet.modules.main.MainModule
+import bitcoin.wallet.modules.settings.AboutSettingsActivity
 import bitcoin.wallet.modules.settings.language.LanguageSettingsModule
 import bitcoin.wallet.modules.settings.security.SecuritySettingsModule
 import bitcoin.wallet.viewHelpers.LayoutHelper
@@ -106,8 +107,8 @@ class MainSettingsFragment : android.support.v4.app.Fragment() {
         })
 
         viewModel.showAboutLiveEvent.observe(this, Observer {
-            context?.let {
-                //open About page
+            activity?.let {
+                AboutSettingsActivity.start(it)
             }
         })
 
