150c1a2169982498d59b1fbde27587d5461dc773
chyngyz <chyngyz.omurov@gmail.com>
2018-11-14 18:06:19 +0600

    Add coin manager

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/CoinManager.kt
@@ -0,0 +1,30 @@
+package io.horizontalsystems.bankwallet.core.managers
+
+import io.horizontalsystems.bankwallet.core.IAppConfigProvider
+import io.horizontalsystems.bankwallet.core.IWalletManager
+import io.horizontalsystems.bankwallet.core.IWordsManager
+import io.reactivex.disposables.CompositeDisposable
+
+class CoinManager(private val wordsManager: IWordsManager,
+                  private val walletManager: IWalletManager,
+                  private val appConfigProvider: IAppConfigProvider) {
+
+    private var disposables: CompositeDisposable = CompositeDisposable()
+
+    init {
+        syncWallets()
+
+        wordsManager.loggedInSubject.subscribe {
+            syncWallets()
+        }.let {
+            disposables.add(it)
+        }
+    }
+
+    private fun syncWallets() {
+        wordsManager.words?.let {
+            walletManager.initWallets(it, appConfigProvider.enabledCoins)
+        } ?: walletManager.clearWallets()
+    }
+
+}
