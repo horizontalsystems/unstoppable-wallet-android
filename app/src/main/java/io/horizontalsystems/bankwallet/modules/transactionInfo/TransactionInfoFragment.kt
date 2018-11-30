f7ed611f2c04824aa9de0aa737a9682b0cd8cd12
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2018-11-30 12:20:52 +0600

    Fix crashing Processing Transaction progress (#307)

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactionInfo/TransactionInfoFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactionInfo/TransactionInfoFragment.kt
@@ -72,7 +72,7 @@ class TransactionInfoFragment : DialogFragment() {
                         else -> R.drawable.checkmark_green
                     }
                     val progress = when (txStatus) {
-                        is TransactionStatus.Processing -> txStatus.progress
+                        is TransactionStatus.Processing -> txStatus.progress.times(100).toInt()
                         else -> null
                     }
                     val statusText = when (txStatus) {
