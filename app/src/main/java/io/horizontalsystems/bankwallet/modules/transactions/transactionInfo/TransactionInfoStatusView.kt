c6981e2251a583d73034950b7e84020899ab7908
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-01-22 18:25:53 +0600

    Implement new design for transactions status in Transactions screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoStatusView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoStatusView.kt
@@ -8,7 +8,7 @@ import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.modules.transactions.TransactionStatus
 import io.horizontalsystems.bankwallet.viewHelpers.LayoutHelper
-import kotlinx.android.synthetic.main.view_transaction_status.view.*
+import kotlinx.android.synthetic.main.view_transaction_info_status.view.*
 
 
 open class TransactionInfoStatusView : ConstraintLayout {
@@ -26,7 +26,7 @@ open class TransactionInfoStatusView : ConstraintLayout {
     }
 
     private fun initializeViews() {
-        ConstraintLayout.inflate(context, R.layout.view_transaction_status, this)
+        ConstraintLayout.inflate(context, R.layout.view_transaction_info_status, this)
     }
 
     fun bind(transactionStatus: TransactionStatus) {
@@ -52,19 +52,15 @@ open class TransactionInfoStatusView : ConstraintLayout {
     }
 
     private fun fillProgress(transactionStatus: TransactionStatus.Processing) {
+        val greyBar = R.drawable.status_progress_bar_grey
         val greenBar = R.drawable.status_progress_bar_green
 
-        progressBar1.setImageResource(greenBar)
-        if (transactionStatus.progress == 1) return
-        progressBar2.setImageResource(greenBar)
-        if (transactionStatus.progress == 2) return
-        progressBar3.setImageResource(greenBar)
-        if (transactionStatus.progress == 3) return
-        progressBar4.setImageResource(greenBar)
-        if (transactionStatus.progress == 4) return
-        progressBar5.setImageResource(greenBar)
-        if (transactionStatus.progress == 5) return
-        progressBar6.setImageResource(greenBar)
+        progressBar1.setImageResource(if (transactionStatus.progress >= 1) greenBar else greyBar)
+        progressBar2.setImageResource(if (transactionStatus.progress >= 2) greenBar else greyBar)
+        progressBar3.setImageResource(if (transactionStatus.progress >= 3) greenBar else greyBar)
+        progressBar4.setImageResource(if (transactionStatus.progress >= 4) greenBar else greyBar)
+        progressBar5.setImageResource(if (transactionStatus.progress >= 5) greenBar else greyBar)
+        progressBar6.setImageResource(if (transactionStatus.progress >= 6) greenBar else greyBar)
     }
 
 }
