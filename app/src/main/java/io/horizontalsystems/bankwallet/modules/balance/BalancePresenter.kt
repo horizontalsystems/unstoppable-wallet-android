ad699a23e504eb0e757e437ec95eb7adc88c9d5f
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-05-23 14:00:37 +0600

    Check freshness of exchange rates on Balance Page each minute (#863)

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalancePresenter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalancePresenter.kt
@@ -5,7 +5,13 @@ import io.horizontalsystems.bankwallet.core.IAdapter
 import io.horizontalsystems.bankwallet.entities.Currency
 import io.horizontalsystems.bankwallet.entities.Rate
 import io.horizontalsystems.bankwallet.modules.transactions.CoinCode
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import java.math.BigDecimal
+import java.util.concurrent.TimeUnit
 
 class BalancePresenter(
         private var interactor: BalanceModule.IInteractor,
@@ -14,6 +20,8 @@ class BalancePresenter(
         private val factory: BalanceViewItemFactory) : BalanceModule.IViewDelegate, BalanceModule.IInteractorDelegate {
 
     var view: BalanceModule.IView? = null
+    private val disposables = CompositeDisposable()
+    private var flushSubject = PublishSubject.create<Unit>()
 
     //
     // BalanceModule.IViewDelegate
@@ -24,6 +32,24 @@ class BalancePresenter(
 
     override fun viewDidLoad() {
         interactor.initAdapters()
+
+        flushSubject
+                .debounce(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext { updateViewItems() }
+                .subscribe()?.let { disposables.add(it) }
+
+        Flowable.interval(1, TimeUnit.MINUTES)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    dataSource.items.forEach { item ->
+                        item.rate?.let { rate ->
+                            didUpdateRate(rate)
+                        }
+                    }
+                }?.let { disposables.add(it) }
     }
 
     override fun getViewItem(position: Int) =
@@ -46,6 +72,7 @@ class BalancePresenter(
 
     override fun onClear() {
         interactor.clear()
+        disposables.clear()
     }
 
     //
@@ -70,21 +97,21 @@ class BalancePresenter(
     override fun didUpdateBalance(coinCode: CoinCode, balance: BigDecimal) {
         val position = dataSource.getPosition(coinCode)
         dataSource.setBalance(position, balance)
-        view?.updateItem(position)
+        updateByPosition(position)
         view?.updateHeader()
     }
 
     override fun didUpdateState(coinCode: String, state: AdapterState) {
         val position = dataSource.getPosition(coinCode)
         dataSource.setState(position, state)
-        view?.updateItem(position)
+        updateByPosition(position)
         view?.updateHeader()
     }
 
     override fun didUpdateRate(rate: Rate) {
         val position = dataSource.getPosition(rate.coinCode)
         dataSource.setRate(position, rate)
-        view?.updateItem(position)
+        updateByPosition(position)
         view?.updateHeader()
     }
 
@@ -94,6 +121,18 @@ class BalancePresenter(
 
     override fun openManageCoins() {
         router.openManageCoins()
+    }
+
+    private fun updateViewItems() {
+        dataSource.getUpdatedPositions().forEach {
+            view?.updateItem(it)
+        }
+        dataSource.clearUpdatedPositions()
+    }
+
+    private fun updateByPosition(position: Int) {
+        dataSource.addUpdatedPosition(position)
+        flushSubject.onNext(Unit)
     }
 
 }
