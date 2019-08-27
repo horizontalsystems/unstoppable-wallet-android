5a2c7c30d59edfbf3968382e9f3e14cfe33e4d88
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-08-27 11:30:43 +0600

    Open WelcomeActivity without slide animation on Android 9

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/welcome/WelcomeModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/welcome/WelcomeModule.kt
@@ -34,7 +34,7 @@ object WelcomeModule {
 
     fun start(context: Context) {
         val intent = Intent(context, WelcomeActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
         context.startActivity(intent)
     }
 
