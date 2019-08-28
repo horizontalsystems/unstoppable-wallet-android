e616075e742ff393b8cb404bccde7cc5666025df
chyngyz <chyngyz.omurov@gmail.com>
2019-08-28 16:57:25 +0600

    Replace SendEthereumPresenter with SendEthereumHandler

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendEthereumHandler.kt
@@ -0,0 +1,106 @@
+package io.horizontalsystems.bankwallet.modules.send
+
+import io.horizontalsystems.bankwallet.modules.send.submodules.address.SendAddressModule
+import io.horizontalsystems.bankwallet.modules.send.submodules.amount.SendAmountModule
+import io.horizontalsystems.bankwallet.modules.send.submodules.fee.SendFeeModule
+import io.reactivex.Single
+import java.math.BigDecimal
+
+class SendEthereumHandler(private val interactor: SendModule.ISendEthereumInteractor,
+                          private val router: SendModule.IRouter) : SendModule.ISendHandler,
+        SendAmountModule.IAmountModuleDelegate,
+        SendAddressModule.IAddressModuleDelegate,
+        SendFeeModule.IFeeModuleDelegate {
+
+    private fun syncValidation() {
+        try {
+            amountModule.validAmount()
+            addressModule.validAddress()
+
+            delegate.onChange(true)
+
+        } catch (e: Exception) {
+            delegate.onChange(false)
+        }
+    }
+
+    private fun syncAvailableBalance() {
+        amountModule.setAvailableBalance(interactor.availableBalance(gasPrice = feeModule.feeRate))
+    }
+
+    private fun syncFee() {
+        feeModule.setFee(interactor.fee(feeModule.feeRate))
+    }
+
+    // SendModule.ISendHandler
+
+    override lateinit var amountModule: SendAmountModule.IAmountModule
+
+    override lateinit var addressModule: SendAddressModule.IAddressModule
+
+    override lateinit var feeModule: SendFeeModule.IFeeModule
+
+    override lateinit var delegate: SendModule.ISendHandlerDelegate
+
+    override val inputItems: List<SendModule.Input>
+        get() = listOf(
+                SendModule.Input.Amount,
+                SendModule.Input.Address,
+                SendModule.Input.Fee(true),
+                SendModule.Input.ProceedButton)
+
+    override fun confirmationViewItems(): List<SendModule.SendConfirmationViewItem> {
+        TODO("not implemented")
+    }
+
+    override fun sendSingle(): Single<Unit> {
+        return interactor.send(amountModule.validAmount(), addressModule.validAddress(), feeModule.feeRate)
+    }
+
+    override fun onModulesDidLoad() {
+        syncAvailableBalance()
+        feeModule.setAvailableFeeBalance(interactor.ethereumBalance)
+        syncFee()
+    }
+
+    override fun onAddressScan(address: String) {
+        addressModule.didScanQrCode(address)
+    }
+
+    // SendAmountModule.IAmountModuleDelegate
+
+    override fun onChangeAmount() {
+        syncValidation()
+    }
+
+    override fun onChangeInputType(inputType: SendModule.InputType) {
+        feeModule.setInputType(inputType)
+    }
+
+    // SendAddressModule.IAddressModuleDelegate
+
+    override fun validate(address: String) {
+        interactor.validate(address)
+    }
+
+    override fun onUpdateAddress() {
+        syncValidation()
+    }
+
+    override fun onUpdateAmount(amount: BigDecimal) {
+        amountModule.setAmount(amount)
+    }
+
+    override fun scanQrCode() {
+        router.scanQrCode()
+    }
+
+    // SendFeeModule.IFeeModuleDelegate
+
+    override fun onUpdateFeeRate(feeRate: Long) {
+        syncAvailableBalance()
+        syncFee()
+        syncValidation()
+    }
+
+}
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/ethereum/SendEthereumInteractor.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/ethereum/SendEthereumInteractor.kt
@@ -2,16 +2,10 @@ package io.horizontalsystems.bankwallet.modules.send.ethereum
 
 import io.horizontalsystems.bankwallet.core.ISendEthereumAdapter
 import io.horizontalsystems.bankwallet.modules.send.SendModule
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.Single
 import java.math.BigDecimal
 
 class SendEthereumInteractor(private val adapter: ISendEthereumAdapter) : SendModule.ISendEthereumInteractor {
-    private val disposables = CompositeDisposable()
-
-    var delegate: SendModule.ISendEthereumInteractorDelegate? = null
-
     override val ethereumBalance: BigDecimal
         get() = adapter.ethereumBalance
 
@@ -27,19 +21,8 @@ class SendEthereumInteractor(private val adapter: ISendEthereumAdapter) : SendMo
         return adapter.fee(gasPrice)
     }
 
-    override fun send(amount: BigDecimal, address: String, gasPrice: Long) {
-        adapter.send(amount, address, gasPrice)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    delegate?.didSend()
-                }, { error ->
-                    delegate?.didFailToSend(error)
-                }).let { disposables.add(it) }
-    }
-
-    override fun clear() {
-        disposables.clear()
+    override fun send(amount: BigDecimal, address: String, gasPrice: Long): Single<Unit> {
+        return adapter.send(amount, address, gasPrice)
     }
 
 }
