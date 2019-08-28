1505ca08eb2da3bce5bacb891de33bb21cb8cecc
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-08-28 14:00:43 +0600

    Disable transition animation for MainActivity

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/main/MainModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/main/MainModule.kt
@@ -1,5 +1,6 @@
 package io.horizontalsystems.bankwallet.modules.main
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import io.horizontalsystems.bankwallet.core.App
@@ -35,12 +36,13 @@ object MainModule {
         context.startActivity(intent)
     }
 
-    fun startAsNewTask(context: Context, activeTab: Int? = null) {
+    fun startAsNewTask(context: Activity, activeTab: Int? = null) {
         val intent = Intent(context, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         activeTab?.let {
             intent.putExtra(MainActivity.ACTIVE_TAB_KEY, it)
         }
         context.startActivity(intent)
+        context.overridePendingTransition(0,0)
     }
 }
