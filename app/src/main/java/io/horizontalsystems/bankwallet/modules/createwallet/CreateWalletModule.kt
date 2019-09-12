398d05a1dec396ba7229b05ba70fb831314f7bcf
bakyt <abdrasulov@gmail.com>
2019-09-12 18:34:46 +0600

    Implement CreateWalletPresenter

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/createwallet/CreateWalletModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/createwallet/CreateWalletModule.kt
@@ -2,9 +2,39 @@ package io.horizontalsystems.bankwallet.modules.createwallet
 
 import android.content.Context
 import android.content.Intent
-import io.horizontalsystems.bankwallet.modules.restore.RestoreActivity
+import io.horizontalsystems.bankwallet.entities.Coin
+import io.horizontalsystems.bankwallet.entities.FeaturedCoin
 
 object CreateWalletModule {
+
+    interface IView {
+        fun setItems(items: List<CoinViewItem>)
+        fun setCreateEnabled(enabled: Boolean)
+    }
+
+    interface IRouter {
+        fun startMainModule()
+    }
+
+    interface IViewDelegate {
+        fun viewDidLoad()
+        fun didEnable(position: Int)
+        fun didDisable(position: Int)
+        fun didCreate()
+    }
+
+    interface IInteractor {
+        val featuredCoins: List<FeaturedCoin>
+
+        fun createWallet(coins: List<Coin>)
+    }
+
+    class State {
+        var coins = listOf<Coin>()
+        var enabledPositions = setOf<Int>()
+    }
+
+    data class CoinViewItem(val title: String, val code: String, val selected: Boolean)
 
     fun start(context: Context) {
         context.startActivity(Intent(context, CreateWalletActivity::class.java))
