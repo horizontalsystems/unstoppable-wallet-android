ad70c054e152d68110fbb9de74833512b3017014
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-10-24 11:18:55 +0600

    Add elements to CellView

--- app/src/main/java/io/horizontalsystems/bankwallet/components/CellLeftView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/components/CellLeftView.kt
@@ -1,10 +1,13 @@
 package io.horizontalsystems.bankwallet.components
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.entities.Coin
 import kotlinx.android.synthetic.main.view_cell_left.view.*
 
 class CellLeftView : ConstraintLayout {
@@ -20,6 +23,40 @@ class CellLeftView : ConstraintLayout {
             field = value
             cellSubtitle.text = value
             cellSubtitle.visibility = if (value == null) View.GONE else View.VISIBLE
+        }
+
+    var imageResource: Int? = null
+        set(value) {
+            field = value
+            value?.let { cellIcon.bind(it) }
+            cellIcon.visibility = if (value == null) View.GONE else View.VISIBLE
+        }
+
+    var imageDrawable: Drawable? = null
+        set(value) {
+            field = value
+            value?.let { cellIcon.bind(it) }
+            cellIcon.visibility = if (value == null) View.GONE else View.VISIBLE
+        }
+
+    var imageCoin: Coin? = null
+        set(value) {
+            field = value
+            value?.let { cellIcon.bind(it) }
+            cellIcon.visibility = if (value == null) View.GONE else View.VISIBLE
+        }
+
+    var imageCoinCode: String? = null
+        set(value) {
+            field = value
+            value?.let { cellIcon.bind(it) }
+            cellIcon.visibility = if (value == null) View.GONE else View.VISIBLE
+        }
+
+    var imageTint: ColorStateList? = null
+        set(value) {
+            field = value
+            value?.let { cellIcon.setTint(it) }
         }
 
     init {
