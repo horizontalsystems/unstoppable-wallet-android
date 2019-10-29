28b6d0ab5d20575b6cf7fd09ac71f9f4624297c1
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-10-29 16:45:31 +0600

    Use animation-list for transaction progress animation

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionProgressView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionProgressView.kt
@@ -2,7 +2,6 @@ package io.horizontalsystems.bankwallet.modules.transactions
 
 import android.content.Context
 import android.graphics.drawable.AnimationDrawable
-import android.os.Handler
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import io.horizontalsystems.bankwallet.R
@@ -27,94 +26,13 @@ class TransactionProgressView : ConstraintLayout {
         inflate(context, R.layout.view_transaction_progress, this)
     }
 
-    private var threadHandler: Handler? = null
-    private var runnable: Runnable? = null
-    private var animationDelayTime: Long = 150
-    private var lastAnimatedIndex = 0
-    private var progressIndex = 0
-
     fun bind(progress: Double) {
         when {
-            progress <= 0.33 -> {
-                progressIndex = 0
-            }
-            progress <= 0.66 -> {
-                progressIndex = 1
-                animateFirstBar()
-            }
-            progress <= 1.0 -> {
-                progressIndex = 2
-                animateFirstBar()
-                animateSecondBar()
-            }
-            else -> {
-                animateFirstBar()
-                animateSecondBar()
-                animateThirdBar()
-                return
-            }
+            progress <= 0.33 -> progressBar.setBackgroundResource(R.drawable.animation_0_percent_progress)
+            progress <= 0.66 -> progressBar.setBackgroundResource(R.drawable.animation_30_percent_progress)
+            progress <= 1.0 -> progressBar.setBackgroundResource(R.drawable.animation_60_percent_progress)
         }
-
-        animateProgress()
-    }
-
-    private fun animateProgress() {
-        if (threadHandler == null && runnable == null) {
-            lastAnimatedIndex = progressIndex
-
-            threadHandler = Handler()
-            runnable = object : Runnable {
-                override fun run() {
-
-                    threadHandler?.postDelayed(this, animationDelayTime)
-                    if (lastAnimatedIndex == 0) {
-                        animateFirstBar()
-                        lastAnimatedIndex = 1
-                    } else if (lastAnimatedIndex == 1) {
-                        animateSecondBar()
-                        lastAnimatedIndex = 2
-                    } else if (lastAnimatedIndex == 2) {
-                        animateThirdBar()
-                        lastAnimatedIndex = 3
-                    } else {
-                        resetBarsColor()
-                        lastAnimatedIndex = progressIndex
-                    }
-                    invalidate()
-                }
-            }
-            runnable?.run()
-        }
-    }
-
-    private fun resetBarsColor() {
-        if (progressIndex < 1) {
-            progressBar1.setBackgroundResource(R.drawable.status_progress_bar_grey_20_small)
-        }
-        if (progressIndex < 2) {
-            progressBar2.setBackgroundResource(R.drawable.status_progress_bar_grey_20_small)
-        }
-        if (progressIndex < 3) {
-            progressBar3.setBackgroundResource(R.drawable.status_progress_bar_grey_20_small)
-        }
-    }
-
-    private fun animateFirstBar() {
-        progressBar1.setBackgroundResource(R.drawable.bar_frame_animated)
-        val frameAnimation = progressBar1.background as AnimationDrawable
-        frameAnimation.start()
-    }
-
-    private fun animateSecondBar() {
-        progressBar2.setBackgroundResource(R.drawable.bar_frame_animated)
-        val frameAnimation2 = progressBar2.background as AnimationDrawable
-        frameAnimation2.start()
-    }
-
-    private fun animateThirdBar() {
-        progressBar3.setBackgroundResource(R.drawable.bar_frame_animated)
-        val frameAnimation3 = progressBar3.background as AnimationDrawable
-        frameAnimation3.start()
+        (progressBar.background as? AnimationDrawable)?.start()
     }
 
 }
