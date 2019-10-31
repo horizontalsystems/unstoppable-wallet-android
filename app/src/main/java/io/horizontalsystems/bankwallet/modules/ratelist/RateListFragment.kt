601a19e7c43df504a9702d3d458f9da4cfcdc980
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2019-10-31 18:29:10 +0600

    Refactor RateList module
    - Use XRateManager instead of RateStatsManager

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/ratelist/RateListFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/ratelist/RateListFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
@@ -29,37 +29,40 @@ class RatesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val presenter = ViewModelProviders.of(this, RateListModule.Factory()).get(RateListPresenter::class.java)
+        val presenter = ViewModelProvider(this, RateListModule.Factory()).get(RateListPresenter::class.java)
         observeView(presenter.view)
         presenter.viewDidLoad()
 
-        adapter = CoinRatesAdapter(presenter)
+        adapter = CoinRatesAdapter()
         coinRatesRecyclerView.adapter = adapter
     }
 
     private fun observeView(view: RateListView) {
-        view.currentDate.observe(viewLifecycleOwner, Observer {
-            dateText.text = DateHelper.formatDate(Date(), "MMM dd")
-        })
-        view.reloadLiveEvent.observe(viewLifecycleOwner, Observer {
+        view.rateListViewItem.observe(viewLifecycleOwner, Observer { rateListViewItem->
+            dateText.text = DateHelper.formatDate(rateListViewItem.currentDate, "MMM dd")
+            setLastUpdatedTime(rateListViewItem.lastUpdateTimestamp)
+
+            adapter.items = rateListViewItem.rateViewItems
             adapter.notifyDataSetChanged()
         })
-        view.timeAgo.observe(viewLifecycleOwner, Observer { secondsAgo ->
-            val timeAgo = when {
-                secondsAgo < 60 -> getString(R.string.LockscreenRateList_secondsAgo, secondsAgo)
-                secondsAgo < 60 * 60 -> getString(R.string.LockscreenRateList_minutesAgo, secondsAgo/60)
-                else -> getString(R.string.LockscreenRateList_hoursAgo, secondsAgo/60/60)
-            }
-            timeAgoText.text = timeAgo
-        })
+    }
+
+    private fun setLastUpdatedTime(lastUpdateTimestamp: Long?) {
+        if (lastUpdateTimestamp == null)
+            return
+
+        val time = DateHelper.formatDate(Date(lastUpdateTimestamp), "HH:mm")
+        timeAgoText.text = getString(R.string.LockscreenRateList_updated, time)
     }
 }
 
 
-class CoinRatesAdapter(private val presenter: RateListPresenter) : RecyclerView.Adapter<ViewHolderCoinRate>() {
+class CoinRatesAdapter : RecyclerView.Adapter<ViewHolderCoinRate>() {
+
+    var items = listOf<RateViewItem>()
 
     override fun getItemCount(): Int {
-        return presenter.itemsCount
+        return items.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderCoinRate {
@@ -67,7 +70,7 @@ class CoinRatesAdapter(private val presenter: RateListPresenter) : RecyclerView.
     }
 
     override fun onBindViewHolder(holder: ViewHolderCoinRate, position: Int) {
-        holder.bind(presenter.getViewItem(position), position == itemCount - 1)
+        holder.bind(items[position], position == itemCount - 1)
     }
 
 }
@@ -79,34 +82,26 @@ class ViewHolderCoinRate(override val containerView: View) : RecyclerView.ViewHo
         txCoinCode.text = viewItem.coin.code
         txCoinName.text = viewItem.coin.title
 
-        txValueInFiat.text = if (viewItem.loadingStatus != RateLoadingStatus.Loading) "----" else ""
+        txValueInFiat.text =  containerView.context.getString(R.string.NotAvailable)
         LayoutHelper.getAttr(R.attr.ColorLeah, containerView.context.theme)?.let { color ->
             txValueInFiat.setTextColor(color)
         }
-        viewItem.rate?.let { exchangeValue ->
-            val rateString = App.numberFormatter.format(exchangeValue, trimmable = true, canUseLessSymbol = false)
+        viewItem.rate?.let { rate ->
+            val rateString = App.numberFormatter.format(rate, trimmable = true, canUseLessSymbol = false)
             txValueInFiat.text = rateString
-            if (viewItem.rateExpired){
+            if (viewItem.rateExpired == true){
                 txValueInFiat.setTextColor(ContextCompat.getColor(containerView.context, R.color.grey_50))
             }
         }
 
-        txDiff.visibility = View.GONE
-        txDiffNa.visibility = View.GONE
-
-        if (viewItem.loadingStatus == RateLoadingStatus.Loading) {
-            loadingSpinner.visibility = View.VISIBLE
+        val diff = viewItem.diff
+        if (viewItem.rateExpired == false && diff != null){
+            txDiff.bind(diff, containerView.context, true)
+            txDiff.visibility = View.VISIBLE
+            txDiffNa.visibility = View.GONE
         } else {
-            loadingSpinner.visibility = View.GONE
-
-            viewItem.diff?.let {
-                txDiff.bind(it, containerView.context, true)
-                txDiff.visibility = View.VISIBLE
-                txDiffNa.visibility = View.GONE
-            } ?: run {
-                txDiff.visibility = View.GONE
-                txDiffNa.visibility = View.VISIBLE
-            }
+            txDiff.visibility = View.GONE
+            txDiffNa.visibility = View.VISIBLE
         }
 
         bottomShade.visibility = if (isLast) View.VISIBLE else View.GONE
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/ratelist/RateListModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/ratelist/RateListModule.kt
@@ -4,63 +4,53 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.managers.CurrentDateProvider
-import io.horizontalsystems.bankwallet.core.managers.StatsData
 import io.horizontalsystems.bankwallet.entities.Coin
 import io.horizontalsystems.bankwallet.entities.Currency
 import io.horizontalsystems.bankwallet.entities.CurrencyValue
-import io.horizontalsystems.bankwallet.entities.Rate
-import io.horizontalsystems.bankwallet.lib.chartview.ChartView
+import io.horizontalsystems.xrateskit.entities.MarketInfo
 import java.math.BigDecimal
 import java.util.*
 
 object RateListModule {
 
     interface IView {
-        fun showCurrentDate(currentDate: Date)
-        fun reload()
-        fun setTimeAgo(secondsAgo: Long)
+        fun show(item: RateListViewItem)
     }
 
-    interface IRouter {
-    }
+    interface IRouter
 
     interface IViewDelegate {
         fun viewDidLoad()
-        val itemsCount: Int
-        fun getViewItem(position: Int): RateViewItem
     }
 
     interface IInteractor {
-        val currentDate: Date
         val currency: Currency
         val coins: List<Coin>
 
         fun clear()
-        fun initRateList()
-        fun getRateStats(coinCodes: List<String>, currencyCode: String)
-        fun fetchRates(coinCodes: List<String>, currencyCode: String)
+        fun getMarketInfo(coinCode: String, currencyCode: String): MarketInfo?
+        fun subscribeToMarketInfo(currencyCode: String)
+        fun setupXRateManager(coinCodes: List<String>)
     }
 
     interface IInteractorDelegate {
-        fun onReceive(statsData: StatsData)
-        fun onFailStats(coinCode: String)
-        fun didUpdateRate(rate: Rate)
-        fun willEnterForeground()
+        fun didUpdateMarketInfo(marketInfos: Map<String, MarketInfo>)
+    }
+
+    interface IRateListFactory{
+        fun marketInfoViewItem(coins: List<Coin>, currency: Currency, marketInfos: Map<String, MarketInfo?>) : RateListViewItem
     }
 
     class Factory : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             val view = RateListView()
             val interactor = RatesInteractor(
-                    App.rateStatsManager,
-                    App.rateStorage,
-                    CurrentDateProvider(),
-                    App.backgroundManager,
+                    App.xRateManager,
                     App.currencyManager,
                     App.walletStorage,
                     App.appConfigProvider,
                     RateListSorter())
-            val presenter = RateListPresenter(view, interactor, DataSource())
+            val presenter = RateListPresenter(view, interactor, RateListFactory(CurrentDateProvider()))
 
             interactor.delegate = presenter
 
@@ -68,47 +58,6 @@ object RateListModule {
         }
     }
 
-    class DataSource() {
-
-        private val chartType = ChartView.ChartType.DAILY.name
-
-        var items = listOf<RateViewItem>()
-
-        fun setViewItems(coins: List<Coin>){
-            items = coins.map { RateViewItem(it) }
-        }
-
-        val coinCodes: List<String>
-            get() {
-                return items.map { it.coin.code }
-            }
-
-        fun setChartData(statsData: StatsData) {
-            val chartDiff = statsData.diff[chartType]
-            getPositionByCoinCode(statsData.coinCode)?.let { position ->
-                items[position].diff = chartDiff
-                items[position].loadingStatus = RateLoadingStatus.Loaded
-            }
-        }
-
-        fun setRate(rate: Rate, baseCurrency: Currency) {
-            getPositionByCoinCode(rate.coinCode)?.let { position ->
-                items[position].rate = CurrencyValue(baseCurrency, rate.value)
-                items[position].rateExpired = rate.expired
-            }
-        }
-
-        fun setStatsFailed(coinCode: String) {
-            getPositionByCoinCode(coinCode)?.let { position ->
-                items[position].loadingStatus = RateLoadingStatus.Failed
-            }
-        }
-
-        private fun getPositionByCoinCode(coinCode: String): Int? {
-            val index = items.indexOfFirst { it.coin.code == coinCode }
-            return if (index >= 0) index else null
-        }
-    }
 }
 
 class RateListSorter {
@@ -126,13 +75,5 @@ class RateListSorter {
     }
 }
 
-class RateViewItem(val coin: Coin) {
-    var rateExpired: Boolean = false
-    var rate: CurrencyValue? = null
-    var diff: BigDecimal? = null
-    var loadingStatus: RateLoadingStatus = RateLoadingStatus.Loading
-}
-
-enum class RateLoadingStatus {
-    Loading, Loaded, Failed
-}
+class RateListViewItem(val currentDate: Date, val lastUpdateTimestamp: Long?, val rateViewItems: List<RateViewItem>)
+class RateViewItem(val coin: Coin, var rateExpired: Boolean?, var rate: CurrencyValue?, var diff: BigDecimal?)
