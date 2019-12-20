a19c960ade916f9c60a7bba478987910afc4f140
chyngyz <chyngyz.omurov@gmail.com>
2019-12-20 17:33:26 +0600

    Add transaction full info provider coin.space for BCH - remove blockdozer.com provider

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/fulltransactioninfo/providers/CoinSpaceBitcoinCashProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/fulltransactioninfo/providers/CoinSpaceBitcoinCashProvider.kt
@@ -5,11 +5,11 @@ import com.google.gson.JsonObject
 import io.horizontalsystems.bankwallet.modules.fulltransactioninfo.FullTransactionInfoModule
 import io.horizontalsystems.bankwallet.modules.fulltransactioninfo.FullTransactionInfoModule.Request.GetRequest
 
-class BlockdozerBitcoinCashProvider(testMode: Boolean) : FullTransactionInfoModule.BitcoinForksProvider {
-    override val name = "Blockdozer.com"
-    override val pingUrl = "https://bch.blockdozer.com" //TODO blockdozer is down, maybe we should remove blockdozer provider
+class CoinSpaceBitcoinCashProvider : FullTransactionInfoModule.BitcoinForksProvider {
+    override val name = "Coin.space"
+    override val pingUrl = "https://bch.coin.space/api/sync"
 
-    private val baseUrl = "https://${if (testMode) "tbch." else ""}blockdozer.com"
+    private val baseUrl = "https://bch.coin.space"
 
     override fun url(hash: String): String {
         return "$baseUrl/tx/$hash"
