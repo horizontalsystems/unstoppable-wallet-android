c17d85a576c4b9bb92bbd46f55219dfc7f8e1bc2
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-01-30 14:18:52 +0600

    Fix fiat formatting for top/low values in charts

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/ratechart/RateFormatter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/ratechart/RateFormatter.kt
@@ -8,6 +8,6 @@ import java.math.BigDecimal
 
 class RateFormatter(private val currency: Currency) : ChartView.RateFormatter {
     override fun format(value: BigDecimal, maxFraction: Int?): String? {
-        return App.numberFormatter.formatForRates(CurrencyValue(currency, value), maxFraction = maxFraction)
+        return App.numberFormatter.formatForRates(CurrencyValue(currency, value), trimmable = true, maxFraction = maxFraction)
     }
 }
