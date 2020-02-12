8fb196cf0655001643fc4aa1ef4ea5d677da39f9
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-02-12 17:00:07 +0600

    Cleanup migrations and remove unnecessary AuthData

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/SecuredStorageManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/SecuredStorageManager.kt
@@ -3,25 +3,15 @@ package io.horizontalsystems.bankwallet.core.managers
 import android.text.TextUtils
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.ISecuredStorage
-import io.horizontalsystems.bankwallet.entities.AuthData
 import io.horizontalsystems.core.IEncryptionManager
 
 class SecuredStorageManager(private val encryptionManager: IEncryptionManager) : ISecuredStorage {
 
-    private val AUTH_DATA = "auth_data"
-    private val LOCK_PIN = "lock_pin"
-
-    override val authData: AuthData?
-        get() {
-            App.preferences.getString(AUTH_DATA, null)?.let { string ->
-                return AuthData(encryptionManager.decrypt(string))
-            }
-            return null
-        }
+    private val lockPin = "lock_pin"
 
     override val savedPin: String?
         get() {
-            val string = App.preferences.getString(LOCK_PIN, null) ?: return null
+            val string = App.preferences.getString(lockPin, null) ?: return null
             return if (TextUtils.isEmpty(string)) {
                 null
             } else {
@@ -30,15 +20,15 @@ class SecuredStorageManager(private val encryptionManager: IEncryptionManager) :
         }
 
     override fun savePin(pin: String) {
-        App.preferences.edit().putString(LOCK_PIN, encryptionManager.encrypt(pin)).apply()
+        App.preferences.edit().putString(lockPin, encryptionManager.encrypt(pin)).apply()
     }
 
     override fun removePin() {
-        App.preferences.edit().remove(LOCK_PIN).apply()
+        App.preferences.edit().remove(lockPin).apply()
     }
 
     override fun pinIsEmpty(): Boolean {
-        val string = App.preferences.getString(LOCK_PIN, null)
+        val string = App.preferences.getString(lockPin, null)
         return string.isNullOrEmpty()
     }
 
