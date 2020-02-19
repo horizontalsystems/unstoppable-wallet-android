a24589bf7e2825a59e257ea563dccaeee7d46f32
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-02-19 18:24:11 +0600

    Rename BaseCurrencySettings to CurrencySwitcher

--- components/currencyswitcher/src/main/java/io/horizontalsystems/currencyswitcher/CurrencySwitcherView.kt
+++ components/currencyswitcher/src/main/java/io/horizontalsystems/currencyswitcher/CurrencySwitcherView.kt
@@ -2,7 +2,7 @@ package io.horizontalsystems.currencyswitcher
 
 import androidx.lifecycle.MutableLiveData
 
-class BaseCurrencySettingsView : BaseCurrencySettingsModule.IView {
+class CurrencySwitcherView : CurrencySwitcherModule.IView {
     val currencyItems = MutableLiveData<List<CurrencyViewItem>>()
 
     override fun show(items: List<CurrencyViewItem>) {
