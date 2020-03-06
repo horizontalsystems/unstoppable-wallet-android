4fb4006829185e7d5ae08c63623a70fb6e3f350f
bakyt <abdrasulov@gmail.com>
2020-03-06 11:43:03 +0600

    Refactor updating of transaction items

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionItemDataSource.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionItemDataSource.kt
@@ -1,8 +1,6 @@
 package io.horizontalsystems.bankwallet.modules.transactions
 
-import io.horizontalsystems.bankwallet.entities.Coin
 import io.horizontalsystems.bankwallet.entities.TransactionRecord
-import io.horizontalsystems.bankwallet.entities.Wallet
 import java.util.concurrent.CopyOnWriteArrayList
 
 class TransactionItemDataSource {
@@ -14,30 +12,6 @@ class TransactionItemDataSource {
 
     fun add(items: List<TransactionViewItem>) {
         this.items.addAll(items)
-    }
-
-    fun itemIndexesForTimestamp(coin: Coin, timestamp: Long): List<Int> {
-        val indexes = mutableListOf<Int>()
-
-        items.forEachIndexed { index, transactionItem ->
-            if (transactionItem.wallet.coin == coin && transactionItem.record.timestamp == timestamp) {
-                indexes.add(index)
-            }
-        }
-
-        return indexes
-    }
-
-    fun itemIndexesForPending(wallet: Wallet, thresholdBlockHeight: Int): List<Int> {
-        val indexes = mutableListOf<Int>()
-
-        items.forEachIndexed { index, item ->
-            if (item.wallet == wallet && (item.record.blockHeight == null || item.record.blockHeight >= thresholdBlockHeight)) {
-                indexes.add(index)
-            }
-        }
-
-        return indexes
     }
 
     fun handleModifiedItems(updatedItems: List<TransactionViewItem>, insertedItems: List<TransactionViewItem>) {
@@ -53,20 +27,6 @@ class TransactionItemDataSource {
 
     fun shouldInsertRecord(record: TransactionRecord): Boolean {
         return items.lastOrNull()?.record?.let { it < record } ?: true
-    }
-
-    fun itemIndexesForLocked(wallet: Wallet, unlockingBefore: Long, oldBlockTimestamp: Long?): List<Int> {
-        val indexes = mutableListOf<Int>()
-
-        items.forEachIndexed { index, item ->
-            if (item.wallet == wallet && item.record.lockInfo != null &&
-                    item.record.lockInfo.lockedUntil.time / 1000 > oldBlockTimestamp ?: 0 &&
-                    item.record.lockInfo.lockedUntil.time / 1000 <= unlockingBefore) {
-                indexes.add(index)
-            }
-        }
-
-        return indexes
     }
 
 }
