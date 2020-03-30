c9070a4c69278111e50c5ffb474244a5a139e602
bakyt <abdrasulov@gmail.com>
2020-03-30 16:16:13 +0600

    Apply changes to intro screen
     * Include status bar into the layout
     * Divide the screen into 2 equal vertical section
     * Align image to the middle of the top half section
     * Set the background for the whole screen and to the bottom buttons section

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroActivity.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroActivity.kt
@@ -1,6 +1,7 @@
 package io.horizontalsystems.bankwallet.modules.intro
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
@@ -15,6 +16,7 @@ class IntroActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         setContentView(R.layout.activity_intro)
 
