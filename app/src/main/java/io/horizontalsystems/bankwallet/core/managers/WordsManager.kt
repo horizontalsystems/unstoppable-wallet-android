11089b3b16f2360000cf1fe45b01b6a774a64215
bakyt <abdrasulov@gmail.com>
2020-04-28 12:50:37 +0600

    Remove redundant code

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/WordsManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/WordsManager.kt
@@ -7,15 +7,6 @@ import io.reactivex.subjects.PublishSubject
 
 class WordsManager(private val localStorage: ILocalStorage) : IWordsManager {
 
-    override var isBackedUp: Boolean
-        get() = localStorage.isBackedUp
-        set(value) {
-            localStorage.isBackedUp = value
-            backedUpSignal.onNext(Unit)
-        }
-
-    override var backedUpSignal = PublishSubject.create<Unit>()
-
     @Throws(Mnemonic.MnemonicException::class)
     override fun validate(words: List<String>) {
         Mnemonic().validate(words)
