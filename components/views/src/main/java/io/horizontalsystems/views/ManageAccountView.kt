b679d2afd2b3d8745d6faac485cca2a0c620c95d
bakyt <abdrasulov@gmail.com>
2020-05-01 17:03:59 +0600

    Refactor ManageAccountView

--- components/views/src/main/java/io/horizontalsystems/views/ManageAccountView.kt
+++ components/views/src/main/java/io/horizontalsystems/views/ManageAccountView.kt
@@ -3,11 +3,11 @@ package io.horizontalsystems.views
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import io.horizontalsystems.views.helpers.LayoutHelper
 import kotlinx.android.synthetic.main.view_manage_account_view.view.*
 
-class ManageAccountView : LinearLayout {
+class ManageAccountView : ConstraintLayout {
 
     init {
         inflate(context, R.layout.view_manage_account_view, this)
@@ -17,27 +17,29 @@ class ManageAccountView : LinearLayout {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    fun bind(titleText: String, type: AccountButtonItemType, showAttentionIcon: Boolean = false, isLast: Boolean = false, onClick: () -> Unit) {
+    fun bind(titleText: String, type: AccountButtonItemType, showAttentionIcon: Boolean = false, isLast: Boolean = false) {
         title.text = titleText
 
         getTextColorForType(type)?.let {
             title.setTextColor(it)
         }
 
-        rightArrow.visibility = if (type == AccountButtonItemType.SimpleButton) View.VISIBLE else View.GONE
+        rightArrow.visibility = if (type == AccountButtonItemType.Regular) View.VISIBLE else View.GONE
         attentionIcon.visibility = if (showAttentionIcon) View.VISIBLE else View.GONE
-        itemWrapper.setOnClickListener { onClick.invoke() }
-        itemWrapper.setBackgroundResource(
-                if (isLast) R.drawable.manage_account_last_button_background
-                else R.drawable.clickable_background_color_lawrence
-        )
+
+        setBackgroundResource(getBackgroundResource(isLast))
+    }
+
+    private fun getBackgroundResource(isLast: Boolean) = when {
+        isLast -> R.drawable.manage_account_last_button_background
+        else -> R.drawable.clickable_background_color_lawrence
     }
 
     private fun getTextColorForType(type: AccountButtonItemType): Int? {
         val colorAttr = when (type) {
-            AccountButtonItemType.SimpleButton -> R.attr.ColorOz
-            AccountButtonItemType.RedButton -> R.attr.ColorLucian
-            AccountButtonItemType.ActionButton -> R.attr.ColorJacob
+            AccountButtonItemType.Regular -> R.attr.ColorOz
+            AccountButtonItemType.DangerAction -> R.attr.ColorLucian
+            AccountButtonItemType.Action -> R.attr.ColorJacob
         }
 
         return LayoutHelper.getAttr(colorAttr, context.theme)
@@ -45,7 +47,7 @@ class ManageAccountView : LinearLayout {
 }
 
 enum class AccountButtonItemType {
-    SimpleButton,
-    RedButton,
-    ActionButton
+    Regular,
+    Action,
+    DangerAction
 }
