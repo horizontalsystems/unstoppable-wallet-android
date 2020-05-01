a4328a004e75192230058bcd55858be272c4293f
bakyt <abdrasulov@gmail.com>
2020-05-01 15:58:07 +0600

    Change style of Create, Restore buttons

--- components/views/src/main/java/io/horizontalsystems/views/ManageAccountView.kt
+++ components/views/src/main/java/io/horizontalsystems/views/ManageAccountView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
+import io.horizontalsystems.views.helpers.LayoutHelper
 import kotlinx.android.synthetic.main.view_manage_account_view.view.*
 
 class ManageAccountView : LinearLayout {
@@ -16,24 +17,14 @@ class ManageAccountView : LinearLayout {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    fun bind(title: String, type: AccountButtonItemType, showAttentionIcon: Boolean = false, isLast: Boolean = false, onClick: () -> Unit) {
-        when (type) {
-            AccountButtonItemType.SimpleButton -> {
-                rightArrow.visibility = View.VISIBLE
-                redTitle.visibility = View.GONE
-                normalTitle.visibility = View.VISIBLE
+    fun bind(titleText: String, type: AccountButtonItemType, showAttentionIcon: Boolean = false, isLast: Boolean = false, onClick: () -> Unit) {
+        title.text = titleText
 
-                normalTitle.text = title
-            }
-            AccountButtonItemType.RedButton -> {
-                rightArrow.visibility = View.GONE
-                redTitle.visibility = View.VISIBLE
-                normalTitle.visibility = View.GONE
-
-                redTitle.text = title
-            }
+        getTextColorForType(type)?.let {
+            title.setTextColor(it)
         }
 
+        rightArrow.visibility = if (type == AccountButtonItemType.SimpleButton) View.VISIBLE else View.GONE
         attentionIcon.visibility = if (showAttentionIcon) View.VISIBLE else View.GONE
         itemWrapper.setOnClickListener { onClick.invoke() }
         itemWrapper.setBackgroundResource(
@@ -41,9 +32,20 @@ class ManageAccountView : LinearLayout {
                 else R.drawable.clickable_background_color_lawrence
         )
     }
+
+    private fun getTextColorForType(type: AccountButtonItemType): Int? {
+        val colorAttr = when (type) {
+            AccountButtonItemType.SimpleButton -> R.attr.ColorOz
+            AccountButtonItemType.RedButton -> R.attr.ColorLucian
+            AccountButtonItemType.ActionButton -> R.attr.ColorJacob
+        }
+
+        return LayoutHelper.getAttr(colorAttr, context.theme)
+    }
 }
 
 enum class AccountButtonItemType {
     SimpleButton,
-    RedButton
+    RedButton,
+    ActionButton
 }
