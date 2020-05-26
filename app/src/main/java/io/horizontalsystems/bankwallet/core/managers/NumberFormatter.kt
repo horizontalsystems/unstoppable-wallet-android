bc414032b51cf020872098789c76889c11c38fe7
bakyt <abdrasulov@gmail.com>
2020-05-26 18:43:24 +0600

    Remove method NumberFormatter::formatForTransactions

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/NumberFormatter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/NumberFormatter.kt
@@ -1,10 +1,5 @@
 package io.horizontalsystems.bankwallet.core.managers
 
-import android.content.Context
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
-import androidx.core.content.ContextCompat
-import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.IAppNumberFormatter
 import io.horizontalsystems.bankwallet.entities.CoinValue
 import io.horizontalsystems.bankwallet.entities.CurrencyValue
@@ -115,17 +110,6 @@ class NumberFormatter(private val languageManager: ILanguageManager) : IAppNumbe
         val formatted = customFormatter.format(value)
 
         return "${currencyValue.currency.symbol}$formatted"
-    }
-
-    override fun formatForTransactions(context: Context, currencyValue: CurrencyValue, isIncoming: Boolean, canUseLessSymbol: Boolean, trimmable: Boolean): SpannableString {
-        val spannable = SpannableString(format(currencyValue, showNegativeSign = false, trimmable = trimmable, canUseLessSymbol = canUseLessSymbol))
-
-        //  set color
-        val amountTextColor = if (isIncoming) R.color.green_d else R.color.yellow_d
-        val color = ContextCompat.getColor(context, amountTextColor)
-
-        spannable.setSpan(ForegroundColorSpan(color), 0, spannable.length, 0)
-        return spannable
     }
 
     override fun format(value: Double, showSign: Boolean, precision: Int): String {
