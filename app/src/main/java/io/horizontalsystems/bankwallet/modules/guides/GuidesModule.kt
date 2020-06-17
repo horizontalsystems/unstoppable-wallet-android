3b54a38167f6c60dc92355b24d9a8118e2d753f4
bakyt <abdrasulov@gmail.com>
2020-06-17 12:07:47 +0600

    Retrieve list of guides from the remote server

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/guides/GuidesModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/guides/GuidesModule.kt
@@ -2,24 +2,26 @@ package io.horizontalsystems.bankwallet.modules.guides
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.managers.GuidesManager
-import io.horizontalsystems.bankwallet.entities.Guide
+import io.horizontalsystems.bankwallet.entities.GuideCategory
 import java.util.*
 
 object GuidesModule {
 
     interface Interactor {
-        val guides: List<Guide>
+        fun fetchGuideCategories()
     }
 
     interface InteractorDelegate {
+        fun didFetchGuideCategories(guideCategories: Array<GuideCategory>)
 
     }
 
     class Factory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            val interactor = GuidesInteractor(GuidesManager)
+            val interactor = GuidesInteractor(GuidesManager, App.networkManager)
             val presenter = GuidesViewModel(interactor)
 
             interactor.delegate = presenter
