49bd32b33ec777e021b568d94d2cdc77ace63997
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-06-17 17:43:44 +0600

    Fix backup confirm not showing words higher than 12

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/RandomProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/RandomProvider.kt
@@ -5,13 +5,13 @@ import java.util.*
 
 class RandomProvider : IRandomProvider {
 
-    override fun getRandomIndexes(count: Int): List<Int> {
+    override fun getRandomIndexes(count: Int, maxIndex: Int): List<Int> {
         val indexes = mutableListOf<Int>()
 
         val random = Random()
 
         while (indexes.size < count) {
-            val index = random.nextInt(12) + 1
+            val index = random.nextInt(maxIndex) + 1
 
             if (!indexes.contains(index)) {
                 indexes.add(index)
