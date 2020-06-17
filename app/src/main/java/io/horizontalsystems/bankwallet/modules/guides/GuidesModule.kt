0c4f2954174f3c58a30c4753f70374c81d4d8fa4
bakyt <abdrasulov@gmail.com>
2020-06-17 18:10:50 +0600

    Merge logic in interactor into viewModel

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/guides/GuidesModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/guides/GuidesModule.kt
@@ -2,32 +2,15 @@ package io.horizontalsystems.bankwallet.modules.guides
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.managers.GuidesManager
-import io.horizontalsystems.bankwallet.entities.GuideCategory
-import java.util.*
 
 object GuidesModule {
-
-    interface Interactor {
-        fun fetchGuideCategories()
-    }
-
-    interface InteractorDelegate {
-        fun didFetchGuideCategories(guideCategories: Array<GuideCategory>)
-        fun onSelectFilter(filterId: String)
-
-    }
 
     class Factory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            val interactor = GuidesInteractor(GuidesManager, App.networkManager)
-            val presenter = GuidesViewModel(interactor)
 
-            interactor.delegate = presenter
-
-            return presenter as T
+            return GuidesViewModel(GuidesManager) as T
         }
     }
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/guides/GuidesViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/guides/GuidesViewModel.kt
@@ -2,11 +2,14 @@ package io.horizontalsystems.bankwallet.modules.guides
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.horizontalsystems.bankwallet.core.managers.GuidesManager
 import io.horizontalsystems.bankwallet.entities.Guide
 import io.horizontalsystems.bankwallet.entities.GuideCategory
 import io.horizontalsystems.core.SingleLiveEvent
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
-class GuidesViewModel(private val interactor: GuidesModule.Interactor) : GuidesModule.InteractorDelegate, ViewModel() {
+class GuidesViewModel(private val guidesManager: GuidesManager) : ViewModel() {
 
     val openGuide = SingleLiveEvent<Guide>()
     val guidesLiveData = MutableLiveData<List<Guide>>()
@@ -16,17 +19,30 @@ class GuidesViewModel(private val interactor: GuidesModule.Interactor) : GuidesM
     private var guideCategories: Array<GuideCategory> = arrayOf()
     private var currentCategoryIndex = 0
 
+    private var disposable: Disposable? = null
 
     init {
         loading.postValue(true)
-        interactor.fetchGuideCategories()
+        disposable = guidesManager.getGuideCategories()
+                .subscribeOn(Schedulers.io())
+                .subscribe { categories, _ ->
+                    didFetchGuideCategories(categories)
+                }
+    }
+
+    fun onSelectFilter(filterId: String) {
+        currentCategoryIndex = guideCategories.indexOfFirst {
+            it.title == filterId
+        }
+
+        syncViewItems()
     }
 
     fun onGuideClick(guide: Guide) {
         openGuide.postValue(guide)
     }
 
-    override fun didFetchGuideCategories(guideCategories: Array<GuideCategory>) {
+    private fun didFetchGuideCategories(guideCategories: Array<GuideCategory>) {
         this.guideCategories = guideCategories
 
         filters.postValue(guideCategories.map { it.title })
@@ -35,15 +51,11 @@ class GuidesViewModel(private val interactor: GuidesModule.Interactor) : GuidesM
         syncViewItems()
     }
 
-    override fun onSelectFilter(filterId: String) {
-        currentCategoryIndex = guideCategories.indexOfFirst {
-            it.title == filterId
-        }
-
-        syncViewItems()
-    }
-
     private fun syncViewItems() {
         guidesLiveData.postValue(guideCategories[currentCategoryIndex].guides)
+    }
+
+    override fun onCleared() {
+        disposable?.dispose()
     }
 }
