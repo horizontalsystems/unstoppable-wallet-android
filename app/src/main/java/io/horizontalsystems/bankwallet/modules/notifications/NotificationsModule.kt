4ad047ff92c40679c1ef113c330bc3608b85b0e8
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-06-26 16:40:54 +0600

    Update Notifications Settings layout and remove Viper

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/notifications/NotificationsModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/notifications/NotificationsModule.kt
@@ -6,68 +6,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.entities.Coin
-import io.horizontalsystems.bankwallet.entities.PriceAlert
 
 object NotificationsModule {
-    interface IView {
-        fun setItems(items: List<PriceAlertViewItem>)
-        fun showWarning()
-        fun hideWarning()
-        fun showStateSelector(itemPosition: Int, priceAlert: PriceAlert)
-        fun setNotificationSwitch(enabled: Boolean)
-    }
-
-    interface IRouter {
-        fun openNotificationSettings()
-    }
-
-    interface IViewDelegate {
-        fun viewDidLoad()
-//        fun didSelectState(itemPosition: Int, state: PriceAlert.State)
-        fun didClickOpenSettings()
-        fun didClickDeactivateAll()
-        fun didTapItem(itemPosition: Int)
-        fun didSwitchAlertNotification(enabled: Boolean)
-    }
-
-    interface IInteractor {
-        val priceAlertsEnabled: Boolean
-        val priceAlerts: List<PriceAlert>
-        var notificationIsOn: Boolean
-
-        fun savePriceAlerts(priceAlerts: List<PriceAlert>)
-        fun startBackgroundRateFetchWorker()
-        fun stopBackgroundRateFetchWorker()
-    }
-
-    interface IInteractorDelegate {
-        fun didEnterForeground()
-    }
 
     fun start(context: Context) {
         val intent = Intent(context, NotificationsActivity::class.java)
         context.startActivity(intent)
     }
 
-    data class PriceAlertViewItem(val title: String, val coin: Coin) {
-//        lateinit var state: PriceAlert.State
-
-//        constructor(title: String, coin: Coin, state: PriceAlert.State) : this(title, coin) {
-//            this.state = state
-//        }
-    }
-
     class Factory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            val view = NotificationsView()
-            val router = NotificationsRouter()
-            val interactor = NotificationsInteractor(App.priceAlertManager, App.backgroundManager, App.localStorage, App.notificationManager)
-            val presenter = NotificationsPresenter(view, router, interactor, PriceAlertViewItemFactory())
-
-            interactor.delegate = presenter
-
-            return presenter as T
+            val viewModel = NotificationsViewModel(App.priceAlertManager, App.walletManager, App.coinManager, App.notificationManager, App.localStorage)
+            return viewModel as T
         }
     }
 }
