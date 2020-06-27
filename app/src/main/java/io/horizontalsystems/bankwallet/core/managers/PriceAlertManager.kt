5d031aaa8d724f776fe6da9262749a9e2dcf3f44
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-06-27 11:05:37 +0600

    Update notifications settings list on change

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/PriceAlertManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/PriceAlertManager.kt
@@ -3,8 +3,16 @@ package io.horizontalsystems.bankwallet.core.managers
 import io.horizontalsystems.bankwallet.core.IPriceAlertManager
 import io.horizontalsystems.bankwallet.core.storage.AppDatabase
 import io.horizontalsystems.bankwallet.entities.PriceAlert
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.subjects.PublishSubject
 
 class PriceAlertManager(private val appDatabase: AppDatabase): IPriceAlertManager {
+
+    private val notificationChangedSubject = PublishSubject.create<Unit>()
+
+    override val notificationChangedFlowable: Flowable<Unit>
+        get() = notificationChangedSubject.toFlowable(BackpressureStrategy.BUFFER)
 
     override fun getPriceAlerts(): List<PriceAlert> {
         return appDatabase.priceAlertsDao().all()
@@ -12,6 +20,7 @@ class PriceAlertManager(private val appDatabase: AppDatabase): IPriceAlertManage
 
     override fun savePriceAlert(priceAlert: PriceAlert) {
         appDatabase.priceAlertsDao().update(priceAlert)
+        notificationChangedSubject.onNext(Unit)
     }
 
     override fun priceAlert(coinCode: String): PriceAlert {
