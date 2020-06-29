421126c10af0a3e19348407d85f2b84e54cf404b
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-06-29 17:43:55 +0600

    Update SNX erc20 token contract

--- app/src/main/java/io/horizontalsystems/bankwallet/core/providers/AppConfigProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/providers/AppConfigProvider.kt
@@ -110,7 +110,7 @@ class AppConfigProvider : IAppConfigProvider, ILanguageConfigProvider, IAppConfi
                 Coin("SAI", "Sai", "SAI", 18, CoinType.Erc20("0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359")),
                 Coin("SNT", "Status", "SNT", 18, CoinType.Erc20("0x744d70FDBE2Ba4CF95131626614a1763DF805B9E")),
                 Coin("CHSB", "SwissBorg", "CHSB", 8, CoinType.Erc20("0xba9d4199fab4f26efe3551d490e3821486f135ba")),
-                Coin("SNX", "Synthetix", "SNX", 18, CoinType.Erc20("0xc011a72400e58ecd99ee497cf89e3775d4bd732f")),
+                Coin("SNX", "Synthetix", "SNX", 18, CoinType.Erc20("0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F")),
                 Coin("TUSD", "TrueUSD", "TUSD", 18, CoinType.Erc20("0x0000000000085d4780B73119b644AE5ecd22b376")),
                 Coin("USDT", "Tether USD", "USDT", 6, CoinType.Erc20("0xdAC17F958D2ee523a2206206994597C13D831ec7")),
                 Coin("USDC", "USD Coin", "USDC", 6, CoinType.Erc20("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")),
