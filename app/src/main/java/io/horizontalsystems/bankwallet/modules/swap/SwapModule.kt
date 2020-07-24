033c29cbce208e6a3b91129a3e48c58b9f3c2d25
chyngyz <chyngyz.omurov@gmail.com>
2020-07-24 17:04:04 +0600

    Create empty module SelectSwapCoinModule, open it from swap page

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapModule.kt
@@ -2,17 +2,26 @@ package io.horizontalsystems.bankwallet.modules.swap
 
 import android.content.Context
 import android.content.Intent
-import io.horizontalsystems.bankwallet.entities.Wallet
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import io.horizontalsystems.bankwallet.entities.Coin
 
 object SwapModule {
 
-    const val swapWalletKey = "swapWalletKey"
+    const val tokenInKey = "tokenInKey"
 
-    fun start(context: Context, wallet: Wallet) {
+    fun start(context: Context, tokenIn: Coin) {
         val intent = Intent(context, SwapActivity::class.java)
-        intent.putExtra(swapWalletKey, wallet)
+        intent.putExtra(tokenInKey, tokenIn)
 
         context.startActivity(intent)
+    }
+
+    class Factory(private val tokenIn: Coin?) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return SwapViewModel(tokenIn) as T
+        }
     }
 
 }
