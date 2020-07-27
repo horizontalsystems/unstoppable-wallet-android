10f7c70e524aa435b656ff06adb32f344ed74698
chyngyz <chyngyz.omurov@gmail.com>
2020-07-27 15:31:33 +0600

    Add implementation for SelectSwapCoinModule

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapActivity.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapActivity.kt
@@ -1,9 +1,12 @@
 package io.horizontalsystems.bankwallet.modules.swap
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseActivity
 import io.horizontalsystems.bankwallet.entities.Coin
@@ -11,6 +14,8 @@ import io.horizontalsystems.bankwallet.modules.swap.coinselect.SelectSwapCoinMod
 import kotlinx.android.synthetic.main.activity_swap.*
 
 class SwapActivity : BaseActivity() {
+
+    lateinit var viewModel: SwapViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +27,27 @@ class SwapActivity : BaseActivity() {
         supportActionBar?.title = getString(R.string.Swap_Title)
 
         val tokenIn = intent.extras?.getParcelable<Coin>(SwapModule.tokenInKey)
-        val viewModel by viewModels<SwapViewModel> { SwapModule.Factory(tokenIn) }
+        viewModel = ViewModelProvider(this, SwapModule.Factory(tokenIn)).get(SwapViewModel::class.java)
 
         fromAmount.onTokenButtonClick {
-            SelectSwapCoinModule.start(this, viewModel.tokenIn)
+            SelectSwapCoinModule.start(this, requestSelectFromCoin, true, viewModel.toCoinLiveData.value)
         }
 
         toAmount.onTokenButtonClick {
-            SelectSwapCoinModule.start(this, viewModel.tokenOut)
+            SelectSwapCoinModule.start(this, requestSelectToCoin, false, viewModel.fromCoinLiveData.value)
         }
+
+        viewModel.fromCoinLiveData.observe(this, Observer { coin ->
+            coin?.let {
+                fromAmount.setSelectedCoin(it.code)
+            }
+        })
+
+        viewModel.toCoinLiveData.observe(this, Observer { coin ->
+            coin?.let {
+                toAmount.setSelectedCoin(it.code)
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,6 +67,23 @@ class SwapActivity : BaseActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val selectedCoin = data?.getParcelableExtra<Coin>(SelectSwapCoinModule.selectedCoinKey)
+                    ?: return
+            when (requestCode) {
+                requestSelectFromCoin -> viewModel.onSelectFromCoin(selectedCoin)
+                requestSelectToCoin -> viewModel.onSelectToCoin(selectedCoin)
+            }
+        }
+    }
+
+    companion object {
+        const val requestSelectFromCoin = 0
+        const val requestSelectToCoin = 1
     }
 
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coinselect/SelectSwapCoinViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coinselect/SelectSwapCoinViewModel.kt
@@ -1,5 +1,42 @@
 package io.horizontalsystems.bankwallet.modules.swap.coinselect
 
-class SelectSwapCoinViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.horizontalsystems.bankwallet.core.IAdapterManager
+import io.horizontalsystems.bankwallet.core.ICoinManager
+import io.horizontalsystems.bankwallet.core.IWalletManager
+import io.horizontalsystems.bankwallet.entities.Coin
+import io.horizontalsystems.bankwallet.entities.CoinType
+import java.math.BigDecimal
+
+class SelectSwapCoinViewModel(
+        private val excludedCoin: Coin?,
+        private val hideZeroBalance: Boolean?,
+        private val coinManager: ICoinManager,
+        private val walletManager: IWalletManager,
+        private val adapterManager: IAdapterManager
+) : ViewModel() {
+
+    val coinItemsLivedData = MutableLiveData<List<SwapCoinItem>>()
+
+    init {
+        val coinItems = coinManager.coins.mapNotNull { coin ->
+            val wallet = walletManager.wallet(coin)
+            val balance = wallet?.let { adapterManager.getBalanceAdapterForWallet(it)?.balance }
+                    ?: BigDecimal.ZERO
+
+            if (!swappable(coin) || coin == excludedCoin || (hideZeroBalance == true && balance <= BigDecimal.ZERO)) {
+                null
+            } else {
+                SwapCoinItem(coin, balance)
+            }
+        }
+
+        coinItemsLivedData.postValue(coinItems)
+    }
+
+    private fun swappable(coin: Coin): Boolean {
+        return coin.type is CoinType.Ethereum || coin.type is CoinType.Erc20
+    }
 
 }
