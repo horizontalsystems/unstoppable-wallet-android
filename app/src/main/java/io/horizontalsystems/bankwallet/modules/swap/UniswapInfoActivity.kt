ac3a0fab81ba2dd659e903cff24deb355b4b1217
bakyt <abdrasulov@gmail.com>
2020-07-27 12:32:33 +0600

    Add uniswap info screen

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/UniswapInfoActivity.kt
@@ -0,0 +1,49 @@
+package io.horizontalsystems.bankwallet.modules.swap
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.BaseActivity
+import kotlinx.android.synthetic.main.activity_swap_info.*
+
+class UniswapInfoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swap_info)
+
+        setSupportActionBar(toolbar)
+
+        buttonUniswap.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://uniswap.org/")
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.select_coin_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuClose -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, UniswapInfoActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
