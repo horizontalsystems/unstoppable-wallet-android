51653b20d9017a8705d22e498a515a3c0774e4dc
chyngyz <chyngyz.omurov@gmail.com>
2020-07-28 18:48:38 +0600

    Don't show zero balances in swap coin selector

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coinselect/SwapCoinItem.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coinselect/SwapCoinItem.kt
@@ -3,4 +3,4 @@ package io.horizontalsystems.bankwallet.modules.swap.coinselect
 import io.horizontalsystems.bankwallet.entities.Coin
 import java.math.BigDecimal
 
-data class SwapCoinItem(val coin: Coin, val balance: BigDecimal)
+data class SwapCoinItem(val coin: Coin, val balance: BigDecimal?)
