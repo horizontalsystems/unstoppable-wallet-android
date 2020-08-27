0fc52aa100c4b65a4b0dc9bfb4f0c132501d5971
Rafael <rafaelekol@gmail.com>
2020-08-27 14:27:24 +0600

    Use empty constructor in IntroFragment.kt

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroViewPagerAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroViewPagerAdapter.kt
@@ -7,9 +7,9 @@ import io.horizontalsystems.bankwallet.R
 
 class IntroViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     private val fragments = listOf<Fragment>(
-            IntroFragment(R.string.Intro_Independence_Title, R.string.Intro_Independence_Description),
-            IntroFragment(R.string.Intro_Knowledge_Title, R.string.Intro_Knowledge_Description),
-            IntroFragment(R.string.Intro_Privacy_Title, R.string.Intro_Privacy_Description)
+            IntroFragment.newInstance(R.string.Intro_Independence_Title, R.string.Intro_Independence_Description),
+            IntroFragment.newInstance(R.string.Intro_Knowledge_Title, R.string.Intro_Knowledge_Description),
+            IntroFragment.newInstance(R.string.Intro_Privacy_Title, R.string.Intro_Privacy_Description)
     )
 
     override fun getCount(): Int = fragments.size
