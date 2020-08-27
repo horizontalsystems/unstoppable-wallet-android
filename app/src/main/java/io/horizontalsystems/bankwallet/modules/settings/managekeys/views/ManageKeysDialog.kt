9c4bc5c4c6cf2d821d66efce4fd5d2712dfa3025
Rafael <rafaelekol@gmail.com>
2020-08-27 17:32:57 +0600

    Remove unused enum in ManageKeysDialog.kt

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/managekeys/views/ManageKeysDialog.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/managekeys/views/ManageKeysDialog.kt
@@ -28,12 +28,10 @@ class ManageKeysDialog : BaseBottomSheetDialogFragment() {
         val title = requireArguments().getString("title")
         val subtitle = requireArguments().getString("subtitle")
         val content = requireArguments().getString("content")
-        val action = requireArguments().getSerializable("action") as ManageAction
 
         setTitle(title)
         setSubtitle(subtitle)
-        val icon = if (action == ManageAction.CREATE) R.drawable.ic_manage_keys else R.drawable.ic_attention_red
-        setHeaderIcon(icon)
+        setHeaderIcon(R.drawable.ic_attention_red)
 
         alertText = view.findViewById(R.id.alertText)
         primaryActionButton = view.findViewById(R.id.primaryActionBtn)
@@ -41,39 +39,31 @@ class ManageKeysDialog : BaseBottomSheetDialogFragment() {
 
         alertText.text = content
 
-        bindActions(action)
+        bindActions()
     }
 
     fun setListener(listener: Listener) {
         this.listener = listener
     }
 
-    private fun bindActions(action: ManageAction) {
+    private fun bindActions() {
         secondaryActionButton.isVisible = false
         primaryActionButton.isVisible = true
 
-        if (action == ManageAction.BACKUP) {
-            primaryActionButton.text = getString(R.string.ManageKeys_Backup)
-            primaryActionButton.setOnClickListener {
-                listener?.onClickBackupKey()
-                dismiss()
-            }
+        primaryActionButton.text = getString(R.string.ManageKeys_Backup)
+        primaryActionButton.setOnClickListener {
+            listener?.onClickBackupKey()
+            dismiss()
         }
     }
 
-    enum class ManageAction {
-        CREATE,
-        BACKUP
-    }
-
     companion object {
-        fun show(fragmentManager: FragmentManager, title: String, subtitle: String, content: String, action: ManageAction) {
+        fun show(fragmentManager: FragmentManager, title: String, subtitle: String, content: String) {
             val fragment = ManageKeysDialog().apply {
                 arguments = Bundle(4).apply {
                     putString("title", title)
                     putString("subtitle", subtitle)
                     putString("content", content)
-                    putSerializable("action", action)
                 }
             }
 
