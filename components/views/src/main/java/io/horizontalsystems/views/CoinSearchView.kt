078405fa3c9bbfd4231ed88d956eccac079d5891
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-09-10 14:48:14 +0600

    Add custom search view

---
+++ components/views/src/main/java/io/horizontalsystems/views/CoinSearchView.kt
@@ -0,0 +1,45 @@
+package io.horizontalsystems.views
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isInvisible
+import kotlinx.android.synthetic.main.view_coin_search.view.*
+
+class CoinSearchView : ConstraintLayout {
+
+    init {
+        inflate(context, R.layout.view_coin_search, this)
+
+        closeButton.setOnClickListener {
+            searchInput.setText("")
+        }
+
+        bindTextChangeListener { closeButton.isInvisible = it.isBlank() }
+    }
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+    fun bind(hint: String, onTextChanged: ((String) -> Unit)) {
+        searchInput.hint = hint
+        bindTextChangeListener(onTextChanged)
+    }
+
+    private fun bindTextChangeListener(onTextChanged: ((String) -> Unit)) {
+        searchInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                val trimmedText = s.toString().trim()
+                onTextChanged.invoke(trimmedText)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+        })
+    }
+
+}
