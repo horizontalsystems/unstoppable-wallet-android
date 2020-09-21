f077a9fad16f35578de96e31ce16f4e78f07c07e
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-09-21 15:57:04 +0600

    Fix navigation in RestoreModule

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/restore/RestoreActivity.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/restore/RestoreActivity.kt
@@ -3,6 +3,7 @@ package io.horizontalsystems.bankwallet.modules.restore
 import android.os.Bundle
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseActivity
+import io.horizontalsystems.bankwallet.modules.main.MainModule
 
 class RestoreActivity: BaseActivity() {
 
@@ -16,6 +17,22 @@ class RestoreActivity: BaseActivity() {
             commit()
         }
 
+    }
+
+    fun close(result: Result){
+        when(result){
+            Result.Success -> {
+                MainModule.start(this)
+                finishAffinity()
+            }
+            Result.Cancelation -> {
+                finish()
+            }
+        }
+    }
+
+    enum class Result{
+        Success, Cancelation
     }
 
 }
