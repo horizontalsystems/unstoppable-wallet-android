685373bb90e526018c83e4df6b882ec81e50da6e
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-10-02 11:58:19 +0600

    Convert SelectSwapCoinActivity to fragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coinselect/SelectSwapCoinModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coinselect/SelectSwapCoinModule.kt
@@ -1,25 +1,11 @@
 package io.horizontalsystems.bankwallet.modules.swap.coinselect
 
-import android.app.Activity
-import android.content.Intent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.entities.Coin
 
 object SelectSwapCoinModule {
-
-    const val excludedCoinKey = "excludedCoinKey"
-    const val hideZeroBalanceKey = "hideZeroBalanceKey"
-    const val selectedCoinKey = "selectedCoinKey"
-
-    fun start(context: Activity, requestCode: Int, hideZeroBalance: Boolean, excludedCoin: Coin?) {
-        val intent = Intent(context, SelectSwapCoinActivity::class.java)
-        intent.putExtra(excludedCoinKey, excludedCoin)
-        intent.putExtra(hideZeroBalanceKey, hideZeroBalance)
-
-        context.startActivityForResult(intent, requestCode)
-    }
 
     class Factory(private val excludedCoin: Coin?, private val hideZeroBalance: Boolean?) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
