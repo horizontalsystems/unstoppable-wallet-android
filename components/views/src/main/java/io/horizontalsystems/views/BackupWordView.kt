77f67356585d7bb66d0ad87f96b712ac2ce9f21d
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-10-06 18:00:27 +0600

    Fix backup words spacing

---
+++ components/views/src/main/java/io/horizontalsystems/views/BackupWordView.kt
@@ -0,0 +1,24 @@
+package io.horizontalsystems.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.view_backup_word_text.view.*
+
+class BackupWordView : ConstraintLayout {
+
+    init {
+        inflate(context, R.layout.view_backup_word_text, this)
+    }
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+    fun bind(number: String, word: String) {
+        numberText.text = number
+        wordText.text = word
+    }
+
+}
