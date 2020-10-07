20f4dd98d4ae95dcb85b3bd7fc4178b69e343332
nk-the-crazy <nurkali@gmail.com>
2020-10-07 16:59:02 +0600

    Add helper class for keyboard base functions

---
+++ core/src/main/java/io/horizontalsystems/core/helpers/KeyboardHelper.kt
@@ -0,0 +1,22 @@
+package io.horizontalsystems.core.helpers
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+object KeyboardHelper {
+
+    private fun showKeyboard(context: Context) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    fun showKeyboard(context: Context, view: View?) {
+        view?.requestFocus()
+        showKeyboard(context)
+    }
+
+    fun showKeyboardDelayed(context: Context, view: View?, delay: Long) {
+        view?.postDelayed({ showKeyboard(context, view) }, delay)
+    }
+}
