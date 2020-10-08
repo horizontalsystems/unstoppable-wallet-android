f5f95f00631b8fa7c096e3979d33c114fcc5df75
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-10-08 15:45:30 +0600

    Animate transaction status in transaction info screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoStatusView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoStatusView.kt
@@ -1,12 +1,14 @@
 package io.horizontalsystems.bankwallet.modules.transactions.transactionInfo
 
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.transactions.TransactionStatus
 import kotlinx.android.synthetic.main.view_transaction_info_status.view.*
+import kotlinx.android.synthetic.main.view_transaction_info_status.view.progressBar
 
 
 open class TransactionInfoStatusView : ConstraintLayout {
@@ -28,7 +30,7 @@ open class TransactionInfoStatusView : ConstraintLayout {
     }
 
     fun bind(transactionStatus: TransactionStatus, incoming: Boolean) {
-        progressBarWrapper.isVisible = false
+        progressBar.isVisible = false
         confirmedText.isVisible = false
         progressText.isVisible = false
         failedText.isVisible = false
@@ -51,15 +53,16 @@ open class TransactionInfoStatusView : ConstraintLayout {
     }
 
     private fun fillProgress(progress: Double = 0.0, incoming: Boolean) {
-        progressBar1.setImageResource(if (progress >= 0.33) getColoredBar(incoming) else R.drawable.status_progress_bar_grey)
-        progressBar2.setImageResource(if (progress >= 0.66) getColoredBar(incoming) else R.drawable.status_progress_bar_grey)
-        progressBar3.setImageResource(if (progress > 1.0) getColoredBar(incoming) else R.drawable.status_progress_bar_grey)
-        progressBarWrapper.isVisible = true
+        when {
+            progress <= 0.33 -> progressBar.setBackgroundResource(R.drawable.animation_pending_big)
+            progress <= 0.66 -> progressBar.setBackgroundResource(if (incoming) R.drawable.animation_receiving_progress_big_30 else R.drawable.animation_sending_progress_big_30)
+            progress <= 1.0 -> progressBar.setBackgroundResource(if (incoming) R.drawable.animation_receiving_progress_big_60 else R.drawable.animation_sending_progress_big_60)
+        }
 
+        progressBar.isVisible = true
+        (progressBar.background as? AnimationDrawable)?.start()
         progressText.setText(if (incoming) R.string.Transactions_Receiving else R.string.Transactions_Sending)
         progressText.isVisible = true
     }
-
-    private fun getColoredBar(incoming: Boolean) = if (incoming) R.drawable.status_progress_bar_green else R.drawable.status_progress_bar_yellow
 
 }
