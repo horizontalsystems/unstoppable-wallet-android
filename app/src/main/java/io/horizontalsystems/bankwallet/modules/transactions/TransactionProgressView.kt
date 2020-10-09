5ddfee97850e9aa0ac2fd1a5a62f0091897ebaec
chyngyz <chyngyz.omurov@gmail.com>
2020-10-09 17:17:29 +0600

    Correctly show approve transactions in transactions list

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionProgressView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionProgressView.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.AnimationDrawable
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.entities.TransactionType
 import kotlinx.android.synthetic.main.view_transaction_progress.view.*
 
 
@@ -26,14 +27,32 @@ class TransactionProgressView : ConstraintLayout {
         inflate(context, R.layout.view_transaction_progress, this)
     }
 
-    fun bind(progress: Double = 0.0, incoming: Boolean) {
+    fun bind(progress: Double = 0.0, type: TransactionType) {
         when {
             progress <= 0.33 -> progressBar.setBackgroundResource(R.drawable.animation_pending)
-            progress <= 0.66 -> progressBar.setBackgroundResource(if (incoming) R.drawable.animation_receiving_progress_30 else R.drawable.animation_sending_progress_30)
-            progress <= 1.0 -> progressBar.setBackgroundResource(if (incoming) R.drawable.animation_receiving_progress_60 else R.drawable.animation_sending_progress_60)
+            progress <= 0.66 -> progressBar.setBackgroundResource(getOneConfirmationBackground(type))
+            progress <= 1.0 -> progressBar.setBackgroundResource(getTwoConfirmationBackground(type))
         }
-        processText.setText(if (incoming) R.string.Transactions_Receiving else R.string.Transactions_Sending)
+        processText.setText(getDescription(type))
         (progressBar.background as? AnimationDrawable)?.start()
+    }
+
+    private fun getOneConfirmationBackground(type: TransactionType) = when (type) {
+        TransactionType.Outgoing,  TransactionType.SentToSelf -> R.drawable.animation_sending_progress_30
+        TransactionType.Incoming ->  R.drawable.animation_receiving_progress_30
+        TransactionType.Approve -> R.drawable.animation_approving_progress_30
+    }
+
+    private fun getTwoConfirmationBackground(type: TransactionType) = when (type) {
+        TransactionType.Outgoing,  TransactionType.SentToSelf -> R.drawable.animation_sending_progress_60
+        TransactionType.Incoming ->  R.drawable.animation_receiving_progress_60
+        TransactionType.Approve -> R.drawable.animation_approving_progress_60
+    }
+
+    private fun getDescription(type: TransactionType) = when (type) {
+        TransactionType.Outgoing, TransactionType.SentToSelf -> R.string.Transactions_Sending
+        TransactionType.Incoming -> R.string.Transactions_Receiving
+        TransactionType.Approve -> R.string.Transactions_Approving
     }
 
 }
