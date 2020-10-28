d864b352ff8268de201fcaa6b5ef296a32e52d55
chyngyz <chyngyz.omurov@gmail.com>
2020-10-28 14:22:28 +0600

    Add Zcash coin info for charts

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/ratechart/CoinInfoMap.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/ratechart/CoinInfoMap.kt
@@ -3,11 +3,11 @@ package io.horizontalsystems.bankwallet.modules.ratechart
 import java.math.BigDecimal
 
 object CoinInfoMap {
-    
+
     val data = mapOf(
             "BTC" to CoinInfo(BigDecimal(21_000_000), "03/01/2009", "https://bitcoin.org/en"),
-            "LTC" to CoinInfo(BigDecimal(84_000_000),  "13/10/2011", "https://litecoin.com"),
-            "ETH" to CoinInfo( null,  "30/07/2015", "https://www.ethereum.org"),
+            "LTC" to CoinInfo(BigDecimal(84_000_000), "13/10/2011", "https://litecoin.com"),
+            "ETH" to CoinInfo(null, "30/07/2015", "https://www.ethereum.org"),
             "BCH" to CoinInfo(BigDecimal(21_000_000), "01/08/2017", "https://www.bitcoincash.org"),
             "DASH" to CoinInfo(BigDecimal(18_900_000), "18/01/2014", "http://dash.org"),
             "BNB" to CoinInfo(BigDecimal(187_536_713), "27/06/2017", "https://www.binance.com"),
@@ -18,7 +18,7 @@ object CoinInfoMap {
             "ANT" to CoinInfo(BigDecimal(39_609_524), "05/05/2017", "https://aragon.one"),
             "BAL" to CoinInfo(BigDecimal(35_725_000), "20/06/2020", "https://balancer.finance"),
             "BAT" to CoinInfo(BigDecimal(1_500_000_000), "31/05/2017", "https://basicattentiontoken.org"),
-            "BNT" to CoinInfo( null,  "13/02/2017", "https://bancor.network"),
+            "BNT" to CoinInfo(null, "13/02/2017", "https://bancor.network"),
             "BTCB" to CoinInfo(BigDecimal(9_001), "17/06/2019", "https://bitcoin.org/en"),
             "BUSD" to CoinInfo(BigDecimal(28_603_822), "10/09/2019", "https://www.paxos.com/busd"),
             "CAS" to CoinInfo(BigDecimal(1_000_000_000), "12/10/2017", "https://cashaa.com"),
@@ -36,14 +36,14 @@ object CoinInfoMap {
             "ELF" to CoinInfo(BigDecimal(880_000_000), "18/12/2017", "http://aelf.io"),
             "ENJ" to CoinInfo(BigDecimal(1_000_000_000), "24/07/2017", "https://enjincoin.io"),
             "EOSDT" to CoinInfo(BigDecimal(170_000_000), "31/05/2019", "https://eosdt.com/en"),
-            "EURS" to CoinInfo( null, "22/06/2018", "https://stasis.net"),
+            "EURS" to CoinInfo(null, "22/06/2018", "https://stasis.net"),
             "IQ" to CoinInfo(BigDecimal(10_006_128_771), "14/07/2018", "https://everipedia.org"),
-            "GUSD" to CoinInfo( null, "09/09/2018", "https://gemini.com/dollar"),
+            "GUSD" to CoinInfo(null, "09/09/2018", "https://gemini.com/dollar"),
             "GTO" to CoinInfo(BigDecimal(1_000_000_000), "14/12/2017", "https://gifto.io"),
             "GNT" to CoinInfo(BigDecimal(1_000_000_000), "17/11/2016", "https://golem.network"),
             "HOT" to CoinInfo(BigDecimal(177_619_433_541), "16/01/2018", "https://thehydrofoundation.com"),
             "HT" to CoinInfo(BigDecimal(500_000_000), "22/01/2018", "https://www.huobi.pro"),
-            "IDXM" to CoinInfo( null, null, "https://idex.market/eth/idex"),
+            "IDXM" to CoinInfo(null, null, "https://idex.market/eth/idex"),
             "IDEX" to CoinInfo(BigDecimal(1_000_000_000), "28/09/2017", "https://auroradao.com"),
             "KCS" to CoinInfo(BigDecimal(176_863_551), "15/09/2017", "https://www.kucoin.com"),
             "KNC" to CoinInfo(BigDecimal(210_590_429), "20/09/2017", "https://kyber.network"),
@@ -60,7 +60,7 @@ object CoinInfoMap {
             "NEXO" to CoinInfo(BigDecimal(1_000_000_000), "29/04/2018", "https://omg.network"),
             "NPXS" to CoinInfo(BigDecimal(259_810_708_833), "27/09/2017", "https://pundix.com"),
             "NUT" to CoinInfo(BigDecimal(10_000_000), "26/02/2019", "https://mith.io"),
-            "OMG" to CoinInfo( null,"23/06/2017", "https://nexo.io") ,
+            "OMG" to CoinInfo(null, "23/06/2017", "https://nexo.io"),
             "ORBS" to CoinInfo(BigDecimal(10_000_000_000), "14/03/2018", "https://www.orbs.com"),
             "OXT" to CoinInfo(BigDecimal(10_000_000_000), "03/12/2019", "https://www.orchid.com"),
             "PAR" to CoinInfo(BigDecimal(999_628_334), null, "https://www.parachutetoken.com"),
@@ -71,26 +71,26 @@ object CoinInfoMap {
             "PPT" to CoinInfo(BigDecimal(53_252_246), "12/04/2017", "https://populous.world"),
             "PTI" to CoinInfo(BigDecimal(3_600_000_000), "13/03/2018", "https://tokensale.paytomat.com"),
             "R" to CoinInfo(BigDecimal(1_000_000_000), "04/08/2017", "http://revain.org"),
-            "REN" to CoinInfo( BigDecimal(999_999_633), "31/12/2017", "https://renproject.io"),
-            "RENBCH" to CoinInfo( null, null, "https://renproject.io"),
-            "RENBTC" to CoinInfo( null, null, "https://renproject.io"),
-            "RENZEC" to CoinInfo( null, null, "https://renproject.io"),
-            "REP" to CoinInfo( null, "17/11/2014", "https://www.augur.net"),
-            "SAI" to CoinInfo( null, "18/12/2017", "https://makerdao.com/en"),
+            "REN" to CoinInfo(BigDecimal(999_999_633), "31/12/2017", "https://renproject.io"),
+            "RENBCH" to CoinInfo(null, null, "https://renproject.io"),
+            "RENBTC" to CoinInfo(null, null, "https://renproject.io"),
+            "RENZEC" to CoinInfo(null, null, "https://renproject.io"),
+            "REP" to CoinInfo(null, "17/11/2014", "https://www.augur.net"),
+            "SAI" to CoinInfo(null, "18/12/2017", "https://makerdao.com/en"),
             "SNT" to CoinInfo(BigDecimal(6_804_870_174), "20/06/2017", "https://status.im"),
             "SNX" to CoinInfo(BigDecimal(190_075_446), "07/01/2018", "https://www.synthetix.io"),
             "SWAP" to CoinInfo(BigDecimal(100_000_000), "09/07/2020", "https://trustswap.org"),
             "TKN" to CoinInfo(BigDecimal(39_406_760), "25/04/2017", "https://monolith.xyz"),
-            "TUSD" to CoinInfo( null, "31/03/2018", "https://www.trusttoken.com"),
+            "TUSD" to CoinInfo(null, "31/03/2018", "https://www.trusttoken.com"),
             "USDC" to CoinInfo(BigDecimal(931_069_099), "17/05/2018", "https://www.centre.io/usdc"),
             "USDT" to CoinInfo(BigDecimal(9_479_177_442), "10/06/2014", "https://tether.to"),
             "WBTC" to CoinInfo(BigDecimal(21_000_000), "27/11/2018", "https://wbtc.network"),
             "WETH" to CoinInfo(BigDecimal(1_153_917), "31/12/2018", "https://weth.io"),
             "WTC" to CoinInfo(BigDecimal(70_000_000), "27/08/2017", "https://www.waltonchain.org/en"),
             "XRP" to CoinInfo(BigDecimal(99_990_932_726), "02/02/2013", "https://ripple.com/xrp"),
-            "ZRX" to CoinInfo(BigDecimal(1_000_000_000), "15/08/2017", "https://www.0xproject.com")
-
-            )
+            "ZRX" to CoinInfo(BigDecimal(1_000_000_000), "15/08/2017", "https://www.0xproject.com"),
+            "ZEC" to CoinInfo(BigDecimal(21_000_000), "28/10/2016", "https://z.cash")
+    )
 }
 
 data class CoinInfo(val supply: BigDecimal?, val startDate: String?, val website: String?)
