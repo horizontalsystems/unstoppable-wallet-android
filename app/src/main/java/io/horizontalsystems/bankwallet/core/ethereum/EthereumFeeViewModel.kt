0ac1fd5be6a5233b3784107631c1c5f4afc05626
bakyt <abdrasulov@gmail.com>
2020-10-29 18:24:02 +0600

    EthereumFeeViewModel additions

--- app/src/main/java/io/horizontalsystems/bankwallet/core/ethereum/EthereumFeeViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/ethereum/EthereumFeeViewModel.kt
@@ -1,5 +1,6 @@
 package io.horizontalsystems.bankwallet.core.ethereum
 
+import android.util.Range
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.horizontalsystems.bankwallet.R
@@ -12,27 +13,76 @@ class EthereumFeeViewModel(
         private val coinService: EthereumCoinService
 ) : ViewModel() {
 
-    private val feeStatusLiveData = MutableLiveData<String>()
+    sealed class Priority {
+        abstract val description: String
+
+        object Recommended : Priority() {
+            override val description by lazy { App.instance.getString(R.string.Send_TxSpeed_Recommended) }
+        }
+
+        object Custom : Priority() {
+            override val description by lazy { App.instance.getString(R.string.Send_TxSpeed_Custom) }
+        }
+    }
+
+    val feeStatusLiveData = MutableLiveData<String>("")
+    val priorityLiveData = MutableLiveData<String>("")
+    val feeSliderLiveData = MutableLiveData<SendFeeSliderViewItem?>(null)
+
+    private val customFeeUnit = "gwei"
+    private val customFeeRange = Range(1, 400)
     private val disposable = CompositeDisposable()
 
     init {
-        sync(transactionService.transactionStatus)
+        syncTransactionStatus(transactionService.transactionStatus)
+        syncGasPriceType(transactionService.gasPriceType)
 
         transactionService.transactionStatusObservable
                 .subscribe { transactionStatus ->
-                    sync(transactionStatus)
+                    syncTransactionStatus(transactionStatus)
+                }
+                .let {
+                    disposable.add(it)
+                }
+
+        transactionService.gasPriceTypeObservable
+                .subscribe { gasPriceType ->
+                    syncGasPriceType(gasPriceType)
                 }
                 .let {
                     disposable.add(it)
                 }
     }
 
-    fun setGasPriceType(gasPriceType: EthereumTransactionService.GasPriceType) {
-        transactionService.gasPriceType = gasPriceType
+    private fun syncTransactionStatus(transactionStatus: DataState<EthereumTransactionService.Transaction>) {
+        feeStatusLiveData.postValue(feeStatus(transactionStatus))
     }
 
-    private fun sync(transactionStatus: DataState<EthereumTransactionService.Transaction>) {
-        feeStatusLiveData.postValue(feeStatus(transactionStatus))
+    private fun syncGasPriceType(gasPriceType: EthereumTransactionService.GasPriceType) {
+        priorityLiveData.postValue(getPriority(gasPriceType).description)
+
+        when (gasPriceType) {
+            EthereumTransactionService.GasPriceType.Recommended -> {
+                feeSliderLiveData.postValue(null)
+            }
+            is EthereumTransactionService.GasPriceType.Custom -> {
+                if (feeSliderLiveData.value == null) {
+                    val gasPrice = gasPriceType.gasPrice
+                    feeSliderLiveData.postValue(SendFeeSliderViewItem(initialValue = gwei(wei = gasPrice), range = customFeeRange, unit = customFeeUnit))
+                }
+            }
+        }
+    }
+
+    private fun gwei(wei: Long): Long {
+        return wei / 1_000_000_000
+    }
+
+    private fun getPriority(gasPriceType: EthereumTransactionService.GasPriceType): Priority {
+        return when (gasPriceType) {
+            EthereumTransactionService.GasPriceType.Recommended -> Priority.Recommended
+            is EthereumTransactionService.GasPriceType.Custom -> Priority.Custom
+        }
     }
 
     private fun feeStatus(transactionStatus: DataState<EthereumTransactionService.Transaction>): String {
@@ -50,3 +100,5 @@ class EthereumFeeViewModel(
     }
 
 }
+
+data class SendFeeSliderViewItem(val initialValue: Long, val range: Range<Int>, val unit: String)
