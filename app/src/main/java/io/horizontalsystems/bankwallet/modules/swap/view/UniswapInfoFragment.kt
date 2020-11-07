359828974622f1a04297c6b2483da9f84faccb28
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2020-11-07 11:15:09 +0600

    Change toolbar in UniswapInfoFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/view/UniswapInfoFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/view/UniswapInfoFragment.kt
@@ -6,10 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.horizontalsystems.core.findNavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
-import io.horizontalsystems.views.TopMenuItem
+import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_swap_info.*
 
 class UniswapInfoFragment : BaseFragment() {
@@ -21,12 +20,15 @@ class UniswapInfoFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        shadowlessToolbar.bind(
-                title = getString(R.string.UniswapInfo_Title),
-                rightBtnItem = TopMenuItem(text = R.string.Button_Close, onClick = {
-                    findNavController().popBackStack()
-                })
-        )
+        toolbar.inflateMenu(R.menu.uniswap_info_menu)
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            if (menuItem.itemId == R.id.menuClose){
+                findNavController().popBackStack()
+                true
+            } else {
+                super.onOptionsItemSelected(menuItem)
+            }
+        }
 
         buttonUniswap.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
