b3dbf3582a1f500697b0c604c4cd0165a1aeecc2
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-11-19 14:21:21 +0600

    Use RecyclerView in MainSettingsFragment

--- components/views/src/main/java/io/horizontalsystems/views/SettingsViewSwitch.kt
+++ components/views/src/main/java/io/horizontalsystems/views/SettingsViewSwitch.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.CompoundButton
 import androidx.core.view.isVisible
-import kotlinx.android.synthetic.main.view_settings_switch.view.*
+import kotlinx.android.synthetic.main.view_settings_item_switch.view.*
 
 class SettingsViewSwitch @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
     : SettingsViewBase(context, attrs, defStyleAttr), CompoundButton.OnCheckedChangeListener {
@@ -49,7 +49,7 @@ class SettingsViewSwitch @JvmOverloads constructor(context: Context, attrs: Attr
     }
 
     init {
-        inflate(context, R.layout.view_settings_switch, this)
+        inflate(context, R.layout.view_settings_item_switch, this)
 
         switchSettings.setOnCheckedChangeListener(this)
 
