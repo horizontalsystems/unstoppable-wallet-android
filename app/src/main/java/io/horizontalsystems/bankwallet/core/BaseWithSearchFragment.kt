8e0fd5a18ad7e6701ce273af0a816eee8af75151
bakyt <abdrasulov@gmail.com>
2020-11-25 15:11:25 +0600

    Fix memory leak

--- app/src/main/java/io/horizontalsystems/bankwallet/core/BaseWithSearchFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/BaseWithSearchFragment.kt
@@ -16,27 +16,31 @@ abstract class BaseWithSearchFragment : BaseFragment() {
     open fun searchExpanded(menu: Menu) {}
     open fun searchCollapsed(menu: Menu){}
 
+    private var searchView: SearchView? = null
+
     protected fun configureSearchMenu(menu: Menu, hint: Int) {
         val menuItem = menu.findItem(R.id.search) ?: return
-        val searchView: SearchView = menuItem.actionView as SearchView
-        searchView.maxWidth = Integer.MAX_VALUE
-        searchView.queryHint = getString(hint)
-        searchView.imeOptions = searchView.imeOptions or EditorInfo.IME_FLAG_NO_EXTRACT_UI
+        searchView = menuItem.actionView as? SearchView
+        searchView?.let { searchView ->
+            searchView.maxWidth = Integer.MAX_VALUE
+            searchView.queryHint = getString(hint)
+            searchView.imeOptions = searchView.imeOptions or EditorInfo.IME_FLAG_NO_EXTRACT_UI
 
-        searchView.findViewById<View>(androidx.appcompat.R.id.search_plate)?.setBackgroundColor(Color.TRANSPARENT)
-        searchView.findViewById<EditText>(R.id.search_src_text)?.let { editText ->
-            context?.getColor(R.color.grey_50)?.let { color -> editText.setHintTextColor(color) }
-        }
-
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newQuery: String): Boolean {
-                val trimmedQuery = newQuery.trim()
-                updateFilter(trimmedQuery)
-                return true
+            searchView.findViewById<View>(androidx.appcompat.R.id.search_plate)?.setBackgroundColor(Color.TRANSPARENT)
+            searchView.findViewById<EditText>(R.id.search_src_text)?.let { editText ->
+                context?.getColor(R.color.grey_50)?.let { color -> editText.setHintTextColor(color) }
             }
 
-            override fun onQueryTextSubmit(query: String): Boolean = false
-        })
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newQuery: String): Boolean {
+                    val trimmedQuery = newQuery.trim()
+                    updateFilter(trimmedQuery)
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String): Boolean = false
+            })
+        }
 
         menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
             override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
@@ -49,5 +53,11 @@ abstract class BaseWithSearchFragment : BaseFragment() {
                 return true
             }
         })
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+        searchView?.setOnQueryTextListener(null)
     }
 }
