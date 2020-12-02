598d08390926760cd6c97795e3eeaf29f7866939
Talgat Medetbekov <medetbekov.t@gmail.com>
2020-12-02 18:59:34 +0600

    Validate words on restore wallet

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/WordsManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/WordsManager.kt
@@ -3,15 +3,28 @@ package io.horizontalsystems.bankwallet.core.managers
 import io.horizontalsystems.bankwallet.core.IWordsManager
 import io.horizontalsystems.bankwallet.core.InvalidMnemonicWordsCountException
 import io.horizontalsystems.hdwalletkit.Mnemonic
+import io.horizontalsystems.hdwalletkit.WordList
+import kotlin.jvm.Throws
 
 class WordsManager : IWordsManager {
+    private val wordList = WordList.getWords()
+    private val wordsString = wordList.joinToString(" ")
 
-    @Throws()
+    @Throws
     override fun validate(words: List<String>, wordCount: Int) {
         if (words.size != wordCount) {
             throw InvalidMnemonicWordsCountException()
         }
+
         Mnemonic().validate(words)
+    }
+
+    override fun wordExists(word: String): Boolean {
+        return wordList.binarySearch(word) >= 0
+    }
+
+    override fun wordContains(word: String): Boolean {
+        return wordsString.contains(word)
     }
 
     override fun generateWords(count: Int): List<String> {
