4f42f67973b114a5f164159a7e10370f61e099a9
bakyt <abdrasulov@gmail.com>
2020-12-21 16:12:30 +0600

    Make text in fee autoscrollable

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top/FeeDataAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top/FeeDataAdapter.kt
@@ -8,20 +8,29 @@ import androidx.recyclerview.widget.RecyclerView
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.views.inflate
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.view_holder_market_fee.*
+import java.util.*
 
-class FeeDataAdapter : ListAdapter<FeeData, FeeViewHolder>(diff) {
+class FeeDataAdapter : ListAdapter<Optional<FeeData>, FeeViewHolder>(diff) {
+
+    init {
+        submitList(listOf(Optional.empty()))
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeeViewHolder {
         return FeeViewHolder(inflate(parent, R.layout.view_holder_market_fee, false))
     }
 
     override fun onBindViewHolder(holder: FeeViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        val item = getItem(position)
+
+        holder.bind(if (item.isPresent) item.get() else null)
     }
 
     companion object {
-        private val diff = object : DiffUtil.ItemCallback<FeeData>() {
-            override fun areItemsTheSame(oldItem: FeeData, newItem: FeeData) = true
-            override fun areContentsTheSame(oldItem: FeeData, newItem: FeeData) = false
+        private val diff = object : DiffUtil.ItemCallback<Optional<FeeData>>() {
+            override fun areItemsTheSame(oldItem: Optional<FeeData>, newItem: Optional<FeeData>) = true
+            override fun areContentsTheSame(oldItem: Optional<FeeData>, newItem: Optional<FeeData>) = false
         }
     }
 }
@@ -31,8 +40,13 @@ class FeeData {
 }
 
 class FeeViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-    fun bind(item: FeeData) {
 
+    fun bind(item: FeeData?) {
+        text.text = item?.let {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+
+        text.isSelected = true
     }
 
 }
