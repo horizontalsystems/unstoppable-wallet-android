5d759d4e3f3215d2d45295832ed63847ed54556d
bakyt <abdrasulov@gmail.com>
2020-12-23 14:00:18 +0600

    Store market top items in service as property

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top/MarketTopService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top/MarketTopService.kt
@@ -4,6 +4,7 @@ import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.IRateManager
 import io.horizontalsystems.bankwallet.entities.DataState
 import io.horizontalsystems.core.ICurrencyManager
+import io.horizontalsystems.xrateskit.entities.TopMarket
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 
@@ -12,27 +13,19 @@ class MarketTopService(
         private val currencyManager: ICurrencyManager
 ) : Clearable {
 
-    val sortingFields: Array<Field> = Field.values()
-    val sortingPeriods: Array<Period> = Period.values()
-
-    var sortingField: Field = Field.HighestCap
+    val periods: Array<Period> = Period.values()
+    var period: Period = Period.Period24h
         set(value) {
             field = value
 
-            sortingFieldObservable.onNext(value)
+            syncTopItemsByPeriod()
         }
 
-    var sortingPeriod: Period = Period.Period24h
-        set(value) {
-            field = value
-
-            sortingPeriodObservable.onNext(value)
-        }
-
-    val sortingFieldObservable = BehaviorSubject.createDefault(sortingField)
-    val sortingPeriodObservable = BehaviorSubject.createDefault(sortingPeriod)
     val marketTopItemsObservable: BehaviorSubject<DataState<List<MarketTopItem>>> = BehaviorSubject.createDefault(DataState.Loading)
     val currency by currencyManager::baseCurrency
+
+    private var fullItems: List<TopMarket> = listOf()
+    private var marketTopItems: List<MarketTopItem> = listOf()
 
     private val disposable = CompositeDisposable()
 
@@ -49,25 +42,29 @@ class MarketTopService(
 
         xRateManager.getTopMarketList(currencyManager.baseCurrency.code)
                 .subscribe({
-                    marketTopItemsObservable.onNext(DataState.Success(
-                            it.map {
-                                MarketTopItem(
-                                        it.coinCode,
-                                        it.coinName,
-                                        it.marketInfo.marketCap,
-                                        it.marketInfo.volume,
-                                        it.marketInfo.rate,
-                                        it.marketInfo.diff,
-                                )
-                            }
-
-                    ))
+                    fullItems = it
+                    syncTopItemsByPeriod()
                 }, {
                     marketTopItemsObservable.onNext(DataState.Error(it))
                 })
                 .let {
                     disposable.add(it)
                 }
+    }
+
+    private fun syncTopItemsByPeriod() {
+        marketTopItems = fullItems.map {
+            MarketTopItem(
+                    it.coinCode,
+                    it.coinName,
+                    it.marketInfo.marketCap,
+                    it.marketInfo.volume,
+                    it.marketInfo.rate,
+                    it.marketInfo.diff,
+            )
+        }
+
+        marketTopItemsObservable.onNext(DataState.Success(marketTopItems))
     }
 
     override fun clear() {
