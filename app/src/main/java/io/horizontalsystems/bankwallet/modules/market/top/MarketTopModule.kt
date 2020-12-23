90b47ede868517fdea5e595792c7ccbe353c28a6
bakyt <abdrasulov@gmail.com>
2020-12-23 15:13:27 +0600

    Add fields for sorting

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top/MarketTopModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top/MarketTopModule.kt
@@ -24,6 +24,8 @@ object MarketTopModule {
 enum class Field(@StringRes val titleResId: Int) {
     HighestCap(R.string.Market_Field_HighestCap), LowestCap(R.string.Market_Field_LowestCap),
     HighestVolume(R.string.Market_Field_HighestVolume), LowestVolume(R.string.Market_Field_LowestVolume),
+    HighestPrice(R.string.Market_Field_HighestPrice), LowestPrice(R.string.Market_Field_LowestPrice),
+    TopGainers(R.string.RateList_TopWinners), TopLosers(R.string.RateList_TopLosers),
 }
 
 enum class Period(@StringRes val titleResId: Int) {
