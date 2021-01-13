59b9195e085aef1a31f06005a1ea28106f3233d3
bakyt <abdrasulov@gmail.com>
2021-01-13 12:33:47 +0600

    Remove marquee line from market tabs

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top100/MarketTop100Fragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/top100/MarketTop100Fragment.kt
@@ -8,9 +8,6 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.ConcatAdapter
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
-import io.horizontalsystems.bankwallet.modules.market.fee.MarketFeeDataAdapter
-import io.horizontalsystems.bankwallet.modules.market.fee.MarketFeeModule
-import io.horizontalsystems.bankwallet.modules.market.fee.MarketFeeViewModel
 import io.horizontalsystems.bankwallet.modules.market.metrics.MarketMetricsAdapter
 import io.horizontalsystems.bankwallet.modules.market.metrics.MarketMetricsModule
 import io.horizontalsystems.bankwallet.modules.market.metrics.MarketMetricsViewModel
@@ -20,17 +17,14 @@ import io.horizontalsystems.bankwallet.ui.extensions.SelectorDialog
 import io.horizontalsystems.bankwallet.ui.extensions.SelectorItem
 import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_rates.*
-import java.util.*
 
 class MarketTop100Fragment : BaseFragment(), MarketTopHeaderAdapter.Listener, MarketTopItemsAdapter.Listener {
 
     private lateinit var marketMetricsAdapter: MarketMetricsAdapter
-    private lateinit var marketFeeDataAdapter: MarketFeeDataAdapter
     private lateinit var marketTopHeaderAdapter: MarketTopHeaderAdapter
     private lateinit var marketTopItemsAdapter: MarketTopItemsAdapter
 
     private val marketMetricsViewModel by viewModels<MarketMetricsViewModel> { MarketMetricsModule.Factory() }
-    private val marketFeeViewModel by viewModels<MarketFeeViewModel> { MarketFeeModule.Factory() }
     private val marketTopViewModel by viewModels<MarketTopViewModel> { MarketTopModule.Factory() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -41,21 +35,16 @@ class MarketTop100Fragment : BaseFragment(), MarketTopHeaderAdapter.Listener, Ma
         super.onViewCreated(view, savedInstanceState)
 
         marketMetricsAdapter = MarketMetricsAdapter(marketMetricsViewModel, viewLifecycleOwner)
-        marketFeeDataAdapter = MarketFeeDataAdapter()
         marketTopHeaderAdapter = MarketTopHeaderAdapter(this, marketTopViewModel, viewLifecycleOwner)
         marketTopItemsAdapter = MarketTopItemsAdapter(this, marketTopViewModel, viewLifecycleOwner)
 
-        coinRatesRecyclerView.adapter = ConcatAdapter(marketMetricsAdapter, marketFeeDataAdapter, marketTopHeaderAdapter, marketTopItemsAdapter)
+        coinRatesRecyclerView.adapter = ConcatAdapter(marketMetricsAdapter, marketTopHeaderAdapter, marketTopItemsAdapter)
 
         pullToRefresh.setOnRefreshListener {
             marketMetricsAdapter.refresh()
             marketTopItemsAdapter.refresh()
 
             pullToRefresh.isRefreshing = false
-        }
-
-        marketFeeViewModel.feeLiveData.observe(viewLifecycleOwner) {
-            marketFeeDataAdapter.submitList(listOf(Optional.of(it)))
         }
     }
 
