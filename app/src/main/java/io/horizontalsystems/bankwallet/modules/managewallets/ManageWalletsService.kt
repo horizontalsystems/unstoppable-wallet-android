528bfed8fa7cd0a88a31dc4ed414f827ebe87e2d
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-01-18 15:17:31 +0600

    Mark token as enabled while create/restore account from 'Add Coins'

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/managewallets/ManageWalletsService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/managewallets/ManageWalletsService.kt
@@ -2,6 +2,7 @@ package io.horizontalsystems.bankwallet.modules.managewallets
 
 import io.horizontalsystems.bankwallet.core.*
 import io.horizontalsystems.bankwallet.entities.*
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
@@ -9,11 +10,11 @@ import io.reactivex.subjects.BehaviorSubject
 class ManageWalletsService(
         private val coinManager: ICoinManager,
         private val walletManager: IWalletManager,
-        private val accountManager: IAccountManager
-) : ManageWalletsModule.IManageWalletsService, Clearable {
+        private val accountManager: IAccountManager)
+    : ManageWalletsModule.IManageWalletsService, Clearable {
 
     private val disposables = CompositeDisposable()
-    private val wallets = mutableMapOf<Coin, Wallet>()
+    private var wallets = mutableMapOf<Coin, Wallet>()
 
     override val stateAsync = BehaviorSubject.create<ManageWalletsModule.State>()
 
@@ -37,37 +38,41 @@ class ManageWalletsService(
                     syncState()
                 })
 
+        walletManager.walletsUpdatedObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { wallets ->
+                    sync(wallets)
+                }.let {
+                    disposables.add(it)
+                }
 
-        walletManager.wallets.forEach { wallet ->
+        sync(walletManager.wallets)
+    }
+
+    private fun sync(walletList: List<Wallet>) {
+        wallets = mutableMapOf()
+        walletList.forEach { wallet ->
             wallets[wallet.coin] = wallet
         }
 
         syncState()
     }
 
-
     override fun enable(coin: Coin, derivationSetting: DerivationSetting?) {
         val account = account(coin) ?: throw EnableCoinError.NoAccount
 
         val wallet = Wallet(coin, account)
-
         walletManager.save(listOf(wallet))
-        wallets[coin] = wallet
-
-        syncState()
     }
 
     override fun disable(coin: Coin) {
         val wallet = wallets[coin] ?: return
-
         walletManager.delete(listOf(wallet))
-        wallets.remove(coin)
-
-        syncState()
     }
 
     override fun account(coin: Coin): Account? {
-        return accountManager.accounts.firstOrNull { coin.type.canSupport(it.type) }
+        return accountManager.account(coin.type)
     }
 
     override fun clear() {
@@ -94,7 +99,7 @@ class ManageWalletsService(
         return ManageWalletsModule.Item(coin, state)
     }
 
-    sealed class EnableCoinError: Exception() {
-        object NoAccount: EnableCoinError()
+    sealed class EnableCoinError : Exception() {
+        object NoAccount : EnableCoinError()
     }
 }
