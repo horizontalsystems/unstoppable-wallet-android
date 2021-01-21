454f61b538b92e075cfe71b0babf8591e4d61ff2
bakyt <abdrasulov@gmail.com>
2021-01-21 16:03:09 +0600

    Show section header only when related items are not empty

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/overview/MarketOverviewSectionHeaderAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/overview/MarketOverviewSectionHeaderAdapter.kt
@@ -1,15 +1,35 @@
 package io.horizontalsystems.bankwallet.modules.market.overview
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.modules.market.top.MarketTopViewItem
 import io.horizontalsystems.bankwallet.modules.settings.main.MainSettingsAdapter
 import io.horizontalsystems.bankwallet.modules.settings.main.SettingsMenuItem
 import io.horizontalsystems.views.inflate
 
-class MarketOverviewSectionHeaderAdapter(val settingsMenuItem: SettingsMenuItem) : RecyclerView.Adapter<MainSettingsAdapter.SettingsViewHolderArrow>() {
+class MarketOverviewSectionHeaderAdapter(
+        private val viewItemsLiveData: LiveData<List<MarketTopViewItem>>,
+        viewLifecycleOwner: LifecycleOwner,
+        private val settingsMenuItem: SettingsMenuItem
+) : ListAdapter<SettingsMenuItem, MainSettingsAdapter.SettingsViewHolderArrow>(diffCallback) {
 
-    override fun getItemCount() = 1
+    init {
+        Transformations
+                .distinctUntilChanged(Transformations.map(viewItemsLiveData) { it.isNotEmpty() })
+                .observe(viewLifecycleOwner) { showHeader ->
+                    val items = if (showHeader) {
+                        listOf(settingsMenuItem)
+                    } else {
+                        listOf()
+                    }
+                    submitList(items)
+                }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainSettingsAdapter.SettingsViewHolderArrow {
         return MainSettingsAdapter.SettingsViewHolderArrow(inflate(parent, R.layout.view_settings_item_arrow))
@@ -17,6 +37,18 @@ class MarketOverviewSectionHeaderAdapter(val settingsMenuItem: SettingsMenuItem)
 
     override fun onBindViewHolder(holder: MainSettingsAdapter.SettingsViewHolderArrow, position: Int) {
         holder.bind(settingsMenuItem)
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<SettingsMenuItem>() {
+            override fun areItemsTheSame(oldItem: SettingsMenuItem, newItem: SettingsMenuItem): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: SettingsMenuItem, newItem: SettingsMenuItem): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 
 }
