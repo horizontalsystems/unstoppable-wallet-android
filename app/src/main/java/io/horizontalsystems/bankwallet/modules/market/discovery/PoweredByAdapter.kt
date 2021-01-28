c3f478aeaf6f3bd33c225a7c3531bfe5c793b865
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-01-28 18:58:12 +0600

    Optimize PoweredByAdapter by using ListAdapter

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/PoweredByAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/PoweredByAdapter.kt
@@ -5,31 +5,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import io.horizontalsystems.bankwallet.R
 
 class PoweredByAdapter(
         showPoweredBy: LiveData<Boolean>,
         viewLifecycleOwner: LifecycleOwner
-) : RecyclerView.Adapter<PoweredByAdapter.ViewHolder>() {
-
-    private var show = false
+) : ListAdapter<Boolean, PoweredByAdapter.ViewHolder>(diffCallback) {
 
     init {
         showPoweredBy.observe(viewLifecycleOwner, { show ->
-            if (this.show != show) {
-                this.show = show
-                notifyDataSetChanged()
-            }
+            submitList(if (show) listOf(true) else listOf())
         })
     }
-
-    override fun getItemCount() = if (show) 1 else 0
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = Unit
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.create(parent)
+    }
+
+    companion object{
+        private val diffCallback = object : DiffUtil.ItemCallback<Boolean>() {
+            override fun areItemsTheSame(oldItem: Boolean, newItem: Boolean): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Boolean, newItem: Boolean): Boolean {
+                return oldItem == newItem
+            }
+        }
     }
 
     class ViewHolder(containerView: View) : RecyclerView.ViewHolder(containerView) {
