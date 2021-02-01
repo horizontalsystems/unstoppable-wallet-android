b8ccc6098bbafecc6605861452bbaa0939edf65d
bakyt <abdrasulov@gmail.com>
2021-02-01 18:18:55 +0600

    Move market list header view outside of recycler view in favorites

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/favorites/MarketFavoritesFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/favorites/MarketFavoritesFragment.kt
@@ -10,28 +10,31 @@ import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.modules.market.top.*
 import io.horizontalsystems.bankwallet.modules.ratechart.RateChartFragment
+import io.horizontalsystems.bankwallet.ui.extensions.MarketListHeaderView
 import io.horizontalsystems.bankwallet.ui.extensions.SelectorDialog
 import io.horizontalsystems.bankwallet.ui.extensions.SelectorItem
 import io.horizontalsystems.core.findNavController
 import io.horizontalsystems.core.helpers.HudHelper
-import kotlinx.android.synthetic.main.fragment_rates.*
+import kotlinx.android.synthetic.main.fragment_market_favorites.*
 
-class MarketFavoritesFragment : BaseFragment(), MarketTopHeaderAdapter.Listener, ViewHolderMarketTopItem.Listener {
+class MarketFavoritesFragment : BaseFragment(), MarketListHeaderView.Listener, ViewHolderMarketTopItem.Listener {
 
-    private lateinit var marketTopHeaderAdapter: MarketTopHeaderAdapter
     private lateinit var marketTopItemsAdapter: MarketTopItemsAdapter
     private lateinit var marketLoadingAdapter: MarketLoadingAdapter
 
     private val marketTopViewModel by viewModels<MarketTopViewModel> { MarketFavoritesModule.Factory() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_rates, container, false)
+        return inflater.inflate(R.layout.fragment_market_favorites, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        marketTopHeaderAdapter = MarketTopHeaderAdapter(this, marketTopViewModel.sortingField, marketTopViewModel.marketField)
+        marketListHeader.listener = this
+        marketListHeader.setSortingField(marketTopViewModel.sortingField)
+        marketListHeader.setMarketField(marketTopViewModel.marketField)
+
         marketTopItemsAdapter = MarketTopItemsAdapter(
                 this,
                 marketTopViewModel.marketTopViewItemsLiveData,
@@ -41,7 +44,7 @@ class MarketFavoritesFragment : BaseFragment(), MarketTopHeaderAdapter.Listener,
         )
         marketLoadingAdapter = MarketLoadingAdapter(marketTopViewModel.loadingLiveData, marketTopViewModel.errorLiveData, marketTopViewModel::onErrorClick, viewLifecycleOwner)
 
-        coinRatesRecyclerView.adapter = ConcatAdapter(marketTopHeaderAdapter, marketLoadingAdapter, marketTopItemsAdapter)
+        coinRatesRecyclerView.adapter = ConcatAdapter(marketLoadingAdapter, marketTopItemsAdapter)
         coinRatesRecyclerView.itemAnimator = null
 
         pullToRefresh.setOnRefreshListener {
@@ -64,7 +67,7 @@ class MarketFavoritesFragment : BaseFragment(), MarketTopHeaderAdapter.Listener,
                 .newInstance(items, getString(R.string.Market_Sort_PopupTitle)) { position ->
                     val selectedSortingField = marketTopViewModel.sortingFields[position]
 
-                    marketTopHeaderAdapter.update(sortingField = selectedSortingField)
+                    marketListHeader.setSortingField(selectedSortingField)
                     marketTopViewModel.update(sortingField = selectedSortingField)
                 }
                 .show(childFragmentManager, "sorting_field_selector")
