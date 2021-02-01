902fadd92a2b9666fbee3925eb3bb3e97ca8daeb
chyngyz <chyngyz.omurov@gmail.com>
2021-02-01 16:53:21 +0600

    Read market coin categories from file

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketCategoryProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketCategoryProvider.kt
@@ -1,15 +1,56 @@
 package io.horizontalsystems.bankwallet.modules.market.discovery
 
+import android.content.Context
+import com.google.gson.Gson
 import io.reactivex.Single
+import java.io.InputStreamReader
 
-class MarketCategoryProvider {
+class MarketCategoryProvider(private val context: Context) {
 
-    fun getCoinCodesByCategoryAsync(categoryId: String): Single<List<String>> {
-        return Single.just(listOf("BTC", "ETH", "BCH", "CAKE"))
+    private val coinCategoriesFileName = "coin_categories.json"
+
+    private val coinCategories: CoinCategories by lazy {
+        val inputStream = context.assets.open(coinCategoriesFileName)
+        Gson().fromJson(InputStreamReader(inputStream), CoinCategories::class.java)
     }
 
-    fun getCoinRatingsAsync(): Single<Map<String, String>> {
-        return Single.just(mapOf("BTC" to "A", "ETH" to "B", "BCH" to "C", "CAKE" to "D"))
-    }
+    fun getCoinCodesByCategoryAsync(categoryId: String): Single<List<String>> =
+            Single.create { emitter ->
+                try {
+                    val coins = coinCategories.coins.filter { it.categories.contains(categoryId) }
+                    emitter.onSuccess(coins.map { it.code })
+                } catch (error: Throwable) {
+                    emitter.onError(error)
+                }
+            }
+
+    fun getCoinRatingsAsync(): Single<Map<String, String>> =
+            Single.create { emitter ->
+                try {
+                    val coinRatingsMap = mutableMapOf<String, String>()
+                    coinCategories.coins.forEach { coin ->
+                        if (coin.rating.isNotEmpty()) {
+                            coinRatingsMap[coin.code] = coin.rating
+                        }
+                    }
+                    emitter.onSuccess(coinRatingsMap)
+                } catch (error: Throwable) {
+                    emitter.onError(error)
+                }
+            }
+
+    private data class CoinCategories(val categories: List<Category>, val coins: List<CategoryCoin>)
+
+    private data class Category(val id: String, val name: String)
+
+    private data class CategoryCoin(
+            val code: String,
+            val name: String,
+            val categories: List<String>,
+            val active: Boolean,
+            val description: String,
+            val details: Map<String, String>,
+            val rating: String
+    )
 
 }
