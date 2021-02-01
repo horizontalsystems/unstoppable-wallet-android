07d401a2ff5b25917e91e7d7576f7df24afb22d8
chyngyz <chyngyz.omurov@gmail.com>
2021-02-01 16:53:21 +0600

    Read market coin categories from file

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryModule.kt
@@ -9,7 +9,7 @@ object MarketDiscoveryModule {
     class Factory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            val provider = MarketCategoryProvider()
+            val provider = MarketCategoryProvider(App.instance)
             val service = MarketDiscoveryService(provider, App.currencyManager, App.xRateManager)
             return MarketDiscoveryViewModel(service, App.connectivityManager) as T
         }
