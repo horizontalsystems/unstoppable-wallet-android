a1130ecbb0a61d2351e2e283e2dadef17021d553
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-02-04 15:15:29 +0600

    Improve animations in Market Discovery

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketCategoriesAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketCategoriesAdapter.kt
@@ -1,18 +1,22 @@
 package io.horizontalsystems.bankwallet.modules.market.discovery
 
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.StringRes
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import com.google.android.material.tabs.TabLayout
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.views.helpers.LayoutHelper
+import kotlin.math.max
 
 class MarketCategoriesAdapter(
         private val context: Context,
@@ -22,6 +26,9 @@ class MarketCategoriesAdapter(
 ) : TabLayout.OnTabSelectedListener {
 
     private val itemViewMaxLength = LayoutHelper.dp(212f, context)
+    private val itemViewMinLength = LayoutHelper.dp(100f, context)
+    private val activeColor = context.getColor(R.color.yellow_d)
+    private val inactiveColor = LayoutHelper.getAttr(R.attr.ColorLawrence, context.theme, context.getColor(R.color.grey))
 
     init {
         marketCategories.forEach { category ->
@@ -54,77 +61,93 @@ class MarketCategoriesAdapter(
         listener.onSelect(marketCategories.getOrNull(tab.position))
 
         //hide icon
-        tab.view.findViewById<ImageView>(android.R.id.icon).apply {
-            ObjectAnimator.ofFloat(this, ImageView.ALPHA, 1f, 0f).apply {
-                duration = 200
-                start()
-            }
-        }
+        val iconView = tab.view.findViewById<ImageView>(android.R.id.icon)
+        animateIcon(iconView, 1f, 0f, 100L)
 
         //translate title to top
-        val topBorderYPosition = tab.view.findViewById<View>(R.id.topBorder).y
-        tab.view.findViewById<TextView>(android.R.id.text1).let { titleTextView ->
-            ObjectAnimator.ofFloat(titleTextView, TextView.TRANSLATION_Y, topBorderYPosition - titleTextView.y).apply {
-                duration = 100
-                start()
-            }
-        }
+        animateTitle(tab.view, 150L)
 
         //expand layout
-        tab.view.let { containerView ->
-            ValueAnimator.ofInt(containerView.width, itemViewMaxLength).apply {
-                addUpdateListener { valueAnimator ->
-                    val params = containerView.layoutParams
-                    params.width = valueAnimator.animatedValue as Int
-                    containerView.layoutParams = params
-                }
-                duration = 100
-                start()
-            }
-        }
+        animateTabWidth(tab.view, itemViewMaxLength, 300L)
 
         //show description
-        val descriptionTextView = tab.view.findViewById<TextView>(R.id.description)
-        descriptionTextView.alpha = 0f
-        descriptionTextView.visibility = View.VISIBLE
+        animateDescription(tab, 0f, 1f, 250L)
 
-        ValueAnimator.ofFloat(0f, 1f).apply {
-            addUpdateListener { valueAnimator ->
-                descriptionTextView.alpha = valueAnimator.animatedValue as Float
-            }
-            duration = 500
+        //color animation
+        tab.customView?.let { animateTabColor(it, inactiveColor, activeColor, 300L) }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab) {
+        tab.customView?.isActivated = false
+
+        //hide description
+        animateDescription(tab, 1f, 0f, 50L)
+
+        //show icon
+        val iconView = tab.view.findViewById<ImageView>(android.R.id.icon)
+        animateIcon(iconView, 0f, 1f, 200L)
+
+        //translate title to bottom
+        animateTitle(tab.view, 200L)
+
+        //collapse layout
+        val titleTextView = tab.view.findViewById<TextView>(android.R.id.text1)
+        val toWidth = max(titleTextView.width + LayoutHelper.dp(40f, context), itemViewMinLength)
+        animateTabWidth(tab.view, toWidth, 200L)
+
+        //color animation
+        tab.customView?.let { animateTabColor(it, activeColor, inactiveColor, 200L) }
+    }
+
+    private fun animateIcon(view: View, fromAlpha: Float, toAlpha: Float, animDuration: Long) {
+        ObjectAnimator.ofFloat(view, ImageView.ALPHA, fromAlpha, toAlpha).apply {
+            duration = animDuration
             start()
         }
     }
 
-    override fun onTabUnselected(tab: TabLayout.Tab) {
-        //hide description
-        tab.view.findViewById<TextView>(R.id.description).visibility = View.GONE
+    private fun animateDescription(tab: TabLayout.Tab, fromAlpha: Float, toAlpha: Float, animDuration: Long) {
+        val descriptionTextView = tab.view.findViewById<TextView>(R.id.description)
 
-        //show icon
-        tab.view.findViewById<ImageView>(android.R.id.icon).apply {
-            ObjectAnimator.ofFloat(this, ImageView.ALPHA, 0f, 1f).apply {
-                duration = 200
-                start()
+        ValueAnimator.ofFloat(fromAlpha, toAlpha).apply {
+            addUpdateListener { valueAnimator ->
+                descriptionTextView.alpha = valueAnimator.animatedValue as Float
             }
-        }
-
-        //translate title to bottom
-        val titleTextView = tab.view.findViewById<TextView>(android.R.id.text1)
-        val topBorderYPosition = tab.view.findViewById<View>(R.id.topBorder).y
-        ObjectAnimator.ofFloat(titleTextView, TextView.TRANSLATION_Y, topBorderYPosition - titleTextView.y).apply {
-            duration = 100
+            doOnStart { if (toAlpha == 1f) descriptionTextView.visibility = View.VISIBLE }
+            doOnEnd { if (toAlpha == 0f) descriptionTextView.visibility = View.GONE }
+            duration = animDuration
             start()
         }
+    }
 
-        //shrink layout
-        val containerView = tab.view as ViewGroup
-        containerView.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        )
+    private fun animateTitle(tabView: TabLayout.TabView, animDuration: Long) {
+        val topBorderYPosition = tabView.findViewById<View>(R.id.topBorder).y
+        val titleTextView = tabView.findViewById<TextView>(android.R.id.text1)
+        ObjectAnimator.ofFloat(titleTextView, View.TRANSLATION_Y, topBorderYPosition - titleTextView.y).apply {
+            duration = animDuration
+            interpolator = DecelerateInterpolator()
+            start()
+        }
+    }
 
-        tab.customView?.isActivated = false
+    private fun animateTabWidth(tabView: TabLayout.TabView, toWidth: Int, animDuration: Long) {
+        ValueAnimator.ofInt(tabView.width, toWidth).apply {
+            addUpdateListener { valueAnimator ->
+                val params = tabView.layoutParams
+                params.width = valueAnimator.animatedValue as Int
+                tabView.layoutParams = params
+            }
+            interpolator = DecelerateInterpolator()
+            duration = animDuration
+            start()
+        }
+    }
+
+    private fun animateTabColor(view: View, fromColor: Int, toColor: Int, duration: Long) {
+        val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), fromColor, toColor)
+        colorAnimation.duration = duration
+        colorAnimation.addUpdateListener { animator -> view.backgroundTintList = ColorStateList.valueOf(animator.animatedValue as Int) }
+        colorAnimation.start()
     }
 
     override fun onTabReselected(tab: TabLayout.Tab) {
