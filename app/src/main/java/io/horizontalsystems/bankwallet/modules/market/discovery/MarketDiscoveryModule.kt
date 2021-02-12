bf6965a4f4aeadfa2369a90e304aec5bce05de89
bakyt <abdrasulov@gmail.com>
2021-02-12 12:02:31 +0600

    Use common MarketListViewModel and Service in MarketDiscoveryModule

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryModule.kt
@@ -4,15 +4,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.modules.market.MarketModule
+import io.horizontalsystems.bankwallet.modules.market.list.MarketListService
+import io.horizontalsystems.bankwallet.modules.market.list.MarketListViewModel
 
 object MarketDiscoveryModule {
 
     class Factory : ViewModelProvider.Factory {
+        val provider by lazy { MarketCategoryProvider(App.instance) }
+        val service by lazy { MarketDiscoveryService(provider, App.xRateManager) }
+
         @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            val provider = MarketCategoryProvider(App.instance)
-            val service = MarketDiscoveryService(provider, MarketModule.currencyUSD, App.xRateManager)
-            return MarketDiscoveryViewModel(service, App.connectivityManager, listOf(service)) as T
+        override fun <T : ViewModel?> create(modelClass: Class<T>) = when (modelClass) {
+            MarketDiscoveryViewModel::class.java -> {
+                MarketDiscoveryViewModel(service) as T
+            }
+            MarketListViewModel::class.java -> {
+                val listService = MarketListService(MarketModule.currencyUSD, service)
+                MarketListViewModel(listService, App.connectivityManager, listOf(listService)) as T
+            }
+            else -> throw IllegalArgumentException()
         }
     }
 
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryService.kt
@@ -1,38 +1,36 @@
 package io.horizontalsystems.bankwallet.modules.market.discovery
 
-import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.IRateManager
-import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.modules.market.MarketItem
 import io.horizontalsystems.bankwallet.modules.market.Score
+import io.horizontalsystems.bankwallet.modules.market.list.IMarketListFetcher
 import io.horizontalsystems.core.entities.Currency
+import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.disposables.Disposable
-import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 
 class MarketDiscoveryService(
         private val marketCategoryProvider: MarketCategoryProvider,
-        private val currency: Currency,
         private val xRateManager: IRateManager
-) : Clearable {
+) : IMarketListFetcher {
 
-    sealed class State {
-        object Loading : State()
-        object Loaded : State()
-        data class Error(val error: Throwable) : State()
+    private val dataUpdatedSubject = PublishSubject.create<Unit>()
+
+    override val dataUpdatedAsync: Observable<Unit>
+        get() = dataUpdatedSubject
+
+    override fun fetchAsync(currency: Currency) = when (val category = marketCategory) {
+        null -> getAllMarketItemsAsync(currency)
+        MarketCategory.Rated -> getRatedMarketItemsAsync(currency)
+        else -> getMarketItemsByCategoryAsync(category, currency)
     }
-
-    val stateObservable: BehaviorSubject<State> = BehaviorSubject.createDefault(State.Loading)
-
-    var marketItems: List<MarketItem> = listOf()
 
     var marketCategory: MarketCategory? = null
         set(value) {
             if (field != value) {
                 field = value
 
-                marketItems = listOf()
-                fetch()
+                dataUpdatedSubject.onNext(Unit)
             }
         }
 
@@ -46,42 +44,7 @@ class MarketDiscoveryService(
             MarketCategory.Governance, MarketCategory.CrossChain, MarketCategory.Computing
     )
 
-    private var itemsDisposable: Disposable? = null
-
-    init {
-        fetch()
-    }
-
-    fun refresh() {
-        fetch()
-    }
-
-    override fun clear() {
-        itemsDisposable?.dispose()
-    }
-
-    private fun fetch() {
-        itemsDisposable?.dispose()
-        itemsDisposable = null
-
-        stateObservable.onNext(State.Loading)
-
-        val marketItemsSingle = when (val category = marketCategory) {
-            null -> getAllMarketItemsAsync()
-            MarketCategory.Rated -> getRatedMarketItemsAsync()
-            else -> getMarketItemsByCategoryAsync(category)
-        }
-
-        itemsDisposable = marketItemsSingle
-                .subscribeIO({
-                    marketItems = it
-                    stateObservable.onNext(State.Loaded)
-                }, {
-                    stateObservable.onNext(State.Error(it))
-                })
-    }
-
-    private fun getAllMarketItemsAsync(): Single<List<MarketItem>> {
+    private fun getAllMarketItemsAsync(currency: Currency): Single<List<MarketItem>> {
         return xRateManager.getTopMarketList(currency.code, 250)
                 .map { coinMarkets ->
                     coinMarkets.mapIndexed { index, coinMarket ->
@@ -90,7 +53,7 @@ class MarketDiscoveryService(
                 }
     }
 
-    private fun getRatedMarketItemsAsync(): Single<List<MarketItem>> {
+    private fun getRatedMarketItemsAsync(currency: Currency): Single<List<MarketItem>> {
         return marketCategoryProvider.getCoinRatingsAsync()
                 .flatMap { coinRatingsMap ->
                     val coinCodes = coinRatingsMap.keys.map { it }
@@ -105,7 +68,7 @@ class MarketDiscoveryService(
                 }
     }
 
-    private fun getMarketItemsByCategoryAsync(category: MarketCategory): Single<List<MarketItem>> {
+    private fun getMarketItemsByCategoryAsync(category: MarketCategory, currency: Currency): Single<List<MarketItem>> {
         return marketCategoryProvider.getCoinCodesByCategoryAsync(category.id)
                 .flatMap { coinCodes ->
                     xRateManager.getCoinMarketList(coinCodes, currency.code)
