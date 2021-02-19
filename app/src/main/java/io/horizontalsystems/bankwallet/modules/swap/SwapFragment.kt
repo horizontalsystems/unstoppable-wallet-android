78c1eb409097ce60a978da329c06130328645a12
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-02-19 12:12:57 +0600

    Fix bottom padding in Swap screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapFragment.kt
@@ -13,7 +13,6 @@ import androidx.navigation.navGraphViewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.core.ethereum.EthereumFeeViewModel
-import io.horizontalsystems.bankwallet.core.fitSystemWindowsAndAdjustResize
 import io.horizontalsystems.bankwallet.modules.swap.allowance.SwapAllowanceViewModel
 import io.horizontalsystems.bankwallet.modules.swap.approve.SwapApproveFragment
 import io.horizontalsystems.bankwallet.modules.swap.coincard.SwapCoinCardViewModel
@@ -36,8 +35,6 @@ class SwapFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        rootView.fitSystemWindowsAndAdjustResize()
 
         toolbar.setOnMenuItemClickListener { item ->
             when (item.itemId) {
