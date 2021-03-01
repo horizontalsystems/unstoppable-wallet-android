ebd9c0a89073588959eb2ef3b8eb9fd3035e3d8b
bakyt <abdrasulov@gmail.com>
2021-03-01 18:32:44 +0600

    Show market quick search results as CoinData view items

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/CoinDataItemsAdapter.kt
@@ -0,0 +1,59 @@
+package io.horizontalsystems.bankwallet.modules.market.search
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.setOnSingleClickListener
+import io.horizontalsystems.bankwallet.ui.helpers.AppLayoutHelper
+import io.horizontalsystems.views.inflate
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.view_holder_market_item.*
+
+class CoinDataItemsAdapter(private val onItemClick: (CoinDataViewItem) -> Unit) : ListAdapter<CoinDataViewItem, CoinDataItemsAdapter.ViewHolder>(diffCallback) {
+
+    class ViewHolder(override val containerView: View, onItemClick: (CoinDataViewItem) -> Unit) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        private var item: CoinDataViewItem? = null
+
+        init {
+            rank.isVisible = false
+
+            itemView.setOnSingleClickListener {
+                item?.let {
+                    onItemClick(it)
+                }
+            }
+        }
+
+        fun bind(item: CoinDataViewItem) {
+            this.item = item
+
+            val drawableResId = AppLayoutHelper.getCoinDrawableResId(containerView.context, item.code)
+                    ?: R.drawable.coin_placeholder
+            icon.setImageResource(drawableResId)
+
+            title.text = item.name
+            subtitle.text = item.code
+        }
+
+        companion object {
+            fun create(parent: ViewGroup, viewType: Int, onItemClick: (CoinDataViewItem) -> Unit) = ViewHolder(inflate(parent, R.layout.view_holder_market_item), onItemClick)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.create(parent, viewType, onItemClick)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<CoinDataViewItem>() {
+            override fun areItemsTheSame(oldItem: CoinDataViewItem, newItem: CoinDataViewItem) = false
+            override fun areContentsTheSame(oldItem: CoinDataViewItem, newItem: CoinDataViewItem) = false
+        }
+    }
+}
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchService.kt
@@ -2,47 +2,38 @@ package io.horizontalsystems.bankwallet.modules.market.search
 
 import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.IRateManager
-import io.horizontalsystems.bankwallet.modules.market.MarketItem
-import io.horizontalsystems.bankwallet.modules.market.Score
-import io.horizontalsystems.bankwallet.modules.market.list.IMarketListFetcher
-import io.horizontalsystems.core.entities.Currency
-import io.reactivex.Observable
-import io.reactivex.Single
-import io.reactivex.subjects.PublishSubject
-import java.util.*
+import io.reactivex.subjects.BehaviorSubject
 
-class MarketSearchService(private val xRateManager: IRateManager) : Clearable, IMarketListFetcher {
-
-    private val dataUpdatedSubject = PublishSubject.create<Unit>()
-
-    override val dataUpdatedAsync: Observable<Unit>
-        get() = dataUpdatedSubject
+class MarketSearchService(private val xRateManager: IRateManager) : Clearable {
 
     var query: String = ""
         set(value) {
             field = value
 
-            dataUpdatedSubject.onNext(Unit)
+            fetch()
         }
 
-    override fun fetchAsync(currency: Currency): Single<List<MarketItem>> {
-        if (query.isBlank()) return Single.just(listOf())
+    sealed class State {
+        object Idle: State()
+        object Loading: State()
+        object Error: State()
+        class Success(val items: List<Any>): State()
+    }
 
-        val queryLowercased = query.toLowerCase(Locale.ENGLISH)
-        return xRateManager.getTopMarketList(currency.code, 250)
-                .map {
-                    it.filter {
-                        it.coin.title.toLowerCase(Locale.ENGLISH).contains(queryLowercased) ||
-                        it.coin.code.toLowerCase(Locale.ENGLISH).contains(queryLowercased)
-                    }
-                }
-                .map { coinMarkets ->
-                    coinMarkets.mapIndexed { index, coinMarket ->
-                        MarketItem.createFromCoinMarket(coinMarket, currency, Score.Rank(index + 1))
-                    }
-                }
+    val stateAsync: BehaviorSubject<State> = BehaviorSubject.createDefault(State.Idle)
 
+    private fun fetch() {
+        val queryTrimmed = query.trim()
 
+        if (queryTrimmed.count() < 2) {
+            stateAsync.onNext(State.Idle)
+        } else {
+            stateAsync.onNext(State.Loading)
+
+//            xRateManager.getCoins(queryTrimmed)
+
+            stateAsync.onNext(State.Success(listOf()))
+        }
     }
 
     override fun clear() = Unit
