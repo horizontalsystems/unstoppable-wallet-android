40ca1a0aaf034d41cdde9aaf94b0100eb9f34a5a
chyngyz <chyngyz.omurov@gmail.com>
2021-03-04 11:30:24 +0600

    Refactor balance error handling for EVM kits and modules

--- app/src/main/java/io/horizontalsystems/bankwallet/core/Exceptions.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/Exceptions.kt
@@ -1,8 +1,28 @@
 package io.horizontalsystems.bankwallet.core
 
+import io.horizontalsystems.ethereumkit.api.jsonrpc.JsonRpc
+
 class UnsupportedAccountException : Exception()
 class WrongAccountTypeForThisProvider : Exception()
 class LocalizedException(val errorTextRes: Int) : Exception()
 class AdapterErrorWrongParameters(override val message: String) : Exception()
 class EthereumKitNotCreated() : Exception()
 class NoFeeSendTransactionError() : Exception()
+
+sealed class EvmError(message: String? = null) : Throwable(message) {
+    object InsufficientBalanceWithFee : EvmError()
+    class ExecutionReverted(message: String?) : EvmError(message)
+}
+
+val Throwable.convertedError: Throwable
+    get() = when (this) {
+        is JsonRpc.ResponseError.RpcError -> {
+            if (error.message.contains("insufficient funds for transfer") || error.message.contains("gas required exceeds allowance")) {
+                EvmError.InsufficientBalanceWithFee
+            } else if (error.message.contains("execution reverted")) {
+                EvmError.ExecutionReverted(error.message)
+            } else
+                this
+        }
+        else -> this
+    }
