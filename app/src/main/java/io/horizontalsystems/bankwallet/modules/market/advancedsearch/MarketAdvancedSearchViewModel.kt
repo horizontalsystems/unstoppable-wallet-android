2cb717f84099c7a292146964c49a499bbb25abc3
bakyt <abdrasulov@gmail.com>
2021-03-04 15:23:50 +0600

    Show error message when service cannot fetch data

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/advancedsearch/MarketAdvancedSearchViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/advancedsearch/MarketAdvancedSearchViewModel.kt
@@ -7,7 +7,9 @@ import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.entities.DataState
 import io.horizontalsystems.bankwallet.ui.selector.ViewItemWrapper
+import io.horizontalsystems.core.SingleLiveEvent
 import io.reactivex.disposables.CompositeDisposable
+import java.net.UnknownHostException
 
 class MarketAdvancedSearchViewModel(
         val service: MarketAdvancedSearchService,
@@ -81,17 +83,21 @@ class MarketAdvancedSearchViewModel(
     val priceChangeViewItemLiveData = MutableLiveData(priceChangeViewItem)
     val numberOfItemsLiveData = MutableLiveData<Int>()
     val showResultsEnabledLiveData = MutableLiveData(false)
+    val errorLiveEvent = SingleLiveEvent<String>()
 
     val disposable = CompositeDisposable()
 
     init {
         service.numberOfItemsAsync
                 .subscribe {
-                    val b = it is DataState.Success && it.data > 0
-                    showResultsEnabledLiveData.postValue(b)
+                    showResultsEnabledLiveData.postValue(it is DataState.Success && it.data > 0)
 
                     it.dataOrNull?.let {
                         numberOfItemsLiveData.postValue(it)
+                    }
+
+                    it.errorOrNull?.let {
+                        errorLiveEvent.postValue(convertErrorMessage(it))
                     }
                 }
                 .let {
@@ -111,6 +117,11 @@ class MarketAdvancedSearchViewModel(
     override fun onCleared() {
         disposable.clear()
         clearables.forEach(Clearable::clear)
+    }
+
+    private fun convertErrorMessage(it: Throwable) = when (it) {
+        is UnknownHostException -> App.instance.getString(R.string.Hud_Text_NoInternet)
+        else -> it.message ?: it.javaClass.simpleName
     }
 }
 
