582a87147594293df24d716cfec2b21e6052df27
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-03-22 12:11:01 +0600

    Show volume over chart

--- components/chartview/src/main/java/io/horizontalsystems/chartview/ChartGridTimeline.kt
+++ components/chartview/src/main/java/io/horizontalsystems/chartview/ChartGridTimeline.kt
@@ -3,7 +3,6 @@ package io.horizontalsystems.chartview
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
-import android.graphics.Typeface
 import io.horizontalsystems.chartview.models.ChartConfig
 import io.horizontalsystems.chartview.models.GridColumn
 
@@ -32,7 +31,7 @@ class ChartGridTimeline(private val config: ChartConfig, override var isVisible:
 
     private fun Canvas.drawColumns() {
         columns.forEach {
-            drawText(it.value, textPosition(it.x, it.value), config.timelineTextSize, textPaint)
+            drawText(it.value, textPosition(it.x, it.value), config.timelineTextSize + config.timelineTextPadding, textPaint)
         }
     }
 
