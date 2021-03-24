8b2f27621b73091bc526aaf45bac250bcabca465
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-03-24 18:44:16 +0600

    Show chart Rsi labels on the right side

--- components/chartview/src/main/java/io/horizontalsystems/chartview/ChartGridRange.kt
+++ components/chartview/src/main/java/io/horizontalsystems/chartview/ChartGridRange.kt
@@ -5,9 +5,10 @@ import io.horizontalsystems.chartview.models.ChartConfig
 
 class ChartGridRange(private val config: ChartConfig, override var isVisible: Boolean = true) : ChartDraw {
 
-    private var top: String = ""
+    private var high: String = ""
     private var low: String = ""
     private var offset = config.curveVerticalOffset
+    private var showOnTheRightSide = false
 
     private var shape = RectF(0f, 0f, 0f, 0f)
 
@@ -28,8 +29,9 @@ class ChartGridRange(private val config: ChartConfig, override var isVisible: Bo
         shape = rect
     }
 
-    fun setValues(top: String, low: String) {
-        this.top = top
+    fun setValues(high: String, low: String, showOnTheRight: Boolean = false) {
+        showOnTheRightSide = showOnTheRight
+        this.high = high
         this.low = low
     }
 
@@ -39,10 +41,10 @@ class ChartGridRange(private val config: ChartConfig, override var isVisible: Bo
 
     override fun draw(canvas: Canvas) {
         if (!isVisible) return
-        canvas.drawTopLow()
+        canvas.drawHighLow()
     }
 
-    private fun Canvas.drawTopLow() {
+    private fun Canvas.drawHighLow() {
         val path = Path()
 
         val topY = shape.top + offset
@@ -57,8 +59,16 @@ class ChartGridRange(private val config: ChartConfig, override var isVisible: Bo
         drawPath(path, dashPaint)
 
         // Texts
-        drawText(top, shape.left + config.gridTextPadding, textPosition(topY, isTop = true), textPaint)
-        drawText(low, shape.left + config.gridTextPadding, textPosition(bottomY, isTop = false), textPaint)
+        var highX = shape.left + config.gridSideTextPadding
+        var lowX = shape.left + config.gridSideTextPadding
+
+        if (showOnTheRightSide) {
+            highX = shape.right - textPaint.measureText(high) - config.gridSideTextPadding
+            lowX = shape.right - textPaint.measureText(low) - config.gridSideTextPadding
+        }
+
+        drawText(high, highX, textPosition(topY, isTop = true), textPaint)
+        drawText(low, lowX, textPosition(bottomY, isTop = false), textPaint)
     }
 
     private fun textPosition(y: Float, isTop: Boolean): Float {
