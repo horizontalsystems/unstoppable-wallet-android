0c88e9717ffd0a711f13d0be0bbff36b61405708
chyngyz <chyngyz.omurov@gmail.com>
2021-04-22 11:38:35 +0600

    Show no added coins page to balance tab

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalanceNoCoinsFragment.kt
@@ -0,0 +1,36 @@
+package io.horizontalsystems.bankwallet.modules.balance
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.BaseFragment
+import io.horizontalsystems.bankwallet.modules.manageaccounts.ManageAccountsModule
+import io.horizontalsystems.core.findNavController
+import kotlinx.android.synthetic.main.fragment_no_coins.*
+
+class BalanceNoCoinsFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_no_coins, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbarTitle.text = arguments?.getString(ACCOUNT_NAME) ?: getString(R.string.Balance_Title)
+        toolbarTitle.setOnClickListener {
+            ManageAccountsModule.start(this, R.id.manageAccountsFragment, navOptionsFromBottom(), ManageAccountsModule.Mode.Switcher)
+        }
+
+        addCoinsButton.setOnClickListener {
+            findNavController().navigate(R.id.manageWalletsFragment, null, navOptions())
+        }
+    }
+
+    companion object {
+        const val ACCOUNT_NAME = "accountName"
+    }
+
+}
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balanceonboarding/BalanceOnboardingContainerFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balanceonboarding/BalanceOnboardingContainerFragment.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.modules.balance.BalanceFragment
+import io.horizontalsystems.bankwallet.modules.balance.BalanceNoCoinsFragment
+import io.horizontalsystems.bankwallet.modules.balanceonboarding.BalanceOnboardingViewModel.BalanceViewType
 import io.horizontalsystems.bankwallet.modules.onboarding.OnboardingFragment
 
-class BalanceOnboardingContainerFragment: BaseFragment() {
+class BalanceOnboardingContainerFragment : BaseFragment() {
 
     private val viewModel by viewModels<BalanceOnboardingViewModel> { BalanceOnboardingModule.Factory() }
-    private val onboardingFragment by lazy { OnboardingFragment() }
-    private val balanceFragment by lazy { BalanceFragment() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_balance_onboarding_container, container, false)
@@ -23,11 +24,21 @@ class BalanceOnboardingContainerFragment: BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.hasAccountsLiveData.observe(viewLifecycleOwner, { hasAccounts ->
+        viewModel.balanceViewTypeLiveData.observe(viewLifecycleOwner, { balancePageType ->
             childFragmentManager.commit {
-                replace(R.id.fragmentContainerView, if (hasAccounts) balanceFragment else onboardingFragment)
+                var args: Bundle? = null
+                val balanceViewFragment = when (balancePageType) {
+                    BalanceViewType.NoAccounts -> OnboardingFragment::class.java
+                    is BalanceViewType.NoCoins -> {
+                        args = bundleOf(BalanceNoCoinsFragment.ACCOUNT_NAME to balancePageType.accountName)
+                        BalanceNoCoinsFragment::class.java
+                    }
+                    BalanceViewType.Balance -> BalanceFragment::class.java
+                }
+                replace(R.id.fragmentContainerView, balanceViewFragment, args)
                 addToBackStack(null)
             }
         })
     }
+
 }
