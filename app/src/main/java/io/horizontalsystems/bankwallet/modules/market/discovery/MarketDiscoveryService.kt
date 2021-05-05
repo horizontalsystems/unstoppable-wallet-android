c1c9b60dbffee0316821456096131e8c11262d2d
_imadia <imanahunovad@gmail.com>
2021-05-05 13:38:03 +0600

    Add market categories 'Fan Tokens' and 'Investment Tools', Remove 'Governance'

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/discovery/MarketDiscoveryService.kt
@@ -39,12 +39,13 @@ class MarketDiscoveryService(
 
     val marketCategories = listOf(
             MarketCategory.Blockchains, MarketCategory.Dexes, MarketCategory.Lending, MarketCategory.Privacy,
-            MarketCategory.Scaling, MarketCategory.Oracles, MarketCategory.Prediction, MarketCategory.YieldAggregators,
-            MarketCategory.FiatStablecoins, MarketCategory.RebaseTokens, MarketCategory.AlgoStablecoins, MarketCategory.TokenizedBitcoin,
-            MarketCategory.StablecoinIssuers,MarketCategory.ExchangeTokens, MarketCategory.RiskManagement, MarketCategory.Wallets,
+            MarketCategory.InvestmentTools, MarketCategory.Oracles, MarketCategory.Prediction, MarketCategory.Scaling,
+            MarketCategory.YieldAggregators,MarketCategory.Wallets, MarketCategory.FiatStablecoins, MarketCategory.RebaseTokens,
+            MarketCategory.AlgoStablecoins, MarketCategory.TokenizedBitcoin, MarketCategory.StablecoinIssuers,
+            MarketCategory.ExchangeTokens, MarketCategory.RiskManagement, MarketCategory.FanTokens,
             MarketCategory.Synthetics, MarketCategory.IndexFunds, MarketCategory.NFT, MarketCategory.FundRaising,
             MarketCategory.Gaming, MarketCategory.B2B, MarketCategory.Infrastructure, MarketCategory.Staking,
-            MarketCategory.Governance, MarketCategory.CrossChain, MarketCategory.Computing
+            MarketCategory.CrossChain, MarketCategory.Computing
     )
 
     init {
