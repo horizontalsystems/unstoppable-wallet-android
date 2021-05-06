06081a58fd60711739cfee7bce753bd9c23d4082
bakyt <abdrasulov@gmail.com>
2021-05-06 11:14:57 +0600

    Show fiat balances in swap token selector

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coincard/SwapCoinProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/coincard/SwapCoinProvider.kt
@@ -2,12 +2,14 @@ package io.horizontalsystems.bankwallet.modules.swap.coincard
 
 import io.horizontalsystems.bankwallet.core.IAdapterManager
 import io.horizontalsystems.bankwallet.core.ICoinManager
+import io.horizontalsystems.bankwallet.core.IRateManager
 import io.horizontalsystems.bankwallet.core.IWalletManager
-import io.horizontalsystems.bankwallet.entities.label
+import io.horizontalsystems.bankwallet.entities.CurrencyValue
 import io.horizontalsystems.bankwallet.modules.swap.SwapModule.CoinBalanceItem
 import io.horizontalsystems.bankwallet.modules.swap.SwapModule.Dex
 import io.horizontalsystems.coinkit.models.Coin
 import io.horizontalsystems.coinkit.models.CoinType
+import io.horizontalsystems.core.ICurrencyManager
 import java.math.BigDecimal
 import java.util.*
 
@@ -15,7 +17,9 @@ class SwapCoinProvider(
         private val dex: Dex,
         private val coinManager: ICoinManager,
         private val walletManager: IWalletManager,
-        private val adapterManager: IAdapterManager
+        private val adapterManager: IAdapterManager,
+        private val currencyManager: ICurrencyManager,
+        private val xRateManager: IRateManager
 ) {
 
     fun coins(enabledCoins: Boolean, exclude: List<Coin> = listOf()): List<CoinBalanceItem> {
@@ -30,7 +34,9 @@ class SwapCoinProvider(
             val disabledCoinItems = coinManager.coins.filter { coin ->
                 dexSupportsCoin(coin) && !exclude.contains(coin) && !enabledCoinItems.any { it.coin == coin }
             }.map { coin ->
-                CoinBalanceItem(coin, balance(coin), coin.type.label)
+                val balance = balance(coin)
+
+                CoinBalanceItem(coin, balance, getFiatValue(coin, balance))
             }.sortedBy { it.coin.title.toLowerCase(Locale.ENGLISH) }
 
             enabledCoinItems + disabledCoinItems
@@ -45,9 +51,29 @@ class SwapCoinProvider(
 
     private val walletItems: List<CoinBalanceItem>
         get() = walletManager.activeWallets.map { wallet ->
-            CoinBalanceItem(wallet.coin, adapterManager.getBalanceAdapterForWallet(wallet)?.balance, wallet.coin.type.label)
+            val balance = adapterManager.getBalanceAdapterForWallet(wallet)?.balance
+
+            CoinBalanceItem(wallet.coin, balance, getFiatValue(wallet.coin, balance))
         }
 
+    private fun getFiatValue(coin: Coin, balance: BigDecimal?): CurrencyValue? {
+        return balance?.let {
+            getXRate(coin)?.multiply(it)
+        }?.let { fiatBalance ->
+            CurrencyValue(currencyManager.baseCurrency, fiatBalance)
+        }
+    }
+
+    private fun getXRate(coin: Coin): BigDecimal? {
+        val currency = currencyManager.baseCurrency
+        return xRateManager.latestRate(coin.type, currency.code)?.let {
+            if (it.isExpired()) {
+                null
+            } else {
+                it.rate
+            }
+        }
+    }
     private fun balance(coin: Coin): BigDecimal? {
         val wallet = walletManager.activeWallets.firstOrNull { it.coin == coin }
         return wallet?.let { adapterManager.getBalanceAdapterForWallet(it)?.balance }
