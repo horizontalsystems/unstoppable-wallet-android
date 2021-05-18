8f43aceda20dec58ed766c2c076b083934896c0e
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-05-18 16:31:36 +0600

    Remove redundant code

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/helpers/AppLayoutHelper.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/helpers/AppLayoutHelper.kt
@@ -31,13 +31,4 @@ object AppLayoutHelper {
             else -> null
         }
     }
-
-    fun getStatusBarHeight(context: Context): Int {
-        var result = 0
-        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
-        if (resourceId > 0) {
-            result = context.resources.getDimensionPixelSize(resourceId)
-        }
-        return result
-    }
 }
