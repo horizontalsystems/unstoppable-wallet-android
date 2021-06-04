1bc4c9544238aa2e14218a9103520b99483bbd3b
bakyt <abdrasulov@gmail.com>
2021-06-04 17:03:33 +0600

    Refactor Receive Module using MVVM

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/receive/ReceiveFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/receive/ReceiveFragment.kt
@@ -5,8 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ShareCompat
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.core.setOnSingleClickListener
@@ -15,7 +14,6 @@ import io.horizontalsystems.bankwallet.ui.helpers.AppLayoutHelper
 import io.horizontalsystems.bankwallet.ui.helpers.TextHelper
 import io.horizontalsystems.core.findNavController
 import io.horizontalsystems.core.helpers.HudHelper
-import io.horizontalsystems.snackbar.SnackbarGravity
 import kotlinx.android.synthetic.main.fragment_backup_words_confirm.*
 import kotlinx.android.synthetic.main.fragment_receive.*
 import kotlinx.android.synthetic.main.fragment_receive.toolbar
@@ -28,81 +26,57 @@ class ReceiveFragment: BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         if (savedInstanceState != null) {
             //close fragment in case it's restoring
             findNavController().popBackStack()
         }
 
-        val wallet = arguments?.getParcelable<Wallet>(WALLET_KEY) ?: run { findNavController().popBackStack(); return }
+        try {
+            val wallet = arguments?.getParcelable<Wallet>(WALLET_KEY) ?: run { findNavController().popBackStack(); return }
+            val viewModel by viewModels<ReceiveViewModel> { ReceiveModule.Factory(wallet) }
 
-        toolbar.title = getString(R.string.Deposit_Title, wallet.coin.code)
-        toolbar.navigationIcon = AppLayoutHelper.getCoinDrawable(requireContext(), wallet.coin.type)
-        toolbar.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                R.id.closeButton -> {
-                    findNavController().popBackStack()
-                    true
+            toolbar.title = getString(R.string.Deposit_Title, wallet.coin.code)
+            toolbar.navigationIcon = AppLayoutHelper.getCoinDrawable(requireContext(), wallet.coin.type)
+            toolbar.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.closeButton -> {
+                        findNavController().popBackStack()
+                        true
+                    }
+                    else -> false
                 }
-                else -> false
             }
-        }
 
-        val presenter = ViewModelProvider(this, ReceiveModule.Factory(wallet)).get(ReceivePresenter::class.java)
-        observeView(presenter.view as ReceiveView)
-        observeRouter(presenter.router as ReceiveRouter)
-        presenter.viewDidLoad()
+            receiveAddressView.text = viewModel.receiveAddress
+            imgQrCode.setImageBitmap(TextHelper.getQrCodeBitmap(viewModel.receiveAddress))
 
-        btnShare.setOnSingleClickListener {
-            presenter.onShareClick()
-        }
+            receiverHint.text = if (viewModel.addressType != null) {
+                getString(R.string.Deposit_Your_Address) + " (${viewModel.addressType})"
+            } else {
+                getString(R.string.Deposit_Your_Address)
+            }
 
-        btnCopy.setOnSingleClickListener {
-            presenter.onAddressClick()
-        }
+            btnShare.setOnSingleClickListener {
+                context?.let {
+                    ShareCompat.IntentBuilder(it)
+                        .setType("text/plain")
+                        .setText(viewModel.receiveAddress)
+                        .startChooser()
+                }
+            }
 
-        btnClose.setOnSingleClickListener {
+            btnCopy.setOnSingleClickListener {
+                TextHelper.copyText(viewModel.receiveAddress)
+                HudHelper.showSuccessMessage(requireView(), R.string.Hud_Text_Copied)
+            }
+
+            btnClose.setOnSingleClickListener {
+                findNavController().popBackStack()
+            }
+        } catch (t: Throwable) {
+            HudHelper.showErrorMessage(this.requireView(), t.message ?: t.javaClass.simpleName)
             findNavController().popBackStack()
         }
-    }
-
-    private fun observeRouter(receiveRouter: ReceiveRouter) {
-        receiveRouter.shareAddress.observe(viewLifecycleOwner, Observer { address ->
-            activity?.let {
-                ShareCompat.IntentBuilder
-                    .from(it)
-                    .setType("text/plain")
-                    .setText(address)
-                    .startChooser()
-            }
-        })
-    }
-
-    private fun observeView(view: ReceiveView) {
-        view.setHintText.observe(viewLifecycleOwner, Observer {
-            receiverHint.setText(it)
-
-            view.hintDetails?.let {
-                receiverHint.text = "${receiverHint.text} (${it})"
-            }
-        })
-
-        view.showAddress.observe(viewLifecycleOwner, Observer {
-            receiveAddressView.text = it.address
-            imgQrCode.setImageBitmap(TextHelper.getQrCodeBitmap(it.address))
-        })
-
-        view.showError.observe(viewLifecycleOwner, Observer { error ->
-            error?.let {
-                HudHelper.showErrorMessage(this.requireView(), it, gravity = SnackbarGravity.TOP_OF_VIEW)
-            }
-
-            findNavController().popBackStack()
-        })
-
-        view.showCopied.observe(viewLifecycleOwner, Observer {
-            HudHelper.showSuccessMessage(requireView(), R.string.Hud_Text_Copied)
-        })
     }
 
     companion object {
