f36ade372b92f576931c94ccc78fd4d81df21e9d
bakyt <abdrasulov@gmail.com>
2021-06-09 16:43:18 +0600

    Add support for TestNet in TransactionInfoModule

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoInteractor.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoInteractor.kt
@@ -3,21 +3,25 @@ package io.horizontalsystems.bankwallet.modules.transactions.transactionInfo
 import io.horizontalsystems.bankwallet.core.IClipboardManager
 import io.horizontalsystems.bankwallet.core.IRateManager
 import io.horizontalsystems.bankwallet.core.ITransactionsAdapter
+import io.horizontalsystems.bankwallet.core.managers.AccountSettingManager
 import io.horizontalsystems.bankwallet.core.providers.FeeCoinProvider
+import io.horizontalsystems.bankwallet.entities.Account
 import io.horizontalsystems.bankwallet.entities.CurrencyValue
 import io.horizontalsystems.bankwallet.entities.LastBlockInfo
 import io.horizontalsystems.coinkit.models.Coin
 import io.horizontalsystems.coinkit.models.CoinType
 import io.horizontalsystems.core.IBuildConfigProvider
 import io.horizontalsystems.core.ICurrencyManager
+import io.horizontalsystems.ethereumkit.core.EthereumKit
 
 class TransactionInfoInteractor(
-        private var clipboardManager: IClipboardManager,
-        private val adapter: ITransactionsAdapter,
-        private val xRateManager: IRateManager,
-        private val currencyManager: ICurrencyManager,
-        private val feeCoinProvider: FeeCoinProvider,
-        buildConfigProvider: IBuildConfigProvider
+    private var clipboardManager: IClipboardManager,
+    private val adapter: ITransactionsAdapter,
+    private val xRateManager: IRateManager,
+    private val currencyManager: ICurrencyManager,
+    private val feeCoinProvider: FeeCoinProvider,
+    buildConfigProvider: IBuildConfigProvider,
+    private val accountSettingManager: AccountSettingManager
 ) : TransactionInfoModule.Interactor {
     var delegate: TransactionInfoModule.InteractorDelegate? = null
 
@@ -44,5 +48,13 @@ class TransactionInfoInteractor(
 
     override fun getRaw(transactionHash: String): String? {
         return adapter.getRawTransaction(transactionHash)
+    }
+
+    override fun ethereumNetworkType(account: Account): EthereumKit.NetworkType {
+        return accountSettingManager.ethereumNetwork(account).networkType
+    }
+
+    override fun binanceSmartChainNetworkType(account: Account): EthereumKit.NetworkType {
+        return accountSettingManager.binanceSmartChainNetwork(account).networkType
     }
 }
