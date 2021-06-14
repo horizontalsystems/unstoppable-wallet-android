50a9075a2864266470d09950076774675335bd22
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-06-14 14:19:43 +0600

    Update allowed symbols for passphrase

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/PassphraseValidator.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/PassphraseValidator.kt
@@ -1,7 +1,7 @@
 package io.horizontalsystems.bankwallet.core.managers
 
 class PassphraseValidator {
-    private val allowedCharacters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    private val allowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 '\"`&/?!:;.,~*$=+-[](){}<>\\_#@|%"
 
     fun validate(text: String?): Boolean {
         return text?.all { allowedCharacters.contains(it, ignoreCase = true) } ?: true
