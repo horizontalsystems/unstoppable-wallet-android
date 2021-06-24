39437915cbd068ff245644f86375c04b0b12f781
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-06-24 16:10:45 +0600

    Show Testnet label in Receive page

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/receive/ReceiveModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/receive/ReceiveModule.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.entities.Wallet
+import io.horizontalsystems.bankwallet.modules.balance.NetworkTypeChecker
 
 object ReceiveModule {
 
     class Factory(private val wallet: Wallet) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return ReceiveViewModel(wallet, App.adapterManager) as T
+            return ReceiveViewModel(wallet, App.adapterManager, NetworkTypeChecker(App.accountSettingManager)) as T
         }
     }
 
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/receive/ReceiveViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/receive/ReceiveViewModel.kt
@@ -4,15 +4,21 @@ import androidx.lifecycle.ViewModel
 import io.horizontalsystems.bankwallet.core.IAdapterManager
 import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.bankwallet.entities.addressType
+import io.horizontalsystems.bankwallet.modules.balance.NetworkTypeChecker
 
-class ReceiveViewModel(wallet: Wallet, adapterManager: IAdapterManager) : ViewModel() {
+class ReceiveViewModel(
+        wallet: Wallet,
+        adapterManager: IAdapterManager,
+        networkTypeChecker: NetworkTypeChecker) : ViewModel() {
 
     val receiveAddress: String
     val addressType: String?
+    val testNet: Boolean
 
     init {
         val receiveAdapter = adapterManager.getReceiveAdapterForWallet(wallet) ?: throw NoReceiverAdapter()
 
+        testNet = !networkTypeChecker.isMainNet(wallet)
         receiveAddress = receiveAdapter.receiveAddress
         addressType = wallet.configuredCoin.settings.derivation?.addressType()
     }
