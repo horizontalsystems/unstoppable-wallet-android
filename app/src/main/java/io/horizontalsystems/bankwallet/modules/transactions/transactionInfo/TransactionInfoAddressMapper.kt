a228997edf8e4a55f1f489a52f3a02c1061b4683
chyngyz <chyngyz.omurov@gmail.com>
2021-07-09 16:33:46 +0600

    Migrate to PancakeSwap V2

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoAddressMapper.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/transactionInfo/TransactionInfoAddressMapper.kt
@@ -7,6 +7,7 @@ object TransactionInfoAddressMapper {
             "0x7a250d5630b4cf539739df2c5dacb4c659f2488d" to "Uniswap v.2",
             "0xe592427a0aece92de3edee1f18e0157c05861564" to "Uniswap v.3",
             "0x05ff2b0db69458a0750badebc4f9e13add608c7f" to "PancakeSwap",
+            "0x10ed43c718714eb63d5aa57b78b54704e256024e" to "PancakeSwap v.2",
             "0x11111112542d85b3ef69ae05771c2dccff4faa26" to "1inch"
     )
 
