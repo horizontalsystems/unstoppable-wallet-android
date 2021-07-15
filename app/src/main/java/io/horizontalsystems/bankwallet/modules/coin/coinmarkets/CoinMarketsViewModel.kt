2854135f3f81bd6aaecf00d98a8cd7b4caeed1bb
Talgat Medetbekov <medetbekov.t@gmail.com>
2021-07-15 14:51:41 +0600

    Add ability to sort coin markets by volume and show in base currency

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/coinmarkets/CoinMarketsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/coinmarkets/CoinMarketsViewModel.kt
@@ -2,36 +2,38 @@ package io.horizontalsystems.bankwallet.modules.coin.coinmarkets
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.horizontalsystems.bankwallet.core.IAppConfigProvider
 import io.horizontalsystems.bankwallet.core.IAppNumberFormatter
+import io.horizontalsystems.bankwallet.core.IRateManager
 import io.horizontalsystems.bankwallet.modules.coin.MarketTickerViewItem
 import io.horizontalsystems.bankwallet.modules.market.SortingField
 import io.horizontalsystems.bankwallet.modules.market.sortedByDescendingNullLast
 import io.horizontalsystems.bankwallet.modules.market.sortedByNullLast
 import io.horizontalsystems.bankwallet.ui.extensions.MarketListHeaderView
+import io.horizontalsystems.coinkit.models.CoinType
+import io.horizontalsystems.core.ICurrencyManager
 import io.horizontalsystems.xrateskit.entities.MarketTicker
 import java.math.BigDecimal
 
 class CoinMarketsViewModel(
-        coinCode: String,
-        private val numberFormatter: IAppNumberFormatter,
-        appConfigProvider: IAppConfigProvider
+    coinCode: String,
+    coinType: CoinType,
+    currencyManager: ICurrencyManager,
+    rateManager: IRateManager,
+    private val numberFormatter: IAppNumberFormatter
 ) : ViewModel() {
 
-    val coinMarketItems = MutableLiveData<List<MarketTickerViewItem>>()
+    val coinMarketItems = MutableLiveData<Pair<List<MarketTickerViewItem>, Boolean>>()
 
     val sortingFields: Array<SortingField> = arrayOf(SortingField.HighestVolume, SortingField.LowestVolume)
     var sortingField: SortingField = sortingFields.first()
         private set
 
-    //MarketTicker has rate field in USD fiat
-    private val usdCurrency = appConfigProvider.currencies.first { it.code == "USD" }
-    private val viewOptions = listOf(coinCode, usdCurrency.code)
+    private val baseCurrency = currencyManager.baseCurrency
+    private val viewOptions = listOf(coinCode, baseCurrency.code)
     private var selectedViewOptionId: Int = 0
+    private val marketRate = rateManager.getLatestRate(coinType, baseCurrency.code) ?: BigDecimal.ONE
     private val showInFiat: Boolean
-        get() {
-            return selectedViewOptionId == 1
-        }
+        get() = selectedViewOptionId == 1
 
     val fieldViewOptions = viewOptions.mapIndexed { index, title ->
         MarketListHeaderView.FieldViewOption(index, title, index == selectedViewOptionId)
@@ -40,40 +42,42 @@ class CoinMarketsViewModel(
     var marketTickers: List<MarketTicker> = listOf()
         set(value) {
             field = value
-            syncCoinMarketItems()
+            syncCoinMarketItems(false)
         }
 
-    fun update(sortingField: SortingField? = null, fieldViewOptionId: Int? = null) {
-        sortingField?.let {
-            this.sortingField = it
+    fun update(sortField: SortingField? = null, fieldViewOptionId: Int? = null, scrollToTop: Boolean) {
+        sortField?.let {
+            sortingField = it
         }
+
         fieldViewOptionId?.let {
-            this.selectedViewOptionId = it
+            selectedViewOptionId = it
         }
-        syncCoinMarketItems()
+
+        syncCoinMarketItems(scrollToTop)
     }
 
-    private fun syncCoinMarketItems() {
+    private fun syncCoinMarketItems(scrollToTop: Boolean) {
         val marketTickersSorted = marketTickers.sort(sortingField)
         val viewItems = getCoinMarketItems(marketTickersSorted, showInFiat)
-        coinMarketItems.postValue(viewItems)
+        coinMarketItems.postValue(Pair(viewItems, scrollToTop))
     }
 
     private fun getCoinMarketItems(tickers: List<MarketTicker>, showInFiat: Boolean): List<MarketTickerViewItem> {
         return tickers.map { ticker ->
-            val subvalue = if (showInFiat) {
-                formatFiatShortened(ticker.volume.multiply(ticker.rate), usdCurrency.symbol)
+            val subValue = if (showInFiat) {
+                formatFiatShortened(ticker.volume.multiply(marketRate), baseCurrency.symbol)
             } else {
                 val (shortenValue, suffix) = numberFormatter.shortenValue(ticker.volume)
                 "$shortenValue $suffix ${ticker.base}"
             }
 
             MarketTickerViewItem(
-                    ticker.marketName,
-                    "${ticker.base}/${ticker.target}",
-                    numberFormatter.formatCoin(ticker.rate, ticker.target, 0, 8),
-                    subvalue,
-                    ticker.imageUrl
+                ticker.marketName,
+                "${ticker.base}/${ticker.target}",
+                numberFormatter.formatCoin(ticker.rate, ticker.target, 0, 8),
+                subValue,
+                ticker.imageUrl
             )
         }
     }
