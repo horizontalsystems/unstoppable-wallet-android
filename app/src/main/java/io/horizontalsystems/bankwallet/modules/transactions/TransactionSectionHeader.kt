e92db6137bacadf3eec2f60f5d99d20c30519a8c
bakyt <abdrasulov@gmail.com>
2021-07-26 12:43:17 +0600

    Use TODAY, YESTERDAY strings in formatting date in transactions tab

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionSectionHeader.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionSectionHeader.kt
@@ -1,6 +1,9 @@
 package io.horizontalsystems.bankwallet.modules.transactions
 
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.core.helpers.DateHelper
+import java.util.*
 
 class TransactionSectionHeader {
     private val headers = mutableMapOf<Int, String>()
@@ -13,15 +16,34 @@ class TransactionSectionHeader {
     fun updateList(items: List<TransactionViewItem>) {
         headers.clear()
 
-        var prevShortDate: String? = null
+        var prevSectionHeader: String? = null
         items.forEachIndexed { index, transactionViewItem ->
             transactionViewItem.date?.let { date ->
-                val shortDate = DateHelper.shortDate(date, "MMMM d", "MMMM d, yyyy").uppercase()
-                if (shortDate != prevShortDate) {
-                    headers[index] = shortDate
-                    prevShortDate = shortDate
+                val sectionHeader = formatDate(date).uppercase()
+
+                if (sectionHeader != prevSectionHeader) {
+                    headers[index] = sectionHeader
+                    prevSectionHeader = sectionHeader
                 }
             }
         }
+    }
+
+    private fun formatDate(date: Date): String {
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+
+        val today = Calendar.getInstance()
+        if (calendar[Calendar.YEAR] == today[Calendar.YEAR] && calendar[Calendar.DAY_OF_YEAR] == today[Calendar.DAY_OF_YEAR]) {
+            return Translator.getString(R.string.Timestamp_Today)
+        }
+
+        val yesterday = Calendar.getInstance()
+        yesterday.add(Calendar.DAY_OF_MONTH, -1)
+        if (calendar[Calendar.YEAR] == yesterday[Calendar.YEAR] && calendar[Calendar.DAY_OF_YEAR] == yesterday[Calendar.DAY_OF_YEAR]) {
+            return Translator.getString(R.string.Timestamp_Yesterday)
+        }
+
+        return DateHelper.shortDate(date, "MMMM d", "MMMM d, yyyy")
     }
 }
