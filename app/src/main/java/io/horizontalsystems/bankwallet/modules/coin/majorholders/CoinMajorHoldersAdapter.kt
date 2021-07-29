8e46ebd445a8d3cfefe5f27ec82546e0fab140c6
bakyt <abdrasulov@gmail.com>
2021-07-29 17:33:37 +0600

    Update UI of Major Holders screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/majorholders/CoinMajorHoldersAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/majorholders/CoinMajorHoldersAdapter.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.setOnSingleClickListener
 import io.horizontalsystems.bankwallet.modules.coin.MajorHolderItem
 import io.horizontalsystems.views.inflate
 import kotlinx.android.extensions.LayoutContainer
@@ -15,17 +16,16 @@ class CoinMajorHoldersAdapter(
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     interface Listener {
-        fun onItemClick(address: String)
+        fun onAddressClick(address: String)
+        fun onDetailsClick(address: String)
     }
 
     private val viewTypeHeader = 0
     private val viewTypeItem = 1
-    private val viewTypeDescription = 2
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
-            viewTypeItem -> ViewHolderItem(inflate(parent, R.layout.view_holder_coin_major_holders_item))
-            viewTypeDescription -> ViewHolderDescription(inflate(parent, R.layout.view_holder_coin_major_holders_description))
+            viewTypeItem -> ViewHolderItem(inflate(parent, R.layout.view_holder_coin_major_holders_item), listener)
             else -> ViewHolderSectionHeader(inflate(parent, R.layout.view_holder_coin_major_holders_section_header))
         }
     }
@@ -35,31 +35,43 @@ class CoinMajorHoldersAdapter(
     override fun getItemViewType(position: Int) = when (items[position]) {
         is MajorHolderItem.Item -> viewTypeItem
         is MajorHolderItem.Header -> viewTypeHeader
-        is MajorHolderItem.Description -> viewTypeDescription
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val item = items[position]
         when (holder) {
-            is ViewHolderItem -> holder.bind(item as MajorHolderItem.Item) {
-                listener.onItemClick(item.address)
-            }
+            is ViewHolderItem -> holder.bind(item as MajorHolderItem.Item)
         }
     }
 
     class ViewHolderSectionHeader(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
 
-    class ViewHolderDescription(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
+    class ViewHolderItem(
+        override val containerView: View,
+        private val listener: Listener
+    ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
-    class ViewHolderItem(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-        fun bind(item: MajorHolderItem.Item, onClick: () -> Unit) {
-            txtHolderAddress.text = item.address
-            txtHolderRate.text = item.sharePercent
-            viewBackground.setBackgroundResource(item.position.getBackground())
-            containerView.setOnClickListener {
-                onClick.invoke()
+        private var item: MajorHolderItem.Item? = null
+
+        init {
+            btnHolderAddress.setOnSingleClickListener {
+                item?.let {
+                    listener.onAddressClick(it.address)
+                }
+            }
+            btnDetails.setOnSingleClickListener {
+                item?.let {
+                    listener.onDetailsClick(it.address)
+                }
             }
         }
-    }
 
+        fun bind(item: MajorHolderItem.Item) {
+            this.item = item
+
+            txtIndex.text = item.index.toString()
+            btnHolderAddress.text = item.address
+            txtHolderRate.text = item.sharePercent
+        }
+    }
 }
