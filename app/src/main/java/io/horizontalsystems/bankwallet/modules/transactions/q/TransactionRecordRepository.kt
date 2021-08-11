cbfa28d02c9555e64bd1afabba0bb66cd5559ee8
bakyt <abdrasulov@gmail.com>
2021-08-11 10:41:55 +0600

    Implement TransactionRecordRepository

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/TransactionRecordRepository.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/TransactionRecordRepository.kt
@@ -1,38 +1,238 @@
 package io.horizontalsystems.bankwallet.modules.transactions.q
 
+import io.horizontalsystems.bankwallet.core.Clearable
+import io.horizontalsystems.bankwallet.core.IAdapterManager
+import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.bankwallet.entities.transactionrecords.TransactionRecord
-import io.horizontalsystems.bankwallet.entities.transactionrecords.bitcoin.BitcoinIncomingTransactionRecord
-import io.horizontalsystems.bankwallet.modules.transactions.TransactionRecordDataSource
-import io.horizontalsystems.coinkit.models.Coin
+import io.horizontalsystems.bankwallet.modules.transactions.TransactionSource
+import io.horizontalsystems.bankwallet.modules.transactions.TransactionWallet
 import io.horizontalsystems.coinkit.models.CoinType
 import io.reactivex.Observable
-import java.math.BigDecimal
-import java.util.*
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.atomic.AtomicBoolean
 
-class TransactionRecordRepository(private val dataSource: TransactionRecordDataSource) {
-    val itemsObservable: Observable<List<TransactionRecord>> = Observable.just(
-        listOf(
-            BitcoinIncomingTransactionRecord(
-                Coin(CoinType.Bitcoin, "BTC", "Super", 8),
-                UUID.randomUUID().toString(),
-                "super",
-                0,
-                null,
-                null,
-                Date().time / 1000,
-                null,
-                false,
-                null,
-                null,
-                false,
-                BigDecimal.TEN,
-                "its daddy",
-                null
-            )
-        )
-    )
+class TransactionRecordRepository(
+    private val adapterManager: IAdapterManager,
+) : Clearable {
 
-    init {
+    private var selectedWallet: Wallet? = null
 
+    private val itemsSubject = PublishSubject.create<List<TransactionRecord>>()
+    val itemsObservable: Observable<List<TransactionRecord>> get() = itemsSubject
+
+    private val disposables = CompositeDisposable()
+
+    override fun clear() {
+        disposables.clear()
     }
+
+    private var wallets = emptyList<Wallet>()
+    private val allTransactionWallets: List<TransactionWallet>
+        get() {
+            val transactionWallets = wallets.map { transactionWallet(it) }
+            val mergedWallets = mutableListOf<TransactionWallet>()
+
+            transactionWallets.forEach { wallet ->
+                when (wallet.source.blockchain) {
+                    TransactionSource.Blockchain.Bitcoin,
+                    TransactionSource.Blockchain.BitcoinCash,
+                    TransactionSource.Blockchain.Litecoin,
+                    TransactionSource.Blockchain.Dash,
+                    TransactionSource.Blockchain.Zcash,
+                    is TransactionSource.Blockchain.Bep2 -> mergedWallets.add(wallet)
+                    TransactionSource.Blockchain.Ethereum,
+                    TransactionSource.Blockchain.BinanceSmartChain -> {
+                        if (mergedWallets.none { it.source == wallet.source }) {
+                            mergedWallets.add(TransactionWallet(null, wallet.source))
+                        }
+                    }
+                }
+            }
+            return mergedWallets
+        }
+
+
+    private val xxxRepos = mutableMapOf<TransactionWallet, TransactionAdapterWrapperXxx>()
+
+    fun setWallets(wallets: List<Wallet>) {
+        this.wallets = wallets
+
+        items.clear()
+        xxxRepos.forEach { t, u ->
+            u.markUsed(null)
+        }
+        xxx()
+    }
+
+    fun setSelectedWallet(wallet: Wallet?) {
+        selectedWallet = wallet
+
+        items.clear()
+        xxxRepos.forEach { t, u ->
+            u.markUsed(null)
+        }
+        xxx()
+    }
+
+    fun loadNext() {
+        xxx()
+    }
+
+    var loading = AtomicBoolean(false)
+
+    @Synchronized
+    private fun xxx() {
+        if (loading.get()) return
+        loading.set(true)
+
+        val activeWallets: List<TransactionWallet> =
+            when (val selectedWalletTmp = selectedWallet) {
+                null -> allTransactionWallets
+                else -> listOf(transactionWallet(selectedWalletTmp))
+            }
+
+        val map: List<Single<List<Pair<TransactionWallet, TransactionRecord>>>> = activeWallets.mapNotNull { transactionWallet ->
+            if (xxxRepos[transactionWallet] == null) {
+                adapterManager.getTransactionsAdapterForWallet(transactionWallet)?.let {
+                    xxxRepos[transactionWallet] = TransactionAdapterWrapperXxx(it, transactionWallet)
+                }
+            }
+
+            xxxRepos[transactionWallet]?.let { transactionAdapterWrapperXxx ->
+                transactionAdapterWrapperXxx
+                    .getNext(itemsPerPage)
+                    .map { transactionRecords: List<TransactionRecord> ->
+                        transactionRecords.map {
+                            Pair(transactionWallet, it)
+                        }
+                    }
+            }
+        }
+
+        Single
+            .zip(map) {
+                it as Array<List<Pair<TransactionWallet, TransactionRecord>>>
+                it.toList().flatten()
+            }
+            .subscribeOn(Schedulers.computation())
+            .observeOn(Schedulers.computation())
+            .subscribe { records ->
+                handleRecords(records)
+            }
+            .let {
+                disposables.add(it)
+            }
+    }
+
+    private val items = CopyOnWriteArrayList<TransactionRecord>()
+
+    private fun handleRecords(records: List<Pair<TransactionWallet, TransactionRecord>>) {
+        if (records.isEmpty()) {
+            loading.set(false)
+            return
+        }
+
+        records
+            .sortedByDescending { it.second }
+            .take(itemsPerPage)
+            .forEach {
+                xxxRepos[it.first]?.markUsed(it.second)
+
+                items.add(it.second)
+            }
+
+        loading.set(false)
+
+        itemsSubject.onNext(items)
+    }
+
+    private fun transactionWallet(wallet: Wallet): TransactionWallet {
+        val coinSettings = wallet.configuredCoin.settings
+        val coin = wallet.coin
+
+        return when (val type = coin.type) {
+            CoinType.Bitcoin -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.Bitcoin,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            CoinType.BitcoinCash -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.BitcoinCash,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            CoinType.Dash -> TransactionWallet(
+                coin,
+                TransactionSource(TransactionSource.Blockchain.Dash, wallet.account, coinSettings)
+            )
+            CoinType.Litecoin -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.Litecoin,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            CoinType.Ethereum -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.Ethereum,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            CoinType.BinanceSmartChain -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.BinanceSmartChain,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            CoinType.Zcash -> TransactionWallet(
+                coin,
+                TransactionSource(TransactionSource.Blockchain.Zcash, wallet.account, coinSettings)
+            )
+            is CoinType.Bep2 -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.Bep2(type.symbol),
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            is CoinType.Erc20 -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.Ethereum,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            is CoinType.Bep20 -> TransactionWallet(
+                coin,
+                TransactionSource(
+                    TransactionSource.Blockchain.BinanceSmartChain,
+                    wallet.account,
+                    coinSettings
+                )
+            )
+            is CoinType.Unsupported -> throw IllegalArgumentException("Unsupported coin may not have transactions to show")
+        }
+    }
+
+    companion object {
+        const val itemsPerPage = 20
+    }
+
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/Transactions2Service.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/Transactions2Service.kt
@@ -2,11 +2,24 @@ package io.horizontalsystems.bankwallet.modules.transactions.q
 
 import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.subscribeIO
+import io.horizontalsystems.bankwallet.entities.Wallet
+import io.horizontalsystems.bankwallet.modules.balance.BalanceActiveWalletRepository
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
+import java.util.*
+import java.util.concurrent.Executors
 
-class Transactions2Service(private val transactionRecordRepository: TransactionRecordRepository) : Clearable {
+class Transactions2Service(
+    private val balanceActiveWalletRepository: BalanceActiveWalletRepository,
+    private val transactionRecordRepository: TransactionRecordRepository
+) : Clearable {
+
+    private val filterCoinsSubject = BehaviorSubject.create<List<Wallet>>()
+    val filterCoinsObservable: Observable<List<Wallet>> = filterCoinsSubject
+
+    private val filterCoinSubject = BehaviorSubject.createDefault<Optional<Wallet>>(Optional.empty())
+    val filterCoinObservable: Observable<Optional<Wallet>> = filterCoinSubject
 
     private val itemsSubject = BehaviorSubject.create<List<TransactionItem>>()
     val itemsObservable: Observable<List<TransactionItem>> get() = itemsSubject
@@ -16,6 +29,14 @@ class Transactions2Service(private val transactionRecordRepository: TransactionR
     private val disposables = CompositeDisposable()
 
     init {
+        balanceActiveWalletRepository.itemsObservable
+            .subscribeIO { wallets ->
+                handleUpdatedWallets(wallets)
+            }
+            .let {
+                disposables.add(it)
+            }
+
         transactionRecordRepository.itemsObservable
             .subscribeIO {
                 itemsSubject.onNext(it.map {
@@ -27,8 +48,27 @@ class Transactions2Service(private val transactionRecordRepository: TransactionR
             }
     }
 
+    private fun handleUpdatedWallets(wallets: List<Wallet>) {
+        filterCoinsSubject.onNext(wallets)
+        transactionRecordRepository.setWallets(wallets)
+    }
+
     override fun clear() {
         disposables.clear()
     }
-}
 
+    private val executorService = Executors.newCachedThreadPool()
+
+    fun setFilterCoin(w: Wallet?) {
+        executorService.submit {
+            filterCoinSubject.onNext(w?.let { Optional.of(it) } ?: Optional.empty())
+            transactionRecordRepository.setSelectedWallet(w)
+        }
+    }
+
+    fun loadNext() {
+        executorService.submit {
+            transactionRecordRepository.loadNext()
+        }
+    }
+}
