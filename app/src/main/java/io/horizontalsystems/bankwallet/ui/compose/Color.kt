6f8c981901994fb97a88005a09b07dddcab14906
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-08-12 16:20:27 +0600

    Add Compose theme and button components

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/Color.kt
@@ -0,0 +1,35 @@
+package io.horizontalsystems.bankwallet.ui.compose
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Dark = Color(0xFF13151A)
+val Light = Color(0xFFF0F0F0)
+val LightGrey = Color(0xFFC8C7CC)
+val SteelLight = Color(0xFFE1E1E5)
+val SteelDark = Color(0xFF252933)
+val Steel20 = Color(0x336E7899)
+val Grey50 = Color(0x80808085)
+
+val YellowD = Color(0xFFFFA800)
+val YellowL = Color(0xFFF58400)
+val GreenD = Color(0xFF05C46B)
+val GreenL = Color(0xFF04AD5F)
+val RedD = Color(0xFFFF4820)
+val RedL = Color(0xFFED402E)
+val OzD = Light
+val OzL = Dark
+val TylerD = Dark
+val TylerL = Light
+val BranD = LightGrey
+val BranL = Dark
+val ClaudeD = Dark
+val ClaudeL = Color.White
+val LeahD = SteelLight
+val LeahL = SteelDark
+val LawrenceD = SteelDark
+val LawrenceL = Color.White
---
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/UwButton.kt
@@ -0,0 +1,168 @@
+package io.horizontalsystems.bankwallet.ui.compose.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import io.horizontalsystems.bankwallet.ui.compose.Grey50
+import io.horizontalsystems.bankwallet.ui.compose.Steel20
+import io.horizontalsystems.bankwallet.ui.compose.UnstoppableComponentsAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.YellowD
+
+@Composable
+fun UwButtonPrimaryDefault(title: String, onClick: () -> Unit, enabled: Boolean = true) {
+    UwButtonPrimary(
+        onClick = onClick,
+        colors = UnButtonDefaults.textButtonColors(
+            backgroundColor = UnstoppableComponentsAppTheme.colors.leah,
+        ),
+        content = { Text(title) },
+        enabled = enabled
+    )
+}
+
+@Composable
+fun UwButtonPrimaryYellow(title: String, onClick: () -> Unit, enabled: Boolean = true) {
+    UwButtonPrimary(
+        onClick = onClick,
+        colors = UnButtonDefaults.textButtonColors(
+            backgroundColor = YellowD,
+        ),
+        content = { Text(title) },
+        enabled = enabled
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun UwButtonPrimary(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = RoundedCornerShape(25.dp),
+    border: BorderStroke? = null,
+    colors: ButtonColors = UnButtonDefaults.textButtonColors(),
+    contentPadding: PaddingValues = UnButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit
+) {
+    val contentColor by colors.contentColor(enabled)
+    Surface(
+        modifier = modifier,
+        shape = shape,
+        color = colors.backgroundColor(enabled).value,
+        contentColor = contentColor.copy(alpha = 1f),
+        border = border,
+        elevation = 0.dp,
+        onClick = onClick,
+        enabled = enabled,
+        role = Role.Button,
+        interactionSource = interactionSource,
+        indication = rememberRipple()
+    ) {
+        CompositionLocalProvider(LocalContentAlpha provides contentColor.alpha) {
+            ProvideTextStyle(
+                value = UnstoppableComponentsAppTheme.typography.headline2
+            ) {
+                Row(
+                    Modifier
+                        .defaultMinSize(
+                            minWidth = UnButtonDefaults.MinWidth,
+                            minHeight = UnButtonDefaults.MinHeight
+                        )
+                        .padding(contentPadding),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = content
+                )
+            }
+        }
+    }
+}
+
+object UnButtonDefaults {
+    private val ButtonHorizontalPadding = 16.dp
+    private val ButtonVerticalPadding = 16.dp
+
+    val ContentPadding = PaddingValues(
+        start = ButtonHorizontalPadding,
+        top = ButtonVerticalPadding,
+        end = ButtonHorizontalPadding,
+        bottom = ButtonVerticalPadding
+    )
+
+
+    /**
+     * The default min width applied for the [Button].
+     * Note that you can override it by applying Modifier.widthIn directly on [Button].
+     */
+    val MinWidth = 64.dp
+
+    /**
+     * The default min width applied for the [Button].
+     * Note that you can override it by applying Modifier.heightIn directly on [Button].
+     */
+    val MinHeight = 36.dp
+
+    @Composable
+    fun textButtonColors(
+        backgroundColor: Color = Color.Transparent,
+        contentColor: Color = UnstoppableComponentsAppTheme.colors.claude,
+        disabledBackgroundColor: Color = Steel20,
+        disabledContentColor: Color = Grey50,
+    ): ButtonColors = DefaultButtonColors(
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        disabledBackgroundColor = disabledBackgroundColor,
+        disabledContentColor = disabledContentColor
+    )
+}
+
+@Immutable
+private class DefaultButtonColors(
+    private val backgroundColor: Color,
+    private val contentColor: Color,
+    private val disabledBackgroundColor: Color,
+    private val disabledContentColor: Color
+) : ButtonColors {
+    @Composable
+    override fun backgroundColor(enabled: Boolean): State<Color> {
+        return rememberUpdatedState(if (enabled) backgroundColor else disabledBackgroundColor)
+    }
+
+    @Composable
+    override fun contentColor(enabled: Boolean): State<Color> {
+        return rememberUpdatedState(if (enabled) contentColor else disabledContentColor)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as DefaultButtonColors
+
+        if (backgroundColor != other.backgroundColor) return false
+        if (contentColor != other.contentColor) return false
+        if (disabledBackgroundColor != other.disabledBackgroundColor) return false
+        if (disabledContentColor != other.disabledContentColor) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = backgroundColor.hashCode()
+        result = 31 * result + contentColor.hashCode()
+        result = 31 * result + disabledBackgroundColor.hashCode()
+        result = 31 * result + disabledContentColor.hashCode()
+        return result
+    }
+}
