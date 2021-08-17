ffe7153ae937de66014b1c991900b707cb90df2f
bakyt <abdrasulov@gmail.com>
2021-08-17 12:04:42 +0600

    Simplify TransactionsXRateRepository removing its cache data

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/TransactionsXRateRepository.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/TransactionsXRateRepository.kt
@@ -3,10 +3,9 @@ package io.horizontalsystems.bankwallet.modules.transactions.q
 import android.util.Log
 import io.horizontalsystems.bankwallet.core.IRateManager
 import io.horizontalsystems.bankwallet.core.subscribeIO
-import io.horizontalsystems.bankwallet.entities.transactionrecords.TransactionRecord
+import io.horizontalsystems.bankwallet.entities.CurrencyValue
 import io.horizontalsystems.coinkit.models.CoinType
 import io.horizontalsystems.core.ICurrencyManager
-import io.horizontalsystems.core.entities.Currency
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
@@ -16,72 +15,54 @@ class TransactionsXRateRepository(
     private val currencyManager: ICurrencyManager,
     private val xRateManager: IRateManager,
 ) {
-    val baseCurrency: Currency get() = currencyManager.baseCurrency
+    private val baseCurrency get() = currencyManager.baseCurrency
 
-    private val itemsUpdatedSubject = PublishSubject.create<Unit>()
-    val itemsUpdatedObservable: Observable<Unit> get() = itemsUpdatedSubject
-
-    private val historicalRates = mutableMapOf<HistoricalRateKey, BigDecimal?>()
     private val disposables = CompositeDisposable()
+
+    private val dataExpiredSubject = PublishSubject.create<Unit>()
+    val dataExpiredObservable: Observable<Unit> = dataExpiredSubject
+
+    private val historicalRateSubject = PublishSubject.create<Pair<HistoricalRateKey, CurrencyValue>>()
+    val historicalRateObservable: Observable<Pair<HistoricalRateKey, CurrencyValue>> = historicalRateSubject
+
+    private val requestedXRates = mutableMapOf<HistoricalRateKey, Unit>()
 
     init {
         currencyManager.baseCurrencyUpdatedSignal
             .subscribeIO {
-                handleUpdatedBaseCurrency()
+                dataExpiredSubject.onNext(Unit)
             }
             .let {
                 disposables.add(it)
             }
     }
 
-    private fun handleUpdatedBaseCurrency() {
-        historicalRates.forEach { (key, _) ->
-            historicalRates[key] = xRateManager.historicalRateCached(key.coinType, baseCurrency.code, key.timestamp)
-        }
-        itemsUpdatedSubject.onNext(Unit)
-    }
-
-    fun setRecords(transactionRecords: List<TransactionRecord>) {
-        val historicalRateKeys = transactionRecords.mapNotNull { record ->
-            record.mainValue?.let { mainValue ->
-                HistoricalRateKey(mainValue.coin.type, record.timestamp)
-            }
-        }
-
-        historicalRates.clear()
-        historicalRateKeys.forEach {
-            historicalRates[it] = xRateManager.historicalRateCached(it.coinType, baseCurrency.code, it.timestamp)
+    fun getHistoricalRate(key: HistoricalRateKey): CurrencyValue? {
+        return xRateManager.historicalRateCached(key.coinType, baseCurrency.code, key.timestamp)?.let {
+            CurrencyValue(baseCurrency, it)
         }
     }
 
-    fun getHistoricalRate(coinType: CoinType, timestamp: Long): BigDecimal? {
-        return historicalRates[HistoricalRateKey(coinType, timestamp)]
-    }
+    fun fetchHistoricalRate(key: HistoricalRateKey) {
+        if (requestedXRates.containsKey(key)) return
 
-    val requestedXRates = mutableMapOf<HistoricalRateKey, Unit>()
+        requestedXRates[key] = Unit
 
-    fun fetchHistoricalRate(coinType: CoinType, timestamp: Long) {
-        val historicalRateKey = HistoricalRateKey(coinType, timestamp)
-        if (requestedXRates.containsKey(historicalRateKey)) return
-
-        requestedXRates[historicalRateKey] = Unit
-
-        xRateManager.historicalRate(coinType, baseCurrency.code, timestamp)
+        xRateManager.historicalRate(key.coinType, baseCurrency.code, key.timestamp)
             .doFinally {
-                requestedXRates.remove(historicalRateKey)
+                requestedXRates.remove(key)
             }
             .subscribeIO({ rate ->
-                historicalRates[historicalRateKey] = if (rate.compareTo(BigDecimal.ZERO) == 0) null else rate
-
-                itemsUpdatedSubject.onNext(Unit)
+                if (rate.compareTo(BigDecimal.ZERO) != 0) {
+                    historicalRateSubject.onNext(Pair(key, CurrencyValue(baseCurrency, rate)))
+                }
             }, {
-                Log.e("AAA", "Could not fetch xrate for ${coinType}:${timestamp}, ${it.javaClass.simpleName}:${it.message}")
+                Log.e("AAA", "Could not fetch xrate for ${key.coinType}:${key.timestamp}, ${it.javaClass.simpleName}:${it.message}")
             })
             .let {
                 disposables.add(it)
             }
     }
-
-    data class HistoricalRateKey(val coinType: CoinType, val timestamp: Long)
-
 }
+
+data class HistoricalRateKey(val coinType: CoinType, val timestamp: Long)
