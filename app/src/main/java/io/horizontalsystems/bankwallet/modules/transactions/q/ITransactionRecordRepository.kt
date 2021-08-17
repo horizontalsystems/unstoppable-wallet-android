c46675ceb6a545c86ff55583d06554b8c1aa43d9
bakyt <abdrasulov@gmail.com>
2021-08-17 15:17:52 +0600

    Move converting Wallet into TransactionWallet to Service

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/ITransactionRecordRepository.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/ITransactionRecordRepository.kt
@@ -1,13 +1,13 @@
 package io.horizontalsystems.bankwallet.modules.transactions.q
 
-import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.bankwallet.entities.transactionrecords.TransactionRecord
+import io.horizontalsystems.bankwallet.modules.transactions.TransactionWallet
 import io.reactivex.Observable
 
 interface ITransactionRecordRepository {
     val itemsObservable: Observable<List<TransactionRecord>>
 
-    fun setWallets(wallets: List<Wallet>)
-    fun setSelectedWallet(wallet: Wallet?)
+    fun setWallets(transactionWallets: List<TransactionWallet>, walletsGroupedBySource: List<TransactionWallet>)
+    fun setSelectedWallet(transactionWallet: TransactionWallet?)
     fun loadNext()
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/TransactionRecordRepository.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/TransactionRecordRepository.kt
@@ -2,11 +2,8 @@ package io.horizontalsystems.bankwallet.modules.transactions.q
 
 import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.IAdapterManager
-import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.bankwallet.entities.transactionrecords.TransactionRecord
-import io.horizontalsystems.bankwallet.modules.transactions.TransactionSource
 import io.horizontalsystems.bankwallet.modules.transactions.TransactionWallet
-import io.horizontalsystems.coinkit.models.CoinType
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
@@ -19,7 +16,7 @@ class TransactionRecordRepository(
     private val adapterManager: IAdapterManager,
 ) : Clearable, ITransactionRecordRepository {
 
-    private var selectedWallet: Wallet? = null
+    private var selectedWallet: TransactionWallet? = null
 
     private val itemsSubject = PublishSubject.create<List<TransactionRecord>>()
     override val itemsObservable: Observable<List<TransactionRecord>> get() = itemsSubject
@@ -31,43 +28,10 @@ class TransactionRecordRepository(
 
     private val disposables = CompositeDisposable()
 
-    private var wallets = emptyList<Wallet>()
-    private val allTransactionWallets: List<TransactionWallet>
-        get() {
-            val transactionWallets = wallets.map { transactionWallet(it) }
-            val mergedWallets = mutableListOf<TransactionWallet>()
+    private var walletsGroupedBySource: List<TransactionWallet> = listOf()
 
-            transactionWallets.forEach { wallet ->
-                when (wallet.source.blockchain) {
-                    TransactionSource.Blockchain.Bitcoin,
-                    TransactionSource.Blockchain.BitcoinCash,
-                    TransactionSource.Blockchain.Litecoin,
-                    TransactionSource.Blockchain.Dash,
-                    TransactionSource.Blockchain.Zcash,
-                    is TransactionSource.Blockchain.Bep2 -> mergedWallets.add(wallet)
-                    TransactionSource.Blockchain.Ethereum,
-                    TransactionSource.Blockchain.BinanceSmartChain -> {
-                        if (mergedWallets.none { it.source == wallet.source }) {
-                            mergedWallets.add(TransactionWallet(null, wallet.source))
-                        }
-                    }
-                }
-            }
-            return mergedWallets
-        }
-
-    override fun setWallets(wallets: List<Wallet>) {
-        this.wallets = wallets
-
-        val transactionWallets = wallets.map { transactionWallet(it) }.toMutableList()
-
-        getAllEthTransactionWallet(wallets)?.let {
-            transactionWallets.add(it)
-        }
-
-        getAllBscTransactionWallet(wallets)?.let {
-            transactionWallets.add(it)
-        }
+    override fun setWallets(transactionWallets: List<TransactionWallet>, walletsGroupedBySource: List<TransactionWallet>) {
+        this.walletsGroupedBySource = walletsGroupedBySource
 
         transactionWallets.forEach { transactionWallet ->
             if (adaptersMap[transactionWallet] == null) {
@@ -78,6 +42,14 @@ class TransactionRecordRepository(
             }
         }
 
+        walletsGroupedBySource.forEach { transactionWallet ->
+            if (adaptersMap[transactionWallet] == null) {
+                adapterManager.getTransactionsAdapterForWallet(transactionWallet)?.let {
+                    val transactionAdapterWrapperXxx = TransactionAdapterWrapper(it, transactionWallet)
+                    adaptersMap[transactionWallet] = transactionAdapterWrapperXxx
+                }
+            }
+        }
 
         items.clear()
         itemsUpdated = true
@@ -87,38 +59,9 @@ class TransactionRecordRepository(
         loadNext()
     }
 
-    private fun getAllEthTransactionWallet(wallets: List<Wallet>): TransactionWallet? {
-        return wallets.firstOrNull {
-            it.coin.type is CoinType.Ethereum || it.coin.type is CoinType.Erc20
-        }?.let { wallet ->
-            TransactionWallet(
-                null,
-                TransactionSource(
-                    TransactionSource.Blockchain.Ethereum,
-                    wallet.account,
-                    wallet.configuredCoin.settings
-                )
-            )
-        }
-    }
-
-    private fun getAllBscTransactionWallet(wallets: List<Wallet>): TransactionWallet? {
-        return wallets.firstOrNull {
-            it.coin.type is CoinType.BinanceSmartChain || it.coin.type is CoinType.Bep20
-        }?.let { wallet ->
-            TransactionWallet(
-                null,
-                TransactionSource(
-                    TransactionSource.Blockchain.BinanceSmartChain,
-                    wallet.account,
-                    wallet.configuredCoin.settings
-                )
-            )
-        }
-    }
-
-    override fun setSelectedWallet(wallet: Wallet?) {
-        selectedWallet = wallet
+    override fun setSelectedWallet(transactionWallet: TransactionWallet?) {
+        selectedWallet = transactionWallet
+        val activeWallets = transactionWallet?.let { listOf(it) } ?: walletsGroupedBySource
 
         items.clear()
         itemsUpdated = true
@@ -132,11 +75,7 @@ class TransactionRecordRepository(
         if (loading.get()) return
         loading.set(true)
 
-        val activeWallets: List<TransactionWallet> =
-            when (val selectedWalletTmp = selectedWallet) {
-                null -> allTransactionWallets
-                else -> listOf(transactionWallet(selectedWalletTmp))
-            }
+        val activeWallets = selectedWallet?.let { listOf(it) } ?: walletsGroupedBySource
 
         val map: List<Single<List<Pair<TransactionWallet, TransactionRecord>>>> = activeWallets.mapNotNull { transactionWallet ->
             adaptersMap[transactionWallet]?.let { transactionAdapterWrapperXxx ->
@@ -186,87 +125,6 @@ class TransactionRecordRepository(
         if (itemsUpdated) {
             itemsSubject.onNext(items)
             itemsUpdated = false
-        }
-    }
-
-    private fun transactionWallet(wallet: Wallet): TransactionWallet {
-        val coinSettings = wallet.configuredCoin.settings
-        val coin = wallet.coin
-
-        return when (val type = coin.type) {
-            CoinType.Bitcoin -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.Bitcoin,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            CoinType.BitcoinCash -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.BitcoinCash,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            CoinType.Dash -> TransactionWallet(
-                coin,
-                TransactionSource(TransactionSource.Blockchain.Dash, wallet.account, coinSettings)
-            )
-            CoinType.Litecoin -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.Litecoin,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            CoinType.Ethereum -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.Ethereum,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            CoinType.BinanceSmartChain -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.BinanceSmartChain,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            CoinType.Zcash -> TransactionWallet(
-                coin,
-                TransactionSource(TransactionSource.Blockchain.Zcash, wallet.account, coinSettings)
-            )
-            is CoinType.Bep2 -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.Bep2(type.symbol),
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            is CoinType.Erc20 -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.Ethereum,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            is CoinType.Bep20 -> TransactionWallet(
-                coin,
-                TransactionSource(
-                    TransactionSource.Blockchain.BinanceSmartChain,
-                    wallet.account,
-                    coinSettings
-                )
-            )
-            is CoinType.Unsupported -> throw IllegalArgumentException("Unsupported coin may not have transactions to show")
         }
     }
 
