8f5c01fafffabd17aa786d649327caa5b6d0b574
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-08-17 14:47:16 +0600

    Replace buttons with compose in OnboardingFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/onboarding/OnboardingFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/onboarding/OnboardingFragment.kt
@@ -4,24 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryDefault
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_no_wallet.*
 
-class OnboardingFragment: BaseFragment() {
+class OnboardingFragment : BaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_no_wallet, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnCreate.setOnClickListener {
-            findNavController().navigate(R.id.createAccountFragment, null, navOptions())
-        }
-        btnRestore.setOnClickListener {
-            findNavController().navigate(R.id.restoreMnemonicFragment, null, navOptions())
+        buttonsCompose.setContent {
+            ComposeAppTheme {
+                Column(modifier = Modifier.width(IntrinsicSize.Max)) {
+                    ButtonPrimaryYellow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 24.dp, end = 24.dp, bottom = 16.dp),
+                        title = getString(R.string.Button_Create),
+                        onClick = {
+                            findNavController().navigate(
+                                R.id.createAccountFragment,
+                                null,
+                                navOptions()
+                            )
+                        }
+                    )
+                    ButtonPrimaryDefault(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 24.dp, end = 24.dp, bottom = 24.dp),
+                        title = getString(R.string.Button_Restore),
+                        onClick = {
+                            findNavController().navigate(
+                                R.id.restoreMnemonicFragment,
+                                null,
+                                navOptions()
+                            )
+                        }
+                    )
+                }
+            }
         }
     }
 }
