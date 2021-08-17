1361732eb415713c448a86f53c49f2a127215673
bakyt <abdrasulov@gmail.com>
2021-08-17 11:10:24 +0600

    Extract interface ITransactionRecordRepository

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/Transactions2Service.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/q/Transactions2Service.kt
@@ -15,7 +15,7 @@ import java.util.concurrent.Executors
 
 class Transactions2Service(
     private val balanceActiveWalletRepository: BalanceActiveWalletRepository,
-    private val transactionRecordRepository: TransactionRecordRepository,
+    private val transactionRecordRepository: ITransactionRecordRepository,
     private val xRateRepository: TransactionsXRateRepository
 ) : Clearable {
 
