14e695535505d4c9c79295ae0946d45bae1b7fd6
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-08-19 17:08:07 +0600

    Replace button with compose in Disclaimer dialog

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/basecurrency/BaseCurrencyDisclaimerDialog.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/basecurrency/BaseCurrencyDisclaimerDialog.kt
@@ -4,12 +4,16 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.DialogFragment
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.core.setOnSingleClickListener
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 
 class BaseCurrencyDisclaimerDialog(val popularCurrencies: String) : DialogFragment() {
 
@@ -18,15 +22,30 @@ class BaseCurrencyDisclaimerDialog(val popularCurrencies: String) : DialogFragme
     override fun onCreateDialog(bundle: Bundle?): Dialog {
         val rootView = View.inflate(context, R.layout.fragment_disclaimer, null) as ViewGroup
 
-        rootView.findViewById<Button>(R.id.buttonConfirm)?.setOnSingleClickListener {
-            onConfirm?.invoke()
-        }
         rootView.findViewById<TextView>(R.id.text)?.text = getString(R.string.SettingsCurrency_DisclaimerText, popularCurrencies)
 
         val builder = activity?.let { AlertDialog.Builder(it, io.horizontalsystems.pin.R.style.AlertDialog) }
         builder?.setView(rootView)
         val mDialog = builder?.create()
         mDialog?.setCanceledOnTouchOutside(true)
+
+        rootView.findViewById<ComposeView>(R.id.buttonConfirmCompose)?.let {
+            it.setContent {
+                ComposeAppTheme {
+                    ButtonPrimaryYellow(
+                        modifier = Modifier.padding(
+                            start = 16.dp,
+                            top = 30.dp,
+                            end = 16.dp,
+                            bottom = 16.dp
+                        ),
+                        title = getString(R.string.SettingsCurrency_Understand),
+                        onClick = { onConfirm?.invoke() }
+                    )
+                }
+            }
+        }
+
 
         return mDialog as Dialog
     }
