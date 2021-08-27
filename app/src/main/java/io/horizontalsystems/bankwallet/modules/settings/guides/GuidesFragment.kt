58605bde91aabc4a653c2aa471237c57e9d8fe7f
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-08-27 16:42:17 +0600

    Replace tabs with Compose in GuidesFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/guides/GuidesFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/guides/GuidesFragment.kt
@@ -4,26 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.*
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.entities.Guide
 import io.horizontalsystems.bankwallet.modules.markdown.MarkdownFragment
-import io.horizontalsystems.bankwallet.ui.FilterAdapter
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.Grey
 import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_guides.*
+import kotlinx.android.synthetic.main.fragment_guides.tabsCompose
+import kotlinx.android.synthetic.main.fragment_guides.toolbar
 
-class GuidesFragment : BaseFragment(), GuidesAdapter.Listener, FilterAdapter.Listener {
+class GuidesFragment : BaseFragment(), GuidesAdapter.Listener {
 
     private val viewModel by viewModels<GuidesViewModel> { GuidesModule.Factory() }
     private val errorAdapter = ErrorAdapter()
     private val guidesAdapter = GuidesAdapter(this)
-    private val filterAdapter = FilterAdapter(this)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_guides, container, false)
@@ -36,7 +42,6 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener, FilterAdapter.Lis
             findNavController().popBackStack()
         }
 
-        recyclerTags.adapter = filterAdapter
         recyclerGuides.adapter = ConcatAdapter(errorAdapter, guidesAdapter)
 
         observeLiveData()
@@ -44,8 +49,6 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener, FilterAdapter.Lis
 
     override fun onDestroyView() {
         super.onDestroyView()
-
-        recyclerTags.adapter = null
         recyclerGuides.adapter = null
     }
 
@@ -64,8 +67,7 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener, FilterAdapter.Lis
         })
 
         viewModel.filters.observe(viewLifecycleOwner, Observer {
-            val selectedFilterItem = viewModel.selectedFilter?.let{ selected -> FilterAdapter.FilterItem(selected) }
-            filterAdapter.setFilters(it.map { filter -> FilterAdapter.FilterItem(filter) }, selectedFilterItem)
+            setTabs(it, viewModel.selectedFilter)
         })
 
         viewModel.loading.observe(viewLifecycleOwner, Observer {
@@ -77,13 +79,46 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener, FilterAdapter.Lis
         })
     }
 
-    override fun onFilterItemClick(item: FilterAdapter.FilterItem?, itemPosition: Int, itemWidth: Int) {
-        item?.filterId?.let {
-            recyclerGuides.layoutManager?.scrollToPosition(0)
-            viewModel.onSelectFilter(it)
+    private fun setTabs(tabs: List<String>, selectedFilter: String?) {
+        var selectedIndex = tabs.indexOf(selectedFilter)
+        if (selectedIndex < 0) {
+            selectedIndex = 0
+        }
 
-            val leftOffset = recyclerTags.width / 2 - itemWidth / 2
-            (recyclerTags.layoutManager as? LinearLayoutManager)?.scrollToPositionWithOffset(itemPosition, leftOffset)
+        tabsCompose.setContent {
+            var tabIndex by remember { mutableStateOf(selectedIndex) }
+            ComposeAppTheme {
+                ScrollableTabRow(
+                    selectedTabIndex = tabIndex,
+                    backgroundColor = ComposeAppTheme.colors.tyler,
+                    contentColor = ComposeAppTheme.colors.tyler,
+                    indicator = @Composable { tabPositions ->
+                        TabRowDefaults.Indicator(
+                            modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                            color = ComposeAppTheme.colors.jacob
+                        )
+                    }
+                ) {
+                    tabs.forEachIndexed { index, tab ->
+                        Tab(
+                            selected = tabIndex == index,
+                            onClick = {
+                                tabIndex = index
+                                viewModel.onSelectFilter(tab)
+                            },
+                            text = {
+                                ProvideTextStyle(ComposeAppTheme.typography.subhead1.copy(textAlign = TextAlign.Center)) {
+                                    Text(
+                                        text = tab,
+                                        color = if (tabIndex == index) ComposeAppTheme.colors.oz else Grey
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
+
 }
