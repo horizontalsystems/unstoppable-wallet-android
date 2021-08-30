0757b5bedab73865c1fb5a61576b08d471186f31
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-08-30 11:11:24 +0600

    Add bottom border to Tabs

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/MarketFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/MarketFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.navGraphViewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseWithSearchFragment
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.Grey
+import io.horizontalsystems.bankwallet.ui.compose.Steel10
 import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_market.*
 
@@ -60,39 +63,42 @@ class MarketFragment : BaseWithSearchFragment() {
         tabsCompose.setContent {
             var tabIndex by remember { mutableStateOf(currentItemIndex) }
             ComposeAppTheme {
-                TabRow(
-                    selectedTabIndex = tabIndex,
-                    backgroundColor = ComposeAppTheme.colors.tyler,
-                    contentColor = ComposeAppTheme.colors.tyler,
-                    indicator = @Composable { tabPositions ->
-                        TabRowDefaults.Indicator(
-                            modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
-                            color = ComposeAppTheme.colors.jacob
-                        )
+                Column {
+                    TabRow(
+                        modifier = Modifier.height(44.dp),
+                        selectedTabIndex = tabIndex,
+                        backgroundColor = ComposeAppTheme.colors.tyler,
+                        contentColor = ComposeAppTheme.colors.tyler,
+                        indicator = @Composable { tabPositions ->
+                            TabRowDefaults.Indicator(
+                                modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                                color = ComposeAppTheme.colors.jacob
+                            )
+                        }
+                    ) {
+                        marketViewModel.tabs.forEachIndexed { index, tab ->
+                            Tab(
+                                selected = tabIndex == index,
+                                onClick = {
+                                    tabIndex = index
+                                    marketViewModel.onSelect(index)
+                                },
+                                text = {
+                                    ProvideTextStyle(
+                                        ComposeAppTheme.typography.subhead1.copy(
+                                            textAlign = TextAlign.Center
+                                        )
+                                    ) {
+                                        Text(
+                                            text = tab.name,
+                                            color = if (tabIndex == index) ComposeAppTheme.colors.oz else Grey
+                                        )
+                                    }
+                                })
+                        }
                     }
-                ) {
-                    marketViewModel.tabs.forEachIndexed { index, tab ->
-                        Tab(
-                            selected = tabIndex == index,
-                            onClick = {
-                                tabIndex = index
-                                marketViewModel.onSelect(index)
-                            },
-                            text = {
-                                ProvideTextStyle(
-                                    ComposeAppTheme.typography.subhead1.copy(
-                                        textAlign = TextAlign.Center
-                                    )
-                                ) {
-                                    Text(
-                                        text = tab.name,
-                                        color = if (tabIndex == index) ComposeAppTheme.colors.oz else Grey
-                                    )
-                                }
-                            })
-                    }
+                    Divider(thickness = 1.dp, color = Steel10)
                 }
-
             }
         }
     }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/guides/GuidesFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/guides/GuidesFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
@@ -20,6 +23,7 @@ import io.horizontalsystems.bankwallet.entities.Guide
 import io.horizontalsystems.bankwallet.modules.markdown.MarkdownFragment
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.Grey
+import io.horizontalsystems.bankwallet.ui.compose.Steel10
 import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_guides.*
 import kotlinx.android.synthetic.main.fragment_guides.tabsCompose
@@ -31,7 +35,11 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener {
     private val errorAdapter = ErrorAdapter()
     private val guidesAdapter = GuidesAdapter(this)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_guides, container, false)
     }
 
@@ -54,10 +62,14 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener {
 
     override fun onItemClick(guide: Guide) {
         val arguments = bundleOf(
-                MarkdownFragment.markdownUrlKey to guide.fileUrl,
-                MarkdownFragment.handleRelativeUrlKey to true
+            MarkdownFragment.markdownUrlKey to guide.fileUrl,
+            MarkdownFragment.handleRelativeUrlKey to true
         )
-        findNavController().navigate(R.id.academyFragment_to_markdownFragment, arguments, navOptions())
+        findNavController().navigate(
+            R.id.academyFragment_to_markdownFragment,
+            arguments,
+            navOptions()
+        )
     }
 
     private fun observeLiveData() {
@@ -88,34 +100,42 @@ class GuidesFragment : BaseFragment(), GuidesAdapter.Listener {
         tabsCompose.setContent {
             var tabIndex by remember { mutableStateOf(selectedIndex) }
             ComposeAppTheme {
-                ScrollableTabRow(
-                    selectedTabIndex = tabIndex,
-                    backgroundColor = ComposeAppTheme.colors.tyler,
-                    contentColor = ComposeAppTheme.colors.tyler,
-                    indicator = @Composable { tabPositions ->
-                        TabRowDefaults.Indicator(
-                            modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
-                            color = ComposeAppTheme.colors.jacob
-                        )
-                    }
-                ) {
-                    tabs.forEachIndexed { index, tab ->
-                        Tab(
-                            selected = tabIndex == index,
-                            onClick = {
-                                tabIndex = index
-                                viewModel.onSelectFilter(tab)
-                            },
-                            text = {
-                                ProvideTextStyle(ComposeAppTheme.typography.subhead1.copy(textAlign = TextAlign.Center)) {
-                                    Text(
-                                        text = tab,
-                                        color = if (tabIndex == index) ComposeAppTheme.colors.oz else Grey
-                                    )
+                Column {
+                    ScrollableTabRow(
+                        modifier = Modifier.height(44.dp),
+                        selectedTabIndex = tabIndex,
+                        backgroundColor = ComposeAppTheme.colors.tyler,
+                        contentColor = ComposeAppTheme.colors.tyler,
+                        indicator = @Composable { tabPositions ->
+                            TabRowDefaults.Indicator(
+                                modifier = Modifier.tabIndicatorOffset(tabPositions[tabIndex]),
+                                color = ComposeAppTheme.colors.jacob
+                            )
+                        }
+                    ) {
+                        tabs.forEachIndexed { index, tab ->
+                            Tab(
+                                selected = tabIndex == index,
+                                onClick = {
+                                    tabIndex = index
+                                    viewModel.onSelectFilter(tab)
+                                },
+                                text = {
+                                    ProvideTextStyle(
+                                        ComposeAppTheme.typography.subhead1.copy(
+                                            textAlign = TextAlign.Center
+                                        )
+                                    ) {
+                                        Text(
+                                            text = tab,
+                                            color = if (tabIndex == index) ComposeAppTheme.colors.oz else Grey
+                                        )
+                                    }
                                 }
-                            }
-                        )
+                            )
+                        }
                     }
+                    Divider(thickness = 1.dp, color = Steel10)
                 }
             }
         }
