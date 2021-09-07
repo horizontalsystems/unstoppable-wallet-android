457065301c559e07d6b1ac65ade2659cfca8acde
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-09-07 16:50:45 +0600

    Replace buttons with Compose in TransactionInfoItemView

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactionInfo/TransactionInfoItemView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactionInfo/TransactionInfoItemView.kt
@@ -2,8 +2,14 @@ package io.horizontalsystems.bankwallet.modules.transactionInfo
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.widget.ConstraintLayout
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonSecondaryDefault
+import io.horizontalsystems.bankwallet.ui.compose.components.Ellipsis
 import io.horizontalsystems.views.ListPosition
 import kotlinx.android.synthetic.main.view_transaction_info_item.view.*
 
@@ -14,15 +20,27 @@ class TransactionInfoItemView : ConstraintLayout {
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
 
     fun bind(title: String, address: String, listPosition: ListPosition, onHashClick: () -> Unit) {
         txtTitle.text = title
-        decoratedText.text = address
         txViewBackground.setBackgroundResource(listPosition.getBackground())
 
-        decoratedText.setOnClickListener {
-            onHashClick.invoke()
+        decoratedTextCompose.setContent {
+            ComposeAppTheme {
+                ButtonSecondaryDefault(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    title = address,
+                    onClick = {
+                        onHashClick.invoke()
+                    },
+                    ellipsis = Ellipsis.Middle()
+                )
+            }
         }
 
         invalidate()
