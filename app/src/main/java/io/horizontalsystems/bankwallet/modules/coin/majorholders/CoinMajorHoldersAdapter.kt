cb97041eda5363f57bff7317d4545a4e75b117db
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-09-07 14:50:06 +0600

    Replace buttons with Compose in CoinMajorHoldersAdapter

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/majorholders/CoinMajorHoldersAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/majorholders/CoinMajorHoldersAdapter.kt
@@ -2,10 +2,17 @@ package io.horizontalsystems.bankwallet.modules.coin.majorholders
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.core.setOnSingleClickListener
 import io.horizontalsystems.bankwallet.modules.coin.MajorHolderItem
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonSecondaryCircle
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonSecondaryDefault
+import io.horizontalsystems.bankwallet.ui.compose.components.Ellipsis
 import io.horizontalsystems.views.inflate
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.view_holder_coin_major_holders_item.*
@@ -25,8 +32,18 @@ class CoinMajorHoldersAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
-            viewTypeItem -> ViewHolderItem(inflate(parent, R.layout.view_holder_coin_major_holders_item), listener)
-            else -> ViewHolderSectionHeader(inflate(parent, R.layout.view_holder_coin_major_holders_section_header))
+            viewTypeItem -> ViewHolderItem(
+                inflate(
+                    parent,
+                    R.layout.view_holder_coin_major_holders_item
+                ), listener
+            )
+            else -> ViewHolderSectionHeader(
+                inflate(
+                    parent,
+                    R.layout.view_holder_coin_major_holders_section_header
+                )
+            )
         }
     }
 
@@ -44,7 +61,8 @@ class CoinMajorHoldersAdapter(
         }
     }
 
-    class ViewHolderSectionHeader(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer
+    class ViewHolderSectionHeader(override val containerView: View) :
+        RecyclerView.ViewHolder(containerView), LayoutContainer
 
     class ViewHolderItem(
         override val containerView: View,
@@ -53,25 +71,33 @@ class CoinMajorHoldersAdapter(
 
         private var item: MajorHolderItem.Item? = null
 
-        init {
-            btnHolderAddress.setOnSingleClickListener {
-                item?.let {
-                    listener.onAddressClick(it.address)
-                }
-            }
-            btnDetails.setOnSingleClickListener {
-                item?.let {
-                    listener.onDetailsClick(it.address)
-                }
-            }
-        }
-
         fun bind(item: MajorHolderItem.Item) {
             this.item = item
 
             txtIndex.text = item.index.toString()
-            btnHolderAddress.text = item.address
             txtHolderRate.text = item.sharePercent
+
+            buttonsCompose.setContent {
+                ComposeAppTheme {
+                    Row() {
+                        ButtonSecondaryDefault(
+                            modifier = Modifier.padding(start = 8.dp, end = 8.dp),
+                            title = item.address,
+                            onClick = {
+                                listener.onAddressClick(item.address)
+                            },
+                            ellipsis = Ellipsis.Middle(8)
+                        )
+                        ButtonSecondaryCircle(
+                            modifier = Modifier.padding(end = 16.dp),
+                            icon = R.drawable.ic_globe_20,
+                            onClick = {
+                                listener.onDetailsClick(item.address)
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
