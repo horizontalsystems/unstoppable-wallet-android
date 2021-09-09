983843bb89eac2f42280beb73d12b36321d81a1f
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-09-09 16:54:43 +0600

    Replace rating button with compose in CoinSubtitleAdapter

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/ButtonCircle.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/ButtonCircle.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
@@ -41,6 +42,8 @@ fun ButtonSecondaryCircle(
     modifier: Modifier = Modifier,
     @DrawableRes icon: Int = R.drawable.ic_arrow_down_20,
     onClick: () -> Unit,
+    enabled: Boolean = true,
+    tint: Color = ComposeAppTheme.colors.leah,
 ) {
     val shape = CircleShape
     Box(modifier = modifier) {
@@ -49,12 +52,13 @@ fun ButtonSecondaryCircle(
             modifier = Modifier
                 .size(28.dp)
                 .clip(shape)
-                .background(ComposeAppTheme.colors.steel20)
+                .background(ComposeAppTheme.colors.steel20),
+            enabled = enabled
         ) {
             Icon(
                 painter = painterResource(id = icon),
                 contentDescription = null,
-                tint = ComposeAppTheme.colors.leah
+                tint = tint
             )
         }
     }
