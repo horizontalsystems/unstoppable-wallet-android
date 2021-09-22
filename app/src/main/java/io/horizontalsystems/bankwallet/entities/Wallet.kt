2f4d317422739f33821943510a6b2108e023c65f
bakyt <abdrasulov@gmail.com>
2021-09-22 12:23:57 +0600

    Update Wallet

--- app/src/main/java/io/horizontalsystems/bankwallet/entities/Wallet.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/entities/Wallet.kt
@@ -2,19 +2,18 @@ package io.horizontalsystems.bankwallet.entities
 
 import android.os.Parcelable
 import io.horizontalsystems.bankwallet.modules.transactions.TransactionSource
-import io.horizontalsystems.bankwallet.ui.FilterAdapter
-import io.horizontalsystems.coinkit.models.Coin
-import io.horizontalsystems.coinkit.models.CoinType
+import io.horizontalsystems.marketkit.models.CoinType
+import io.horizontalsystems.marketkit.models.PlatformCoin
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
 data class Wallet(
-    val configuredCoin: ConfiguredCoin,
+    val configuredPlatformCoin: ConfiguredPlatformCoin,
     val account: Account
-) : Parcelable, FilterAdapter.FilterItem(configuredCoin.coin.code) {
+) : Parcelable {
     private val blockchain: TransactionSource.Blockchain
-        get() = when (val type = coin.type) {
+        get() = when (val coinType = coinType) {
             CoinType.Bitcoin -> TransactionSource.Blockchain.Bitcoin
             CoinType.BitcoinCash -> TransactionSource.Blockchain.BitcoinCash
             CoinType.Dash -> TransactionSource.Blockchain.Dash
@@ -22,26 +21,43 @@ data class Wallet(
             CoinType.Ethereum -> TransactionSource.Blockchain.Ethereum
             CoinType.BinanceSmartChain -> TransactionSource.Blockchain.BinanceSmartChain
             CoinType.Zcash -> TransactionSource.Blockchain.Zcash
-            is CoinType.Bep2 -> TransactionSource.Blockchain.Bep2(type.symbol)
+            is CoinType.Bep2 -> TransactionSource.Blockchain.Bep2(coinType.symbol)
             is CoinType.Erc20 -> TransactionSource.Blockchain.Ethereum
             is CoinType.Bep20 -> TransactionSource.Blockchain.BinanceSmartChain
-            is CoinType.Unsupported -> throw IllegalArgumentException("Unsupported coin may not have transactions to show")
+            is CoinType.Sol20, is CoinType.Unsupported -> throw IllegalArgumentException("Unsupported coin may not have transactions to show")
         }
 
-    val coin get() = configuredCoin.coin
-    val transactionSource get() = TransactionSource(blockchain, account, configuredCoin.settings)
+    val platformCoin
+        get() = configuredPlatformCoin.platformCoin
 
-    constructor(coin: Coin, account: Account) : this(ConfiguredCoin(coin), account)
+    val coinSettings
+        get() = configuredPlatformCoin.coinSettings
+
+    val coin
+        get() = platformCoin.coin
+
+    val platform
+        get() = platformCoin.platform
+
+    val coinType
+        get() = platformCoin.coinType
+
+    val decimal
+        get() = platform.decimal
+
+    val transactionSource get() = TransactionSource(blockchain, account, coinSettings)
+
+    constructor(platformCoin: PlatformCoin, account: Account) : this(ConfiguredPlatformCoin(platformCoin), account)
 
     override fun equals(other: Any?): Boolean {
         if (other is Wallet) {
-            return configuredCoin == other.configuredCoin && account == other.account
+            return platformCoin == other.platformCoin && account == other.account
         }
 
         return super.equals(other)
     }
 
     override fun hashCode(): Int {
-        return Objects.hash(configuredCoin, account)
+        return Objects.hash(platformCoin, account)
     }
 }
