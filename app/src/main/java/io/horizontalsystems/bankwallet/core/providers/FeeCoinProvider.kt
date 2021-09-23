921dd3e5bdd464f3469f04259a76ecce24950750
bakyt <abdrasulov@gmail.com>
2021-09-23 15:19:56 +0600

    Update FeeCoinProvider

--- app/src/main/java/io/horizontalsystems/bankwallet/core/providers/FeeCoinProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/providers/FeeCoinProvider.kt
@@ -1,47 +1,35 @@
 package io.horizontalsystems.bankwallet.core.providers
 
-import io.horizontalsystems.coinkit.CoinKit
-import io.horizontalsystems.coinkit.models.Coin
-import io.horizontalsystems.coinkit.models.CoinType
+import io.horizontalsystems.marketkit.MarketKit
+import io.horizontalsystems.marketkit.models.CoinType
+import io.horizontalsystems.marketkit.models.PlatformCoin
 
-class FeeCoinProvider(private val coinKit: CoinKit) {
+class FeeCoinProvider(private val marketKit: MarketKit) {
 
-    fun feeCoinData(coin: Coin): Pair<Coin, String>? = when (val type = coin.type) {
+    fun feeCoinData(coinType: CoinType): Pair<PlatformCoin, String>? = when (coinType) {
         is CoinType.Erc20 -> erc20()
         is CoinType.Bep20 -> bep20()
-        is CoinType.Bep2 -> binance(type.symbol)
+        is CoinType.Bep2 -> binance(coinType.symbol)
         else -> null
     }
 
-    fun feeCoinType(coinType: CoinType): CoinType? = when (coinType) {
-        is CoinType.Erc20 -> CoinType.Ethereum
-        is CoinType.Bep20 -> CoinType.BinanceSmartChain
-        is CoinType.Bep2 -> {
-            when (coinType.symbol) {
-                "BNB" -> null
-                else -> CoinType.Bep2("BNB")
-            }
-        }
-        else -> null
-    }
-
-    private fun erc20(): Pair<Coin, String>? {
-        return coinKit.getCoin(CoinType.Ethereum)?.let {
+    private fun erc20(): Pair<PlatformCoin, String>? {
+        return marketKit.platformCoin(CoinType.Ethereum)?.let {
             Pair(it, "ERC20")
         }
     }
 
-    private fun bep20(): Pair<Coin, String>? {
-        return coinKit.getCoin(CoinType.BinanceSmartChain)?.let {
+    private fun bep20(): Pair<PlatformCoin, String>? {
+        return marketKit.platformCoin(CoinType.BinanceSmartChain)?.let {
             Pair(it, "BEP20")
         }
     }
 
-    private fun binance(symbol: String): Pair<Coin, String>? {
+    private fun binance(symbol: String): Pair<PlatformCoin, String>? {
         if (symbol == "BNB") {
             return null
         }
-        return coinKit.getCoin(CoinType.Bep2("BNB"))?.let {
+        return marketKit.platformCoin(CoinType.Bep2("BNB"))?.let {
             Pair(it, "BEP2")
         }
     }
