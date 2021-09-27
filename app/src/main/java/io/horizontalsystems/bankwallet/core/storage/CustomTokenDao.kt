3af598e2b611c12f1c4db79996b7bf00514a89b8
chyngyz <chyngyz.omurov@gmail.com>
2021-09-27 18:59:08 +0600

    Implement CustomTokenStorage methods

--- app/src/main/java/io/horizontalsystems/bankwallet/core/storage/CustomTokenDao.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/storage/CustomTokenDao.kt
@@ -1,3 +1,30 @@
 package io.horizontalsystems.bankwallet.core.storage
 
-interface CustomTokenDao
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import io.horizontalsystems.bankwallet.entities.CustomToken
+import io.horizontalsystems.marketkit.models.CoinType
+
+@Dao
+interface CustomTokenDao {
+    @Query("SELECT * FROM CustomToken")
+    fun getCustomTokens(): List<CustomToken>
+
+    @Query(
+        "SELECT * FROM CustomToken " +
+                "WHERE coinName LIKE :filter OR coinCode LIKE :filter " +
+                "ORDER BY coinName"
+    )
+    fun getCustomTokens(filter: String): List<CustomToken>
+
+    @Query("SELECT * FROM CustomToken WHERE coinType in (:coinTypeIds)")
+    fun getCustomTokens(coinTypeIds: List<String>): List<CustomToken>
+
+    @Query("SELECT * FROM CustomToken WHERE coinType = :coinType")
+    fun getCustomToken(coinType: CoinType): CustomToken?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(customTokens: List<CustomToken>)
+}
