c09e37674cec09fb5af5e9678b237604a9361fd8
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-10-04 16:55:20 +0600

    Load coin icons from web for Market Search results

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchModule.kt
@@ -3,7 +3,6 @@ package io.horizontalsystems.bankwallet.modules.market.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.marketkit.models.CoinCategory
 
 object MarketSearchModule {
 
@@ -14,8 +13,4 @@ object MarketSearchModule {
             return MarketSearchViewModel(service, listOf(service)) as T
         }
     }
-}
-
-fun CoinCategory.imageUrl(): String {
-    return "https://markets.nyc3.digitaloceanspaces.com/category-icons/ios/$uid@3x.png"
 }
