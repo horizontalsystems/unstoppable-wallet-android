a674c802822e235a879de22ff3646e81895065b3
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-10-05 12:31:13 +0600

    Replace hardcoded strings for Top Coin category

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchService.kt
@@ -1,16 +1,21 @@
 package io.horizontalsystems.bankwallet.modules.market.search
 
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.Clearable
+import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.marketkit.MarketKit
 import io.horizontalsystems.marketkit.models.CoinCategory
 import io.horizontalsystems.marketkit.models.FullCoin
 
-class MarketSearchService(private val marketKit: MarketKit) : Clearable {
+class MarketSearchService(
+    private val marketKit: MarketKit,
+    translator: Translator
+) : Clearable {
 
     private val topCoinCategory = CoinCategory(
         "top_coins",
-        "Top Coins",
-        mapOf("en" to "Top Coins Description")
+        translator.getString(R.string.Market_Category_TopCoins),
+        mapOf("en" to translator.getString(R.string.Market_Category_TopCoins_Description))
     )
 
     fun getCoinsByQuery(query: String): List<FullCoin> {
