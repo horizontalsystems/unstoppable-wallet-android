c8527841386148f4358291b8d517b610c9684cc0
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-10-11 15:10:01 +0600

    Simplify code in Market Search module

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchModule.kt
@@ -3,7 +3,6 @@ package io.horizontalsystems.bankwallet.modules.market.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.marketkit.models.CoinCategory
 
 object MarketSearchModule {
@@ -12,19 +11,12 @@ object MarketSearchModule {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             val service = MarketSearchService(App.marketKit)
-            return MarketSearchViewModel(service, Translator, listOf(service)) as T
+            return MarketSearchViewModel(service, listOf(service)) as T
         }
     }
 
-    data class ViewItem(
-        val name: String,
-        val description: String,
-        val imageUrl: String,
-        val type: ViewType
-    )
-
-    sealed class ViewType{
-        object TopCoinsType: ViewType()
-        data class CoinCategoryType(val coinCategory: CoinCategory): ViewType()
+    sealed class ViewItem{
+        object MarketTopCoins: ViewItem()
+        data class MarketCoinCategory(val coinCategory: CoinCategory): ViewItem()
     }
 }
