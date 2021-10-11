db638564bdc82ace9a506a2622a915a95879adac
bakyt <abdrasulov@gmail.com>
2021-10-11 16:09:50 +0600

    Show CoinMarketFragment as tab in CoinFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/coinmarkets/CoinMarketsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/coinmarkets/CoinMarketsFragment.kt
@@ -8,31 +8,20 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.navGraphViewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
-import io.horizontalsystems.bankwallet.modules.coin.overview.CoinOverviewViewModel
+import io.horizontalsystems.bankwallet.modules.coin.CoinViewModel
 import io.horizontalsystems.bankwallet.ui.extensions.MarketListHeaderView
-import io.horizontalsystems.core.findNavController
 import kotlinx.android.synthetic.main.fragment_coin_markets.*
 import kotlinx.android.synthetic.main.fragment_coin_markets.recyclerView
-import kotlinx.android.synthetic.main.fragment_coin_markets.toolbar
 
-class CoinMarketsFragment : BaseFragment(), MarketListHeaderView.Listener {
+class CoinMarketsFragment : BaseFragment(R.layout.fragment_coin_markets), MarketListHeaderView.Listener {
 
-    private val coinViewModel by navGraphViewModels<CoinOverviewViewModel>(R.id.coinFragment)
+    private val coinViewModel by navGraphViewModels<CoinViewModel>(R.id.coinFragment)
     private val viewModel by viewModels<CoinMarketsViewModel>{
-        CoinMarketsModule.Factory(coinViewModel.coinCode, coinViewModel.coinUid)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_coin_markets, container, false)
+        CoinMarketsModule.Factory(coinViewModel.fullCoin)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        toolbar.title = getString(R.string.CoinMarket_Title, coinViewModel.coinCode)
-        toolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
 
         marketListHeader.listener = this
 
