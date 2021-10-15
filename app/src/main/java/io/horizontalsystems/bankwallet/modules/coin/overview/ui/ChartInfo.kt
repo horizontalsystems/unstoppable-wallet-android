bb214431141f8363d099aedf31c6083642e1a468
bakyt <abdrasulov@gmail.com>
2021-10-15 16:52:27 +0600

    Handle chart updates

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/overview/ui/ChartInfo.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/overview/ui/ChartInfo.kt
@@ -1,7 +1,7 @@
 package io.horizontalsystems.bankwallet.modules.coin.overview.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import io.horizontalsystems.bankwallet.modules.coin.adapters.CoinChartAdapter
@@ -14,27 +14,18 @@ fun ChartInfo(
     currency: Currency,
     listener: CoinChartAdapter.Listener
 ) {
-    // Adds view to Compose
     AndroidView(
         modifier = Modifier.fillMaxSize(), // Occupy the max size in the Compose UI tree
         factory = { context ->
-            // Creates custom view
             CoinChartView(context).apply {
-                // Sets up listeners for View -> Compose communication
                 setCurrency(currency)
                 setChartViewType(CoinChartAdapter.ChartViewType.CoinChart)
                 setListener(listener)
-                bind(chartInfo)
+                bindNew(chartInfo)
             }
         },
         update = { view ->
-            // View's been inflated or state read in this block has been updated
-            // Add logic here if necessary
-
-            // As selectedItem is read here, AndroidView will recompose
-            // whenever the state changes
-            // Example of Compose -> View communication
-//            view.coordinator.selectedItem = selectedItem.value
+            view.bindNew(chartInfo)
         }
     )
 }
