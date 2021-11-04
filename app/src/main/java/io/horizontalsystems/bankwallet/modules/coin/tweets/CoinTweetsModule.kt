c37233f075c514637fdf6a79774872487d3bb8c5
bakyt <abdrasulov@gmail.com>
2021-11-04 17:56:57 +0600

    Add TweetViewItem for displaying in view

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/tweets/CoinTweetsModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/tweets/CoinTweetsModule.kt
@@ -2,6 +2,7 @@ package io.horizontalsystems.bankwallet.modules.coin.tweets
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.twitter.twittertext.Extractor
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.marketkit.models.FullCoin
 
@@ -12,7 +13,21 @@ object CoinTweetsModule {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             val service = CoinTweetsService(fullCoin.coin.uid, TweetsProvider("AAAAAAAAAAAAAAAAAAAAAJgeNwEAAAAA6xVpR6xLKTrxIA3kkSyRA92LDpA%3Da6auybDwcymUyh2BcS6zZwicUdxGtrzJC0qvOSdRwKLeqBGhwB"), App.marketKit)
 
-            return CoinTweetsViewModel(service) as T
+            return CoinTweetsViewModel(service, Extractor()) as T
         }
     }
 }
+
+data class TweetViewItem(
+    val title: String,
+    val subtitle: String,
+    val titleImageUrl: String,
+    val text: String,
+    val attachments: List<Tweet.Attachment>,
+    val date: String,
+    val referencedTweet: ReferencedTweetViewItem?,
+    val entities: List<Extractor.Entity>,
+    val url: String,
+)
+
+data class ReferencedTweetViewItem(val title: String, val text: String)
