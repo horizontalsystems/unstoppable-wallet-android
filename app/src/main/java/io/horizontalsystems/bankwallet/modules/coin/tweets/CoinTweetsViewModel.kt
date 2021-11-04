0e0cf4002b9dc3a491289117d994166bb7c9c2a3
bakyt <abdrasulov@gmail.com>
2021-11-04 17:06:20 +0600

    Handle error and loading cases

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/tweets/CoinTweetsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/tweets/CoinTweetsViewModel.kt
@@ -4,20 +4,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.entities.DataState
+import io.horizontalsystems.bankwallet.modules.market.tvl.TvlModule
 import io.reactivex.disposables.CompositeDisposable
 
 class CoinTweetsViewModel(
     private val service: CoinTweetsService,
 ) : ViewModel() {
-    private val disposables = CompositeDisposable()
-
-    val itemsLiveData = MutableLiveData<DataState<List<Tweet>>>()
     val username by service::username
+
+    val isRefreshingLiveData = MutableLiveData<Boolean>(false)
+    val itemsLiveData = MutableLiveData<List<Tweet>>()
+    val viewStateLiveData = MutableLiveData<TvlModule.ViewState>()
+
+    private val disposables = CompositeDisposable()
 
     init {
         service.stateObservable
-            .subscribeIO {
-                itemsLiveData.postValue(it)
+            .subscribeIO { state ->
+                isRefreshingLiveData.postValue(state == DataState.Loading)
+
+                state.dataOrNull?.let {
+                    itemsLiveData.postValue(it)
+                }
+
+                state.viewState?.let {
+                    viewStateLiveData.postValue(it)
+                }
+
             }
             .let {
                 disposables.add(it)
@@ -26,8 +39,16 @@ class CoinTweetsViewModel(
         service.start()
     }
 
+    val DataState<*>.viewState: TvlModule.ViewState?
+        get() = when (this) {
+            is DataState.Error -> TvlModule.ViewState.Error
+            is DataState.Success -> TvlModule.ViewState.Success
+            else -> null
+        }
+
     fun refresh() {
-        service.start()
+        isRefreshingLiveData.postValue(true)
+        service.refresh()
     }
 
     override fun onCleared() {
