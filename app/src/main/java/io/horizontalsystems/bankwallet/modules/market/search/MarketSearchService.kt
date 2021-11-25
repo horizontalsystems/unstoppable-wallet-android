f7c870cb780e4c94435f980713c6703fc01587a0
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-11-25 16:38:11 +0600

    Refactor Search module

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/search/MarketSearchService.kt
@@ -1,18 +1,58 @@
 package io.horizontalsystems.bankwallet.modules.market.search
 
 import io.horizontalsystems.bankwallet.core.managers.MarketFavoritesManager
+import io.horizontalsystems.bankwallet.core.subscribeIO
+import io.horizontalsystems.bankwallet.modules.market.search.MarketSearchModule.CoinItem
+import io.horizontalsystems.bankwallet.modules.market.search.MarketSearchModule.DataState
+import io.horizontalsystems.bankwallet.modules.market.search.MarketSearchModule.DataState.Discovery
+import io.horizontalsystems.bankwallet.modules.market.search.MarketSearchModule.DataState.SearchResult
+import io.horizontalsystems.bankwallet.modules.market.search.MarketSearchModule.DiscoveryItem.Category
+import io.horizontalsystems.bankwallet.modules.market.search.MarketSearchModule.DiscoveryItem.TopCoins
 import io.horizontalsystems.marketkit.MarketKit
-import io.horizontalsystems.marketkit.models.FullCoin
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
 
 class MarketSearchService(
     private val marketKit: MarketKit,
     private val marketFavoritesManager: MarketFavoritesManager,
 ) {
+    private val disposables = CompositeDisposable()
+    private var filter = ""
 
-    val favoritedCoinUids = marketFavoritesManager.getAll().map { it.coinUid }
+    private val discoveryItems by lazy {
+        val discoveryItems: MutableList<MarketSearchModule.DiscoveryItem> = mutableListOf(TopCoins)
 
-    fun getCoinsByQuery(query: String): List<FullCoin> {
-        return marketKit.fullCoins(query)
+        marketKit.coinCategories().forEach {
+            discoveryItems.add(Category(it))
+        }
+
+        discoveryItems
+    }
+
+    val stateObservable: BehaviorSubject<DataState> =
+        BehaviorSubject.createDefault(Discovery(discoveryItems))
+
+    init {
+        marketFavoritesManager.dataUpdatedAsync
+            .subscribeIO {
+                syncState()
+            }.let {
+                disposables.add(it)
+            }
+    }
+
+    private fun syncState() {
+        if (filter.isBlank()) {
+            stateObservable.onNext(Discovery(discoveryItems))
+        } else {
+            stateObservable.onNext(SearchResult(getCoinItems(filter)))
+        }
+    }
+
+    private fun getCoinItems(filter: String): List<CoinItem> {
+        return marketKit.fullCoins(filter).map {
+            CoinItem(it, marketFavoritesManager.isCoinInFavorites(it.coin.uid))
+        }
     }
 
     fun unFavorite(coinUid: String) {
@@ -23,6 +63,13 @@ class MarketSearchService(
         marketFavoritesManager.add(coinUid)
     }
 
-    val coinCategories by lazy { marketKit.coinCategories() }
+    fun setFilter(filter: String) {
+        this.filter = filter
+        syncState()
+    }
+
+    fun stop() {
+        disposables.clear()
+    }
 
 }
