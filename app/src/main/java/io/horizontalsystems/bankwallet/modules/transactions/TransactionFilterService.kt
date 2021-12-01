f4587bbc14c00bf907c76d3bc7bb00e013ea3b14
bakyt <abdrasulov@gmail.com>
2021-12-01 15:18:06 +0600

    Remove redundant

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionFilterService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionFilterService.kt
@@ -29,41 +29,4 @@ class TransactionFilterService {
     fun getFilterTypes(): List<FilterTransactionType> {
         return FilterTransactionType.values().toList()
     }
-
-
-//    private val walletsSubject = BehaviorSubject.create<List<TransactionWallet>>()
-//    val walletsObservable: Observable<List<TransactionWallet>> = walletsSubject
-//
-//    val typesObservable: Observable<List<FilterTransactionType>> = Observable.just(FilterTransactionType.values().toList())
-//
-//    private var selectedTransactionWallet: TransactionWallet? = null
-//    private val selectedWalletSubject = BehaviorSubject.create<Optional<TransactionWallet>>()
-//    val selectedWalletObservable: Observable<Optional<TransactionWallet>> = selectedWalletSubject
-//
-//    private val selectedTransactionTypeSubject = BehaviorSubject.createDefault(FilterTransactionType.All)
-//    val selectedTransactionTypeObservable: Observable<FilterTransactionType> = selectedTransactionTypeSubject
-//
-//    fun setWallets(transactionWallets: List<TransactionWallet>) {
-//        walletsSubject.onNext(transactionWallets)
-//
-//        // Reset selectedWallet if it does not exist in the new wallets list or leave it if it does
-//        // When there is a coin added or removed then the selectedWallet should be left
-//        // When the whole wallets list is changed (switch account) it should be reset
-//        if (!transactionWallets.contains(selectedTransactionWallet)) {
-//            selectedTransactionWallet = null
-//            selectedWalletSubject.onNext(Optional.ofNullable(selectedTransactionWallet))
-//
-//            selectedTransactionTypeSubject.onNext(FilterTransactionType.All)
-//        }
-//    }
-//
-//    fun setFilterWallet(transactionWallet: TransactionWallet?) {
-//        selectedTransactionWallet = transactionWallet
-//        selectedWalletSubject.onNext(Optional.ofNullable(selectedTransactionWallet))
-//    }
-//
-//    fun setFilterType(transactionType: FilterTransactionType) {
-//        selectedTransactionTypeSubject.onNext(transactionType)
-//    }
-
 }
