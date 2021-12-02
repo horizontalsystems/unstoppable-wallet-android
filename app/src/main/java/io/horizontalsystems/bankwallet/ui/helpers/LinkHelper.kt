dfebce964837e4ed39e9cdd013d4ed8b205856a2
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-12-02 14:46:37 +0600

    Handle malformed urls when opening them

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/helpers/LinkHelper.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/helpers/LinkHelper.kt
@@ -5,9 +5,13 @@ import android.net.Uri
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import io.horizontalsystems.bankwallet.R
+import java.net.MalformedURLException
+import java.net.URL
 
 object LinkHelper {
-    fun openLinkInAppBrowser(context: Context, link: String){
+    fun openLinkInAppBrowser(context: Context, link: String) {
+        val urlString = getValidUrl(link) ?: return
+
         val builder = CustomTabsIntent.Builder()
 
         val color = context.getColor(R.color.tyler)
@@ -27,6 +31,23 @@ object LinkHelper {
         )
 
         val intent = builder.build()
-        intent.launchUrl(context, Uri.parse(link))
+        intent.launchUrl(context, Uri.parse(urlString))
+    }
+
+    private fun getValidUrl(urlString: String): String? {
+        if (urlString.isBlank())
+            return null
+
+        val url = createUrl(urlString) ?: createUrl(urlString, "https://") ?: return null
+
+        return url.toString()
+    }
+
+    private fun createUrl(urlString: String, protocol: String = ""): URL? {
+        return try {
+            URL("$protocol$urlString")
+        } catch (e: MalformedURLException) {
+            null
+        }
     }
 }
