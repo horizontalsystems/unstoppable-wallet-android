172c44755d974433f078ac79af21dd35f9c91d74
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-01-13 12:36:55 +0600

    Convert to compose EnableCoinsDialog

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/enablecoins/EnableCoinsDialog.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/enablecoins/EnableCoinsDialog.kt
@@ -1,61 +1,90 @@
 package io.horizontalsystems.bankwallet.modules.enablecoins
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentActivity
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
-import io.horizontalsystems.bankwallet.ui.extensions.BaseBottomSheetDialogFragment
-import kotlinx.android.synthetic.main.fragment_enable_coins.*
+import io.horizontalsystems.bankwallet.ui.compose.components.TextImportant
+import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
+import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
 
-class EnableCoinsDialog(private val listener: Listener, private val tokenType: String) : BaseBottomSheetDialogFragment() {
+class EnableCoinsDialog(
+    private val listener: Listener,
+    private val tokenType: String
+) : BaseComposableBottomSheetFragment() {
 
     interface Listener {
         fun onClickEnable()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setContentView(R.layout.fragment_enable_coins)
-
-        setTitle(activity?.getString(R.string.EnalbeToken_Title))
-        setSubtitle(tokenType)
-        alertText.text = getString(R.string.EnalbeToken_Description, tokenType)
-
-        val icon = getIcon(tokenType)
-
-        setHeaderIcon(icon)
-
-        buttonEnableCompose.setViewCompositionStrategy(
-            ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
-        )
-
-        buttonEnableCompose.setContent {
-            ComposeAppTheme {
-                ButtonPrimaryYellow(
-                    modifier = Modifier.padding(
-                        start = 16.dp,
-                        top = 29.dp,
-                        end = 16.dp,
-                        bottom = 16.dp
-                    ),
-                    title = getString(R.string.EnalbeToken_EnableKey),
-                    onClick = {
-                        listener.onClickEnable()
-                        dismiss()
-                    }
-                )
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+            setContent {
+                ComposeAppTheme {
+                    BottomSheetScreen()
+                }
             }
         }
     }
 
+    @Composable
+    private fun BottomSheetScreen() {
+        BottomSheetHeader(
+            iconPainter = painterResource(getIcon(tokenType)),
+            title = stringResource(R.string.EnalbeToken_Title),
+            subtitle = tokenType,
+            onCloseClick = { close() }
+        ) {
+            Divider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = ComposeAppTheme.colors.steel10
+            )
+            Box(
+                modifier = Modifier.padding(horizontal = 21.dp, vertical = 12.dp)
+            ) {
+                TextImportant(stringResource(R.string.EnalbeToken_Description, tokenType))
+            }
+            Divider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = ComposeAppTheme.colors.steel10
+            )
+            ButtonPrimaryYellow(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                title = getString(R.string.EnalbeToken_EnableKey),
+                onClick = {
+                    listener.onClickEnable()
+                    dismiss()
+                }
+            )
+        }
+    }
+
     private fun getIcon(tokenType: String): Int {
-        return when(tokenType){
+        return when (tokenType) {
             "BEP2" -> R.drawable.bep2
             "BEP20" -> R.drawable.bep20
             else -> R.drawable.erc20
