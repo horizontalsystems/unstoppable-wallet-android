1e5f4589eebf654492d04f53ac260dfa81c31b9d
bakyt <abdrasulov@gmail.com>
2022-01-13 17:41:35 +0600

    Run auto-enabling tokens for new txs

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/AutoEnableTokensService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/AutoEnableTokensService.kt
@@ -5,6 +5,7 @@ import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.entities.EvmAccountState
 import io.horizontalsystems.bankwallet.modules.enablecoins.EnableCoinsEip20Provider
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 class AutoEnableTokensService(
     private val kitManager: EvmKitManager,
@@ -13,12 +14,26 @@ class AutoEnableTokensService(
     private val evmAccountStateDao: EvmAccountStateDao
 ) {
     private val disposables = CompositeDisposable()
+    private var transactionsDisposable: Disposable? = null
 
     fun start() {
         kitManager.kitStartedObservable
             .subscribeIO { started ->
                 if (started) {
                     enableTokensWithTx()
+
+                    transactionsDisposable?.dispose()
+                    transactionsDisposable = kitManager.evmKitWrapper?.evmKit
+                        ?.getTransactionsFlowable(listOf())
+                        ?.subscribeIO {
+                            val lastBlockNumber = it.maxOf {
+                                it.receiptWithLogs?.receipt?.blockNumber ?: 0
+                            }
+
+                            enableTokensWithTx(lastBlockNumber)
+                        }
+                } else {
+                    transactionsDisposable?.dispose()
                 }
             }
             .let {
@@ -26,25 +41,24 @@ class AutoEnableTokensService(
             }
     }
 
-    private fun enableTokensWithTx() {
+    private fun enableTokensWithTx(lastBlockNumber: Long = 0) {
         val account = kitManager.currentAccount ?: return
         val evmKit = kitManager.evmKitWrapper?.evmKit ?: return
 
-        val address = evmKit.receiveAddress.hex
         val chainId = evmKit.networkType.chainId
+        val evmAccountState = evmAccountStateDao.get(account.id, chainId) ?: EvmAccountState(account.id, chainId, -1)
 
-        val evmAccountState = evmAccountStateDao.get(account.id, chainId) ?: EvmAccountState(account.id, chainId, 0)
+        if (evmAccountState.transactionsSyncedBlockNumber >= lastBlockNumber) return
 
-        enableCoinsEip20Provider.getCoinTypesAsync(address, evmAccountState.transactionsSyncedBlockNumber)
-            .subscribeIO { (coinTypes, lastTxBlockNumber) ->
+        val address = evmKit.receiveAddress.hex
+        enableCoinsEip20Provider.getCoinTypesAsync(address, evmAccountState.transactionsSyncedBlockNumber + 1)
+            .subscribeIO { (coinTypes, lastBlockNumber) ->
                 val notEnabled = coinTypes.filter { !walletActivator.isEnabled(account, it) }
                 if (notEnabled.isNotEmpty()) {
                     walletActivator.activateWallets(account, notEnabled)
                 }
 
-                lastTxBlockNumber?.let {
-                    evmAccountStateDao.insert(evmAccountState.copy(transactionsSyncedBlockNumber = it))
-                }
+                evmAccountStateDao.insert(evmAccountState.copy(transactionsSyncedBlockNumber = lastBlockNumber))
             }
             .let {
                 disposables.add(it)
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/enablecoins/EnableCoinsEip20Provider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/enablecoins/EnableCoinsEip20Provider.kt
@@ -3,12 +3,14 @@ package io.horizontalsystems.bankwallet.modules.enablecoins
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import io.horizontalsystems.bankwallet.core.INetworkManager
+import io.horizontalsystems.ethereumkit.core.hexStringToLongOrNull
 import io.horizontalsystems.marketkit.models.CoinType
 import io.reactivex.Single
 
 class EnableCoinsEip20Provider(
     private val networkManager: INetworkManager,
-    private val mode: EnableCoinMode
+    private val mode: EnableCoinMode,
+    private val apiKey: String
 ) {
 
     private val url: String
@@ -44,23 +46,30 @@ class EnableCoinsEip20Provider(
                 }
     }
 
-    fun getCoinTypesAsync(address: String, startBlock: Long = 0): Single<Pair<List<CoinType>, Long?>> {
-        val params = "api?module=account&action=tokentx&sort=asc&address=$address&startBlock=$startBlock"
+    fun getCoinTypesAsync(address: String, startBlock: Long = 0): Single<Pair<List<CoinType>, Long>> {
+        val paramsTxs = "api?module=account&action=tokentx&sort=asc&address=$address&startBlock=$startBlock&apikey=$apiKey"
         val gson = Gson()
 
-        return networkManager.getEvmInfo(url, params)
-                .map { jsonObject ->
-                    if (jsonObject.get("status").asString == "1" && jsonObject.has("result")) {
-                        val type = object : TypeToken<ArrayList<Transaction>>() {}.type
-                        val transactions = gson.fromJson<ArrayList<Transaction>>(jsonObject.get("result"), type).distinct()
-                        val coinTypes = coinTypes(transactions.map { it.contractAddress }.distinct())
-                        val lastTxBlockNumber = transactions.lastOrNull()?.blockNumber
-
-                        Pair(coinTypes, lastTxBlockNumber)
-                    } else {
-                        Pair(listOf(), null)
-                    }
+        val coinTypesSingle = networkManager.getEvmInfo(url, paramsTxs)
+            .map { jsonObject ->
+                if (jsonObject.get("status").asString == "1" && jsonObject.has("result")) {
+                    val type = object : TypeToken<ArrayList<Transaction>>() {}.type
+                    val transactions = gson.fromJson<ArrayList<Transaction>>(jsonObject.get("result"), type).distinct()
+                    coinTypes(transactions.map { it.contractAddress }.distinct())
+                } else {
+                    listOf()
                 }
+            }
+
+        val paramsBlockNumber = "api?module=proxy&action=eth_blockNumber&apikey=$apiKey"
+        val blockNumberSingle = networkManager.getEvmInfo(url, paramsBlockNumber)
+            .map {
+                it.get("result").asString.hexStringToLongOrNull() ?: 0
+            }
+
+        return blockNumberSingle.zipWith(coinTypesSingle, { blockNumber, coinTypes ->
+            Pair(coinTypes, blockNumber)
+        })
     }
 
     enum class EnableCoinMode {
