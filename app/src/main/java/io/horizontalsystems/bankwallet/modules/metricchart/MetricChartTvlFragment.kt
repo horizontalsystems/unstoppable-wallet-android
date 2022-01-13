5d41ccd8ad85683de520ac851fed0d2968f25ae7
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-01-13 14:23:51 +0600

    Convert to compose MetricChartTvlFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/MetricChartTvlFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/MetricChartTvlFragment.kt
@@ -1,50 +1,57 @@
 package io.horizontalsystems.bankwallet.modules.metricchart
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.core.os.bundleOf
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.chart.ChartViewModel
-import io.horizontalsystems.bankwallet.ui.extensions.BaseBottomSheetDialogFragment
-import kotlinx.android.synthetic.main.fragment_market_global.*
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
+import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
 
-class MetricChartTvlFragment : BaseBottomSheetDialogFragment() {
+class MetricChartTvlFragment : BaseComposableBottomSheetFragment() {
 
     private val coinUid by lazy {
         requireArguments().getString(coinUidKey) ?: ""
-    }
-
-    private val coinName by lazy {
-        requireArguments().getString(coinNameKey) ?: ""
     }
 
     private val chartViewModel by viewModels<ChartViewModel> {
         MetricChartTvlModule.Factory(coinUid)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        setContentView(R.layout.fragment_market_global)
-
-        setTitle(getString(R.string.CoinPage_Tvl))
-        setSubtitle(getString(R.string.MarketGlobalMetrics_Chart))
-        setHeaderIcon(R.drawable.ic_chart_24)
-
-        composeView.setViewCompositionStrategy(
-            ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
-        )
-
-        composeView.setContent {
-            MetricChartScreen(
-                chartViewModel = chartViewModel,
-                description = stringResource(R.string.CoinPage_TvlDescription),
-                poweredBy = stringResource(R.string.Market_PoweredByDefiLlamaApi)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
+            setContent {
+                ComposeAppTheme {
+                    BottomSheetHeader(
+                        iconPainter = painterResource(R.drawable.ic_chart_24),
+                        title = getString(R.string.CoinPage_Tvl),
+                        subtitle = getString(R.string.MarketGlobalMetrics_Chart),
+                        onCloseClick = { close() }
+                    ) {
+                        MetricChartScreen(
+                            chartViewModel = chartViewModel,
+                            description = stringResource(R.string.CoinPage_TvlDescription),
+                            poweredBy = stringResource(R.string.Market_PoweredByDefiLlamaApi)
+                        )
+                    }
+                }
+            }
         }
     }
 
