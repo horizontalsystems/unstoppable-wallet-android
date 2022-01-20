033e84d08ef28ff317eef4e3698192036d4a2e3f
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-01-20 15:36:24 +0600

    Update custom Compose checkbox component

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/HsCheckbox.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/HsCheckbox.kt
@@ -1,8 +1,13 @@
 package io.horizontalsystems.bankwallet.ui.compose.components
 
-import androidx.compose.material.Checkbox
-import androidx.compose.material.CheckboxDefaults
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 
 @Composable
@@ -10,13 +15,21 @@ fun HsCheckbox(
     checked: Boolean,
     onCheckedChange: ((Boolean) -> Unit)?,
 ) {
-    Checkbox(
-        checked = checked,
-        onCheckedChange = onCheckedChange,
-        colors = CheckboxDefaults.colors(
-            checkedColor = ComposeAppTheme.colors.jacob,
-            uncheckedColor = ComposeAppTheme.colors.grey,
-            checkmarkColor = ComposeAppTheme.colors.lawrence
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.clickable { onCheckedChange?.invoke(!checked) }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_checkbox_frame),
+            contentDescription = null,
+            tint = ComposeAppTheme.colors.grey
         )
-    )
+        if (checked) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_checkbox_check),
+                contentDescription = null,
+                tint = ComposeAppTheme.colors.jacob
+            )
+        }
+    }
 }
