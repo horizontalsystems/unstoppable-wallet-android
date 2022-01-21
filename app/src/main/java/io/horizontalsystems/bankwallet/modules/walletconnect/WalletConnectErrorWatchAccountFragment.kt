5722ee6612533aa30c4c844178ac5705c50b070e
bakyt <abdrasulov@gmail.com>
2022-01-21 15:19:04 +0600

    Open manage accounts on switch button click

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/WalletConnectErrorWatchAccountFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/WalletConnectErrorWatchAccountFragment.kt
@@ -2,6 +2,7 @@ package io.horizontalsystems.bankwallet.modules.walletconnect
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,33 +14,31 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.slideFromRight
+import io.horizontalsystems.bankwallet.modules.manageaccounts.ManageAccountsModule
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.bankwallet.ui.compose.components.TextImportant
 import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
 import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
+import io.horizontalsystems.core.findNavController
 
 class WalletConnectErrorWatchAccountFragment : BaseComposableBottomSheetFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): ComposeView {
+    ): View {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
             setContent {
                 ComposeAppTheme {
-                    WalletConnectErrorWatchAccount(
-                        onClickClose = {
-                            close()
-                        },
-                        onClickSwitch = {
-                            close()
-                        }
-                    )
+                    WalletConnectErrorWatchAccount(findNavController())
                 }
             }
         }
@@ -47,16 +46,15 @@ class WalletConnectErrorWatchAccountFragment : BaseComposableBottomSheetFragment
 }
 
 @Composable
-fun WalletConnectErrorWatchAccount(
-    onClickClose: () -> Unit,
-    onClickSwitch: () -> Unit
-) {
+fun WalletConnectErrorWatchAccount(navController: NavController) {
     BottomSheetHeader(
         iconPainter = painterResource(R.drawable.ic_wallet_connect_24),
         iconTint = ColorFilter.tint(ComposeAppTheme.colors.jacob),
         title = stringResource(R.string.WalletConnect_Title),
         subtitle = stringResource(R.string.WalletConnect_Alert),
-        onCloseClick = onClickClose
+        onCloseClick = {
+            navController.popBackStack()
+        }
     ) {
         TextImportant(
             modifier = Modifier.padding(vertical = 12.dp, horizontal = 22.dp),
@@ -67,7 +65,13 @@ fun WalletConnectErrorWatchAccount(
                 .padding(vertical = 16.dp, horizontal = 22.dp)
                 .fillMaxWidth(),
             title = stringResource(R.string.Button_Switch),
-            onClick = onClickSwitch
+            onClick = {
+                navController.popBackStack()
+                navController.slideFromRight(
+                    R.id.manageAccountsFragment,
+                    bundleOf(ManageAccountsModule.MODE to ManageAccountsModule.Mode.Manage)
+                )
+            }
         )
     }
 }
