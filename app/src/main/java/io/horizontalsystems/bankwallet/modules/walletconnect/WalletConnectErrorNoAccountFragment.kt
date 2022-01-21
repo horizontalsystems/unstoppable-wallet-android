a75baf6ec08dd1a687eef5e863667b98d2ad5233
bakyt <abdrasulov@gmail.com>
2022-01-21 14:47:10 +0600

    Make navigation via navController

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/WalletConnectErrorNoAccountFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/WalletConnectErrorNoAccountFragment.kt
@@ -2,6 +2,7 @@ package io.horizontalsystems.bankwallet.modules.walletconnect
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,33 +14,28 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.bankwallet.ui.compose.components.TextImportant
 import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
 import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
+import io.horizontalsystems.core.findNavController
 
 class WalletConnectErrorNoAccountFragment : BaseComposableBottomSheetFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): ComposeView {
+    ): View {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
             setContent {
                 ComposeAppTheme {
-                    WalletConnectErrorNoAccount(
-                        onClickClose = {
-                            close()
-                        },
-                        onClickUnderstand = {
-                            close()
-                        }
-                    )
+                    WalletConnectErrorNoAccount(findNavController())
                 }
             }
         }
@@ -47,16 +43,15 @@ class WalletConnectErrorNoAccountFragment : BaseComposableBottomSheetFragment() 
 }
 
 @Composable
-fun WalletConnectErrorNoAccount(
-    onClickClose: () -> Unit,
-    onClickUnderstand: () -> Unit
-) {
+fun WalletConnectErrorNoAccount(navController: NavController) {
     BottomSheetHeader(
         iconPainter = painterResource(R.drawable.ic_wallet_connect_24),
         iconTint = ColorFilter.tint(ComposeAppTheme.colors.jacob),
         title = stringResource(R.string.WalletConnect_Title),
         subtitle = stringResource(R.string.WalletConnect_Alert),
-        onCloseClick = onClickClose
+        onCloseClick = {
+            navController.popBackStack()
+        }
     ) {
         TextImportant(
             modifier = Modifier.padding(vertical = 12.dp, horizontal = 22.dp),
@@ -67,7 +62,9 @@ fun WalletConnectErrorNoAccount(
                 .padding(vertical = 16.dp, horizontal = 22.dp)
                 .fillMaxWidth(),
             title = stringResource(R.string.Button_Understand),
-            onClick = onClickUnderstand
+            onClick = {
+                navController.popBackStack()
+            }
         )
     }
 }
