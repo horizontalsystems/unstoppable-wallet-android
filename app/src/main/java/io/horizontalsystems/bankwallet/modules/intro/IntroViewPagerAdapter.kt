1fb27f6cbd5cf125a6bb0fb30eab1da9819d5846
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-01-25 16:49:11 +0600

    Change image and text animation in Intro Slider

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroViewPagerAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroViewPagerAdapter.kt
@@ -7,9 +7,9 @@ import io.horizontalsystems.bankwallet.R
 
 class IntroViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     private val fragments = listOf<Fragment>(
-            IntroSlideFragment.newInstance(R.string.Intro_Wallet_Screen2Title, R.string.Intro_Wallet_Screen2Description),
-            IntroSlideFragment.newInstance(R.string.Intro_Wallet_Screen3Title, R.string.Intro_Wallet_Screen3Description),
-            IntroSlideFragment.newInstance(R.string.Intro_Wallet_Screen4Title, R.string.Intro_Wallet_Screen4Description)
+            IntroSlideFragment.newInstance(R.drawable.ic_independence),
+            IntroSlideFragment.newInstance(R.drawable.ic_knowledge),
+            IntroSlideFragment.newInstance(R.drawable.ic_privacy),
     )
 
     override fun getCount(): Int = fragments.size
