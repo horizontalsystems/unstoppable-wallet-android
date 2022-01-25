86fa4d3b2ee222d13c5b8267c7bb2c7e65f3abab
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-01-25 17:40:26 +0600

    Add intro images for light mode

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroViewPagerAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/intro/IntroViewPagerAdapter.kt
@@ -1,15 +1,25 @@
 package io.horizontalsystems.bankwallet.modules.intro
 
+import android.content.res.Configuration
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.App
 
-class IntroViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class IntroViewPagerAdapter(fragmentManager: FragmentManager) :
+    FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val nightMode by lazy {
+        val uiMode =
+            App.instance.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)
+        uiMode == Configuration.UI_MODE_NIGHT_YES
+    }
+
     private val fragments = listOf<Fragment>(
-            IntroSlideFragment.newInstance(R.drawable.ic_independence),
-            IntroSlideFragment.newInstance(R.drawable.ic_knowledge),
-            IntroSlideFragment.newInstance(R.drawable.ic_privacy),
+        IntroSlideFragment.newInstance(if (nightMode) R.drawable.ic_independence else R.drawable.ic_independence_light),
+        IntroSlideFragment.newInstance(if (nightMode) R.drawable.ic_knowledge else R.drawable.ic_knowledge_light),
+        IntroSlideFragment.newInstance(if (nightMode) R.drawable.ic_privacy else R.drawable.ic_privacy_light),
     )
 
     override fun getCount(): Int = fragments.size
