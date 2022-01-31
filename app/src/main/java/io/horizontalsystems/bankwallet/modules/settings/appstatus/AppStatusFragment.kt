4189c645bb78cb0a09444460072306f197a278ef
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-01-31 18:13:03 +0600

    Convert AppStatusFragment to Composable

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/appstatus/AppStatusFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/appstatus/AppStatusFragment.kt
@@ -4,108 +4,100 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
-import io.horizontalsystems.bankwallet.databinding.FragmentAppStatusBinding
-import io.horizontalsystems.core.findNavController
-import io.horizontalsystems.core.helpers.DateHelper
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
+import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
+import io.horizontalsystems.bankwallet.ui.compose.components.MenuItem
 import io.horizontalsystems.core.helpers.HudHelper
-import java.util.*
 
 class AppStatusFragment : BaseFragment() {
-
-    private val presenter by viewModels<AppStatusPresenter> { AppStatusModule.Factory() }
-
-    private var _binding: FragmentAppStatusBinding? = null
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAppStatusBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.toolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
-        binding.toolbar.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                R.id.menuCopy -> {
-                    presenter.didTapCopy(binding.textAppStatus.text.toString())
-                    true
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+            setContent {
+                ComposeAppTheme {
+                    AppStatusScreen(findNavController())
                 }
-                else -> false
             }
         }
-
-        observeView(presenter.view as AppStatusView)
-
-        presenter.viewDidLoad()
     }
+}
 
-    private fun observeView(view: AppStatusView) {
-        view.appStatusLiveData.observe(viewLifecycleOwner, Observer { appStatusMap ->
-            binding.textAppStatus.text = formatMapToString(appStatusMap)
-        })
+@Composable
+private fun AppStatusScreen(navController: NavController) {
+    val viewModel = viewModel<AppStatusViewModel>(factory = AppStatusModule.Factory())
+    val appStatusText = viewModel.appStatus
+    val clipboardManager = LocalClipboardManager.current
+    val localView = LocalView.current
 
-        view.showCopiedLiveEvent.observe(viewLifecycleOwner, Observer {
-            activity?.let {
-                HudHelper.showSuccessMessage(
-                    it.findViewById(android.R.id.content),
-                    R.string.Hud_Text_Copied
-                )
-            }
-        })
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun formatMapToString(
-        status: Map<String, Any>?,
-        indentation: String = "",
-        bullet: String = "",
-        level: Int = 0
-    ): String? {
-        if (status == null)
-            return null
-
-        val sb = StringBuilder()
-        status.toList().forEach { (key, value) ->
-            val title = "$indentation$bullet$key"
-            when (value) {
-                is Date -> {
-                    val date = DateHelper.formatDate(value, "MMM d, yyyy, HH:mm")
-                    sb.appendLine("$title: $date")
-                }
-                is Map<*, *> -> {
-                    val formattedValue = formatMapToString(
-                        value as? Map<String, Any>,
-                        "\t\t$indentation",
-                        " - ",
-                        level + 1
+    ComposeAppTheme {
+        Column {
+            AppBar(
+                TranslatableString.ResString(R.string.Settings_AppStatus),
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_back),
+                            tint = ComposeAppTheme.colors.jacob,
+                            contentDescription = null,
+                        )
+                    }
+                },
+                menuItems = listOf(
+                    MenuItem(
+                        title = TranslatableString.ResString(R.string.Alert_Copy),
+                        onClick = {
+                            appStatusText?.let { clipboardManager.setText(AnnotatedString(it)) }
+                            HudHelper.showSuccessMessage(localView, R.string.Hud_Text_Copied)
+                        },
                     )
-                    sb.append("$title:\n$formattedValue${if (level < 2) "\n" else ""}")
-                }
-                else -> {
-                    sb.appendLine("$title: $value")
+                )
+            )
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .background(color = ComposeAppTheme.colors.tyler)
+                    .fillMaxSize()
+            ) {
+                appStatusText?.let {
+                    Text(
+                        text = it,
+                        style = ComposeAppTheme.typography.subhead2,
+                        color = ComposeAppTheme.colors.grey,
+                        modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp)
+                    )
                 }
             }
         }
-
-        val statusString = sb.trimEnd()
-
-        return if (statusString.isEmpty()) "" else "$statusString\n"
     }
 }
