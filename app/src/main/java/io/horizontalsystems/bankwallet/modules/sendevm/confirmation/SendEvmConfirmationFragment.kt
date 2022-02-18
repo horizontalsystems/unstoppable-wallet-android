826774bd3e8ce20fdf52879ed4550e2cf174dfdd
chyngyz <chyngyz.omurov@gmail.com>
2022-02-18 10:50:40 +0600

    Rename SendEvmFeeSettingsFragment to EvmFeeSettingsFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/sendevm/confirmation/SendEvmConfirmationFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/sendevm/confirmation/SendEvmConfirmationFragment.kt
@@ -16,14 +16,12 @@ import androidx.navigation.navGraphViewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.AppLogger
 import io.horizontalsystems.bankwallet.core.BaseFragment
-import io.horizontalsystems.bankwallet.modules.evmfee.EvmFeeCellViewModel
-import io.horizontalsystems.bankwallet.core.slideFromRight
 import io.horizontalsystems.bankwallet.databinding.FragmentConfirmationSendEvmBinding
+import io.horizontalsystems.bankwallet.modules.evmfee.EvmFeeCellViewModel
 import io.horizontalsystems.bankwallet.modules.sendevm.SendEvmData
 import io.horizontalsystems.bankwallet.modules.sendevm.SendEvmModule
 import io.horizontalsystems.bankwallet.modules.sendevm.SendEvmViewModel
 import io.horizontalsystems.bankwallet.modules.sendevmtransaction.SendEvmTransactionViewModel
-import io.horizontalsystems.bankwallet.modules.evmfee.SendEvmFeeSettingsFragment
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.core.findNavController
