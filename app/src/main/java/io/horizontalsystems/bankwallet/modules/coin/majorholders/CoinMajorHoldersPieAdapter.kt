0f264381ab163d290742764682d083876d690ab9
chyngyz <chyngyz.omurov@gmail.com>
2022-02-24 13:03:52 +0600

    Implement SemiCircleView using Compose
    - apply new design
    - fix paddings

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/majorholders/CoinMajorHoldersPieAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/majorholders/CoinMajorHoldersPieAdapter.kt
@@ -2,10 +2,23 @@ package io.horizontalsystems.bankwallet.modules.coin.majorholders
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.bankwallet.databinding.ViewHolderCoinMajorHoldersPieBinding
+import io.horizontalsystems.bankwallet.databinding.ViewHolderCoinMajorHoldersPieComposeBinding
 import io.horizontalsystems.bankwallet.modules.coin.MajorHolderItem
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.SemiCircleChart
 import kotlin.math.min
 
 class CoinMajorHoldersPieAdapter(private val holders: List<MajorHolderItem.Item>) :
@@ -13,7 +26,7 @@ class CoinMajorHoldersPieAdapter(private val holders: List<MajorHolderItem.Item>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
         return ViewHolderItem(
-            ViewHolderCoinMajorHoldersPieBinding.inflate(
+            ViewHolderCoinMajorHoldersPieComposeBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
             )
         )
@@ -26,14 +39,49 @@ class CoinMajorHoldersPieAdapter(private val holders: List<MajorHolderItem.Item>
     }
 }
 
-class ViewHolderItem(private val binding: ViewHolderCoinMajorHoldersPieBinding) :
+class ViewHolderItem(private val binding: ViewHolderCoinMajorHoldersPieComposeBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(holders: List<MajorHolderItem.Item>) {
         val portionMajor = min(holders.sumOf { it.share }.toFloat(), 100f)
         val portionRest = 100 - portionMajor
 
-        binding.majorHolderView.setProportion(portionMajor, portionRest)
-        binding.majorHolderPercent.text =
-            App.numberFormatter.format(portionMajor, 0, 2, suffix = "%")
+        binding.root.apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+            )
+            setContent {
+                ComposeAppTheme {
+                    Column(modifier = Modifier.padding(top = 24.dp)) {
+                        SemiCircleChart(
+                            modifier = Modifier.padding(horizontal = 32.dp),
+                            percentValues = listOf(portionMajor, portionRest),
+                            title = App.numberFormatter.format(portionMajor, 0, 2, suffix = "%")
+                        )
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 32.dp, end = 32.dp, top = 12.dp),
+                            textAlign = TextAlign.Center,
+                            overflow = TextOverflow.Ellipsis,
+                            text = stringResource(R.string.CoinPage_MajorHolders_InTopWallets),
+                            color = ComposeAppTheme.colors.grey,
+                            style = ComposeAppTheme.typography.subhead1
+                        )
+
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 24.dp, end = 24.dp, top = 38.dp),
+                            textAlign = TextAlign.Start,
+                            overflow = TextOverflow.Ellipsis,
+                            text = stringResource(R.string.CoinPage_MajorHolders_Description),
+                            color = ComposeAppTheme.colors.grey,
+                            style = ComposeAppTheme.typography.subhead2
+                        )
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/SemiCircleChart.kt
@@ -0,0 +1,109 @@
+package io.horizontalsystems.bankwallet.ui.compose.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+
+@Composable
+fun SemiCircleChart(
+    modifier: Modifier = Modifier,
+    percentValues: List<Float>,
+    title: String
+) {
+    val paintColor = ComposeAppTheme.colors.yellowD
+    var startAngle = 180F
+    val colorParts = 255 / percentValues.size
+
+    val proportions = percentValues.mapIndexed { index, item ->
+        val sweepAngle = item / 100 * 180F
+        val colorAlpha = (255 - index * colorParts) / 255f
+        val proportion = Triple(startAngle, sweepAngle, paintColor.copy(alpha = colorAlpha))
+
+        startAngle += sweepAngle
+        proportion
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(2f)
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            val strokeWidth = size.width * 0.2f // 20% of total width of chart
+            val diameter = size.width - strokeWidth
+
+            proportions.forEach { (startAngle, sweepAngle, color) ->
+                drawArc(
+                    color = color,
+                    startAngle = startAngle,
+                    sweepAngle = sweepAngle,
+                    useCenter = false,
+                    topLeft = Offset(x = strokeWidth / 2f, y = strokeWidth / 2f),
+                    style = Stroke(width = strokeWidth),
+                    size = Size(diameter, diameter)
+                )
+            }
+        }
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 12.dp),
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis,
+            text = title,
+            color = ComposeAppTheme.colors.jacob,
+            style = ComposeAppTheme.typography.title3
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SemiCircleChartPreview() {
+    ComposeAppTheme {
+        Column(modifier = Modifier.padding(top = 24.dp)) {
+            SemiCircleChart(
+                Modifier.padding(horizontal = 32.dp),
+                listOf(33f, 33f, 34f),
+                "38.95%"
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 32.dp, end = 32.dp, top = 12.dp),
+                textAlign = TextAlign.Center,
+                overflow = TextOverflow.Ellipsis,
+                text = "Топ-10 кошельков Ethereum",
+                color = ComposeAppTheme.colors.grey,
+                style = ComposeAppTheme.typography.subhead1
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, end = 24.dp, top = 38.dp),
+                textAlign = TextAlign.Start,
+                overflow = TextOverflow.Ellipsis,
+                text = "Chart above shows the % of all tokens in circulation held by top 10 wallets. The lower the % the more distributed the coin is.",
+                color = ComposeAppTheme.colors.grey,
+                style = ComposeAppTheme.typography.subhead1
+            )
+        }
+    }
+}
