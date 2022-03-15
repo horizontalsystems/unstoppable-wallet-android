3b4acc9f88fc1e4f37df638f15d863ad1ac732de
bakyt <abdrasulov@gmail.com>
2022-03-15 13:59:31 +0600

    Adjust volume bar width depending on point invervals

--- components/chartview/src/main/java/io/horizontalsystems/chartview/ChartVolume.kt
+++ components/chartview/src/main/java/io/horizontalsystems/chartview/ChartVolume.kt
@@ -6,12 +6,13 @@ import android.graphics.RectF
 import io.horizontalsystems.chartview.helpers.ChartAnimator
 import io.horizontalsystems.chartview.models.ChartConfig
 import io.horizontalsystems.chartview.models.ChartPointF
+import java.lang.Float.max
 
 class ChartVolume(private val config: ChartConfig, private val animator: ChartAnimator, override var isVisible: Boolean = true) : ChartDraw {
 
     private var shape = RectF(0f, 0f, 0f, 0f)
-
     private var bars = listOf<ChartPointF>()
+    private var volumeWidth = 0f
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
@@ -24,25 +25,30 @@ class ChartVolume(private val config: ChartConfig, private val animator: ChartAn
 
     fun setPoints(points: List<ChartPointF>) {
         bars = points
+
+        if (bars.isNotEmpty()) {
+            var minDiff = config.volumeWidth
+            for (i in 0 until (bars.size - 1)) {
+                val diff = bars[i + 1].x - bars[i].x - 1 // 1 is horizontal space between bars
+                if (diff < minDiff) {
+                    minDiff = diff
+                }
+            }
+
+            volumeWidth = max(minDiff, 1f)
+        }
     }
 
     override fun draw(canvas: Canvas) {
-        if (!isVisible) return
+        if (!isVisible || bars.isEmpty()) return
 
         val height = shape.height()
-        var prevEnd = -100f
 
+        val halfWidth = volumeWidth / 2
         bars.forEach { bar ->
             val barTop = animator.getAnimatedY(bar.y, height)
-            var barStart = bar.x - config.volumeWidth
-            var barEnd = bar.x
-
-            if (prevEnd > barStart) {
-                barStart = bar.x
-                barEnd = bar.x + config.volumeWidth
-            }
-
-            prevEnd = barEnd
+            val barStart = bar.x - halfWidth
+            val barEnd = bar.x + halfWidth
 
             canvas.drawRect(RectF(barStart, barTop, barEnd, height), paint)
         }
