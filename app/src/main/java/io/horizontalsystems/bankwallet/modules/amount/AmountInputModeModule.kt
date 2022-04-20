c0a4fc3dde72d31252d5b9f7ee9e3d45dda7cada
bakyt <abdrasulov@gmail.com>
2022-04-20 13:33:59 +0600

    Handle xRates updates in AmountInput

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/amount/AmountInputModeModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/amount/AmountInputModeModule.kt
@@ -3,14 +3,19 @@ package io.horizontalsystems.bankwallet.modules.amount
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.entities.Wallet
+import io.horizontalsystems.bankwallet.modules.xrate.XRateService
 
 object AmountInputModeModule {
 
-    class Factory : ViewModelProvider.Factory {
+    class Factory(private val wallet: Wallet) : ViewModelProvider.Factory {
 
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return AmountInputModeViewModel(App.localStorage) as T
+
+            val xRateService = XRateService(App.marketKit, App.currencyManager.baseCurrency)
+
+            return AmountInputModeViewModel(App.localStorage, xRateService, wallet.coin.uid) as T
         }
     }
 }
