1bf0fd603acda02b34a3cc58ae3979afd0b7e829
chyngyz <chyngyz.omurov@gmail.com>
2022-04-20 17:09:37 +0600

    Handle legacy eth_sign WalletConnect request

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/request/signmessage/v1/WC1SignMessageRequestService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/request/signmessage/v1/WC1SignMessageRequestService.kt
@@ -2,7 +2,6 @@ package io.horizontalsystems.bankwallet.modules.walletconnect.request.signmessag
 
 import com.trustwallet.walletconnect.models.ethereum.WCEthereumSignMessage
 import com.trustwallet.walletconnect.models.ethereum.WCEthereumSignMessage.WCSignType
-import io.horizontalsystems.bankwallet.core.managers.EvmKitWrapper
 import io.horizontalsystems.bankwallet.modules.walletconnect.request.signmessage.SignMessage
 import io.horizontalsystems.bankwallet.modules.walletconnect.request.signmessage.WCSignMessageRequestModule
 import io.horizontalsystems.bankwallet.modules.walletconnect.version1.WC1Service
@@ -16,13 +15,16 @@ class WC1SignMessageRequestService(
     private val signer: Signer
 ) : WCSignMessageRequestModule.RequestAction {
 
-    private val evmKitWrapper: EvmKitWrapper?
-        get() = baseService.evmKitWrapper
-
     override val message: SignMessage by lazy {
         val messageData = request.message.data
         when (request.message.type) {
-            WCSignType.MESSAGE -> SignMessage.Message(hexStringToUtf8String(messageData))
+            WCSignType.MESSAGE -> {
+                if (isLegacySignRequest(request.message)) {
+                    SignMessage.Message(messageData)
+                } else {
+                    SignMessage.Message(hexStringToUtf8String(messageData))
+                }
+            }
             WCSignType.PERSONAL_MESSAGE -> SignMessage.PersonalMessage(
                 hexStringToUtf8String(messageData)
             )
@@ -36,7 +38,7 @@ class WC1SignMessageRequestService(
 
     override fun sign() {
         val signedMessage = signMessage(request.message)
-        baseService.approveRequest(request.id, signedMessage ?: byteArrayOf())
+        baseService.approveRequest(request.id, signedMessage)
     }
 
     override fun reject() {
@@ -51,18 +53,27 @@ class WC1SignMessageRequestService(
         hexString
     }
 
-    private fun signMessage(message: WCEthereumSignMessage): ByteArray? {
-        return evmKitWrapper?.evmKit?.let { evmKit ->
-            when (message.type) {
-                WCSignType.MESSAGE,
-                WCSignType.PERSONAL_MESSAGE -> {
+    private fun signMessage(message: WCEthereumSignMessage): ByteArray {
+        return when (message.type) {
+            WCSignType.MESSAGE -> {
+                if (isLegacySignRequest(message)) {
+                    signer.signByteArrayLegacy(message = message.data.hexStringToByteArray())
+                } else {
                     signer.signByteArray(message = message.data.hexStringToByteArray())
                 }
-                WCSignType.TYPED_MESSAGE -> {
-                    signer.signTypedData(rawJsonMessage = message.data)
-                }
+            }
+            WCSignType.PERSONAL_MESSAGE -> {
+                signer.signByteArray(message = message.data.hexStringToByteArray())
+            }
+            WCSignType.TYPED_MESSAGE -> {
+                signer.signTypedData(rawJsonMessage = message.data)
             }
         }
+
+    }
+
+    private fun isLegacySignRequest(message: WCEthereumSignMessage): Boolean {
+        return message.type == WCSignType.MESSAGE && message.data.hexStringToByteArray().size == 32
     }
 
 }
