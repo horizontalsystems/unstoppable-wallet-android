4af77d7546dfc8e29cba9dacc21f861513bcf1f7
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-05-05 11:48:27 +0600

    Refactor by removing Card wrapper in Tweets Cell

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/CellTweet.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/CellTweet.kt
@@ -1,13 +1,12 @@
 package io.horizontalsystems.bankwallet.ui.compose.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -97,26 +96,22 @@ private fun TweetDate(tweet: TweetViewItem) {
 
 @Composable
 private fun TweetReferencedTweet(referencedTweet: ReferencedTweetViewItem) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        elevation = 0.dp,
-        shape = RoundedCornerShape(8.dp),
-        border = BorderStroke(1.dp, ComposeAppTheme.colors.steel20),
-        backgroundColor = ComposeAppTheme.colors.lawrence
+    Column(Modifier
+        .fillMaxWidth()
+        .border(1.dp, ComposeAppTheme.colors.steel20, RoundedCornerShape(8.dp))
+        .padding(12.dp)
     ) {
-        Column(Modifier.padding(12.dp)) {
-            Text(
-                text = referencedTweet.title.getString(),
-                color = ComposeAppTheme.colors.grey,
-                style = ComposeAppTheme.typography.micro
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text(
-                text = referencedTweet.text,
-                color = ComposeAppTheme.colors.leah,
-                style = ComposeAppTheme.typography.subhead2
-            )
-        }
+        Text(
+            text = referencedTweet.title.getString(),
+            color = ComposeAppTheme.colors.grey,
+            style = ComposeAppTheme.typography.micro
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = referencedTweet.text,
+            color = ComposeAppTheme.colors.leah,
+            style = ComposeAppTheme.typography.subhead2
+        )
     }
 }
 
