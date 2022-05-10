825da71a34e3126c3dd94db3534c3a247cd79520
bakyt <abdrasulov@gmail.com>
2022-05-10 14:13:54 +0600

    Simplify LaunchModule

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/launcher/LaunchModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/launcher/LaunchModule.kt
@@ -2,47 +2,19 @@ package io.horizontalsystems.bankwallet.modules.launcher
 
 import android.content.Context
 import android.content.Intent
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.core.security.KeyStoreValidationResult
 
 object LaunchModule {
 
-    interface IView
+    class Factory : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            val interactor = LaunchInteractor(App.accountManager, App.pinComponent, App.systemInfoManager, App.keyStoreManager, App.localStorage)
 
-    interface IViewDelegate {
-        fun viewDidLoad()
-        fun didUnlock()
-        fun didCancelUnlock()
-    }
-
-    interface IInteractor {
-        val isLocked: Boolean
-        val isAccountsEmpty: Boolean
-        val isSystemLockOff: Boolean
-        val mainShowedOnce: Boolean
-
-        fun validateKeyStore(): KeyStoreValidationResult
-    }
-
-    interface IInteractorDelegate
-
-    interface IRouter {
-        fun openWelcomeModule()
-        fun openMainModule()
-        fun openUnlockModule()
-        fun closeApplication()
-        fun openNoSystemLockModule()
-        fun openKeyInvalidatedModule()
-        fun openUserAuthenticationModule()
-    }
-
-    fun init(view: LaunchViewModel, router: IRouter) {
-        val interactor = LaunchInteractor(App.accountManager, App.pinComponent, App.systemInfoManager, App.keyStoreManager, App.localStorage)
-        val presenter = LaunchPresenter(interactor, router)
-
-        view.delegate = presenter
-        presenter.view = view
-        interactor.delegate = presenter
+            return LaunchViewModel(interactor) as T
+        }
     }
 
     fun start(context: Context) {
