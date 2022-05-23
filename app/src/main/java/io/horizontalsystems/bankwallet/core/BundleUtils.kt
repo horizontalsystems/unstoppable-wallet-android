fe349541b8566156fa80c8a56ce6c654f5763ac4
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2022-05-23 11:58:20 +0600

    Serialize ProChartModule.ChartType when using in bundle

--- app/src/main/java/io/horizontalsystems/bankwallet/core/BundleUtils.kt
+++
@@ -1,9 +0,0 @@
-package io.horizontalsystems.bankwallet.core
-
-import android.os.Bundle
-
-inline fun <reified T : Enum<T>> Bundle.getEnum(key: String, default: T) =
-    getInt(key).let { if (it >= 0) enumValues<T>()[it] else default }
-
-fun <T : Enum<T>> Bundle.putEnum(key: String, value: T?) =
-    putInt(key, value?.ordinal ?: -1)
