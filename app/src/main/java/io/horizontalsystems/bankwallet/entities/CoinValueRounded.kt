54a491f8e60dea00bba740ab7e02756eecf7eae6
bakyt <abdrasulov@gmail.com>
2022-05-24 15:22:25 +0600

    Format balance values in VM

--- app/src/main/java/io/horizontalsystems/bankwallet/entities/CoinValueRounded.kt
+++
@@ -1,8 +0,0 @@
-package io.horizontalsystems.bankwallet.entities
-
-import io.horizontalsystems.bankwallet.core.managers.BigDecimalRounded
-import io.horizontalsystems.core.entities.Currency
-import io.horizontalsystems.marketkit.models.PlatformCoin
-
-data class CoinValueRounded(val platformCoin: PlatformCoin, val value: BigDecimalRounded)
-data class CurrencyValueRounded(val currency: Currency, val value: BigDecimalRounded)
