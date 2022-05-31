2398b246c2f5b96e916f92dcd4e01dabac0d7bea
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-05-31 16:52:23 +0600

    Make Send screen scrollable

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -48,6 +50,8 @@ fun SendScreen(
             )
         )
 
-        content.invoke(this)
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            content.invoke(this)
+        }
     }
 }
