8106d98c938d827238b9444efb6f71a7caa09bb8
bakyt <abdrasulov@gmail.com>
2022-06-06 12:33:29 +0600

    Update view via `uiState` property of `AppearanceViewModel`

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/appearance/AppearanceViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/appearance/AppearanceViewModel.kt
@@ -1,21 +1,41 @@
 package io.horizontalsystems.bankwallet.modules.settings.appearance
 
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import io.horizontalsystems.bankwallet.entities.LaunchPage
 import io.horizontalsystems.bankwallet.ui.compose.Select
 
-class AppearanceViewModel(private val service: AppearanceService) : ViewModel() {
+class AppearanceViewModel(
+    private val launchScreenService: LaunchScreenService
+) : ViewModel() {
+    private var launchScreenOptions = getLaunchScreenOptions()
 
-    val optionsLiveData = MutableLiveData(getItems())
+    var uiState by mutableStateOf(
+        AppearanceUIState(
+            launchScreenOptions = launchScreenOptions
+        )
+    )
 
     fun onLaunchPageSelect(launchPage: LaunchPage) {
-        service.selectLaunchPage(launchPage)
-        optionsLiveData.postValue(getItems())
+        launchScreenService.selectLaunchPage(launchPage)
+        launchScreenOptions = getLaunchScreenOptions()
+        emitState()
     }
 
-    private fun getItems(): Select<LaunchPage> {
-        return Select(service.selectedOption, service.options)
+    private fun emitState() {
+        uiState = AppearanceUIState(
+            launchScreenOptions = launchScreenOptions
+        )
+    }
+
+    private fun getLaunchScreenOptions(): Select<LaunchPage> {
+        return Select(launchScreenService.selectedOption, launchScreenService.options)
     }
 
 }
+
+data class AppearanceUIState(
+    val launchScreenOptions: Select<LaunchPage>
+)
