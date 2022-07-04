05fc6dc6c1eabc8438459a1d4637db7e729bee94
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-07-04 16:10:22 +0600

    Convert BackupConfirmKey module to compose

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/backupconfirmkey/BackupConfirmKeyModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/backupconfirmkey/BackupConfirmKeyModule.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.managers.RandomProvider
-import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.bankwallet.entities.Account
 
 object BackupConfirmKeyModule {
@@ -14,9 +13,7 @@ object BackupConfirmKeyModule {
     class Factory(private val account: Account) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            val service = BackupConfirmKeyService(account, App.accountManager, RandomProvider())
-
-            return BackupConfirmKeyViewModel(service, Translator) as T
+            return BackupConfirmKeyViewModel(account, App.accountManager, RandomProvider()) as T
         }
     }
 
