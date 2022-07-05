0c3722248f7d3ed5addb612c84d9649a7540c067
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2022-07-05 16:20:15 +0600

    Remove highlight market overview section headers press

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/overview/ui/MarketOverviewTopSectors.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/overview/ui/MarketOverviewTopSectors.kt
@@ -2,12 +2,14 @@ package io.horizontalsystems.bankwallet.modules.market.overview.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -34,7 +36,12 @@ fun TopSectorsBoardView(
     )
     Row(modifier = Modifier.height(42.dp)) {
         Row(
-            modifier = Modifier.height(42.dp).clickable { onClickSeeAll.invoke() },
+            modifier = Modifier
+                    .height(42.dp)
+                    .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null
+                    ) { onClickSeeAll.invoke() },
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
