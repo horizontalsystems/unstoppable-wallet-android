b8a684f3da2f86660994d5ca21dab7172b60113f
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-07-08 15:01:53 +0600

    Refactor language icons

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/language/LanguageSettingsModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/language/LanguageSettingsModule.kt
@@ -1,7 +1,9 @@
 package io.horizontalsystems.bankwallet.modules.settings.language
 
+import androidx.annotation.DrawableRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
 
 object LanguageSettingsModule {
@@ -11,11 +13,24 @@ object LanguageSettingsModule {
             return LanguageSettingsViewModel(App.languageManager, App.localStorage) as T
         }
     }
+
+    enum class LocaleType(@DrawableRes val icon: Int) {
+        de(R.drawable.icon_24_flags_germany),
+        en(R.drawable.icon_24_flags_england),
+        es(R.drawable.icon_24_flags_spain),
+        fa(R.drawable.icon_24_flags_iran),
+        fr(R.drawable.icon_24_flags_france),
+        ko(R.drawable.icon_24_flags_korea),
+        ru(R.drawable.icon_24_flags_russia),
+        tr(R.drawable.icon_24_flags_turkey),
+        zh(R.drawable.icon_24_flags_china),
+    }
 }
 
 data class LanguageViewItem(
-    val language: String,
+    val localeType: LanguageSettingsModule.LocaleType,
     val name: String,
     val nativeName: String,
+    val icon: Int,
     var current: Boolean,
 )
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/language/LanguageSettingsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/language/LanguageSettingsViewModel.kt
@@ -12,19 +12,17 @@ class LanguageSettingsViewModel(
     private val localStorage: ILocalStorage,
 ) : ViewModel() {
 
-    private val localizations = "de,en,es,fa,fr,ko,ru,tr,zh"
-    private val availableLanguages = localizations.split(",")
-
-    val languageItems = availableLanguages.map {
+    val languageItems = LanguageSettingsModule.LocaleType.values().map {
         LanguageViewItem(
             it,
-            languageManager.getName(it),
-            languageManager.getNativeName(it),
-            currentLanguage == it
+            languageManager.getName(it.name),
+            languageManager.getNativeName(it.name),
+            it.icon,
+            currentLocale == it.name
         )
     }
 
-    private var currentLanguage: String
+    private var currentLocale: String
         get() = languageManager.currentLanguage
         set(value) {
             languageManager.currentLanguage = value
@@ -36,12 +34,12 @@ class LanguageSettingsViewModel(
     var reloadApp by mutableStateOf(false)
         private set
 
-    fun onSelectLanguage(language: String) {
-        if (language == currentLanguage) {
+    fun onSelectLocale(localeType: LanguageSettingsModule.LocaleType) {
+        if (localeType.name == currentLocale) {
             closeScreen = true
         } else {
             localStorage.relaunchBySettingChange = true
-            currentLanguage = language
+            currentLocale = localeType.name
             reloadApp = true
         }
     }
