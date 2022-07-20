8aca711c95df14837071fbb8763252886734d32d
bakyt <abdrasulov@gmail.com>
2022-07-20 13:57:26 +0600

    Implement Guides screen in compose

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/guides/GuidesViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/guides/GuidesViewModel.kt
@@ -1,31 +1,42 @@
 package io.horizontalsystems.bankwallet.modules.settings.guides
 
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import io.horizontalsystems.bankwallet.entities.Guide
 import io.horizontalsystems.bankwallet.entities.GuideCategory
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.launch
 
 class GuidesViewModel(private val repository: GuidesRepository) : ViewModel() {
 
-    var categories = listOf<GuideCategory>()
-    val guides = MutableLiveData<List<Guide>>()
-    val loading = MutableLiveData(false)
-    val selectedCategory = MutableLiveData<GuideCategory>()
-    val error = MutableLiveData<Throwable?>()
+    var categories by mutableStateOf<List<GuideCategory>>(listOf())
+        private set
+    var selectedCategory by mutableStateOf<GuideCategory?>(null)
+        private set
+    var guides by mutableStateOf<List<Guide>>(listOf())
+        private set
+    var loading by mutableStateOf(false)
+        private set
+    var error by mutableStateOf<Throwable?>(null)
+        private set
 
     private var disposables = CompositeDisposable()
 
     init {
         repository.guideCategories
                 .subscribe {
-                    loading.postValue(it is DataState.Loading)
+                    viewModelScope.launch {
+                        loading = it is DataState.Loading
 
-                    if (it is DataState.Success) {
-                        didFetchGuideCategories(it.data)
+                        if (it is DataState.Success) {
+                            didFetchGuideCategories(it.data)
+                        }
+
+                        error = (it as? DataState.Error)?.throwable
                     }
-
-                    error.postValue((it as? DataState.Error)?.throwable)
                 }
                 .let {
                     disposables.add(it)
@@ -33,8 +44,8 @@ class GuidesViewModel(private val repository: GuidesRepository) : ViewModel() {
     }
 
     fun onSelectCategory(category: GuideCategory) {
-        selectedCategory.postValue(category)
-        guides.postValue(category.guides)
+        selectedCategory = category
+        guides = category.guides
     }
 
     override fun onCleared() {
