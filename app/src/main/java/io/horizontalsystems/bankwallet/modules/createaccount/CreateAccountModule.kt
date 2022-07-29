161845ca9610f6e39b2989f99011842f4b86e3e0
bakyt <abdrasulov@gmail.com>
2022-07-29 13:55:17 +0600

    Add 15/18/21 mnemonic words as options on wallet creation

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/createaccount/CreateAccountModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/createaccount/CreateAccountModule.kt
@@ -27,8 +27,13 @@ object CreateAccountModule {
         }
     }
 
-    enum class Kind(val title: String) {
-        Mnemonic12(Translator.getString(R.string.CreateWallet_N_Words, 12)),
-        Mnemonic24(Translator.getString(R.string.CreateWallet_N_Words, 24)),
+    enum class Kind(val wordsCount: Int) {
+        Mnemonic12(12),
+        Mnemonic15(15),
+        Mnemonic18(18),
+        Mnemonic21(21),
+        Mnemonic24(24);
+
+        val title = Translator.getString(R.string.CreateWallet_N_Words, wordsCount)
     }
 }
