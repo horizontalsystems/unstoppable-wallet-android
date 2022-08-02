3157171fc180ecb088911a11259797d0b4db134e
Saka <62377315+MonkeyD-Core@users.noreply.github.com>
2022-08-02 17:50:14 +0700

    Create TyzenAdapter.kt

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/adapters/TyzenAdapter.kt
@@ -0,0 +1,108 @@
+package io.horizontalsystems.bankwallet.core.adapters
+
+import io.horizontalsystems.bankwallet.core.AdapterErrorWrongParameters
+import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.ISendBitcoinAdapter
+import io.horizontalsystems.bankwallet.core.UnsupportedAccountException
+import io.horizontalsystems.bankwallet.entities.AccountType
+import io.horizontalsystems.bankwallet.entities.BtcBlockchain
+import io.horizontalsystems.bankwallet.entities.Wallet
+import io.horizontalsystems.bankwallet.entities.transactionrecords.TransactionRecord
+import io.horizontalsystems.bitcoincore.BitcoinCore
+import io.horizontalsystems.bitcoincore.models.BalanceInfo
+import io.horizontalsystems.bitcoincore.models.BlockInfo
+import io.horizontalsystems.bitcoincore.models.TransactionInfo
+import io.horizontalsystems.core.BackgroundManager
+import io.horizontalsystems.tyzenkit.TyzenKit
+import io.horizontalsystems.tyzenkit.TyzenKit.NetworkType
+import java.math.BigDecimal
+
+class TyzenAdapter(
+        override val kit: TyzenKit,
+        syncMode: BitcoinCore.SyncMode,
+        backgroundManager: BackgroundManager,
+        wallet: Wallet,
+        testMode: Boolean
+) : BitcoinBaseAdapter(kit, syncMode, backgroundManager, wallet, testMode), TyzenKit.Listener, ISendBitcoinAdapter {
+
+    constructor(wallet: Wallet, syncMode: BitcoinCore.SyncMode, testMode: Boolean, backgroundManager: BackgroundManager) : this(createKit(wallet, syncMode, testMode), syncMode, backgroundManager, wallet, testMode)
+
+    init {
+        kit.listener = this
+    }
+
+    //
+    // BitcoinBaseAdapter
+    //
+
+    override val satoshisInBitcoin: BigDecimal = BigDecimal.valueOf(Math.pow(10.0, decimal.toDouble()))
+
+    //
+    // TyzenKit Listener
+    //
+
+    override val explorerTitle: String
+        get() = "blockchair.com"
+
+
+    override fun getTransactionUrl(transactionHash: String): String? =
+        if (testMode) null else "https://explorer.tyzen.io/tx/$transactionHash"
+
+    override fun onBalanceUpdate(balance: BalanceInfo) {
+        balanceUpdatedSubject.onNext(Unit)
+    }
+
+    override fun onLastBlockInfoUpdate(blockInfo: BlockInfo) {
+        lastBlockUpdatedSubject.onNext(Unit)
+    }
+
+    override fun onKitStateUpdate(state: BitcoinCore.KitState) {
+        setState(state)
+    }
+
+    override fun onTransactionsUpdate(inserted: List<TransactionInfo>, updated: List<TransactionInfo>) {
+        val records = mutableListOf<TransactionRecord>()
+
+        for (info in inserted) {
+            records.add(transactionRecord(info))
+        }
+
+        for (info in updated) {
+            records.add(transactionRecord(info))
+        }
+
+        transactionRecordsSubject.onNext(records)
+    }
+
+    override fun onTransactionsDelete(hashes: List<String>) {
+        // ignored for now
+    }
+
+    override val blockchain = BtcBlockchain.Tyzen
+
+
+    companion object {
+
+        private fun getNetworkType(testMode: Boolean) =
+                if (testMode) NetworkType.TestNet else NetworkType.MainNet
+
+        private fun createKit(wallet: Wallet, syncMode: BitcoinCore.SyncMode, testMode: Boolean): TyzenKit {
+            val account = wallet.account
+            val accountType = (account.type as? AccountType.Mnemonic) ?: throw UnsupportedAccountException()
+            val derivation = wallet.coinSettings.derivation ?: throw AdapterErrorWrongParameters("Derivation not set")
+
+            return TyzenKit(context = App.instance,
+                    words = accountType.words,
+                    passphrase = accountType.passphrase,
+                    walletId = account.id,
+                    syncMode = syncMode,
+                    networkType = getNetworkType(testMode),
+                    confirmationsThreshold = confirmationsThreshold,
+                    bip = getBip(derivation))
+        }
+
+        fun clear(walletId: String, testMode: Boolean) {
+            TyzenKit.clear(App.instance, getNetworkType(testMode), walletId)
+        }
+    }
+}
