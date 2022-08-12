1f94820113cbd6146608bc80f4476efcc3b81cd1
chyngyz <chyngyz.omurov@gmail.com>
2022-08-12 21:03:39 +0600

    Add possibility to select chain in WalletConnect
    - Fix bug in WalletConnect list

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/session/ui/WCSessionCells.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/session/ui/WCSessionCells.kt
@@ -1,16 +1,21 @@
 package io.horizontalsystems.bankwallet.modules.walletconnect.session.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.walletconnect.session.v1.WCSessionViewModel
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.HsCheckbox
+import io.horizontalsystems.bankwallet.ui.compose.components.body_leah
 import io.horizontalsystems.bankwallet.ui.compose.components.subhead1_leah
 import io.horizontalsystems.bankwallet.ui.compose.components.subhead2_grey
 
@@ -80,6 +85,46 @@ fun StatusCell(connectionStatus: WCSessionViewModel.Status?) {
                 color = color,
                 style = ComposeAppTheme.typography.subhead1
             )
+        }
+    }
+}
+
+@Composable
+fun DropDownCell(
+    title: String,
+    value: String,
+    enabled: Boolean,
+    onSelect: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(horizontal = 16.dp)
+            .clickable(enabled = enabled, onClick = onSelect),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        subhead2_grey(
+            text = title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+        Spacer(Modifier.weight(1f))
+        Row {
+            body_leah(
+                text = value,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+            if (enabled) {
+                Icon(
+                    modifier = Modifier
+                        .padding(start = 4.dp, top = 2.dp)
+                        .align(Alignment.CenterVertically),
+                    painter = painterResource(id = R.drawable.ic_down_arrow_20),
+                    contentDescription = null,
+                    tint = ComposeAppTheme.colors.grey
+                )
+            }
         }
     }
 }
