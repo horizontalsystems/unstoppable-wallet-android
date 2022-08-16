29e3e41c88551d37864a722c7f40542ed4f40179
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-08-16 16:04:32 +0600

    Fix fee calculation for Evm blockchains
    - Add nAvax to Evm fee settings
    - Show small value for tips

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/entities/FeePriceScale.kt
@@ -0,0 +1,18 @@
+package io.horizontalsystems.bankwallet.entities
+
+enum class FeePriceScale {
+    Satoshi, Gwei, Navax;
+
+    val unit: String
+        get() = when (this) {
+            Satoshi -> "satoshi"
+            Gwei -> "Gwei"
+            Navax -> "nAvax"
+        }
+
+    val scaleValue: Int
+        get() = when (this) {
+            Satoshi -> 1
+            Gwei, Navax -> 1_000_000_000
+        }
+}
