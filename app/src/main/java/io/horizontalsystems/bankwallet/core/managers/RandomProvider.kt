903cfa25304b73306899fa9580657a767c5c7475
bakyt <abdrasulov@gmail.com>
2022-08-22 13:13:44 +0600

    Confirm 2, 3, or 4 words depending on the seed phrase size

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/RandomProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/RandomProvider.kt
@@ -5,20 +5,20 @@ import java.util.*
 
 class RandomProvider : IRandomProvider {
 
-    override fun getRandomIndexes(count: Int, maxIndex: Int): List<Int> {
-        val indexes = mutableListOf<Int>()
+    override fun getRandomNumbers(count: Int, maxIndex: Int): List<Int> {
+        val numbers = mutableListOf<Int>()
 
         val random = Random()
 
-        while (indexes.size < count) {
-            val index = random.nextInt(maxIndex)
+        while (numbers.size < count) {
+            val number = random.nextInt(maxIndex)
 
-            if (!indexes.contains(index)) {
-                indexes.add(index)
+            if (!numbers.contains(number)) {
+                numbers.add(number)
             }
         }
 
-        return indexes
+        return numbers
     }
 
 }
