c533f2906eaf690e15bdcf5877361d9a894ca7fe
bakyt <abdrasulov@gmail.com>
2022-08-26 13:54:15 +0600

    Fix memory leak

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/Keyboard.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/Keyboard.kt
@@ -1,6 +1,7 @@
 package io.horizontalsystems.bankwallet.ui.compose
 
 import android.graphics.Rect
+import android.view.View
 import android.view.ViewTreeObserver
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalView
@@ -14,20 +15,28 @@ fun observeKeyboardState(): State<Keyboard> {
     val keyboardState = remember { mutableStateOf(Keyboard.Closed) }
     val view = LocalView.current
     DisposableEffect(view) {
-        val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
-            val rect = Rect()
-            view.getWindowVisibleDisplayFrame(rect)
-            val screenHeight = view.rootView.height
-            val keypadHeight = screenHeight - rect.bottom
-            keyboardState.value = if (keypadHeight > screenHeight * 0.15) {
-                Keyboard.Opened
-            } else {
-                Keyboard.Closed
+        val onGlobalListener = object : ViewTreeObserver.OnGlobalLayoutListener {
+            var currentView: View? = null
+
+            override fun onGlobalLayout() {
+                val viewTmp = currentView?: return
+
+                val rect = Rect()
+                viewTmp.getWindowVisibleDisplayFrame(rect)
+                val screenHeight = viewTmp.rootView.height
+                val keypadHeight = screenHeight - rect.bottom
+                keyboardState.value = if (keypadHeight > screenHeight * 0.15) {
+                    Keyboard.Opened
+                } else {
+                    Keyboard.Closed
+                }
             }
         }
+        onGlobalListener.currentView = view
         view.viewTreeObserver.addOnGlobalLayoutListener(onGlobalListener)
 
         onDispose {
+            onGlobalListener.currentView = null
             view.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalListener)
         }
     }
