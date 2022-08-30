86812d19d466fc333a0b988c970a1f3822d55a13
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-08-30 17:50:16 +0600

    Update Terms page

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/terms/TermsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/terms/TermsFragment.kt
@@ -9,23 +9,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
+import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.*
@@ -44,37 +38,38 @@ class TermsFragment : BaseFragment() {
             )
             setContent {
                 ComposeAppTheme {
-                    TermsScreen(
-                        findNavController(),
-                    )
+                    TermsScreen(onCloseClick = { findNavController().popBackStack() })
                 }
             }
         }
     }
 
     @Composable
-    private fun TermsScreen(navController: NavController) {
-        val viewModel = viewModel<TermsViewModel>(factory = TermsModule.Factory())
-        val terms by viewModel.termsLiveData.observeAsState()
+    private fun TermsScreen(
+        onCloseClick: () -> Unit,
+        viewModel: TermsViewModel = viewModel(factory = TermsModule.Factory())
+    ) {
 
-        Column(modifier = Modifier.fillMaxSize().background(ComposeAppTheme.colors.tyler)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(ComposeAppTheme.colors.tyler)
+        ) {
             AppBar(
                 title = TranslatableString.ResString(R.string.Settings_Terms),
-                navigationIcon = {
-                    HsIconButton(onClick = { navController.popBackStack() }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_back),
-                            contentDescription = "back",
-                            tint = ComposeAppTheme.colors.jacob
-                        )
-                    }
-                }
-            )
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-
-                InfoTextBody(
-                    text = stringResource(R.string.SettingsTerms_Text),
+                menuItems = listOf(
+                    MenuItem(
+                        title = TranslatableString.ResString(R.string.Button_Close),
+                        icon = R.drawable.ic_close,
+                        onClick = onCloseClick
+                    )
                 )
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState())
+            ) {
 
                 Spacer(modifier = Modifier.height(12.dp))
 
@@ -83,15 +78,15 @@ class TermsFragment : BaseFragment() {
                         .padding(horizontal = 16.dp)
                         .clip(RoundedCornerShape(12.dp))
                 ) {
-                    terms?.forEachIndexed { index, item ->
+                    viewModel.termsViewItems.forEach { item ->
                         CellLawrence(
                             borderBottom = true,
-                            onClick = { viewModel.onTapTerm(index, !item.checked) }
+                            onClick = { viewModel.onTapTerm(item.termType, !item.checked) }
                         ) {
                             HsCheckbox(
                                 checked = item.checked,
                                 onCheckedChange = { checked ->
-                                    viewModel.onTapTerm(index, checked)
+                                    viewModel.onTapTerm(item.termType, checked)
                                 },
                             )
                             Spacer(Modifier.width(16.dp))
@@ -100,33 +95,18 @@ class TermsFragment : BaseFragment() {
                     }
                 }
 
-                Spacer(Modifier.height(46.dp))
+                Spacer(Modifier.height(60.dp))
+            }
 
-                title3_jacob(
-                    text = stringResource(R.string.SettingsTerms_BottomThankYou),
+            ButtonsGroupWithShade {
+                ButtonPrimaryYellow(
                     modifier = Modifier
-                        .padding(horizontal = 24.dp)
-                        .align(Alignment.CenterHorizontally)
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    title = stringResource(R.string.Button_IAgree),
+                    onClick = onCloseClick,
+                    enabled = viewModel.buttonEnabled
                 )
-
-                Spacer(Modifier.height(32.dp))
-
-                Divider(
-                    modifier = Modifier.fillMaxWidth(),
-                    thickness = 1.dp,
-                    color = ComposeAppTheme.colors.steel10
-                )
-
-                Spacer(Modifier.height(12.dp))
-
-                caption_grey(
-                    text = stringResource(R.string.FooterText),
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .align(Alignment.CenterHorizontally)
-                )
-
-                Spacer(Modifier.height(28.dp))
             }
         }
 
