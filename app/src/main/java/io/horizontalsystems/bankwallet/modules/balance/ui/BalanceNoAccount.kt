ff612fd6bebb2e18152a47e2b3f9cb4d3ebc336a
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-09-01 14:49:26 +0600

    Open Terms page when user clicks on Creat/Restore account

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/ui/BalanceNoAccount.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/ui/BalanceNoAccount.kt
@@ -7,51 +7,63 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.slideFromBottom
 import io.horizontalsystems.bankwallet.core.slideFromRight
-import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.modules.settings.terms.TermsFragment
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryDefault
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryTransparent
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.bankwallet.ui.compose.components.ScreenMessageWithAction
+import io.horizontalsystems.core.getNavigationResult
 
 @Composable
 fun BalanceNoAccount(navController: NavController) {
-    ComposeAppTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
-            ScreenMessageWithAction(
-                text = stringResource(id = R.string.Balance_NoWalletAlert),
-                icon = R.drawable.ic_wallet_48,
-            ){
-                ButtonPrimaryYellow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 48.dp),
-                    title = stringResource(R.string.Button_Create),
-                    onClick = {
-                        navController.slideFromRight(R.id.createAccountFragment)
-                    }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                ButtonPrimaryDefault(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 48.dp),
-                    title = stringResource(R.string.Button_Restore),
-                    onClick = {
-                        navController.slideFromRight(R.id.restoreMnemonicFragment)
-                    }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                ButtonPrimaryTransparent(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 48.dp),
-                    title = stringResource(R.string.Button_WatchAddress),
-                    onClick = {
-                        navController.slideFromRight(R.id.watchAddressFragment)
-                    }
-                )
-            }
+    Column(modifier = Modifier.fillMaxSize()) {
+        ScreenMessageWithAction(
+            text = stringResource(id = R.string.Balance_NoWalletAlert),
+            icon = R.drawable.ic_wallet_48,
+        ) {
+            ButtonPrimaryYellow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 48.dp),
+                title = stringResource(R.string.Button_Create),
+                onClick = {
+                    openPageWithTermsAgreed(navController, R.id.createAccountFragment)
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            ButtonPrimaryDefault(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 48.dp),
+                title = stringResource(R.string.Button_Restore),
+                onClick = {
+                    openPageWithTermsAgreed(navController, R.id.restoreMnemonicFragment)
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            ButtonPrimaryTransparent(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 48.dp),
+                title = stringResource(R.string.Button_WatchAddress),
+                onClick = {
+                    navController.slideFromRight(R.id.watchAddressFragment)
+                }
+            )
         }
     }
+}
+
+private fun openPageWithTermsAgreed(navController: NavController, destination: Int) {
+    navController.getNavigationResult(TermsFragment.resultBundleKey) { bundle ->
+        val agreedToTerms = bundle.getInt(TermsFragment.requestResultKey)
+
+        if (agreedToTerms == TermsFragment.RESULT_OK) {
+            navController.slideFromRight(destination)
+        }
+    }
+
+    navController.slideFromBottom(R.id.termsFragment)
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/terms/TermsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/terms/TermsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -16,7 +17,9 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.os.bundleOf
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
@@ -24,6 +27,7 @@ import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.*
 import io.horizontalsystems.core.findNavController
+import io.horizontalsystems.core.setNavigationResult
 
 class TermsFragment : BaseFragment() {
 
@@ -32,83 +36,110 @@ class TermsFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        activity?.onBackPressedDispatcher?.addCallback(this) {
+            findNavController().setNavigationResult(
+                resultBundleKey,
+                bundleOf(requestResultKey to RESULT_CANCELLED)
+            )
+            findNavController().popBackStack()
+        }
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
             setContent {
                 ComposeAppTheme {
-                    TermsScreen(onCloseClick = { findNavController().popBackStack() })
+                    TermsScreen(navController = findNavController())
                 }
             }
         }
     }
 
-    @Composable
-    private fun TermsScreen(
-        onCloseClick: () -> Unit,
-        viewModel: TermsViewModel = viewModel(factory = TermsModule.Factory())
-    ) {
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(ComposeAppTheme.colors.tyler)
-        ) {
-            AppBar(
-                title = TranslatableString.ResString(R.string.Settings_Terms),
-                menuItems = listOf(
-                    MenuItem(
-                        title = TranslatableString.ResString(R.string.Button_Close),
-                        icon = R.drawable.ic_close,
-                        onClick = onCloseClick
-                    )
-                )
-            )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState())
-            ) {
-
-                Spacer(modifier = Modifier.height(12.dp))
-
-                Column(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .clip(RoundedCornerShape(12.dp))
-                ) {
-                    viewModel.termsViewItems.forEach { item ->
-                        CellLawrence(
-                            borderBottom = true,
-                            onClick = { viewModel.onTapTerm(item.termType, !item.checked) }
-                        ) {
-                            HsCheckbox(
-                                checked = item.checked,
-                                onCheckedChange = { checked ->
-                                    viewModel.onTapTerm(item.termType, checked)
-                                },
-                            )
-                            Spacer(Modifier.width(16.dp))
-                            subhead2_leah(text = stringResource(item.termType.description))
-                        }
-                    }
-                }
-
-                Spacer(Modifier.height(60.dp))
-            }
-
-            ButtonsGroupWithShade {
-                ButtonPrimaryYellow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 24.dp),
-                    title = stringResource(R.string.Button_IAgree),
-                    onClick = onCloseClick,
-                    enabled = viewModel.buttonEnabled
-                )
-            }
-        }
-
+    companion object {
+        const val RESULT_OK = 1
+        const val RESULT_CANCELLED = 2
+        const val resultBundleKey = "resultBundleKey"
+        const val requestResultKey = "requestResultKey"
     }
 }
+
+@Composable
+private fun TermsScreen(
+    navController: NavController,
+    viewModel: TermsViewModel = viewModel(factory = TermsModule.Factory())
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(ComposeAppTheme.colors.tyler)
+    ) {
+        AppBar(
+            title = TranslatableString.ResString(R.string.Settings_Terms),
+            menuItems = listOf(
+                MenuItem(
+                    title = TranslatableString.ResString(R.string.Button_Close),
+                    icon = R.drawable.ic_close,
+                    onClick = {
+                        navController.setNavigationResult(
+                            TermsFragment.resultBundleKey,
+                            bundleOf(TermsFragment.requestResultKey to TermsFragment.RESULT_CANCELLED)
+                        )
+                        navController.popBackStack()
+                    }
+                )
+            )
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        ) {
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .clip(RoundedCornerShape(12.dp))
+            ) {
+                viewModel.termsViewItems.forEach { item ->
+                    CellLawrence(
+                        borderBottom = true,
+                        onClick = { viewModel.onTapTerm(item.termType, !item.checked) }
+                    ) {
+                        HsCheckbox(
+                            checked = item.checked,
+                            onCheckedChange = { checked ->
+                                viewModel.onTapTerm(item.termType, checked)
+                            },
+                        )
+                        Spacer(Modifier.width(16.dp))
+                        subhead2_leah(text = stringResource(item.termType.description))
+                    }
+                }
+            }
+
+            Spacer(Modifier.height(60.dp))
+        }
+
+        ButtonsGroupWithShade {
+            ButtonPrimaryYellow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                title = stringResource(R.string.Button_IAgree),
+                onClick = {
+                    navController.setNavigationResult(
+                        TermsFragment.resultBundleKey,
+                        bundleOf(TermsFragment.requestResultKey to TermsFragment.RESULT_OK)
+                    )
+                    navController.popBackStack()
+                },
+                enabled = viewModel.buttonEnabled
+            )
+        }
+    }
+
+}
+
