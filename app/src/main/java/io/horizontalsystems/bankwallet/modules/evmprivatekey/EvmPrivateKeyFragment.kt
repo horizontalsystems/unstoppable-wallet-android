fe1ca5c5ca0f08e2a2f81b87d0d54b7f16eede47
bakyt <abdrasulov@gmail.com>
2022-09-07 15:06:50 +0600

    Update design for confirm copy alert bottom sheet

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/evmprivatekey/EvmPrivateKeyFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/evmprivatekey/EvmPrivateKeyFragment.kt
@@ -7,28 +7,31 @@ import android.view.ViewGroup
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.entities.Account
 import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
+import io.horizontalsystems.bankwallet.modules.recoveryphrase.ConfirmCopyBottomSheet
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.*
-import io.horizontalsystems.bankwallet.ui.extensions.ConfirmationDialog
 import io.horizontalsystems.bankwallet.ui.helpers.TextHelper
 import io.horizontalsystems.core.findNavController
 import io.horizontalsystems.core.helpers.HudHelper
+import kotlinx.coroutines.launch
 
 class EvmPrivateKeyFragment : BaseFragment() {
 
@@ -37,82 +40,96 @@ class EvmPrivateKeyFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        disallowScreenshot()
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
             setContent {
                 EvmPrivateKeyScreen(
-                    account = arguments?.getParcelable(EvmPrivateKeyModule.ACCOUNT)!!,
-                    onBackPress = { findNavController().popBackStack() },
-                    showKeyWarning = { key -> showPrivateKeyCopyWarning(key) },
+                    navController = findNavController(),
+                    account = arguments?.getParcelable(EvmPrivateKeyModule.ACCOUNT)!!
                 )
             }
         }
     }
 
-    private fun showPrivateKeyCopyWarning(key: String) {
-        ConfirmationDialog.show(
-            title = getString(R.string.RecoveryPhrase_CopyWarning_Title),
-            warningText = getString(R.string.ShowKey_PrivateKeyCopyWarning_Text),
-            actionButtonTitle = getString(R.string.Button_Ok),
-            transparentButtonTitle = getString(R.string.ShowKey_PrivateKeyCopyWarning_Proceed),
-            fragmentManager = childFragmentManager,
-            listener = object : ConfirmationDialog.Listener {
-                override fun onTransparentButtonClick() {
-                    TextHelper.copyText(key)
-                    HudHelper.showSuccessMessage(requireView(), R.string.Hud_Text_Copied)
-                }
-
-            }
-        )
+    override fun onDestroyView() {
+        allowScreenshot()
+        super.onDestroyView()
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun EvmPrivateKeyScreen(
+    navController: NavController,
     account: Account,
-    onBackPress: () -> Unit,
-    showKeyWarning: (String) -> Unit,
-    viewModel: EvmPrivateKeyViewModel = viewModel(
-        factory = EvmPrivateKeyModule.Factory(account)
-    )
 ) {
+    val viewModel = viewModel<EvmPrivateKeyViewModel>(factory = EvmPrivateKeyModule.Factory(account))
+    val view = LocalView.current
+    val coroutineScope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+    )
 
     ComposeAppTheme {
-        Column(modifier = Modifier.background(color = ComposeAppTheme.colors.tyler)) {
-            AppBar(
-                title = TranslatableString.ResString(R.string.EvmPrivateKey_Title),
-                navigationIcon = {
-                    HsIconButton(onClick = onBackPress) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_back),
-                            contentDescription = "back button",
-                            tint = ComposeAppTheme.colors.jacob
-                        )
+        ModalBottomSheetLayout(
+            sheetState = sheetState,
+            sheetBackgroundColor = ComposeAppTheme.colors.transparent,
+            sheetContent = {
+                ConfirmCopyBottomSheet(
+                    onConfirm = {
+                        coroutineScope.launch {
+                            TextHelper.copyText(viewModel.ethereumPrivateKey)
+                            HudHelper.showSuccessMessage(view, R.string.Hud_Text_Copied)
+                            sheetState.hide()
+                        }
+                    },
+                    onCancel = {
+                        coroutineScope.launch {
+                            sheetState.hide()
+                        }
                     }
-                },
-            )
-
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .verticalScroll(rememberScrollState()),
-                verticalArrangement = Arrangement.Top
-            ) {
-                Spacer(Modifier.height(12.dp))
-                TextImportantWarning(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    text = stringResource(R.string.EvmPrivateKey_Warning)
                 )
-                Spacer(Modifier.height(24.dp))
-                var hidden by remember { mutableStateOf(true) }
-                HidableContent(viewModel.ethereumPrivateKey, hidden) {
-                    hidden = it
-                }
             }
-            ActionButton(R.string.Alert_Copy) {
-                showKeyWarning.invoke(viewModel.ethereumPrivateKey)
+        ) {
+            Column(modifier = Modifier.background(color = ComposeAppTheme.colors.tyler)) {
+                AppBar(
+                    title = TranslatableString.ResString(R.string.EvmPrivateKey_Title),
+                    navigationIcon = {
+                        HsIconButton(onClick = navController::popBackStack) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_back),
+                                contentDescription = "back button",
+                                tint = ComposeAppTheme.colors.jacob
+                            )
+                        }
+                    },
+                )
+
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Spacer(Modifier.height(12.dp))
+                    TextImportantWarning(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = stringResource(R.string.EvmPrivateKey_Warning)
+                    )
+                    Spacer(Modifier.height(24.dp))
+                    var hidden by remember { mutableStateOf(true) }
+                    HidableContent(viewModel.ethereumPrivateKey, hidden) {
+                        hidden = it
+                    }
+                }
+                ActionButton(R.string.Alert_Copy) {
+                    coroutineScope.launch {
+                        sheetState.show()
+                    }
+                }
             }
         }
     }
