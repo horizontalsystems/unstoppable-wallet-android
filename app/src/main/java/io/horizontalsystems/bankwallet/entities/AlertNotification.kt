27c397f52ba5643fad56ed0f85384945c45d5adc
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-09-16 11:56:55 +0600

    Convert QRScannerActivity.kt to Compose

--- app/src/main/java/io/horizontalsystems/bankwallet/entities/AlertNotification.kt
+++
@@ -1,8 +0,0 @@
-package io.horizontalsystems.bankwallet.entities
-
-class AlertNotification(
-        val id: Int,
-        val title: String,
-        val body: String,
-        val timestamp: Long
-)
