5832c5ec834a74339ebc6af195692e96ad70d170
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2022-09-26 16:29:44 +0600

    Fill image view in NFT Activity list

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/nft/collection/events/NftCollectionEventsScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/nft/collection/events/NftCollectionEventsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -166,6 +167,7 @@ fun NftEvent(
                     .padding(end = 16.dp)
                     .size(24.dp)
                     .clip(RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
             )
         }
         Column(
