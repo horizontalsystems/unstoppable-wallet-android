bac7c5fbb3c419bc06cfe6f8caf6ae2a8e2e1d23
bakyt <abdrasulov@gmail.com>
2022-11-17 16:26:52 +0600

    Move wc2 related stuff to its own ViewModel

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/session/v2/WC2MainViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/session/v2/WC2MainViewModel.kt
@@ -4,14 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.modules.walletconnect.version2.WC2Request
 import io.horizontalsystems.bankwallet.modules.walletconnect.version2.WC2Service
+import io.horizontalsystems.bankwallet.modules.walletconnect.version2.WC2SessionManager
 import io.horizontalsystems.core.SingleLiveEvent
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactive.asFlow
 
-class WC2MainViewModel(private val wc2Service: WC2Service) : ViewModel() {
+class WC2MainViewModel(
+    private val wc2Service: WC2Service,
+    private val wcSessionManager: WC2SessionManager,
+) : ViewModel() {
 
     val sessionProposalLiveEvent = SingleLiveEvent<Unit>()
+    val openWalletConnectRequestLiveEvent = SingleLiveEvent<WC2Request>()
 
     init {
         viewModelScope.launch {
@@ -22,12 +28,19 @@ class WC2MainViewModel(private val wc2Service: WC2Service) : ViewModel() {
                     }
                 }
         }
+
+        viewModelScope.launch {
+            wcSessionManager.pendingRequestObservable.asFlow()
+                .collect {
+                    openWalletConnectRequestLiveEvent.postValue(it)
+                }
+        }
     }
 
     class Factory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return WC2MainViewModel(App.wc2Service) as T
+            return WC2MainViewModel(App.wc2Service, App.wc2SessionManager) as T
         }
     }
 }
