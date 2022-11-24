8bd55481648d423fb0bc3156dd49cd14896e3ab6
bakyt <abdrasulov@gmail.com>
2022-11-24 18:41:25 +0600

    Add pending requests block to wc2 session screen

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/version2/WC2RequestViewItem.kt
@@ -0,0 +1,7 @@
+package io.horizontalsystems.bankwallet.modules.walletconnect.version2
+
+data class WC2RequestViewItem(
+    val requestId: Long,
+    val title: String,
+    val subtitle: String,
+)
