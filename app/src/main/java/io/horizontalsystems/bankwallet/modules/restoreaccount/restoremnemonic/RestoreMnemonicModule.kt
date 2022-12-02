57e62e86c3d64f899043e8e2d78b53bcbd0909e7
bakyt <abdrasulov@gmail.com>
2022-12-02 14:03:30 +0600

    Allow any symbols for passphrase when restoring account

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/restoreaccount/restoremnemonic/RestoreMnemonicModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/restoreaccount/restoremnemonic/RestoreMnemonicModule.kt
@@ -3,7 +3,6 @@ package io.horizontalsystems.bankwallet.modules.restoreaccount.restoremnemonic
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.bankwallet.core.managers.PassphraseValidator
 import io.horizontalsystems.bankwallet.entities.AccountType
 import io.horizontalsystems.hdwalletkit.Language
 
@@ -14,7 +13,6 @@ object RestoreMnemonicModule {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return RestoreMnemonicViewModel(
                 App.accountFactory,
-                PassphraseValidator(),
                 App.wordsManager,
                 App.thirdKeyboardStorage,
             ) as T
