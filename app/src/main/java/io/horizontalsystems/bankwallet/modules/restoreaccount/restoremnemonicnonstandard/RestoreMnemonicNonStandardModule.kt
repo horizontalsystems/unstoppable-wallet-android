692ae8cae6a565da9f1c8410e334a0c4ef2a3dfa
bakyt <abdrasulov@gmail.com>
2022-12-05 12:55:02 +0600

    Add non-standard restore screen

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/restoreaccount/restoremnemonicnonstandard/RestoreMnemonicNonStandardModule.kt
@@ -0,0 +1,32 @@
+package io.horizontalsystems.bankwallet.modules.restoreaccount.restoremnemonicnonstandard
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.entities.AccountType
+import io.horizontalsystems.bankwallet.modules.restoreaccount.restoremnemonic.RestoreMnemonicModule
+import io.horizontalsystems.hdwalletkit.Language
+
+object RestoreMnemonicNonStandardModule {
+
+    class Factory : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return RestoreMnemonicNonStandardViewModel(
+                App.accountFactory,
+                App.wordsManager,
+                App.thirdKeyboardStorage,
+            ) as T
+        }
+    }
+
+    data class UiState(
+        val passphraseEnabled: Boolean,
+        val passphraseError: String?,
+        val invalidWordRanges: List<IntRange>,
+        val error: String?,
+        val accountType: AccountType?,
+        val wordSuggestions: RestoreMnemonicModule.WordSuggestions?,
+        val language: Language,
+    )
+}
