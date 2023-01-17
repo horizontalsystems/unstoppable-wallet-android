4f44bcda42a6d0e32b5f8028811027480b41a7eb
chyngyz <chyngyz.omurov@gmail.com>
2023-01-17 13:38:34 +0600

    Create wallet flow changes
    - remove background image from default create wallet screen
    - add wallet name input field for both default and advanced screens

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/createaccount/CreateAccountIntroFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/createaccount/CreateAccountIntroFragment.kt
@@ -4,22 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -28,14 +24,13 @@ import io.horizontalsystems.bankwallet.core.BaseFragment
 import io.horizontalsystems.bankwallet.core.slideFromRight
 import io.horizontalsystems.bankwallet.modules.manageaccounts.ManageAccountsModule
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.*
 import io.horizontalsystems.core.findNavController
 import io.horizontalsystems.core.helpers.HudHelper
 import kotlinx.coroutines.delay
 
 class CreateAccountIntroFragment : BaseFragment() {
-
-    private val nightMode by lazy { AppCompatDelegate.getDefaultNightMode() == MODE_NIGHT_YES }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,14 +42,14 @@ class CreateAccountIntroFragment : BaseFragment() {
             setContent {
                 val popUpToInclusiveId =
                     arguments?.getInt(ManageAccountsModule.popOffOnSuccessKey, R.id.createAccountIntroFragment) ?: R.id.createAccountIntroFragment
-                CreateAccountIntroScreen(findNavController(), popUpToInclusiveId, nightMode)
+                CreateAccountIntroScreen(findNavController(), popUpToInclusiveId)
             }
         }
     }
 }
 
 @Composable
-private fun CreateAccountIntroScreen(navController: NavController, popUpToInclusiveId: Int, nightMode: Boolean) {
+private fun CreateAccountIntroScreen(navController: NavController, popUpToInclusiveId: Int) {
     val viewModel = viewModel<CreateAccountViewModel>(factory = CreateAccountModule.Factory())
     val view = LocalView.current
 
@@ -74,58 +69,58 @@ private fun CreateAccountIntroScreen(navController: NavController, popUpToInclus
     }
 
     ComposeAppTheme {
-        Box {
-            Image(
-                painter = painterResource(if (nightMode) R.drawable.ic_intro_background else R.drawable.ic_intro_background_light),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
-            )
-        }
-
-        Column(Modifier.fillMaxSize()) {
-            AppBar(
-                navigationIcon = {
-                    HsIconButton(onClick = { navController.popBackStack() }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_back),
-                            contentDescription = "back button",
-                            tint = ComposeAppTheme.colors.jacob
+        Surface(color = ComposeAppTheme.colors.tyler) {
+            Column(Modifier.fillMaxSize()) {
+                AppBar(
+                    title = TranslatableString.ResString(R.string.CreateWallet_Default_Title),
+                    menuItems = listOf(
+                        MenuItem(
+                            title = TranslatableString.ResString(R.string.Button_Create),
+                            onClick = viewModel::createAccount
                         )
+                    ),
+                    navigationIcon = {
+                        HsIconButton(onClick = { navController.popBackStack() }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_back),
+                                contentDescription = "back button",
+                                tint = ComposeAppTheme.colors.jacob
+                            )
+                        }
+                    },
+                    backgroundColor = Color.Transparent
+                )
+                Spacer(Modifier.height(12.dp))
+
+                HeaderText(stringResource(id = R.string.ManageAccount_Name))
+                FormsInput(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    initial = viewModel.accountName,
+                    hint = "",
+                    onValueChange = viewModel::onChangeAccountName
+                )
+
+                Spacer(Modifier.weight(1f))
+
+                ButtonPrimaryYellow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    title = stringResource(R.string.Button_Create),
+                    onClick = viewModel::createAccount
+                )
+                Spacer(Modifier.height(16.dp))
+                ButtonPrimaryTransparent(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    title = stringResource(R.string.Button_Advanced),
+                    onClick = {
+                        navController.slideFromRight(R.id.createAccountFragment, ManageAccountsModule.prepareParams(popUpToInclusiveId))
                     }
-                },
-                backgroundColor = Color.Transparent
-            )
-            Spacer(Modifier.weight(1f))
-
-            title2_leah(
-                modifier = Modifier.padding(horizontal = 48.dp),
-                text = stringResource(R.string.CreateWallet_Intro_Title),
-                textAlign = TextAlign.Center
-            )
-
-            Spacer(Modifier.weight(1f))
-
-            ButtonPrimaryYellow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp),
-                title = stringResource(R.string.Button_Create),
-                onClick = {
-                    viewModel.createAccount()
-                }
-            )
-            Spacer(Modifier.height(16.dp))
-            ButtonPrimaryTransparent(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp),
-                title = stringResource(R.string.Button_Advanced),
-                onClick = {
-                    navController.slideFromRight(R.id.createAccountFragment, ManageAccountsModule.prepareParams(popUpToInclusiveId))
-                }
-            )
-            Spacer(Modifier.height(32.dp))
+                )
+                Spacer(Modifier.height(32.dp))
+            }
         }
     }
 }
