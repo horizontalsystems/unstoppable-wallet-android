533b37456abb12c0ff4bc92f5ff1e4b975b9d19f
bakyt <abdrasulov@gmail.com>
2023-02-08 13:00:12 +0600

    Fix showing error of address

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/address/AddressValidationException.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/address/AddressValidationException.kt
@@ -1,9 +1,12 @@
 package io.horizontalsystems.bankwallet.modules.address
 
+import android.os.Parcelable
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.providers.Translator
+import kotlinx.parcelize.Parcelize
 
-sealed class AddressValidationException : Exception() {
+@Parcelize
+sealed class AddressValidationException : Exception(), Parcelable {
     class Blank : AddressValidationException()
     class Unsupported : AddressValidationException()
     class Invalid(override val cause: Throwable) : AddressValidationException()
