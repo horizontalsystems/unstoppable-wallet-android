44f1a0d54473c5af2a87a7947ef49639208d2e4c
bakyt <abdrasulov@gmail.com>
2023-02-08 13:00:12 +0600

    Fix showing error of address

--- app/src/main/java/cash/p/terminal/modules/address/AddressValidationException.kt
+++ app/src/main/java/cash/p/terminal/modules/address/AddressValidationException.kt
@@ -1,9 +1,12 @@
 package cash.p.terminal.modules.address
 
+import android.os.Parcelable
 import cash.p.terminal.R
 import cash.p.terminal.core.providers.Translator
+import kotlinx.parcelize.Parcelize
 
-sealed class AddressValidationException : Exception() {
+@Parcelize
+sealed class AddressValidationException : Exception(), Parcelable {
     class Blank : AddressValidationException()
     class Unsupported : AddressValidationException()
     class Invalid(override val cause: Throwable) : AddressValidationException()
