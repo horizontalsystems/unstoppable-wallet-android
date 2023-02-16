bbe1d37189c9a474feecc322874268613c76deb0
bakyt <abdrasulov@gmail.com>
2023-02-16 16:54:03 +0600

    Remove CoinState

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/CoinService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/CoinService.kt
@@ -1,6 +1,6 @@
 package io.horizontalsystems.bankwallet.modules.coin
 
-import io.horizontalsystems.bankwallet.core.*
+import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.managers.MarketFavoritesManager
 import io.horizontalsystems.marketkit.models.FullCoin
 import io.reactivex.Observable
@@ -10,49 +10,16 @@ import io.reactivex.subjects.BehaviorSubject
 class CoinService(
     val fullCoin: FullCoin,
     private val marketFavoritesManager: MarketFavoritesManager,
-    private val walletManager: IWalletManager,
-    private val accountManager: IAccountManager,
 ) : Clearable {
 
     private val _isFavorite = BehaviorSubject.create<Boolean>()
     val isFavorite: Observable<Boolean>
         get() = _isFavorite
 
-    private val _coinState = BehaviorSubject.create<CoinState>()
-    val coinState: Observable<CoinState>
-        get() = _coinState
-
-    private val initialCoinInWallet = walletManager.activeWallets.any { it.coin.uid == fullCoin.coin.uid }
     private val disposables = CompositeDisposable()
 
     init {
-        emitCoinState()
         emitIsFavorite()
-
-        walletManager.activeWalletsUpdatedObservable
-            .subscribeIO {
-                emitCoinState()
-            }
-            .let {
-                disposables.add(it)
-            }
-    }
-
-    private fun emitCoinState() {
-        val activeAccount = accountManager.activeAccount
-        _coinState.onNext(when {
-            activeAccount == null -> CoinState.NoActiveAccount
-            activeAccount.isWatchAccount -> CoinState.WatchAccount
-            fullCoin.eligibleTokens(activeAccount.type).isEmpty() -> CoinState.Unsupported
-            walletManager.activeWallets.any { it.coin.uid == fullCoin.coin.uid } -> {
-                if (initialCoinInWallet) {
-                    CoinState.InWallet
-                } else {
-                    CoinState.AddedToWallet
-                }
-            }
-            else -> CoinState.NotInWallet
-        })
     }
 
     override fun clear() {
