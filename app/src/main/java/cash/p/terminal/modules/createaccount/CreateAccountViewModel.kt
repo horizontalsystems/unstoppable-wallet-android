b82c35affbdbdcf7b1cda9294aa8ae784f01ff38
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2023-02-19 00:51:15 +0200

    Fix restore wallet from mnemonic phrase

--- app/src/main/java/cash/p/terminal/modules/createaccount/CreateAccountViewModel.kt
+++ app/src/main/java/cash/p/terminal/modules/createaccount/CreateAccountViewModel.kt
@@ -139,8 +139,8 @@ class CreateAccountViewModel(
             TokenQuery(BlockchainType.Ethereum, TokenType.Native),
             TokenQuery(BlockchainType.BinanceSmartChain, TokenType.Native),
             //TokenQuery(BlockchainType.Ethereum, TokenType.Eip20("0xdac17f958d2ee523a2206206994597c13d831ec7")),
-            TokenQuery(BlockchainType.BinanceSmartChain, TokenType.Eip20("0x35f3fffFcb622bC9f64fA561D74e983Fd488D90c")),
-            TokenQuery(BlockchainType.BinanceSmartChain, TokenType.Eip20("0x499954F9C977B74a48D4BB14BA9904bEA6CB7B01")),
+            TokenQuery(BlockchainType.BinanceSmartChain, TokenType.Eip20("0x35f3ffffcb622bc9f64fa561d74e983fd488d90c")),
+            TokenQuery(BlockchainType.BinanceSmartChain, TokenType.Eip20("0x499954f9c977b74a48d4bb14ba9904bea6cb7b01")),
             TokenQuery(BlockchainType.BinanceSmartChain, TokenType.Eip20("0xe9e7cea3dedca5984780bafc599bd69add087d56")),
         )
         walletActivator.activateWallets(account, tokenQueries)
