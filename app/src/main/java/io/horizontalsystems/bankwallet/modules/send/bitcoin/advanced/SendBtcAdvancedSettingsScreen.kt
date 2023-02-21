f28c89e9c1d53387da627bff5f005f837c525d95
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-02-21 12:46:11 +0600

    Change Transaction order setting layout

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/advanced/SendBtcAdvancedSettingsScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/advanced/SendBtcAdvancedSettingsScreen.kt
@@ -1,13 +1,15 @@
 package io.horizontalsystems.bankwallet.modules.send.bitcoin.advanced
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -15,16 +17,17 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.entities.TransactionDataSortMode
-import io.horizontalsystems.bankwallet.modules.btcblockchainsettings.BlockchainSettingCell
 import io.horizontalsystems.bankwallet.modules.hodler.HSHodlerInput
 import io.horizontalsystems.bankwallet.modules.send.bitcoin.TransactionInputsSortInfoPage
-import io.horizontalsystems.bankwallet.modules.send.bitcoin.advanced.SendBtcAdvancedSettingsModule.SortModeViewItem
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.*
+import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
 import io.horizontalsystems.hodler.LockTimeInterval
 import io.horizontalsystems.marketkit.models.BlockchainType
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SendBtcAdvancedSettingsScreen(
     navController: NavHostController,
@@ -34,86 +37,150 @@ fun SendBtcAdvancedSettingsScreen(
     lockTimeInterval: LockTimeInterval?,
     onEnterLockTimeInterval: (LockTimeInterval?) -> Unit,
 ) {
+    val viewModel: SendBtcAdvancedSettingsViewModel =
+        viewModel(factory = SendBtcAdvancedSettingsModule.Factory(blockchainType))
+
+    val coroutineScope = rememberCoroutineScope()
+    val modalBottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     var selectedLockTimeInterval by remember { mutableStateOf(lockTimeInterval) }
     ComposeAppTheme {
-        Column(modifier = Modifier.background(color = ComposeAppTheme.colors.tyler)) {
-            AppBar(
-                title = TranslatableString.ResString(R.string.Send_Advanced),
-                navigationIcon = {
-                    HsBackButton(onClick = { navController.popBackStack() })
-                },
-            )
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                val viewModel: SendBtcAdvancedSettingsViewModel =
-                    viewModel(factory = SendBtcAdvancedSettingsModule.Factory(blockchainType))
-
-                TransactionDataSortSettings(
-                    navController,
-                    viewModel.sortModeViewItems
-                ) { viewModel.setTransactionMode(it) }
-
-                if (lockTimeEnabled) {
-                    Spacer(Modifier.height(32.dp))
-                    CellUniversalLawrenceSection(
-                        listOf {
-                            HSHodlerInput(
-                                lockTimeIntervals = lockTimeIntervals,
-                                lockTimeInterval = selectedLockTimeInterval,
-                                onSelect = {
-                                    selectedLockTimeInterval = it
-                                    onEnterLockTimeInterval(it)
-                                }
-                            )
+        ModalBottomSheetLayout(
+            sheetState = modalBottomSheetState,
+            sheetBackgroundColor = ComposeAppTheme.colors.transparent,
+            sheetContent = {
+                BottomSheetTransactionOrderSelector(
+                    items = viewModel.uiState.transactionSortOptions,
+                    onSelect = { mode ->
+                        viewModel.setTransactionMode(mode)
+                    },
+                    onCloseClick = {
+                        coroutineScope.launch {
+                            modalBottomSheetState.animateTo(ModalBottomSheetValue.Hidden)
                         }
-                    )
-                    InfoText(
-                        text = stringResource(R.string.Send_Hodler_Description),
-                    )
+                    }
+                )
+            },
+        ) {
+            Column(modifier = Modifier.background(color = ComposeAppTheme.colors.tyler)) {
+                AppBar(
+                    title = TranslatableString.ResString(R.string.Send_Advanced),
+                    navigationIcon = {
+                        HsBackButton(onClick = { navController.popBackStack() })
+                    },
+                )
+                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+
+                    TransactionDataSortSettings(
+                        navController,
+                        viewModel.uiState.transactionSortTitle,
+                    ) {
+                        coroutineScope.launch {
+                            modalBottomSheetState.animateTo(ModalBottomSheetValue.Expanded)
+                        }
+                    }
+
+                    if (lockTimeEnabled) {
+                        Spacer(Modifier.height(32.dp))
+                        CellUniversalLawrenceSection(
+                            listOf {
+                                HSHodlerInput(
+                                    lockTimeIntervals = lockTimeIntervals,
+                                    lockTimeInterval = selectedLockTimeInterval,
+                                    onSelect = {
+                                        selectedLockTimeInterval = it
+                                        onEnterLockTimeInterval(it)
+                                    }
+                                )
+                            }
+                        )
+                        InfoText(
+                            text = stringResource(R.string.Send_Hodler_Description),
+                        )
+                    }
+                    Spacer(Modifier.height(32.dp))
                 }
-                Spacer(Modifier.height(32.dp))
             }
         }
     }
 }
 
 @Composable
-private fun TransactionDataSortSettings(
-    navController: NavController,
-    sortModeViewItems: List<SortModeViewItem>,
-    onSelect: (TransactionDataSortMode) -> Unit
+private fun BottomSheetTransactionOrderSelector(
+    items: List<SendBtcAdvancedSettingsModule.SortModeViewItem>,
+    onSelect: (TransactionDataSortMode) -> Unit,
+    onCloseClick: () -> Unit
 ) {
-    SettingSection(
-        items = sortModeViewItems,
-        settingTitleTextRes = R.string.BtcBlockchainSettings_TransactionInputsOutputs,
-        settingDescriptionTextRes = R.string.BtcBlockchainSettings_TransactionInputsOutputsSettingsDescription,
-        onItemClick = { viewItem -> onSelect.invoke(viewItem.mode) },
-        navController = navController
-    )
+    BottomSheetHeader(
+        iconPainter = painterResource(R.drawable.ic_arrow_up_right_12),
+        title = stringResource(R.string.BtcBlockchainSettings_TransactionSettings),
+        onCloseClick = onCloseClick,
+        iconTint = ColorFilter.tint(ComposeAppTheme.colors.grey)
+    ) {
+        Spacer(Modifier.height(12.dp))
+        CellUniversalLawrenceSection(items, showFrame = true) { item ->
+            RowUniversal(
+                onClick = {
+                    onSelect.invoke(item.mode)
+                    onCloseClick.invoke()
+                },
+            ) {
+                Spacer(Modifier.width(16.dp))
+                Column(Modifier.weight(1f)) {
+                    body_leah(text = stringResource(item.mode.title))
+                    subhead2_grey(text = stringResource(item.mode.description))
+                }
+                Box(
+                    modifier = Modifier
+                        .width(52.dp)
+                        .fillMaxHeight(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    if (item.selected) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_checkmark_20),
+                            tint = ComposeAppTheme.colors.jacob,
+                            contentDescription = null,
+                        )
+                    }
+                }
+            }
+        }
+        Spacer(Modifier.height(44.dp))
+    }
 }
 
 @Composable
-private fun SettingSection(
-    items: List<SortModeViewItem>,
-    settingTitleTextRes: Int,
-    settingDescriptionTextRes: Int,
-    onItemClick: (SortModeViewItem) -> Unit,
-    navController: NavController
+private fun TransactionDataSortSettings(
+    navController: NavController,
+    valueTitle: String,
+    onClick: () -> Unit
 ) {
     HeaderText(
-        text = stringResource(settingTitleTextRes),
+        text = stringResource(R.string.BtcBlockchainSettings_TransactionSettings),
         onInfoClick = {
             navController.navigate(TransactionInputsSortInfoPage)
         })
-    CellUniversalLawrenceSection(items) { item ->
-        BlockchainSettingCell(
-            title = stringResource(item.mode.title),
-            subtitle = stringResource(item.mode.description),
-            checked = item.selected
-        ) {
-            onItemClick(item)
+    CellUniversalLawrenceSection(
+        listOf {
+            RowUniversal() {
+                body_leah(
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .weight(1f),
+                    text = stringResource(R.string.BtcBlockchainSettings_InputsOutputs)
+                )
+                ButtonSecondaryWithIcon(
+                    modifier = Modifier
+                        .padding(end = 16.dp)
+                        .height(28.dp),
+                    title = valueTitle,
+                    iconRight = painterResource(R.drawable.ic_down_arrow_20),
+                    onClick = onClick
+                )
+            }
         }
-    }
+    )
     InfoText(
-        text = stringResource(settingDescriptionTextRes),
+        text = stringResource(R.string.BtcBlockchainSettings_TransactionInputsOutputsSettingsDescription),
     )
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/advanced/SendBtcAdvancedSettingsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/advanced/SendBtcAdvancedSettingsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import io.horizontalsystems.bankwallet.core.managers.BtcBlockchainManager
+import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.bankwallet.entities.TransactionDataSortMode
 import io.horizontalsystems.bankwallet.modules.send.bitcoin.advanced.SendBtcAdvancedSettingsModule.SortModeViewItem
 import io.horizontalsystems.marketkit.models.BlockchainType
@@ -15,13 +16,27 @@ class SendBtcAdvancedSettingsViewModel(
 ) : ViewModel() {
 
     private var sortMode = btcBlockchainManager.transactionSortMode(blockchainType)
+    private val sortOptions: List<SortModeViewItem>
+        get() = getTransactionSortModeViewItems()
 
-    var sortModeViewItems by mutableStateOf(getTransactionSortModeViewItems())
+    var uiState by mutableStateOf(
+        SendBtcAdvancedSettingsModule.UiState(
+            transactionSortOptions = sortOptions,
+            transactionSortTitle = Translator.getString(sortMode.titleShort)
+        )
+    )
 
     fun setTransactionMode(mode: TransactionDataSortMode) {
         sortMode = mode
         btcBlockchainManager.save(sortMode, blockchainType)
-        sortModeViewItems = getTransactionSortModeViewItems()
+        syncState()
+    }
+
+    private fun syncState() {
+        uiState = SendBtcAdvancedSettingsModule.UiState(
+            transactionSortOptions = sortOptions,
+            transactionSortTitle = Translator.getString(sortMode.titleShort)
+        )
     }
 
     private fun getTransactionSortModeViewItems(): List<SortModeViewItem> {
