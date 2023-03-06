b8b65c9e095f01d154d7398bce8f2724434f2c11
bakyt <abdrasulov@gmail.com>
2023-03-06 17:45:15 +0600

    Show contacts icon when contact list is not empty

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/address/AddressViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/address/AddressViewModel.kt
@@ -2,17 +2,24 @@ package io.horizontalsystems.bankwallet.modules.address
 
 import androidx.lifecycle.ViewModel
 import io.horizontalsystems.bankwallet.entities.Address
+import io.horizontalsystems.bankwallet.modules.contacts.ContactsRepository
 import io.horizontalsystems.marketkit.models.BlockchainType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class AddressViewModel(val blockchainType: BlockchainType) : ViewModel() {
+class AddressViewModel(
+    val blockchainType: BlockchainType,
+    private val contactsRepository: ContactsRepository
+) : ViewModel() {
 
     private val addressHandlers = mutableListOf<IAddressHandler>()
 
     fun addAddressHandler(handler: IAddressHandler) {
         addressHandlers.add(handler)
     }
+
+    fun hasContacts() =
+        contactsRepository.getContactsByBlockchainType(blockchainType).isNotEmpty()
 
     @Throws(AddressValidationException::class)
     suspend fun parseAddress(value: String): Address = withContext(Dispatchers.IO) {
