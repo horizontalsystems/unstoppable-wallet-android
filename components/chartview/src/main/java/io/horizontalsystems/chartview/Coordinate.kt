1714dd46d4fce180b48d2ec354606942392906d0
bakyt <abdrasulov@gmail.com>
2023-03-15 19:06:19 +0600

    Get rid off `ChartDataValueImmutable`

--- components/chartview/src/main/java/io/horizontalsystems/chartview/Coordinate.kt
+++ components/chartview/src/main/java/io/horizontalsystems/chartview/Coordinate.kt
@@ -1,10 +1,7 @@
 package io.horizontalsystems.chartview
 
-import io.horizontalsystems.chartview.models.PointInfo
-
 data class Coordinate(
     val x: Float,
     val y: Float,
-    val point: PointInfo,
     val item: ChartDataItemImmutable,
 )
