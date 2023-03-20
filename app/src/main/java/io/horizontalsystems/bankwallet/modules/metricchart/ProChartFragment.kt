79e43a2b7dd3f48ce468fb0b3c7633f6bda81453
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-03-20 18:43:09 +0600

    Make fixes in Coin Analytics bottom charts
    - Remove unused Info fragments and strings

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/ProChartFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/ProChartFragment.kt
@@ -7,13 +7,15 @@ import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.chart.ChartViewModel
+import io.horizontalsystems.bankwallet.modules.coin.overview.ui.Chart
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
 import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
 import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
 
@@ -30,10 +32,6 @@ class ProChartFragment : BaseComposableBottomSheetFragment() {
 
     private val title by lazy {
         requireArguments().getString(titleKey) ?: ""
-    }
-
-    private val description by lazy {
-        requireArguments().getString(descriptionKey) ?: ""
     }
 
     private val chartViewModel by viewModels<ChartViewModel> {
@@ -58,11 +56,8 @@ class ProChartFragment : BaseComposableBottomSheetFragment() {
                         title = title,
                         onCloseClick = { close() }
                     ) {
-                        MetricChartScreen(
-                            chartViewModel = chartViewModel,
-                            description = description,
-                            poweredBy = stringResource(R.string.Market_PoweredByHorizontalApi)
-                        )
+                        Chart(chartViewModel = chartViewModel)
+                        VSpacer(32.dp)
                     }
                 }
             }
@@ -73,11 +68,10 @@ class ProChartFragment : BaseComposableBottomSheetFragment() {
         private const val coinUidKey = "coinUidKey"
         private const val chartTypeKey = "chartTypeKey"
         private const val titleKey = "titleKey"
-        private const val descriptionKey = "descriptionKey"
 
-        fun show(fragmentManager: FragmentManager, coinUid: String, chartType: ProChartModule.ChartType, title: String, description: String) {
+        fun show(fragmentManager: FragmentManager, coinUid: String, chartType: ProChartModule.ChartType, title: String) {
             val fragment = ProChartFragment()
-            fragment.arguments = bundleOf(coinUidKey to coinUid, titleKey to title, descriptionKey to description, chartTypeKey to chartType.ordinal)
+            fragment.arguments = bundleOf(coinUidKey to coinUid, titleKey to title, chartTypeKey to chartType.ordinal)
             fragment.show(fragmentManager, "pro_chart_dialog")
         }
     }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/ProChartModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/ProChartModule.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.bankwallet.modules.chart.ChartCoinValueFormatterShortened
 import io.horizontalsystems.bankwallet.modules.chart.ChartCurrencyValueFormatterShortened
 import io.horizontalsystems.bankwallet.modules.chart.ChartModule
 import io.horizontalsystems.bankwallet.modules.chart.ChartNumberFormatterShortened
@@ -15,24 +14,24 @@ object ProChartModule {
 
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            val fullCoin = App.marketKit.fullCoins(coinUids = listOf(coinUid)).first()
-
             val chartService = ProChartService(App.currencyManager, App.proFeatureAuthorizationManager, App.marketKit, coinUid, chartType)
             val chartNumberFormatter = when (chartType) {
-                ChartType.DexVolume, ChartType.DexLiquidity -> ChartCurrencyValueFormatterShortened()
-                ChartType.TxVolume -> ChartCoinValueFormatterShortened(fullCoin)
-                ChartType.TxCount, ChartType.AddressesCount -> ChartNumberFormatterShortened()
+                ChartType.DexVolume,
+                ChartType.Tvl,
+                ChartType.DexLiquidity -> ChartCurrencyValueFormatterShortened()
+                ChartType.TxCount,
+                ChartType.AddressesCount -> ChartNumberFormatterShortened()
             }
             return ChartModule.createViewModel(chartService, chartNumberFormatter) as T
         }
     }
 
-    enum class ChartType(val titleRes: Int, val descriptionRes: Int) {
-        DexVolume(R.string.CoinPage_DetailsDexVolume, R.string.CoinPage_DetailsDexVolume_Description),
-        DexLiquidity(R.string.CoinPage_DetailsDexLiquidity, R.string.CoinPage_DetailsDexLiquidity_Description),
-        TxCount(R.string.CoinPage_DetailsTxCount, R.string.CoinPage_DetailsTxCount_Description),
-        TxVolume(R.string.CoinPage_DetailsTxVolume, R.string.CoinPage_DetailsTxVolume_Description),
-        AddressesCount(R.string.CoinPage_DetailsActiveAddresses, R.string.CoinPage_DetailsActiveAddresses_Description)
+    enum class ChartType(val titleRes: Int) {
+        DexVolume(R.string.CoinAnalytics_DexVolume),
+        DexLiquidity(R.string.CoinAnalytics_DexLiquidity),
+        TxCount(R.string.CoinAnalytics_TransactionCount),
+        AddressesCount(R.string.CoinAnalytics_ActiveAddresses),
+        Tvl(R.string.CoinAnalytics_ProjectTvl)
     }
 
 }
