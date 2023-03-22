399c543b0318593cd5e96cbb18f797f18ba3cda6
bakyt <abdrasulov@gmail.com>
2023-03-22 16:27:23 +0600

    Simplify chart start and end timestamps

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/chart/ChartPointsWrapper.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/chart/ChartPointsWrapper.kt
@@ -4,14 +4,5 @@ import io.horizontalsystems.chartview.models.ChartPoint
 
 data class ChartPointsWrapper(
     val items: List<ChartPoint>,
-    val startTimestamp: Long?,
-    val endTimestamp: Long?,
     val isMovementChart: Boolean = true,
-) {
-    constructor(items: List<ChartPoint>, isMovementChart: Boolean = true) : this(
-        items,
-        items.firstOrNull()?.timestamp,
-        items.lastOrNull()?.timestamp,
-        isMovementChart
-    )
-}
+)
