6753456a2f549ece5cb4f38fbf397a39c43b7e55
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-06-07 16:58:31 +0600

    Show attempts left on failed pin input

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/core/LockoutState.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/core/LockoutState.kt
@@ -1,8 +1,8 @@
 package io.horizontalsystems.bankwallet.modules.pin.core
 
-import java.util.*
+import java.util.Date
 
 sealed class LockoutState {
-    data class Unlocked(val hasFailedAttempts: Boolean) : LockoutState()
+    data class Unlocked(val attemptsLeft: Int?) : LockoutState()
     data class Locked(val until: Date) : LockoutState()
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinNumpad.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinNumpad.kt
@@ -3,7 +3,16 @@ package io.horizontalsystems.bankwallet.modules.pin.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -28,10 +37,10 @@ fun PinNumpad(
     onNumberClick: (Int) -> Unit,
     onDeleteClick: () -> Unit,
     showBiometricPrompt: (() -> Unit)? = null,
-    inputState: InputState = InputState.Enabled
+    inputState: InputState = InputState.Enabled()
 ) {
 
-    val enabled = inputState == InputState.Enabled
+    val enabled = inputState is InputState.Enabled
 
     Column(
         modifier = Modifier
@@ -160,7 +169,7 @@ private fun ImageKey(
             Icon(
                 modifier = Modifier.align(Alignment.Center),
                 painter = painterResource(image),
-                tint = ComposeAppTheme.colors.grey,
+                tint = if (enabled) ComposeAppTheme.colors.grey else ComposeAppTheme.colors.steel20,
                 contentDescription = contentDescription,
             )
         }
