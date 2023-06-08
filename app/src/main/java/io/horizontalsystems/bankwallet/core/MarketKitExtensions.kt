466c288c93c3a2aa7640d2686ec2f93f929fa8c1
bakyt <abdrasulov@gmail.com>
2023-06-08 14:25:59 +0600

    Fix coin page when it's opened with active cex account

--- app/src/main/java/io/horizontalsystems/bankwallet/core/MarketKitExtensions.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/MarketKitExtensions.kt
@@ -4,22 +4,10 @@ import androidx.compose.ui.graphics.Color
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.managers.RestoreSettingType
 import io.horizontalsystems.bankwallet.core.providers.Translator
-import io.horizontalsystems.bankwallet.entities.AccountType
-import io.horizontalsystems.bankwallet.entities.BitcoinCashCoinType
-import io.horizontalsystems.bankwallet.entities.CoinSettingType
-import io.horizontalsystems.bankwallet.entities.CoinSettings
-import io.horizontalsystems.bankwallet.entities.FeePriceScale
-import io.horizontalsystems.bankwallet.entities.derivation
+import io.horizontalsystems.bankwallet.entities.*
 import io.horizontalsystems.hdwalletkit.ExtendedKeyCoinType
 import io.horizontalsystems.hdwalletkit.HDWallet
-import io.horizontalsystems.marketkit.models.Blockchain
-import io.horizontalsystems.marketkit.models.BlockchainType
-import io.horizontalsystems.marketkit.models.Coin
-import io.horizontalsystems.marketkit.models.FullCoin
-import io.horizontalsystems.marketkit.models.Token
-import io.horizontalsystems.marketkit.models.TokenQuery
-import io.horizontalsystems.marketkit.models.TokenType
-import io.horizontalsystems.marketkit.models.TopPlatform
+import io.horizontalsystems.marketkit.models.*
 import io.horizontalsystems.nftkit.models.NftType
 
 val Token.protocolType: String?
@@ -332,7 +320,7 @@ fun BlockchainType.supports(accountType: AccountType): Boolean {
         is AccountType.TronAddress ->
             this == BlockchainType.Tron
 
-        is AccountType.Cex -> TODO()
+        is AccountType.Cex -> false
     }
 }
 
