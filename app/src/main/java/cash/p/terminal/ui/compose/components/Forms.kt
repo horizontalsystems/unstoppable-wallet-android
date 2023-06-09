4de30fbc6571983ad1021ae2e430fc068cd2cd3a
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-06-09 11:56:43 +0600

    Fix password hide/show icon color

--- app/src/main/java/cash/p/terminal/ui/compose/components/Forms.kt
+++ app/src/main/java/cash/p/terminal/ui/compose/components/Forms.kt
@@ -8,14 +8,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -341,7 +354,7 @@ fun FormsInputPassword(
                     .clickable(onClick = onToggleHide, interactionSource = MutableInteractionSource(), indication = null),
                 painter = painterResource(id = if (hide) R.drawable.ic_eye_off_20 else R.drawable.ic_eye_20),
                 contentDescription = null,
-                tint = ComposeAppTheme.colors.leah
+                tint = ComposeAppTheme.colors.grey
             )
             Spacer(Modifier.width(16.dp))
         }
