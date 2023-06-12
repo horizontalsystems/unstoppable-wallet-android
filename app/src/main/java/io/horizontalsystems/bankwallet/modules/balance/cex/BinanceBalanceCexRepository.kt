5cff877d10b5a7881dd0e38b6fea6269d83797f8
bakyt <abdrasulov@gmail.com>
2023-06-12 19:07:15 +0600

    Add BinanceBalanceCexRepository

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/cex/BinanceBalanceCexRepository.kt
@@ -0,0 +1,53 @@
+package io.horizontalsystems.bankwallet.modules.balance.cex
+
+import com.binance.connector.client.impl.SpotClientImpl
+import com.google.gson.Gson
+import io.horizontalsystems.bankwallet.core.customCoinPrefix
+import io.horizontalsystems.marketkit.models.Coin
+import io.horizontalsystems.marketkit.models.TokenQuery
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import java.math.BigDecimal
+
+
+class BinanceBalanceCexRepository() : IBalanceCexRepository {
+    override val itemsFlow = MutableStateFlow<List<BalanceCexItem>?>(null)
+
+    private val apiKey = ""
+    private val secretKey = ""
+    private val gson = Gson()
+
+    override fun start() {
+        val client = SpotClientImpl(apiKey, secretKey)
+        val wallet = client.createWallet()
+
+        val coinInfo = gson.fromJson(wallet.coinInfo(mutableMapOf()), Array<CoinInfo>::class.java)
+
+        val balance = coinInfo.filter { it.free > BigDecimal.ZERO }
+
+        itemsFlow.update {
+            balance.map {
+                BalanceCexItem(
+                    balance = it.free,
+                    coin = Coin(
+                        uid = "${TokenQuery.customCoinPrefix}${it.coin}",
+                        name = it.name,
+                        code = it.coin
+                    ),
+                    decimals = 8
+                )
+            }
+        }
+    }
+
+    override fun stop() {
+
+    }
+
+    data class CoinInfo(
+        val coin: String,
+        val name: String,
+        val free: BigDecimal,
+        val freeze: BigDecimal,
+    )
+}
