131c2de6d83ece911bc3950b7ce6c5a30632976b
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-06-15 16:15:06 +0600

    Implement form UI for Coinzix

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/importcexaccount/ImportCexAccountEnterCexDataScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/importcexaccount/ImportCexAccountEnterCexDataScreen.kt
@@ -1,18 +1,38 @@
 package cash.p.terminal.modules.importcexaccount
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cash.p.terminal.R
 import cash.p.terminal.modules.evmfee.ButtonsGroupWithShade
 import cash.p.terminal.ui.compose.ComposeAppTheme
 import cash.p.terminal.ui.compose.TranslatableString
-import cash.p.terminal.ui.compose.components.*
+import cash.p.terminal.ui.compose.components.AppBar
+import cash.p.terminal.ui.compose.components.ButtonPrimaryTransparent
+import cash.p.terminal.ui.compose.components.ButtonPrimaryYellow
+import cash.p.terminal.ui.compose.components.ButtonPrimaryYellowWithSpinner
+import cash.p.terminal.ui.compose.components.FormsInput
+import cash.p.terminal.ui.compose.components.FormsInputPassword
+import cash.p.terminal.ui.compose.components.HsBackButton
+import cash.p.terminal.ui.compose.components.InfoText
+import cash.p.terminal.ui.compose.components.MenuItem
+import cash.p.terminal.ui.compose.components.VSpacer
 
 @Composable
 fun ImportCexAccountEnterCexDataScreen(
@@ -64,14 +84,68 @@ fun ImportCexAccountEnterCexDataScreen(
                 is CexBinance -> {
                     EnterCexDataBinanceForm(paddingValues, onAccountCreate)
                 }
-                is CexCoinzix -> {
 
+                is CexCoinzix -> {
+                    EnterCexDataCoinzixForm(paddingValues, onAccountCreate)
                 }
             }
 
         }
     }
 
+}
+
+@Composable
+private fun EnterCexDataCoinzixForm(paddingValues: PaddingValues, onAccountCreate: () -> Unit) {
+    var hidePassphrase by remember { mutableStateOf(true) }
+    Column(modifier = Modifier.padding(paddingValues)) {
+        InfoText(text = stringResource(R.string.ImportCexAccountConzix_Description))
+        VSpacer(height = 20.dp)
+        FormsInput(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            hint = stringResource(R.string.ImportCexAccountConzix_Email)
+        ) {
+            //viewModel.onEnterEmail(it)
+        }
+        VSpacer(height = 16.dp)
+        FormsInputPassword(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            hint = stringResource(R.string.Password),
+            //state = uiState.passphraseState,
+            onValueChange = {
+                //viewModel::onEnterPassword(it)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            hide = hidePassphrase,
+            onToggleHide = {
+                hidePassphrase = !hidePassphrase
+            }
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        ButtonsGroupWithShade {
+            Column(Modifier.padding(horizontal = 24.dp)) {
+                ButtonPrimaryYellowWithSpinner(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = stringResource(R.string.Button_Login),
+                    showSpinner = false,
+                    enabled = true,
+                    onClick = {
+                        //viewModel.onLogin()
+                    },
+                )
+                Spacer(Modifier.height(16.dp))
+                ButtonPrimaryTransparent(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = stringResource(R.string.Button_SignUp),
+                    onClick = {
+                        //viewModel.onSignUp()
+                    }
+                )
+            }
+        }
+    }
 }
 
 @Composable
