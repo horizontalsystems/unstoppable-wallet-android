ab04f0638fcf0280c3c66203c01659559ff05f32
chyngyz <chyngyz.omurov@gmail.com>
2023-06-19 16:46:14 +0600

    Implement importing Binance Cex account via QR code

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/importcexaccount/ImportCexAccountEnterCexDataScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/importcexaccount/ImportCexAccountEnterCexDataScreen.kt
@@ -3,6 +3,7 @@ package io.horizontalsystems.bankwallet.modules.importcexaccount
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
+import androidx.activity.result.ActivityResult
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
@@ -11,15 +12,19 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.utils.ModuleField
 import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
+import io.horizontalsystems.bankwallet.modules.qrscanner.QRScannerActivity
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.*
+import io.horizontalsystems.core.helpers.HudHelper
 
 @Composable
 fun ImportCexAccountEnterCexDataScreen(
@@ -28,6 +33,9 @@ fun ImportCexAccountEnterCexDataScreen(
     onClose: () -> Unit,
     onAccountCreate: () -> Unit,
 ) {
+    val context = LocalContext.current
+    var scannedData by remember { mutableStateOf<Pair<String, Long>?>(null) }
+
     val viewModel =
         viewModel<ImportCexAccountEnterCexDataViewModel>(factory = ImportCexAccountEnterCexDataViewModel.Factory(cexId))
 
@@ -35,12 +43,18 @@ fun ImportCexAccountEnterCexDataScreen(
 
     val menuItems = buildList {
         if (cex is CexBinance) {
+            val qrScannerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data = result.data?.getStringExtra(ModuleField.SCAN_ADDRESS) ?: ""
+                    scannedData = Pair(data, System.currentTimeMillis())
+                }
+            }
             add(
                 MenuItem(
                     title = TranslatableString.ResString(R.string.Button_ScanQr),
                     icon = R.drawable.ic_qr_scan_24px,
                     onClick = {
-
+                        qrScannerLauncher.launch(QRScannerActivity.getScanQrIntent(context))
                     }
                 )
             )
@@ -69,7 +83,7 @@ fun ImportCexAccountEnterCexDataScreen(
         ) { paddingValues ->
             when (cex) {
                 is CexBinance -> {
-                    EnterCexDataBinanceForm(paddingValues, onAccountCreate)
+                    EnterCexDataBinanceForm(paddingValues, onAccountCreate, scannedData)
                 }
 
                 is CexCoinzix -> {
@@ -157,18 +171,42 @@ private fun EnterCexDataCoinzixForm(paddingValues: PaddingValues, onAccountCreat
 }
 
 @Composable
-private fun EnterCexDataBinanceForm(paddingValues: PaddingValues, onAccountCreate: () -> Unit) {
+private fun EnterCexDataBinanceForm(
+    paddingValues: PaddingValues,
+    onAccountCreate: () -> Unit,
+    scannedData: Pair<String, Long>?
+) {
     val viewModel = viewModel<EnterCexDataBinanceViewModel>()
+    val view = LocalView.current
+    val uiState = viewModel.uiState
+    val accountCreated = uiState.accountCreated
+    val apiKey = uiState.apiKey
+    val secretKey = uiState.secretKey
+    val errorMessage = uiState.errorMessage
+    val connectEnabled = uiState.connectEnabled
 
-    if (viewModel.accountCreated) {
+    if (accountCreated) {
         LaunchedEffect(Unit) {
             onAccountCreate.invoke()
+        }
+    }
+
+    LaunchedEffect(scannedData) {
+        scannedData?.let { (text, _) ->
+            viewModel.onScannedData(text)
+        }
+    }
+
+    LaunchedEffect(errorMessage, uiState) {
+        errorMessage?.let {
+            HudHelper.showErrorMessage(view, it)
         }
     }
 
     Column(modifier = Modifier.padding(paddingValues)) {
         InfoText(text = stringResource(R.string.ImportCexAccountBinance_Description))
         FormsInput(
+            initial = apiKey,
             modifier = Modifier.padding(horizontal = 16.dp),
             hint = stringResource(R.string.ImportCexAccountBinance_ApiKey)
         ) {
@@ -176,6 +214,7 @@ private fun EnterCexDataBinanceForm(paddingValues: PaddingValues, onAccountCreat
         }
         VSpacer(height = 16.dp)
         FormsInput(
+            initial = secretKey,
             modifier = Modifier.padding(horizontal = 16.dp),
             hint = stringResource(R.string.ImportCexAccountBinance_SecretKey)
         ) {
@@ -189,7 +228,7 @@ private fun EnterCexDataBinanceForm(paddingValues: PaddingValues, onAccountCreat
                 ButtonPrimaryYellow(
                     modifier = Modifier.fillMaxWidth(),
                     title = stringResource(R.string.Button_Connect),
-                    enabled = viewModel.connectEnabled,
+                    enabled = connectEnabled,
                     onClick = {
                         viewModel.onClickConnect()
                     },
