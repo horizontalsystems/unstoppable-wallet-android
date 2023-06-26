d925aa949384186487cf9418cccc8f7998721e5f
bakyt <abdrasulov@gmail.com>
2023-06-26 14:52:45 +0600

    Add seperate fragment for DepositCexChooseAsset

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/depositcex/DepositCexChooseAssetFragment.kt
@@ -0,0 +1,46 @@
+package io.horizontalsystems.bankwallet.modules.depositcex
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.BaseFragment
+import io.horizontalsystems.bankwallet.core.slideFromRight
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+
+class DepositCexChooseAssetFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+
+            setContent {
+                DepositCexFragmentChooseAssetScreen(findNavController())
+            }
+        }
+    }
+}
+
+@Composable
+fun DepositCexFragmentChooseAssetScreen(navController: NavController) {
+    ComposeAppTheme {
+        SelectCoinScreen(
+            onClose = { navController.popBackStack() },
+            openNetworkSelect = { assetId ->
+                navController.slideFromRight(R.id.depositCexFragment, DepositCexFragment.args(assetId))
+            },
+        )
+    }
+}
---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/depositcex/SelectCexAssetViewModel.kt
@@ -0,0 +1,34 @@
+package io.horizontalsystems.bankwallet.modules.depositcex
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.IAccountManager
+import io.horizontalsystems.bankwallet.core.imageUrl
+import io.horizontalsystems.bankwallet.core.managers.CexAssetManager
+
+class SelectCexAssetViewModel(
+    private val cexAssetManager: CexAssetManager,
+    private val accountManager: IAccountManager
+) : ViewModel() {
+    val items = accountManager.activeAccount?.let {
+        cexAssetManager.getAll(it)
+            .map { cexAsset ->
+                DepositCexModule.CexCoinViewItem(
+                    title = cexAsset.id,
+                    subtitle = cexAsset.name,
+                    coinIconUrl = cexAsset.coin?.imageUrl,
+                    coinIconPlaceholder = R.drawable.coin_placeholder,
+                    assetId = cexAsset.id,
+                )
+            }
+    } ?: listOf()
+
+    class Factory : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return SelectCexAssetViewModel(App.cexAssetManager, App.accountManager) as T
+        }
+    }
+}
