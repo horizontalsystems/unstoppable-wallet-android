0401d8add134c86c61f3cd6ca705b7788dee06e6
bakyt <abdrasulov@gmail.com>
2023-07-12 16:27:32 +0600

    Implement chart indicators toggling

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/indicators/ChartIndicatorsViewModel.kt
@@ -0,0 +1,70 @@
+package io.horizontalsystems.bankwallet.modules.coin.indicators
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.modules.coin.overview.ChartIndicator
+import io.horizontalsystems.bankwallet.modules.coin.overview.ChartIndicatorManager
+import io.horizontalsystems.chartview.models.ChartIndicatorType
+import kotlinx.coroutines.launch
+
+class ChartIndicatorsViewModel(
+    private val chartIndicatorManager: ChartIndicatorManager
+) : ViewModel() {
+    private var maIndicators: List<ChartIndicator> = listOf()
+    private var oscillatorIndicators: List<ChartIndicator> = listOf()
+
+    var uiState by mutableStateOf(
+        ChartIndicatorsUiState(
+            maIndicators = maIndicators,
+            oscillatorIndicators = oscillatorIndicators
+        )
+    )
+        private set
+
+    init {
+        viewModelScope.launch {
+            chartIndicatorManager.allIndicatorsFlow.collect {
+                maIndicators = it.filter { it.indicatorType is ChartIndicatorType.MovingAverage }
+                oscillatorIndicators = it.filter { it.indicatorType is ChartIndicatorType.Rsi || it.indicatorType is ChartIndicatorType.Macd }
+
+                emitState()
+            }
+        }
+    }
+
+    private fun emitState() {
+        viewModelScope.launch {
+            uiState = ChartIndicatorsUiState(
+                maIndicators = maIndicators,
+                oscillatorIndicators = oscillatorIndicators
+            )
+        }
+    }
+
+    fun enable(indicatorId: String) {
+        chartIndicatorManager.enableIndicator(indicatorId)
+    }
+
+    fun disable(indicatorId: String) {
+        chartIndicatorManager.disableIndicator(indicatorId)
+    }
+
+    class Factory : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return ChartIndicatorsViewModel(App.chartIndicatorManager) as T
+        }
+    }
+
+}
+
+
+data class ChartIndicatorsUiState(
+    val maIndicators: List<ChartIndicator>,
+    val oscillatorIndicators: List<ChartIndicator>
+)
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/overview/ChartIndicatorManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/overview/ChartIndicatorManager.kt
@@ -1,6 +1,7 @@
 package io.horizontalsystems.bankwallet.modules.coin.overview
 
-import io.horizontalsystems.chartview.models.ChartIndicator
+import io.horizontalsystems.bankwallet.core.ILocalStorage
+import io.horizontalsystems.chartview.models.ChartIndicatorType
 import io.horizontalsystems.chartview.models.MovingAverageType
 import io.horizontalsystems.marketkit.models.ChartPoint
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -8,17 +9,58 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlin.random.Random
 
-class ChartIndicatorManager {
+class ChartIndicatorManager(private val localStorage: ILocalStorage) {
     private val _isEnabledFlow = MutableStateFlow(false)
     val isEnabledFlow: StateFlow<Boolean>
         get() = _isEnabledFlow
 
-    fun calculateIndicators(points: List<ChartPoint>): Map<Long, Map<ChartIndicator, Float>> {
-        val chartIndicator = ChartIndicator.MovingAverage(20, MovingAverageType.SMA)
+    private val _allIndicatorsFlow = MutableStateFlow(getAllIndicators())
+    val allIndicatorsFlow: StateFlow<List<ChartIndicator>>
+        get() = _allIndicatorsFlow
+
+    private fun getAllIndicators(): List<ChartIndicator> {
+        val enabledIds = localStorage.enabledChartIndicatorIds
+
+        return listOf(
+            ChartIndicator(
+                id = "ma1",
+                name = "EMA 1",
+                indicatorType = ChartIndicatorType.MovingAverage(20, MovingAverageType.SMA),
+                enabled = enabledIds.contains("ma1"),
+            ),
+            ChartIndicator(
+                id = "ma2",
+                name = "EMA 2",
+                indicatorType = ChartIndicatorType.MovingAverage(10, MovingAverageType.SMA),
+                enabled = enabledIds.contains("ma2")
+            ),
+            ChartIndicator(
+                id = "ma3",
+                name = "EMA 3",
+                indicatorType = ChartIndicatorType.MovingAverage(30, MovingAverageType.SMA),
+                enabled = enabledIds.contains("ma3")
+            ),
+            ChartIndicator(
+                id = "rsi",
+                name = "RSI",
+                indicatorType = ChartIndicatorType.Rsi,
+                enabled = enabledIds.contains("rsi")
+            ),
+            ChartIndicator(
+                id = "macd",
+                name = "MACD",
+                indicatorType = ChartIndicatorType.Macd,
+                enabled = enabledIds.contains("macd")
+            ),
+        )
+    }
+
+    fun calculateIndicators(points: List<ChartPoint>): Map<Long, Map<ChartIndicatorType, Float>> {
+        val chartIndicatorType = ChartIndicatorType.MovingAverage(20, MovingAverageType.SMA)
 
         return points.map {
-            it.timestamp to mapOf<ChartIndicator, Float>(
-                chartIndicator to Random.nextDouble(18.0, 20.0).toFloat()
+            it.timestamp to mapOf<ChartIndicatorType, Float>(
+                chartIndicatorType to Random.nextDouble(18.0, 20.0).toFloat()
             )
         }.toMap()
     }
@@ -30,4 +72,27 @@ class ChartIndicatorManager {
     fun disable() {
         _isEnabledFlow.update { false }
     }
+
+    fun enableIndicator(indicatorId: String) {
+        localStorage.enabledChartIndicatorIds += indicatorId
+
+        _allIndicatorsFlow.update {
+            getAllIndicators()
+        }
+    }
+
+    fun disableIndicator(indicatorId: String) {
+        localStorage.enabledChartIndicatorIds -= indicatorId
+
+        _allIndicatorsFlow.update {
+            getAllIndicators()
+        }
+    }
 }
+
+data class ChartIndicator(
+    val id: String,
+    val name: String,
+    val indicatorType: ChartIndicatorType,
+    val enabled: Boolean
+)
