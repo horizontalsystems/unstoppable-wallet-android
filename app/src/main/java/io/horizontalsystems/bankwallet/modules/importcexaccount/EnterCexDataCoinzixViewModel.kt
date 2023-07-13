7d2f1fcd4968f01baed328a4482466f5364834d6
chyngyz <chyngyz.omurov@gmail.com>
2023-07-13 17:54:32 +0600

    Handle Coinzix login errors and add loading state

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/importcexaccount/EnterCexDataCoinzixViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/importcexaccount/EnterCexDataCoinzixViewModel.kt
@@ -20,13 +20,29 @@ class EnterCexDataCoinzixViewModel : ViewModel() {
     private var email: String? = null
     private var password: String? = null
 
-    var loginEnabled by mutableStateOf(false)
-        private set
-    var accountCreated by mutableStateOf(false)
-        private set
+    private var loginEnabled = false
+    private var accountCreated = false
+    private var loading = false
+    private var error: Throwable? = null
+
+    var uiState by mutableStateOf(
+        UiState(
+            loginEnabled = loginEnabled,
+            accountCreated = accountCreated,
+            loading = loading,
+            error = error
+        )
+    )
 
     private fun emitState() {
-        loginEnabled = !(email.isNullOrBlank() || password.isNullOrBlank())
+        loginEnabled = !(email.isNullOrBlank() || password.isNullOrBlank()) && !loading
+
+        uiState = UiState(
+            loginEnabled = loginEnabled,
+            accountCreated = accountCreated,
+            loading = loading,
+            error = error
+        )
     }
 
     fun onEnterEmail(v: String) {
@@ -44,19 +60,36 @@ class EnterCexDataCoinzixViewModel : ViewModel() {
         val tmpPassword = password ?: return
 
         viewModelScope.launch {
-            val login = coinzixCexApiService.login(tmpEmail, tmpPassword, token)
-            val cexType = CexType.Coinzix(login.token, login.data.secret)
-            val name = accountFactory.getNextCexAccountName(cexType)
-            val account = accountFactory.account(
-                name,
-                AccountType.Cex(cexType),
-                AccountOrigin.Restored,
-                true,
-                false
-            )
+            loading = true
+            emitState()
 
-            accountManager.save(account)
-            accountCreated = true
+            try {
+                val login = coinzixCexApiService.login(tmpEmail, tmpPassword, token)
+                val cexType = CexType.Coinzix(login.token, login.data.secret)
+                val name = accountFactory.getNextCexAccountName(cexType)
+                val account = accountFactory.account(
+                    name,
+                    AccountType.Cex(cexType),
+                    AccountOrigin.Restored,
+                    true,
+                    false
+                )
+
+                accountManager.save(account)
+                accountCreated = true
+            } catch (err: Throwable) {
+                loading = false
+                error = err
+            }
+
+            emitState()
         }
     }
+
+    data class UiState(
+        var loginEnabled: Boolean,
+        var accountCreated: Boolean,
+        var loading: Boolean,
+        var error: Throwable?,
+    )
 }
