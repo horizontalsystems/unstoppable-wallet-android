878f51c4249ab4bc4a343a4d8273545b83a58188
bakyt <abdrasulov@gmail.com>
2023-07-19 16:55:02 +0600

    Allow setting updated value

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/settings/ui/SwapSettingsComponents.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/settings/ui/SwapSettingsComponents.kt
@@ -2,19 +2,12 @@ package io.horizontalsystems.bankwallet.modules.swap.settings.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,12 +26,7 @@ import io.horizontalsystems.bankwallet.modules.swap.settings.SwapDeadlineViewMod
 import io.horizontalsystems.bankwallet.modules.swap.settings.SwapSlippageViewModel
 import io.horizontalsystems.bankwallet.ui.compose.ColoredTextStyle
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
-import io.horizontalsystems.bankwallet.ui.compose.components.ButtonSecondaryCircle
-import io.horizontalsystems.bankwallet.ui.compose.components.ButtonSecondaryDefault
-import io.horizontalsystems.bankwallet.ui.compose.components.FormsInputStateWarning
-import io.horizontalsystems.bankwallet.ui.compose.components.HeaderText
-import io.horizontalsystems.bankwallet.ui.compose.components.InfoText
-import io.horizontalsystems.bankwallet.ui.compose.components.body_grey50
+import io.horizontalsystems.bankwallet.ui.compose.components.*
 import io.horizontalsystems.marketkit.models.BlockchainType
 import io.horizontalsystems.marketkit.models.TokenQuery
 import io.horizontalsystems.marketkit.models.TokenType
@@ -154,6 +142,12 @@ fun InputWithButtons(
         ) {
             var textState by rememberSaveable(stateSaver = TextFieldValue.Saver) {
                 mutableStateOf(TextFieldValue(initial ?: ""))
+            }
+
+            LaunchedEffect(initial) {
+                if (textState.text != initial) {
+                    textState = textState.copy(text = initial ?: "")
+                }
             }
 
             BasicTextField(
