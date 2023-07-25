a45ff80642bf5003776fb501dd651f92c8ac4ea1
bakyt <abdrasulov@gmail.com>
2023-07-25 17:11:52 +0600

    Remove xml implementation of Chart

--- components/chartview/src/main/java/io/horizontalsystems/chartview/Coordinate.kt
+++
@@ -1,9 +0,0 @@
-package io.horizontalsystems.chartview
-
-import io.horizontalsystems.chartview.models.ChartPoint
-
-data class Coordinate(
-    val x: Float,
-    val y: Float,
-    val item: ChartPoint,
-)
