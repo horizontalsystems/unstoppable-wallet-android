ebc4f3b85dbde525675bfd4fd82b6a5f9e05c4cb
bakyt <abdrasulov@gmail.com>
2023-07-27 18:25:36 +0600

    Remove switch component paddings
    See https://stackoverflow.com/a/75408239/2098878 for details

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/HsSwitch.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/HsSwitch.kt
@@ -1,11 +1,15 @@
 package io.horizontalsystems.bankwallet.ui.compose.components
 
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LocalMinimumTouchTargetEnforcement
 import androidx.compose.material.Switch
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HsSwitch(
     checked: Boolean,
@@ -13,18 +17,20 @@ fun HsSwitch(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
 ){
-    Switch(
-        modifier = modifier,
-        checked = checked,
-        onCheckedChange = onCheckedChange,
-        colors = SwitchDefaults.colors(
-            checkedThumbColor = ComposeAppTheme.colors.white,
-            uncheckedThumbColor = ComposeAppTheme.colors.lightGrey,
-            checkedTrackColor = ComposeAppTheme.colors.yellowD,
-            uncheckedTrackColor = ComposeAppTheme.colors.elenaD,
-            checkedTrackAlpha = 1f,
-            uncheckedTrackAlpha = 0.2f,
-        ),
-        enabled = enabled
-    )
+    CompositionLocalProvider(LocalMinimumTouchTargetEnforcement provides false) {
+        Switch(
+            modifier = modifier,
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = ComposeAppTheme.colors.white,
+                uncheckedThumbColor = ComposeAppTheme.colors.lightGrey,
+                checkedTrackColor = ComposeAppTheme.colors.yellowD,
+                uncheckedTrackColor = ComposeAppTheme.colors.elenaD,
+                checkedTrackAlpha = 1f,
+                uncheckedTrackAlpha = 0.2f,
+            ),
+            enabled = enabled
+        )
+    }
 }
