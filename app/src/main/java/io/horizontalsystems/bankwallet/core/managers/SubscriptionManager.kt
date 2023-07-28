cf1f5c05514624276ff2b7fe58a7635939fbabc5
bakyt <abdrasulov@gmail.com>
2023-07-28 12:35:23 +0600

    Remove showing and handling preview in CoinAnalytics module

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/SubscriptionManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/SubscriptionManager.kt
@@ -3,22 +3,16 @@ package io.horizontalsystems.bankwallet.core.managers
 import io.horizontalsystems.bankwallet.core.ILocalStorage
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
 
 class SubscriptionManager(private val localStorage: ILocalStorage) {
 
-    var authToken: String?
-        get() = localStorage.authToken
-        set(value) {
-            localStorage.authToken = value
-            _authTokenFlow.update { value }
-        }
+    var authToken: String? = ""
 
     private val _authTokenFlow = MutableStateFlow(authToken)
     val authTokenFlow: StateFlow<String?> = _authTokenFlow
 
     fun hasSubscription(): Boolean {
-        return !authToken.isNullOrBlank()
+        return true
     }
 
 }
