04ffd99648377cdc4c33a1b2e3b775cf197f18ea
bakyt <abdrasulov@gmail.com>
2023-08-08 12:02:35 +0600

    Fix SwapCoinCardView for compose 1.4.3

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapCoinCardView.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapCoinCardView.kt
@@ -2,28 +2,14 @@ package io.horizontalsystems.bankwallet.modules.swap
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -222,27 +208,8 @@ private fun SwapAmountInput(
                             style = ComposeAppTheme.typography.headline1,
                             textAlign = TextAlign.Start
                         )
-                        innerTextField()
-                    } else if (!focused) {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "${state.primaryPrefix ?: ""}${textState.text}",
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            color = if (state.dimAmount) ComposeAppTheme.colors.grey50 else ComposeAppTheme.colors.leah,
-                            style = ComposeAppTheme.typography.headline1,
-                            textAlign = TextAlign.Start
-                        )
-                        Box(
-                            modifier = Modifier
-                                .height(0.dp)
-                                .fillMaxWidth()
-                        ) {
-                            innerTextField()
-                        }
-                    } else {
-                        innerTextField()
                     }
+                    innerTextField()
                 }
             }
         )
