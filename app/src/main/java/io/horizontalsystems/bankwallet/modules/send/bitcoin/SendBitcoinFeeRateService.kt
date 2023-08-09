f3e9538f2c08bf2531cbb8b2ba523c0e6d6ad6bd
bakyt <abdrasulov@gmail.com>
2023-08-09 19:11:36 +0600

    Add validation for min fee rate for BTC fee settings

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/SendBitcoinFeeRateService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/SendBitcoinFeeRateService.kt
@@ -1,10 +1,10 @@
 package io.horizontalsystems.bankwallet.modules.send.bitcoin
 
-import io.horizontalsystems.bankwallet.core.FeeRatePriority
 import io.horizontalsystems.bankwallet.core.HSCaution
 import io.horizontalsystems.bankwallet.core.IFeeRateProvider
 import io.horizontalsystems.bankwallet.modules.send.SendErrorFetchFeeRateFailed
-import io.horizontalsystems.bankwallet.modules.send.SendWarningLowFee
+import io.horizontalsystems.bankwallet.modules.send.SendErrorLowFee
+import io.horizontalsystems.bankwallet.modules.send.SendWarningRiskOfGettingStuck
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,92 +12,89 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
 
 class SendBitcoinFeeRateService(private val feeRateProvider: IFeeRateProvider) {
+    val feeRateChangeable = feeRateProvider.feeRateChangeable
 
-    private var feeRatePriorities: List<FeeRatePriority> = listOf()
-    var feeRateChangeable: Boolean = false
-        private set
-
-    private var feeRatePriority: FeeRatePriority = FeeRatePriority.RECOMMENDED
-    private var feeRate: Long? = null
+    private var feeRate: Int? = null
     private var feeRateCaution: HSCaution? = null
+    private var canBeSend = false
 
-    private var lowFeeRate: Long? = null
-    private var mediumFeeRate: Long? = null
+    private var recommendedFeeRate: Int? = null
+    private var minimumFeeRate = 0
 
     private val _stateFlow = MutableStateFlow(
         State(
             feeRate = feeRate,
             feeRateCaution = feeRateCaution,
-            feeRatePriority = feeRatePriority,
-            canBeSend = false
+            canBeSend = canBeSend
         )
     )
     val stateFlow = _stateFlow.asStateFlow()
 
-    suspend fun start() {
-        withContext(Dispatchers.IO) {
-            try {
-                feeRatePriorities = feeRateProvider.feeRatePriorityList
-                feeRateChangeable = feeRatePriorities.isNotEmpty()
-                lowFeeRate = feeRateProvider.getFeeRate(FeeRatePriority.LOW)
-                mediumFeeRate = feeRateProvider.getFeeRate(FeeRatePriority.RECOMMENDED)
-            } catch (e: Exception) {
+    suspend fun start() = withContext(Dispatchers.IO) {
+        try {
+            val feeRates = feeRateProvider.getFeeRates()
 
-            }
-
-            refreshFeeRate()
-            validateFeeRate()
-            emitState()
+            recommendedFeeRate = feeRates.recommended
+            minimumFeeRate = feeRates.minimum
+            feeRate = recommendedFeeRate
+        } catch (e: Throwable) {
         }
+
+        validateFeeRate()
+        emitState()
     }
 
-    suspend fun setFeeRatePriority(feeRatePriority: FeeRatePriority) {
-        this.feeRatePriority = feeRatePriority
+    fun setFeeRate(v: Int) {
+        feeRate = v
 
-        refreshFeeRate()
+        validateFeeRate()
+        emitState()
+    }
+
+    fun reset() {
+        feeRate = recommendedFeeRate
+
         validateFeeRate()
         emitState()
     }
 
     private fun emitState() {
-        val tmpFeeRateCaution = feeRateCaution
-
         _stateFlow.update {
             State(
                 feeRate = feeRate,
                 feeRateCaution = feeRateCaution,
-                feeRatePriority = feeRatePriority,
-                canBeSend = tmpFeeRateCaution == null || tmpFeeRateCaution.isWarning()
+                canBeSend = canBeSend
             )
         }
     }
 
     private fun validateFeeRate() {
-        val tmpLowFeeRate = lowFeeRate
-        val tmpMediumFeeRate = mediumFeeRate
         val tmpFeeRate = feeRate
+        val tmpRecommendedFeeRate = recommendedFeeRate
 
-        feeRateCaution = if (tmpFeeRate == null) {
-            SendErrorFetchFeeRateFailed
-        } else if (tmpLowFeeRate != null && tmpMediumFeeRate != null && tmpLowFeeRate < tmpMediumFeeRate && tmpFeeRate <= tmpLowFeeRate) {
-            SendWarningLowFee
-        } else {
-            null
-        }
-    }
-
-    private suspend fun refreshFeeRate() {
-        try {
-            feeRate = feeRateProvider.getFeeRate(feeRatePriority)
-        } catch (e: Exception) {
-            feeRate = null
+        when {
+            tmpFeeRate == null -> {
+                feeRateCaution = SendErrorFetchFeeRateFailed
+                canBeSend = false
+            }
+            tmpFeeRate < minimumFeeRate -> {
+                feeRateCaution = SendErrorLowFee
+                canBeSend = true
+            }
+            tmpRecommendedFeeRate != null && tmpFeeRate < tmpRecommendedFeeRate -> {
+                feeRateCaution = SendWarningRiskOfGettingStuck
+                canBeSend = true
+            }
+            else -> {
+                feeRateCaution = null
+                canBeSend = true
+            }
         }
     }
 
     data class State(
-        val feeRate: Long?,
+        val feeRate: Int?,
         val feeRateCaution: HSCaution?,
-        val feeRatePriority: FeeRatePriority,
         val canBeSend: Boolean,
     )
 }
