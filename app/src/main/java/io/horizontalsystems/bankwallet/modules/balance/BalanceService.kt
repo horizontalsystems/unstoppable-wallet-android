dc9f8744e4edcbfa1ce08a92e5f567b7aae37c08
bakyt <abdrasulov@gmail.com>
2023-08-22 12:40:10 +0600

    Handle Bitcoin and Bitcoin Cash token types as native

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalanceService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalanceService.kt
@@ -3,13 +3,13 @@ package io.horizontalsystems.bankwallet.modules.balance
 import io.horizontalsystems.bankwallet.core.Clearable
 import io.horizontalsystems.bankwallet.core.IAccountManager
 import io.horizontalsystems.bankwallet.core.ILocalStorage
+import io.horizontalsystems.bankwallet.core.isNative
 import io.horizontalsystems.bankwallet.core.managers.ConnectivityManager
 import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.entities.Account
 import io.horizontalsystems.bankwallet.entities.AccountType
 import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.marketkit.models.CoinPrice
-import io.horizontalsystems.marketkit.models.TokenType
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -51,7 +51,7 @@ class BalanceService(
 
     /* getBalanceItems should return new immutable list */
     private fun getBalanceItems(): List<BalanceModule.BalanceItem> = if (hideZeroBalances) {
-        allBalanceItems.filter { it.wallet.token.type == TokenType.Native || it.balanceData.total > BigDecimal.ZERO }
+        allBalanceItems.filter { it.wallet.token.type.isNative || it.balanceData.total > BigDecimal.ZERO }
     } else {
         allBalanceItems.toList()
     }
