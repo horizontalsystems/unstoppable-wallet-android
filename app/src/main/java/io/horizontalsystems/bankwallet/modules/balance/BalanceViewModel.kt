ddd30f1340117e2be51b14f97356bd601f958010
bakyt <abdrasulov@gmail.com>
2023-08-29 15:29:27 +0600

    Show backup required dialog for non backed up account

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalanceViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalanceViewModel.kt
@@ -187,10 +187,23 @@ class BalanceViewModel(
         else -> SyncError.NetworkNotAvailable()
     }
 
+    fun getReceiveAllowedState(): ReceiveAllowedState? {
+        val tmpAccount = service.account ?: return null
+        return when {
+            tmpAccount.hasAnyBackup -> ReceiveAllowedState.Allowed
+            else -> ReceiveAllowedState.BackupRequired(tmpAccount)
+        }
+    }
+
     sealed class SyncError {
         class NetworkNotAvailable : SyncError()
         class Dialog(val wallet: Wallet, val errorMessage: String?) : SyncError()
     }
+}
+
+sealed class ReceiveAllowedState {
+    object Allowed : ReceiveAllowedState()
+    data class BackupRequired(val account: Account) : ReceiveAllowedState()
 }
 
 class BackupRequiredError(val account: Account, val coinTitle: String) : Error("Backup Required")
