7d971caee2c0d78b4fadbec8464f34621e224e4c
bakyt <abdrasulov@gmail.com>
2023-09-04 17:33:36 +0600

    Add back button to send screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendScreen.kt
@@ -3,21 +3,15 @@ package io.horizontalsystems.bankwallet.modules.send
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.core.iconPlaceholder
-import io.horizontalsystems.bankwallet.core.imageUrl
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
-import io.horizontalsystems.bankwallet.ui.compose.components.CoinImage
-import io.horizontalsystems.bankwallet.ui.compose.components.MenuItem
+import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
 import io.horizontalsystems.marketkit.models.FullCoin
 
 @Composable
@@ -30,21 +24,9 @@ fun SendScreen(
         AppBar(
             title = TranslatableString.ResString(R.string.Send_Title, fullCoin.coin.code),
             navigationIcon = {
-                CoinImage(
-                    iconUrl = fullCoin.coin.imageUrl,
-                    placeholder = fullCoin.iconPlaceholder,
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .size(24.dp)
-                )
+                HsBackButton(onClick = onCloseClick)
             },
-            menuItems = listOf(
-                MenuItem(
-                    title = TranslatableString.ResString(R.string.Button_Close),
-                    icon = R.drawable.ic_close,
-                    onClick = onCloseClick
-                )
-            )
+            menuItems = listOf()
         )
 
         Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
