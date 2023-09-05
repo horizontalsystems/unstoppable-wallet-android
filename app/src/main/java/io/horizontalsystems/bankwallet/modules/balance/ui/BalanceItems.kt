f57a9f221f35e9aa0f60b348a42fe1ce6aff63d0
bakyt <abdrasulov@gmail.com>
2023-09-05 16:41:26 +0600

    Hide common send/receive/swap buttons for watch accounts

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/ui/BalanceItems.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/ui/BalanceItems.kt
@@ -183,57 +183,59 @@ fun BalanceItems(
                 )
             }
 
-            item {
-                Row(
-                    modifier = Modifier.padding(horizontal = 24.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    ButtonPrimaryYellowWithIcon(
-                        modifier = Modifier.weight(1f),
-                        icon = R.drawable.ic_arrow_up_right_24,
-                        title = stringResource(R.string.Balance_Send),
-                        onClick = {
-                            navController.slideFromRight(R.id.sendTokenSelectFragment)
-                        }
-                    )
-                    HSpacer(8.dp)
-                    ButtonPrimaryCircle(
-                        icon = R.drawable.ic_arrow_down_left_24,
-                        contentDescription = stringResource(R.string.Balance_Receive),
-                        onClick = {
-                            when (val receiveAllowedState = viewModel.getReceiveAllowedState()) {
-                                ReceiveAllowedState.Allowed -> {
-                                    navController.slideFromRight(R.id.receiveTokenSelectFragment)
-                                }
-
-                                is ReceiveAllowedState.BackupRequired -> {
-                                    val account = receiveAllowedState.account
-                                    val text = Translator.getString(
-                                        R.string.Balance_Receive_BackupRequired_Description,
-                                        account.name
-                                    )
-                                    navController.slideFromBottom(
-                                        R.id.backupRequiredDialog,
-                                        BackupRequiredDialog.prepareParams(account, text)
-                                    )
-                                }
-
-                                null -> Unit
-                            }
-                        },
-                    )
-                    if (true) {
-                        HSpacer(8.dp)
-                        ButtonPrimaryCircle(
-                            icon = R.drawable.ic_swap_24,
-                            contentDescription = stringResource(R.string.Swap),
+            if (!accountViewItem.isWatchAccount) {
+                item {
+                    Row(
+                        modifier = Modifier.padding(horizontal = 24.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        ButtonPrimaryYellowWithIcon(
+                            modifier = Modifier.weight(1f),
+                            icon = R.drawable.ic_arrow_up_right_24,
+                            title = stringResource(R.string.Balance_Send),
                             onClick = {
-                                navController.slideFromRight(R.id.swapTokenSelectFragment)
+                                navController.slideFromRight(R.id.sendTokenSelectFragment)
                             }
                         )
+                        HSpacer(8.dp)
+                        ButtonPrimaryCircle(
+                            icon = R.drawable.ic_arrow_down_left_24,
+                            contentDescription = stringResource(R.string.Balance_Receive),
+                            onClick = {
+                                when (val receiveAllowedState = viewModel.getReceiveAllowedState()) {
+                                    ReceiveAllowedState.Allowed -> {
+                                        navController.slideFromRight(R.id.receiveTokenSelectFragment)
+                                    }
+
+                                    is ReceiveAllowedState.BackupRequired -> {
+                                        val account = receiveAllowedState.account
+                                        val text = Translator.getString(
+                                            R.string.Balance_Receive_BackupRequired_Description,
+                                            account.name
+                                        )
+                                        navController.slideFromBottom(
+                                            R.id.backupRequiredDialog,
+                                            BackupRequiredDialog.prepareParams(account, text)
+                                        )
+                                    }
+
+                                    null -> Unit
+                                }
+                            },
+                        )
+                        if (true) {
+                            HSpacer(8.dp)
+                            ButtonPrimaryCircle(
+                                icon = R.drawable.ic_swap_24,
+                                contentDescription = stringResource(R.string.Swap),
+                                onClick = {
+                                    navController.slideFromRight(R.id.swapTokenSelectFragment)
+                                }
+                            )
+                        }
                     }
+                    VSpacer(12.dp)
                 }
-                VSpacer(12.dp)
             }
 
             item {
