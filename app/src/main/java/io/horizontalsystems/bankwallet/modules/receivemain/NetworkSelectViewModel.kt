295453c7c56f01a153352ada1d916f843502cbaf
bakyt <abdrasulov@gmail.com>
2023-09-13 18:11:27 +0600

    Fix the issue with flicker on navigation to receive screen
    Fixed by waiting for IReceiveAdapter is ready for newly enabled wallet before navigating to Receive Screen.

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/receivemain/NetworkSelectViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/receivemain/NetworkSelectViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.IWalletManager
 import io.horizontalsystems.bankwallet.core.eligibleTokens
+import io.horizontalsystems.bankwallet.core.utils.Utils
 import io.horizontalsystems.bankwallet.entities.Account
 import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.marketkit.models.FullCoin
@@ -17,17 +18,22 @@ class NetworkSelectViewModel(
 ) : ViewModel() {
     val eligibleTokens = fullCoin.eligibleTokens(activeAccount.type)
 
-    fun getOrCreateWallet(token: Token): Wallet {
+    suspend fun getOrCreateWallet(token: Token): Wallet {
         return walletManager
             .activeWallets
             .find { it.token == token }
             ?: createWallet(token)
     }
 
-    private fun createWallet(token: Token): Wallet {
+    private suspend fun createWallet(token: Token): Wallet {
         val wallet = Wallet(token, activeAccount)
 
         walletManager.save(listOf(wallet))
+
+        Utils.waitUntil(1000L, 100L) {
+            App.adapterManager.getReceiveAdapterForWallet(wallet) != null
+        }
+
         return wallet
     }
 
