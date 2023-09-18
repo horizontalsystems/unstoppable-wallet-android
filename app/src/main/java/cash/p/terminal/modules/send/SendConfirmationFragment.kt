eyovvalupe/Ethereum-wallet-payment

Implement send TRX and TRC20

app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendConfirmationFragment.kt
@@ -18,6 +18,8 @@ import io.horizontalsystems.bankwallet.modules.send.bitcoin.SendBitcoinConfirmat
 import io.horizontalsystems.bankwallet.modules.send.bitcoin.SendBitcoinViewModel
 import io.horizontalsystems.bankwallet.modules.send.solana.SendSolanaConfirmationScreen
 import io.horizontalsystems.bankwallet.modules.send.solana.SendSolanaViewModel
+import io.horizontalsystems.bankwallet.modules.send.tron.SendTronConfirmationScreen
+import io.horizontalsystems.bankwallet.modules.send.tron.SendTronViewModel
 import io.horizontalsystems.bankwallet.modules.send.zcash.SendZCashConfirmationScreen
 import io.horizontalsystems.bankwallet.modules.send.zcash.SendZCashViewModel
 import io.horizontalsystems.core.findNavController
@@ -64,6 +66,14 @@ class SendConfirmationFragment : BaseFragment() {
                             amountInputModeViewModel
                         )
                     }
+                    Type.Tron -> {
+                        val sendTronViewModel by navGraphViewModels<SendTronViewModel>(R.id.sendXFragment)
+                        SendTronConfirmationScreen(
+                            findNavController(),
+                            sendTronViewModel,
+                            amountInputModeViewModel
+                        )
+                    }
                     Type.Solana -> {
                         val sendSolanaViewModel by navGraphViewModels<SendSolanaViewModel>(R.id.sendXFragment)
 
@@ -81,7 +91,7 @@ class SendConfirmationFragment : BaseFragment() {
 
     @Parcelize
     enum class Type : Parcelable {
-        Bitcoin, Bep2, ZCash, Solana
+        Bitcoin, Bep2, ZCash, Solana, Tron
     }
 
     companion object {

app/src/main/java/io/horizontalsystems/bankwallet/modules/send/SendFragment.kt
@@ -28,6 +28,9 @@ import io.horizontalsystems.bankwallet.modules.send.evm.confirmation.EvmKitWrapp
 import io.horizontalsystems.bankwallet.modules.send.solana.SendSolanaModule
 import io.horizontalsystems.bankwallet.modules.send.solana.SendSolanaScreen
 import io.horizontalsystems.bankwallet.modules.send.solana.SendSolanaViewModel
+import io.horizontalsystems.bankwallet.modules.send.tron.SendTronModule
+import io.horizontalsystems.bankwallet.modules.send.tron.SendTronScreen
+import io.horizontalsystems.bankwallet.modules.send.tron.SendTronViewModel
 import io.horizontalsystems.bankwallet.modules.send.zcash.SendZCashModule
 import io.horizontalsystems.bankwallet.modules.send.zcash.SendZCashScreen
 import io.horizontalsystems.bankwallet.modules.send.zcash.SendZCashViewModel
@@ -124,6 +127,17 @@ class SendFragment : BaseFragment() {
                             SendSolanaScreen(
                                 findNavController(),
                                 sendSolanaViewModel,
+                                amountInputModeViewModel
+                            )
+                        }
+                    }
+                    BlockchainType.Tron -> {
+                        val factory = SendTronModule.Factory(wallet)
+                        val sendTronViewModel by navGraphViewModels<SendTronViewModel>(R.id.sendXFragment) { factory }
+                        setContent {
+                            SendTronScreen(
+                                findNavController(),
+                                sendTronViewModel,
                                 amountInputModeViewModel
                             )
                         }

app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TronContractLabels.kt
@@ -1,0 +1,27 @@
+package io.horizontalsystems.bankwallet.modules.transactions
+
+import io.horizontalsystems.tronkit.models.AssetIssueContract
+import io.horizontalsystems.tronkit.models.Contract
+import io.horizontalsystems.tronkit.models.CreateSmartContract
+import io.horizontalsystems.tronkit.models.FreezeBalanceV2Contract
+import io.horizontalsystems.tronkit.models.TransferAssetContract
+import io.horizontalsystems.tronkit.models.TransferContract
+import io.horizontalsystems.tronkit.models.TriggerSmartContract
+import io.horizontalsystems.tronkit.models.UnfreezeBalanceV2Contract
+import io.horizontalsystems.tronkit.models.Unknown
+import io.horizontalsystems.tronkit.models.VoteWitnessContract
+import io.horizontalsystems.tronkit.models.WithdrawBalanceContract
+
+val Contract.label: String
+    get() = when (this) {
+        is AssetIssueContract -> "Issue TRC10 token"
+        is CreateSmartContract -> "Create Smart Contract"
+        is FreezeBalanceV2Contract -> "TRX Stake 2.0"
+        is TransferAssetContract -> "Transfer TRC10 Token"
+        is TransferContract -> "TRX Transfer"
+        is TriggerSmartContract -> "Trigger Smart Contract"
+        is UnfreezeBalanceV2Contract -> "TRX Unstake 2.0"
+        is VoteWitnessContract -> "Vote"
+        is WithdrawBalanceContract -> "Claim Rewards"
+        is Unknown -> this.javaClass.simpleName
+    }

app/src/main/java/io/horizontalsystems/bankwallet/modules/send/tron/SendTronAddressService.kt
@@ -1,0 +1,63 @@
+package io.horizontalsystems.bankwallet.modules.send.tron
+
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.providers.Translator
+import io.horizontalsystems.bankwallet.entities.Address
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import io.horizontalsystems.tronkit.models.Address as TronAddress
+
+class SendTronAddressService {
+    private var address: Address? = null
+    private var addressError: Throwable? = null
+    private var tronAddress: TronAddress? = null
+
+    private val _stateFlow = MutableStateFlow(
+        State(
+            address = address,
+            tronAddress = tronAddress,
+            addressError = addressError,
+            canBeSend = tronAddress != null,
+        )
+    )
+    val stateFlow = _stateFlow.asStateFlow()
+
+    fun setAddress(address: Address?) {
+        this.address = address
+
+        validateAddress()
+
+        emitState()
+    }
+
+    private fun validateAddress() {
+        addressError = null
+        tronAddress = null
+        val address = this.address ?: return
+
+        try {
+            tronAddress = TronAddress.fromBase58(address.hex)
+        } catch (e: Exception) {
+            addressError = Throwable(Translator.getString(R.string.SwapSettings_Error_InvalidAddress))
+        }
+    }
+
+    private fun emitState() {
+        _stateFlow.update {
+            State(
+                address = address,
+                tronAddress = tronAddress,
+                addressError = addressError,
+                canBeSend = tronAddress != null
+            )
+        }
+    }
+
+    data class State(
+        val address: Address?,
+        val tronAddress: TronAddress?,
+        val addressError: Throwable?,
+        val canBeSend: Boolean
+    )
+}

app/src/main/java/io/horizontalsystems/bankwallet/modules/send/tron/SendTronConfirmationData.kt
@@ -1,0 +1,18 @@
+package io.horizontalsystems.bankwallet.modules.send.tron
+
+import io.horizontalsystems.bankwallet.entities.Address
+import io.horizontalsystems.bankwallet.modules.contacts.model.Contact
+import io.horizontalsystems.marketkit.models.Coin
+import java.math.BigDecimal
+
+data class SendTronConfirmationData(
+    val amount: BigDecimal,
+    val address: Address,
+    val fee: BigDecimal?,
+    val activationFee: BigDecimal?,
+    val resourcesConsumed: String?,
+    val contact: Contact?,
+    val coin: Coin,
+    val feeCoin: Coin,
+    val memo: String? = null,
+)

horizontalsystems/unstoppable-wallet-android

app/src/main/java/cash/p/terminal/modules/send/SendConfirmationFragment.kt
@@ -18,6 +18,8 @@ import cash.p.terminal.modules.send.bitcoin.SendBitcoinConfirmationScreen
 import cash.p.terminal.modules.send.bitcoin.SendBitcoinViewModel
 import cash.p.terminal.modules.send.solana.SendSolanaConfirmationScreen
 import cash.p.terminal.modules.send.solana.SendSolanaViewModel
+import cash.p.terminal.modules.send.tron.SendTronConfirmationScreen
+import cash.p.terminal.modules.send.tron.SendTronViewModel
 import cash.p.terminal.modules.send.zcash.SendZCashConfirmationScreen
 import cash.p.terminal.modules.send.zcash.SendZCashViewModel
 import io.horizontalsystems.core.findNavController
@@ -64,6 +66,14 @@ class SendConfirmationFragment : BaseFragment() {
                             amountInputModeViewModel
                         )
                     }
+                    Type.Tron -> {
+                        val sendTronViewModel by navGraphViewModels<SendTronViewModel>(R.id.sendXFragment)
+                        SendTronConfirmationScreen(
+                            findNavController(),
+                            sendTronViewModel,
+                            amountInputModeViewModel
+                        )
+                    }
                     Type.Solana -> {
                         val sendSolanaViewModel by navGraphViewModels<SendSolanaViewModel>(R.id.sendXFragment)
 
@@ -81,7 +91,7 @@ class SendConfirmationFragment : BaseFragment() {
 
     @Parcelize
     enum class Type : Parcelable {
-        Bitcoin, Bep2, ZCash, Solana
+        Bitcoin, Bep2, ZCash, Solana, Tron
     }
 
     companion object {

app/src/main/java/cash/p/terminal/modules/send/SendFragment.kt
@@ -28,6 +28,9 @@ import cash.p.terminal.modules.send.evm.confirmation.EvmKitWrapperHoldingViewMod
 import cash.p.terminal.modules.send.solana.SendSolanaModule
 import cash.p.terminal.modules.send.solana.SendSolanaScreen
 import cash.p.terminal.modules.send.solana.SendSolanaViewModel
+import cash.p.terminal.modules.send.tron.SendTronModule
+import cash.p.terminal.modules.send.tron.SendTronScreen
+import cash.p.terminal.modules.send.tron.SendTronViewModel
 import cash.p.terminal.modules.send.zcash.SendZCashModule
 import cash.p.terminal.modules.send.zcash.SendZCashScreen
 import cash.p.terminal.modules.send.zcash.SendZCashViewModel
@@ -123,6 +126,17 @@ class SendFragment : BaseFragment() {
                             SendSolanaScreen(
                                 findNavController(),
                                 sendSolanaViewModel,
+                                amountInputModeViewModel
+                            )
+                        }
+                    }
+                    BlockchainType.Tron -> {
+                        val factory = SendTronModule.Factory(wallet)
+                        val sendTronViewModel by navGraphViewModels<SendTronViewModel>(R.id.sendXFragment) { factory }
+                        setContent {
+                            SendTronScreen(
+                                findNavController(),
+                                sendTronViewModel,
                                 amountInputModeViewModel
                             )
                         }

app/src/main/java/cash/p/terminal/modules/transactions/TronContractLabels.kt
@@ -1,0 +1,27 @@
+package cash.p.terminal.modules.transactions
+
+import io.horizontalsystems.tronkit.models.AssetIssueContract
+import io.horizontalsystems.tronkit.models.Contract
+import io.horizontalsystems.tronkit.models.CreateSmartContract
+import io.horizontalsystems.tronkit.models.FreezeBalanceV2Contract
+import io.horizontalsystems.tronkit.models.TransferAssetContract
+import io.horizontalsystems.tronkit.models.TransferContract
+import io.horizontalsystems.tronkit.models.TriggerSmartContract
+import io.horizontalsystems.tronkit.models.UnfreezeBalanceV2Contract
+import io.horizontalsystems.tronkit.models.Unknown
+import io.horizontalsystems.tronkit.models.VoteWitnessContract
+import io.horizontalsystems.tronkit.models.WithdrawBalanceContract
+
+val Contract.label: String
+    get() = when (this) {
+        is AssetIssueContract -> "Issue TRC10 token"
+        is CreateSmartContract -> "Create Smart Contract"
+        is FreezeBalanceV2Contract -> "TRX Stake 2.0"
+        is TransferAssetContract -> "Transfer TRC10 Token"
+        is TransferContract -> "TRX Transfer"
+        is TriggerSmartContract -> "Trigger Smart Contract"
+        is UnfreezeBalanceV2Contract -> "TRX Unstake 2.0"
+        is VoteWitnessContract -> "Vote"
+        is WithdrawBalanceContract -> "Claim Rewards"
+        is Unknown -> this.javaClass.simpleName
+    }

app/src/main/java/io/horizontalsystems/bankwallet/modules/send/tron/SendTronAddressService.kt
@@ -1,0 +1,63 @@
+package cash.p.terminal.modules.send.tron
+
+import cash.p.terminal.R
+import cash.p.terminal.core.providers.Translator
+import cash.p.terminal.entities.Address
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import io.horizontalsystems.tronkit.models.Address as TronAddress
+
+class SendTronAddressService {
+    private var address: Address? = null
+    private var addressError: Throwable? = null
+    private var tronAddress: TronAddress? = null
+
+    private val _stateFlow = MutableStateFlow(
+        State(
+            address = address,
+            tronAddress = tronAddress,
+            addressError = addressError,
+            canBeSend = tronAddress != null,
+        )
+    )
+    val stateFlow = _stateFlow.asStateFlow()
+
+    fun setAddress(address: Address?) {
+        this.address = address
+
+        validateAddress()
+
+        emitState()
+    }
+
+    private fun validateAddress() {
+        addressError = null
+        tronAddress = null
+        val address = this.address ?: return
+
+        try {
+            tronAddress = TronAddress.fromBase58(address.hex)
+        } catch (e: Exception) {
+            addressError = Throwable(Translator.getString(R.string.SwapSettings_Error_InvalidAddress))
+        }
+    }
+
+    private fun emitState() {
+        _stateFlow.update {
+            State(
+                address = address,
+                tronAddress = tronAddress,
+                addressError = addressError,
+                canBeSend = tronAddress != null
+            )
+        }
+    }
+
+    data class State(
+        val address: Address?,
+        val tronAddress: TronAddress?,
+        val addressError: Throwable?,
+        val canBeSend: Boolean
+    )
+}

app/src/main/java/io/horizontalsystems/bankwallet/modules/send/tron/SendTronConfirmationData.kt
@@ -1,0 +1,18 @@
+package cash.p.terminal.modules.send.tron
+
+import cash.p.terminal.entities.Address
+import cash.p.terminal.modules.contacts.model.Contact
+import io.horizontalsystems.marketkit.models.Coin
+import java.math.BigDecimal
+
+data class SendTronConfirmationData(
+    val amount: BigDecimal,
+    val address: Address,
+    val fee: BigDecimal?,
+    val activationFee: BigDecimal?,
+    val resourcesConsumed: String?,
+    val contact: Contact?,
+    val coin: Coin,
+    val feeCoin: Coin,
+    val memo: String? = null,
+)