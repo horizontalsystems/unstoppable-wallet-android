eyovvalupe/Ethereum-wallet-payment

Initial Tron integration
- show TRX balance and transactions
- implement displaying transaction info

app/src/main/java/io/horizontalsystems/bankwallet/core/MarketKitExtensions.kt
@@ -4,10 +4,22 @@ import androidx.compose.ui.graphics.Color
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.managers.RestoreSettingType
 import io.horizontalsystems.bankwallet.core.providers.Translator
-import io.horizontalsystems.bankwallet.entities.*
+import io.horizontalsystems.bankwallet.entities.AccountType
+import io.horizontalsystems.bankwallet.entities.BitcoinCashCoinType
+import io.horizontalsystems.bankwallet.entities.CoinSettingType
+import io.horizontalsystems.bankwallet.entities.CoinSettings
+import io.horizontalsystems.bankwallet.entities.FeePriceScale
+import io.horizontalsystems.bankwallet.entities.derivation
 import io.horizontalsystems.hdwalletkit.ExtendedKeyCoinType
 import io.horizontalsystems.hdwalletkit.HDWallet
-import io.horizontalsystems.marketkit.models.*
+import io.horizontalsystems.marketkit.models.Blockchain
+import io.horizontalsystems.marketkit.models.BlockchainType
+import io.horizontalsystems.marketkit.models.Coin
+import io.horizontalsystems.marketkit.models.FullCoin
+import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.marketkit.models.TokenQuery
+import io.horizontalsystems.marketkit.models.TokenType
+import io.horizontalsystems.marketkit.models.TopPlatform
 import io.horizontalsystems.nftkit.models.NftType
 
 val Token.protocolType: String?
@@ -139,6 +151,9 @@ val TokenQuery.isSupported: Boolean
         BlockchainType.Solana -> {
             tokenType is TokenType.Native || tokenType is TokenType.Spl
         }
+        BlockchainType.Tron -> {
+            tokenType is TokenType.Native || tokenType is TokenType.Eip20
+        }
         else -> false
     }
 
@@ -161,6 +176,7 @@ val Blockchain.description: String
         BlockchainType.Solana -> "SOL, SPL tokens"
         BlockchainType.Gnosis -> "xDAI, ERC20 tokens"
         BlockchainType.Fantom -> "FTM, ERC20 tokens"
+        BlockchainType.Tron -> "TRX, TRC20 tokens"
         else -> ""
     }
 
@@ -242,6 +258,7 @@ val BlockchainType.tokenIconPlaceholder: Int
         BlockchainType.ArbitrumOne -> R.drawable.arbitrum_erc20
         BlockchainType.Gnosis -> R.drawable.gnosis_erc20
         BlockchainType.Fantom -> R.drawable.fantom_erc20
+        BlockchainType.Tron -> R.drawable.fantom_erc20 //TODO
         else -> R.drawable.coin_placeholder
     }
 

app/src/main/java/io/horizontalsystems/bankwallet/core/adapters/BaseTronAdapter.kt
@@ -1,0 +1,29 @@
+package io.horizontalsystems.bankwallet.core.adapters
+
+import io.horizontalsystems.bankwallet.core.IAdapter
+import io.horizontalsystems.bankwallet.core.IBalanceAdapter
+import io.horizontalsystems.bankwallet.core.IReceiveAdapter
+import io.horizontalsystems.bankwallet.core.managers.TronKitWrapper
+import io.horizontalsystems.tronkit.transaction.Signer
+
+abstract class BaseTronAdapter(
+    tronKitWrapper: TronKitWrapper,
+    val decimal: Int
+) : IAdapter, IBalanceAdapter, IReceiveAdapter {
+
+    val tronKit = tronKitWrapper.tronKit
+    protected val signer: Signer? = tronKitWrapper.signer
+
+    override val debugInfo: String
+        get() = ""
+
+    // IReceiveAdapter
+
+    override val receiveAddress: String
+        get() = tronKit.address.base58
+
+    companion object {
+        const val confirmationsThreshold: Int = 19
+    }
+
+}

app/src/main/java/io/horizontalsystems/bankwallet/core/adapters/TronAdapter.kt
@@ -1,0 +1,88 @@
+package io.horizontalsystems.bankwallet.core.adapters
+
+import io.horizontalsystems.bankwallet.core.AdapterState
+import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.BalanceData
+import io.horizontalsystems.bankwallet.core.ISendTronAdapter
+import io.horizontalsystems.bankwallet.core.managers.TronKitWrapper
+import io.horizontalsystems.tronkit.TronKit
+import io.horizontalsystems.tronkit.models.Address
+import io.horizontalsystems.tronkit.network.Network
+import io.reactivex.Flowable
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.rx2.asFlowable
+import java.math.BigDecimal
+import java.math.BigInteger
+
+class TronAdapter(kitWrapper: TronKitWrapper) : BaseTronAdapter(kitWrapper, decimal), ISendTronAdapter {
+
+    // IAdapter
+
+    override fun start() {
+        // started via TronKitManager
+    }
+
+    override fun stop() {
+        // stopped via TronKitManager
+    }
+
+    override fun refresh() {
+        // refreshed via TronKitManager
+    }
+
+    // IBalanceAdapter
+
+    override val balanceState: AdapterState
+        get() = convertToAdapterState(tronKit.syncState)
+
+    override val balanceStateUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.syncStateFlow.map {}.asFlowable()
+
+    override val balanceData: BalanceData
+        get() = BalanceData(balanceInBigDecimal(tronKit.trxBalance, decimal))
+
+    override val balanceUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.trxBalanceFlow.map {}.asFlowable()
+
+    // ISendTronAdapter
+
+    override suspend fun send(amount: BigInteger, to: Address): String {
+        if (signer == null) throw Exception()
+
+        val transferContract = tronKit.transferContract(amount, to)
+
+        return tronKit.send(transferContract, signer)
+    }
+
+    private fun convertToAdapterState(syncState: TronKit.SyncState): AdapterState =
+        when (syncState) {
+            is TronKit.SyncState.Synced -> AdapterState.Synced
+            is TronKit.SyncState.NotSynced -> AdapterState.NotSynced(syncState.error)
+            is TronKit.SyncState.Syncing -> AdapterState.Syncing()
+        }
+
+    private fun scaleDown(amount: BigDecimal, decimals: Int = decimal): BigDecimal {
+        return amount.movePointLeft(decimals).stripTrailingZeros()
+    }
+
+    private fun scaleUp(amount: BigDecimal, decimals: Int = decimal): BigInteger {
+        return amount.movePointRight(decimals).toBigInteger()
+    }
+
+    private fun balanceInBigDecimal(balance: BigInteger?, decimal: Int): BigDecimal {
+        balance?.toBigDecimal()?.let {
+            return scaleDown(it, decimal)
+        } ?: return BigDecimal.ZERO
+    }
+
+    companion object {
+        const val decimal = 6
+
+        fun clear(walletId: String) {
+            Network.values().forEach { network ->
+                TronKit.clear(App.instance, network, walletId)
+            }
+        }
+    }
+
+}

app/src/main/java/io/horizontalsystems/bankwallet/core/adapters/TronTransactionsAdapter.kt
@@ -1,0 +1,122 @@
+package io.horizontalsystems.bankwallet.core.adapters
+
+import io.horizontalsystems.bankwallet.core.AdapterState
+import io.horizontalsystems.bankwallet.core.ITransactionsAdapter
+import io.horizontalsystems.bankwallet.core.managers.TronKitWrapper
+import io.horizontalsystems.bankwallet.entities.LastBlockInfo
+import io.horizontalsystems.bankwallet.entities.transactionrecords.TransactionRecord
+import io.horizontalsystems.bankwallet.modules.transactions.FilterTransactionType
+import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.marketkit.models.TokenType
+import io.horizontalsystems.tronkit.TronKit
+import io.horizontalsystems.tronkit.hexStringToByteArray
+import io.horizontalsystems.tronkit.models.TransactionTag
+import io.horizontalsystems.tronkit.network.Network
+import io.reactivex.Flowable
+import io.reactivex.Single
+import kotlinx.coroutines.rx2.asFlowable
+import kotlinx.coroutines.rx2.rxSingle
+
+class TronTransactionsAdapter(
+    val tronKitWrapper: TronKitWrapper,
+    private val transactionConverter: TronTransactionConverter
+) : ITransactionsAdapter {
+
+    private val tronKit = tronKitWrapper.tronKit
+
+    override val explorerTitle: String
+        get() = "Tronscan"
+
+    override fun getTransactionUrl(transactionHash: String): String = when (tronKit.network) {
+        Network.Mainnet -> "https://tronscan.io/#/transaction/$transactionHash"
+        Network.ShastaTestnet -> "https://shasta.tronscan.org/#/transaction/$transactionHash"
+        Network.NileTestnet -> "https://nile.tronscan.org/#/transaction/$transactionHash"
+    }
+
+    override val lastBlockInfo: LastBlockInfo
+        get() = tronKit.lastBlockHeight.toInt().let { LastBlockInfo(it) }
+
+    override val lastBlockUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.lastBlockHeightFlow.asFlowable().map { }
+
+    override val transactionsState: AdapterState
+        get() = convertToAdapterState(tronKit.syncState)
+
+    override val transactionsStateUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.syncStateFlow.asFlowable().map {}
+
+    override fun getTransactionsAsync(
+        from: TransactionRecord?,
+        token: Token?,
+        limit: Int,
+        transactionType: FilterTransactionType
+    ): Single<List<TransactionRecord>> {
+        return rxSingle {
+            tronKit.getFullTransactions(
+                getFilters(token, transactionType),
+                from?.transactionHash?.hexStringToByteArray(),
+                limit
+            )
+        }.map {
+            it.map { tx -> transactionConverter.transactionRecord(tx) }
+        }
+    }
+
+    override fun getTransactionRecordsFlowable(
+        token: Token?,
+        transactionType: FilterTransactionType
+    ): Flowable<List<TransactionRecord>> {
+        return tronKit.getFullTransactionsFlow(getFilters(token, transactionType)).asFlowable().map {
+            it.map { tx -> transactionConverter.transactionRecord(tx) }
+        }
+    }
+
+    private fun convertToAdapterState(syncState: TronKit.SyncState): AdapterState =
+        when (syncState) {
+            is TronKit.SyncState.Synced -> AdapterState.Synced
+            is TronKit.SyncState.NotSynced -> AdapterState.NotSynced(syncState.error)
+            is TronKit.SyncState.Syncing -> AdapterState.Syncing()
+        }
+
+    private fun coinTagName(token: Token) = when (val type = token.type) {
+        TokenType.Native -> TransactionTag.TRX_COIN
+        is TokenType.Eip20 -> type.address
+        else -> ""
+    }
+
+    private fun incomingTag(token: Token) = when (val type = token.type) {
+        TokenType.Native -> TransactionTag.TRX_COIN_INCOMING
+        is TokenType.Eip20 -> TransactionTag.trc20Incoming(type.address)
+        else -> ""
+    }
+
+    private fun outgoingTag(token: Token) = when (val type = token.type) {
+        TokenType.Native -> TransactionTag.TRX_COIN_OUTGOING
+        is TokenType.Eip20 -> TransactionTag.trc20Outgoing(type.address)
+        else -> ""
+    }
+
+    private fun getFilters(token: Token?, filter: FilterTransactionType): List<List<String>> {
+        val filterCoin = token?.let {
+            coinTagName(it)
+        }
+
+        val filterTag = when (filter) {
+            FilterTransactionType.All -> null
+            FilterTransactionType.Incoming -> when {
+                token != null -> incomingTag(token)
+                else -> TransactionTag.INCOMING
+            }
+
+            FilterTransactionType.Outgoing -> when {
+                token != null -> outgoingTag(token)
+                else -> TransactionTag.OUTGOING
+            }
+
+            FilterTransactionType.Swap -> TransactionTag.SWAP
+            FilterTransactionType.Approve -> TransactionTag.TRC20_APPROVE
+        }
+
+        return listOfNotNull(filterCoin, filterTag).map { listOf(it) }
+    }
+}

app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/BalanceViewItem.kt
@@ -2,9 +2,13 @@ package io.horizontalsystems.bankwallet.modules.balance
 
 import androidx.compose.runtime.Immutable
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.core.*
+import io.horizontalsystems.bankwallet.core.AdapterState
+import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.adapters.zcash.ZcashAdapter
+import io.horizontalsystems.bankwallet.core.iconPlaceholder
+import io.horizontalsystems.bankwallet.core.imageUrl
 import io.horizontalsystems.bankwallet.core.providers.Translator
+import io.horizontalsystems.bankwallet.core.swappable
 import io.horizontalsystems.bankwallet.entities.Currency
 import io.horizontalsystems.bankwallet.entities.Wallet
 import io.horizontalsystems.core.helpers.DateHelper
@@ -118,8 +122,9 @@ class BalanceViewItemFactory {
         BlockchainType.Solana,
         BlockchainType.Gnosis,
         BlockchainType.Fantom,
-        BlockchainType.ArbitrumOne -> 50
-        BlockchainType.Solana -> 50
+        BlockchainType.ArbitrumOne,
+        BlockchainType.Solana,
+        BlockchainType.Tron -> 50
         is BlockchainType.Unsupported -> 0
     }
 

app/src/main/java/io/horizontalsystems/bankwallet/entities/transactionrecords/tron/TronApproveTransactionRecord.kt
@@ -1,0 +1,18 @@
+package io.horizontalsystems.bankwallet.entities.transactionrecords.tron
+
+import io.horizontalsystems.bankwallet.entities.TransactionValue
+import io.horizontalsystems.bankwallet.modules.transactions.TransactionSource
+import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.tronkit.models.Transaction
+
+class TronApproveTransactionRecord(
+    transaction: Transaction,
+    baseToken: Token,
+    source: TransactionSource,
+    val spender: String,
+    val value: TransactionValue
+) : TronTransactionRecord(transaction, baseToken, source) {
+
+    override val mainValue = value
+
+}

horizontalsystems/unstoppable-wallet-android

app/src/main/java/cash/p/terminal/core/MarketKitExtensions.kt
@@ -4,10 +4,22 @@ import androidx.compose.ui.graphics.Color
 import cash.p.terminal.R
 import cash.p.terminal.core.managers.RestoreSettingType
 import cash.p.terminal.core.providers.Translator
-import cash.p.terminal.entities.*
+import cash.p.terminal.entities.AccountType
+import cash.p.terminal.entities.BitcoinCashCoinType
+import cash.p.terminal.entities.CoinSettingType
+import cash.p.terminal.entities.CoinSettings
+import cash.p.terminal.entities.FeePriceScale
+import cash.p.terminal.entities.derivation
 import io.horizontalsystems.hdwalletkit.ExtendedKeyCoinType
 import io.horizontalsystems.hdwalletkit.HDWallet
-import io.horizontalsystems.marketkit.models.*
+import io.horizontalsystems.marketkit.models.Blockchain
+import io.horizontalsystems.marketkit.models.BlockchainType
+import io.horizontalsystems.marketkit.models.Coin
+import io.horizontalsystems.marketkit.models.FullCoin
+import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.marketkit.models.TokenQuery
+import io.horizontalsystems.marketkit.models.TokenType
+import io.horizontalsystems.marketkit.models.TopPlatform
 import io.horizontalsystems.nftkit.models.NftType
 
 val Token.protocolType: String?
@@ -139,6 +151,9 @@ val TokenQuery.isSupported: Boolean
         BlockchainType.Solana -> {
             tokenType is TokenType.Native || tokenType is TokenType.Spl
         }
+        BlockchainType.Tron -> {
+            tokenType is TokenType.Native || tokenType is TokenType.Eip20
+        }
         else -> false
     }
 
@@ -161,6 +176,7 @@ val Blockchain.description: String
         BlockchainType.Solana -> "SOL, SPL tokens"
         BlockchainType.Gnosis -> "xDAI, ERC20 tokens"
         BlockchainType.Fantom -> "FTM, ERC20 tokens"
+        BlockchainType.Tron -> "TRX, TRC20 tokens"
         else -> ""
     }
 
@@ -242,6 +258,7 @@ val BlockchainType.tokenIconPlaceholder: Int
         BlockchainType.ArbitrumOne -> R.drawable.arbitrum_erc20
         BlockchainType.Gnosis -> R.drawable.gnosis_erc20
         BlockchainType.Fantom -> R.drawable.fantom_erc20
+        BlockchainType.Tron -> R.drawable.fantom_erc20 //TODO
         else -> R.drawable.coin_placeholder
     }
 

app/src/main/java/cash/p/terminal/core/adapters/BaseTronAdapter.kt
@@ -1,0 +1,29 @@
+package cash.p.terminal.core.adapters
+
+import cash.p.terminal.core.IAdapter
+import cash.p.terminal.core.IBalanceAdapter
+import cash.p.terminal.core.IReceiveAdapter
+import cash.p.terminal.core.managers.TronKitWrapper
+import io.horizontalsystems.tronkit.transaction.Signer
+
+abstract class BaseTronAdapter(
+    tronKitWrapper: TronKitWrapper,
+    val decimal: Int
+) : IAdapter, IBalanceAdapter, IReceiveAdapter {
+
+    val tronKit = tronKitWrapper.tronKit
+    protected val signer: Signer? = tronKitWrapper.signer
+
+    override val debugInfo: String
+        get() = ""
+
+    // IReceiveAdapter
+
+    override val receiveAddress: String
+        get() = tronKit.address.base58
+
+    companion object {
+        const val confirmationsThreshold: Int = 19
+    }
+
+}

app/src/main/java/cash/p/terminal/core/adapters/TronAdapter.kt
@@ -1,0 +1,88 @@
+package cash.p.terminal.core.adapters
+
+import cash.p.terminal.core.AdapterState
+import cash.p.terminal.core.App
+import cash.p.terminal.core.BalanceData
+import cash.p.terminal.core.ISendTronAdapter
+import cash.p.terminal.core.managers.TronKitWrapper
+import io.horizontalsystems.tronkit.TronKit
+import io.horizontalsystems.tronkit.models.Address
+import io.horizontalsystems.tronkit.network.Network
+import io.reactivex.Flowable
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.rx2.asFlowable
+import java.math.BigDecimal
+import java.math.BigInteger
+
+class TronAdapter(kitWrapper: TronKitWrapper) : BaseTronAdapter(kitWrapper, decimal), ISendTronAdapter {
+
+    // IAdapter
+
+    override fun start() {
+        // started via TronKitManager
+    }
+
+    override fun stop() {
+        // stopped via TronKitManager
+    }
+
+    override fun refresh() {
+        // refreshed via TronKitManager
+    }
+
+    // IBalanceAdapter
+
+    override val balanceState: AdapterState
+        get() = convertToAdapterState(tronKit.syncState)
+
+    override val balanceStateUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.syncStateFlow.map {}.asFlowable()
+
+    override val balanceData: BalanceData
+        get() = BalanceData(balanceInBigDecimal(tronKit.trxBalance, decimal))
+
+    override val balanceUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.trxBalanceFlow.map {}.asFlowable()
+
+    // ISendTronAdapter
+
+    override suspend fun send(amount: BigInteger, to: Address): String {
+        if (signer == null) throw Exception()
+
+        val transferContract = tronKit.transferContract(amount, to)
+
+        return tronKit.send(transferContract, signer)
+    }
+
+    private fun convertToAdapterState(syncState: TronKit.SyncState): AdapterState =
+        when (syncState) {
+            is TronKit.SyncState.Synced -> AdapterState.Synced
+            is TronKit.SyncState.NotSynced -> AdapterState.NotSynced(syncState.error)
+            is TronKit.SyncState.Syncing -> AdapterState.Syncing()
+        }
+
+    private fun scaleDown(amount: BigDecimal, decimals: Int = decimal): BigDecimal {
+        return amount.movePointLeft(decimals).stripTrailingZeros()
+    }
+
+    private fun scaleUp(amount: BigDecimal, decimals: Int = decimal): BigInteger {
+        return amount.movePointRight(decimals).toBigInteger()
+    }
+
+    private fun balanceInBigDecimal(balance: BigInteger?, decimal: Int): BigDecimal {
+        balance?.toBigDecimal()?.let {
+            return scaleDown(it, decimal)
+        } ?: return BigDecimal.ZERO
+    }
+
+    companion object {
+        const val decimal = 6
+
+        fun clear(walletId: String) {
+            Network.values().forEach { network ->
+                TronKit.clear(App.instance, network, walletId)
+            }
+        }
+    }
+
+}

app/src/main/java/cash/p/terminal/core/adapters/TronTransactionsAdapter.kt
@@ -1,0 +1,122 @@
+package cash.p.terminal.core.adapters
+
+import cash.p.terminal.core.AdapterState
+import cash.p.terminal.core.ITransactionsAdapter
+import cash.p.terminal.core.managers.TronKitWrapper
+import cash.p.terminal.entities.LastBlockInfo
+import cash.p.terminal.entities.transactionrecords.TransactionRecord
+import cash.p.terminal.modules.transactions.FilterTransactionType
+import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.marketkit.models.TokenType
+import io.horizontalsystems.tronkit.TronKit
+import io.horizontalsystems.tronkit.hexStringToByteArray
+import io.horizontalsystems.tronkit.models.TransactionTag
+import io.horizontalsystems.tronkit.network.Network
+import io.reactivex.Flowable
+import io.reactivex.Single
+import kotlinx.coroutines.rx2.asFlowable
+import kotlinx.coroutines.rx2.rxSingle
+
+class TronTransactionsAdapter(
+    val tronKitWrapper: TronKitWrapper,
+    private val transactionConverter: TronTransactionConverter
+) : ITransactionsAdapter {
+
+    private val tronKit = tronKitWrapper.tronKit
+
+    override val explorerTitle: String
+        get() = "Tronscan"
+
+    override fun getTransactionUrl(transactionHash: String): String = when (tronKit.network) {
+        Network.Mainnet -> "https://tronscan.io/#/transaction/$transactionHash"
+        Network.ShastaTestnet -> "https://shasta.tronscan.org/#/transaction/$transactionHash"
+        Network.NileTestnet -> "https://nile.tronscan.org/#/transaction/$transactionHash"
+    }
+
+    override val lastBlockInfo: LastBlockInfo
+        get() = tronKit.lastBlockHeight.toInt().let { LastBlockInfo(it) }
+
+    override val lastBlockUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.lastBlockHeightFlow.asFlowable().map { }
+
+    override val transactionsState: AdapterState
+        get() = convertToAdapterState(tronKit.syncState)
+
+    override val transactionsStateUpdatedFlowable: Flowable<Unit>
+        get() = tronKit.syncStateFlow.asFlowable().map {}
+
+    override fun getTransactionsAsync(
+        from: TransactionRecord?,
+        token: Token?,
+        limit: Int,
+        transactionType: FilterTransactionType
+    ): Single<List<TransactionRecord>> {
+        return rxSingle {
+            tronKit.getFullTransactions(
+                getFilters(token, transactionType),
+                from?.transactionHash?.hexStringToByteArray(),
+                limit
+            )
+        }.map {
+            it.map { tx -> transactionConverter.transactionRecord(tx) }
+        }
+    }
+
+    override fun getTransactionRecordsFlowable(
+        token: Token?,
+        transactionType: FilterTransactionType
+    ): Flowable<List<TransactionRecord>> {
+        return tronKit.getFullTransactionsFlow(getFilters(token, transactionType)).asFlowable().map {
+            it.map { tx -> transactionConverter.transactionRecord(tx) }
+        }
+    }
+
+    private fun convertToAdapterState(syncState: TronKit.SyncState): AdapterState =
+        when (syncState) {
+            is TronKit.SyncState.Synced -> AdapterState.Synced
+            is TronKit.SyncState.NotSynced -> AdapterState.NotSynced(syncState.error)
+            is TronKit.SyncState.Syncing -> AdapterState.Syncing()
+        }
+
+    private fun coinTagName(token: Token) = when (val type = token.type) {
+        TokenType.Native -> TransactionTag.TRX_COIN
+        is TokenType.Eip20 -> type.address
+        else -> ""
+    }
+
+    private fun incomingTag(token: Token) = when (val type = token.type) {
+        TokenType.Native -> TransactionTag.TRX_COIN_INCOMING
+        is TokenType.Eip20 -> TransactionTag.trc20Incoming(type.address)
+        else -> ""
+    }
+
+    private fun outgoingTag(token: Token) = when (val type = token.type) {
+        TokenType.Native -> TransactionTag.TRX_COIN_OUTGOING
+        is TokenType.Eip20 -> TransactionTag.trc20Outgoing(type.address)
+        else -> ""
+    }
+
+    private fun getFilters(token: Token?, filter: FilterTransactionType): List<List<String>> {
+        val filterCoin = token?.let {
+            coinTagName(it)
+        }
+
+        val filterTag = when (filter) {
+            FilterTransactionType.All -> null
+            FilterTransactionType.Incoming -> when {
+                token != null -> incomingTag(token)
+                else -> TransactionTag.INCOMING
+            }
+
+            FilterTransactionType.Outgoing -> when {
+                token != null -> outgoingTag(token)
+                else -> TransactionTag.OUTGOING
+            }
+
+            FilterTransactionType.Swap -> TransactionTag.SWAP
+            FilterTransactionType.Approve -> TransactionTag.TRC20_APPROVE
+        }
+
+        return listOfNotNull(filterCoin, filterTag).map { listOf(it) }
+    }
+}

app/src/main/java/cash/p/terminal/modules/balance/BalanceViewItem.kt
@@ -2,9 +2,13 @@ package cash.p.terminal.modules.balance
 
 import androidx.compose.runtime.Immutable
 import cash.p.terminal.R
-import cash.p.terminal.core.*
+import cash.p.terminal.core.AdapterState
+import cash.p.terminal.core.App
 import cash.p.terminal.core.adapters.zcash.ZcashAdapter
+import cash.p.terminal.core.iconPlaceholder
+import cash.p.terminal.core.imageUrl
 import cash.p.terminal.core.providers.Translator
+import cash.p.terminal.core.swappable
 import cash.p.terminal.entities.Currency
 import cash.p.terminal.entities.Wallet
 import io.horizontalsystems.core.helpers.DateHelper
@@ -118,8 +122,9 @@ class BalanceViewItemFactory {
         BlockchainType.Solana,
         BlockchainType.Gnosis,
         BlockchainType.Fantom,
-        BlockchainType.ArbitrumOne -> 50
-        BlockchainType.Solana -> 50
+        BlockchainType.ArbitrumOne,
+        BlockchainType.Solana,
+        BlockchainType.Tron -> 50
         is BlockchainType.Unsupported -> 0
     }
 

app/src/main/java/io/horizontalsystems/bankwallet/entities/transactionrecords/tron/TronApproveTransactionRecord.kt
@@ -1,0 +1,18 @@
+package cash.p.terminal.entities.transactionrecords.tron
+
+import cash.p.terminal.entities.TransactionValue
+import cash.p.terminal.modules.transactions.TransactionSource
+import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.tronkit.models.Transaction
+
+class TronApproveTransactionRecord(
+    transaction: Transaction,
+    baseToken: Token,
+    source: TransactionSource,
+    val spender: String,
+    val value: TransactionValue
+) : TronTransactionRecord(transaction, baseToken, source) {
+
+    override val mainValue = value
+
+}