eyovvalupe/Ethereum-wallet-payment

Implement auto enabling of Trc20 tokens
- refactor auto enabling of Evm Eip20 tokens

app/src/main/java/io/horizontalsystems/bankwallet/core/App.kt
@@ -56,8 +56,10 @@ import io.horizontalsystems.bankwallet.core.managers.SolanaRpcSourceManager
 import io.horizontalsystems.bankwallet.core.managers.SolanaWalletManager
 import io.horizontalsystems.bankwallet.core.managers.SystemInfoManager
 import io.horizontalsystems.bankwallet.core.managers.TermsManager
+import io.horizontalsystems.bankwallet.core.managers.TokenAutoEnableManager
 import io.horizontalsystems.bankwallet.core.managers.TorManager
 import io.horizontalsystems.bankwallet.core.managers.TransactionAdapterManager
+import io.horizontalsystems.bankwallet.core.managers.TronAccountManager
 import io.horizontalsystems.bankwallet.core.managers.TronKitManager
 import io.horizontalsystems.bankwallet.core.managers.WalletActivator
 import io.horizontalsystems.bankwallet.core.managers.WalletManager
@@ -135,6 +137,7 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         lateinit var transactionAdapterManager: TransactionAdapterManager
         lateinit var walletManager: IWalletManager
         lateinit var walletActivator: WalletActivator
+        lateinit var tokenAutoEnableManager: TokenAutoEnableManager
         lateinit var walletStorage: IWalletStorage
         lateinit var accountManager: IAccountManager
         lateinit var accountFactory: IAccountFactory
@@ -276,12 +279,13 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         encryptionManager = EncryptionManager(keyProvider)
 
         walletActivator = WalletActivator(walletManager, marketKit)
+        tokenAutoEnableManager = TokenAutoEnableManager(appDatabase.tokenAutoEnabledBlockchainDao())
 
         val evmAccountManagerFactory = EvmAccountManagerFactory(
             accountManager,
             walletManager,
             marketKit,
-            appDatabase.evmAccountStateDao()
+            tokenAutoEnableManager
         )
         evmBlockchainManager = EvmBlockchainManager(
             backgroundManager,
@@ -289,6 +293,15 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
             marketKit,
             evmAccountManagerFactory,
         )
+
+        val tronAccountManager = TronAccountManager(
+            accountManager,
+            walletManager,
+            marketKit,
+            tronKitManager,
+            tokenAutoEnableManager
+        )
+        tronAccountManager.start()
 
         systemInfoManager = SystemInfoManager()
 

app/src/main/java/io/horizontalsystems/bankwallet/core/managers/TokenAutoEnableManager.kt
@@ -1,0 +1,18 @@
+package io.horizontalsystems.bankwallet.core.managers
+
+import io.horizontalsystems.bankwallet.core.storage.TokenAutoEnabledBlockchainDao
+import io.horizontalsystems.bankwallet.entities.Account
+import io.horizontalsystems.bankwallet.entities.TokenAutoEnabledBlockchain
+import io.horizontalsystems.marketkit.models.BlockchainType
+
+class TokenAutoEnableManager(
+    private val tokenAutoEnabledBlockchainDao: TokenAutoEnabledBlockchainDao
+) {
+    fun markAutoEnable(account: Account, blockchainType: BlockchainType) {
+        tokenAutoEnabledBlockchainDao.insert(TokenAutoEnabledBlockchain(account.id, blockchainType))
+    }
+
+    fun isAutoEnabled(account: Account, blockchainType: BlockchainType): Boolean {
+        return tokenAutoEnabledBlockchainDao.get(account.id, blockchainType) != null
+    }
+}

app/src/main/java/io/horizontalsystems/bankwallet/modules/restoreaccount/restoreblockchains/RestoreBlockchainsService.kt
@@ -8,6 +8,7 @@ import io.horizontalsystems.bankwallet.core.coinSettingType
 import io.horizontalsystems.bankwallet.core.managers.EvmBlockchainManager
 import io.horizontalsystems.bankwallet.core.managers.MarketKitWrapper
 import io.horizontalsystems.bankwallet.core.managers.RestoreSettings
+import io.horizontalsystems.bankwallet.core.managers.TokenAutoEnableManager
 import io.horizontalsystems.bankwallet.core.order
 import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.core.supportedTokens
@@ -37,6 +38,7 @@ class RestoreBlockchainsService(
     private val marketKit: MarketKitWrapper,
     private val enableCoinService: EnableCoinService,
     private val evmBlockchainManager: EvmBlockchainManager,
+    private val tokenAutoEnableManager: TokenAutoEnableManager
 ) : Clearable {
 
     private val disposables = CompositeDisposable()
@@ -198,10 +200,7 @@ class RestoreBlockchainsService(
         }
 
         items.filter { it.enabled }.forEach { item ->
-            val isEvm = evmBlockchainManager.allBlockchainTypes.contains(item.blockchain.type)
-            if (isEvm) {
-                evmBlockchainManager.getEvmAccountManager(item.blockchain.type).markAutoEnable(account)
-            }
+            tokenAutoEnableManager.markAutoEnable(account, item.blockchain.type)
         }
 
         if (enabledCoins.isEmpty()) return

horizontalsystems/unstoppable-wallet-android

app/src/main/java/cash/p/terminal/core/App.kt
@@ -54,11 +54,13 @@ import cash.p.terminal.core.managers.RestoreSettingsManager
 import cash.p.terminal.core.managers.SolanaKitManager
 import cash.p.terminal.core.managers.SolanaRpcSourceManager
 import cash.p.terminal.core.managers.SolanaWalletManager
-import cash.p.terminal.core.managers.SubscriptionManager
 import cash.p.terminal.core.managers.SystemInfoManager
 import cash.p.terminal.core.managers.TermsManager
+import cash.p.terminal.core.managers.TokenAutoEnableManager
 import cash.p.terminal.core.managers.TorManager
 import cash.p.terminal.core.managers.TransactionAdapterManager
+import cash.p.terminal.core.managers.TronAccountManager
+import cash.p.terminal.core.managers.TronKitManager
 import cash.p.terminal.core.managers.WalletActivator
 import cash.p.terminal.core.managers.WalletManager
 import cash.p.terminal.core.managers.WalletStorage
@@ -135,6 +137,7 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         lateinit var transactionAdapterManager: TransactionAdapterManager
         lateinit var walletManager: IWalletManager
         lateinit var walletActivator: WalletActivator
+        lateinit var tokenAutoEnableManager: TokenAutoEnableManager
         lateinit var walletStorage: IWalletStorage
         lateinit var accountManager: IAccountManager
         lateinit var accountFactory: IAccountFactory
@@ -277,12 +280,13 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         encryptionManager = EncryptionManager(keyProvider)
 
         walletActivator = WalletActivator(walletManager, marketKit)
+        tokenAutoEnableManager = TokenAutoEnableManager(appDatabase.tokenAutoEnabledBlockchainDao())
 
         val evmAccountManagerFactory = EvmAccountManagerFactory(
             accountManager,
             walletManager,
             marketKit,
-            appDatabase.evmAccountStateDao()
+            tokenAutoEnableManager
         )
         evmBlockchainManager = EvmBlockchainManager(
             backgroundManager,
@@ -290,6 +294,15 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
             marketKit,
             evmAccountManagerFactory,
         )
+
+        val tronAccountManager = TronAccountManager(
+            accountManager,
+            walletManager,
+            marketKit,
+            tronKitManager,
+            tokenAutoEnableManager
+        )
+        tronAccountManager.start()
 
         systemInfoManager = SystemInfoManager()
 

app/src/main/java/cash/p/terminal/core/managers/TokenAutoEnableManager.kt
@@ -1,0 +1,18 @@
+package cash.p.terminal.core.managers
+
+import cash.p.terminal.core.storage.TokenAutoEnabledBlockchainDao
+import cash.p.terminal.entities.Account
+import cash.p.terminal.entities.TokenAutoEnabledBlockchain
+import io.horizontalsystems.marketkit.models.BlockchainType
+
+class TokenAutoEnableManager(
+    private val tokenAutoEnabledBlockchainDao: TokenAutoEnabledBlockchainDao
+) {
+    fun markAutoEnable(account: Account, blockchainType: BlockchainType) {
+        tokenAutoEnabledBlockchainDao.insert(TokenAutoEnabledBlockchain(account.id, blockchainType))
+    }
+
+    fun isAutoEnabled(account: Account, blockchainType: BlockchainType): Boolean {
+        return tokenAutoEnabledBlockchainDao.get(account.id, blockchainType) != null
+    }
+}

app/src/main/java/cash/p/terminal/modules/restoreaccount/restoreblockchains/RestoreBlockchainsService.kt
@@ -8,6 +8,7 @@ import cash.p.terminal.core.coinSettingType
 import cash.p.terminal.core.managers.EvmBlockchainManager
 import cash.p.terminal.core.managers.MarketKitWrapper
 import cash.p.terminal.core.managers.RestoreSettings
+import cash.p.terminal.core.managers.TokenAutoEnableManager
 import cash.p.terminal.core.order
 import cash.p.terminal.core.subscribeIO
 import cash.p.terminal.core.supportedTokens
@@ -37,6 +38,7 @@ class RestoreBlockchainsService(
     private val marketKit: MarketKitWrapper,
     private val enableCoinService: EnableCoinService,
     private val evmBlockchainManager: EvmBlockchainManager,
+    private val tokenAutoEnableManager: TokenAutoEnableManager
 ) : Clearable {
 
     private val disposables = CompositeDisposable()
@@ -198,10 +200,7 @@ class RestoreBlockchainsService(
         }
 
         items.filter { it.enabled }.forEach { item ->
-            val isEvm = evmBlockchainManager.allBlockchainTypes.contains(item.blockchain.type)
-            if (isEvm) {
-                evmBlockchainManager.getEvmAccountManager(item.blockchain.type).markAutoEnable(account)
-            }
+            tokenAutoEnableManager.markAutoEnable(account, item.blockchain.type)
         }
 
         if (enabledCoins.isEmpty()) return