6b4fc549bfad7e9ab2b96833f4f6fb8e9372f7b6
bakyt <abdrasulov@gmail.com>
2023-09-21 14:40:38 +0600

    Show the PinUnlock content in activity itself

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/PinModule.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/PinModule.kt
@@ -6,7 +6,6 @@ object PinModule {
     const val RESULT_CANCELLED = 2
     const val PIN_COUNT = 6
 
-    const val keyAttachedToLockScreen = "attached_to_lock_screen"
     const val keyShowCancel = "show_cancel"
     const val requestKey = "pin_request_key"
     const val requestResult = "pin_request_result"
