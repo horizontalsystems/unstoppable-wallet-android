aabc73c67046171da2bfb7ef302b513c630d0b6b
bakyt <abdrasulov@gmail.com>
2023-09-21 16:45:54 +0600

    Update for confirm pin
    - Remove biometric
    - Validate entered pin with only current level pin

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinConfirm.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinConfirm.kt
@@ -1,17 +1,14 @@
 package io.horizontalsystems.bankwallet.modules.pin.ui
 
-import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.pin.unlock.PinConfirmViewModel
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.headline1_leah
@@ -23,33 +20,10 @@ fun PinConfirm(
     onCancel: () -> Unit,
 ) {
     val viewModel = viewModel<PinConfirmViewModel>(factory = PinConfirmViewModel.Factory())
-    var showBiometricPrompt by remember { mutableStateOf(viewModel.uiState.fingerScannerEnabled) }
-    var showBiometricDisabledAlert by remember { mutableStateOf(false) }
 
     if (viewModel.uiState.unlocked) {
         onSuccess.invoke()
         viewModel.unlocked()
-    }
-
-    if (showBiometricPrompt) {
-        BiometricPromptDialog(
-            onSuccess = {
-                viewModel.onBiometricsUnlock()
-                showBiometricPrompt = false
-            },
-            onError = { errorCode ->
-                if (errorCode == BiometricPrompt.ERROR_LOCKOUT) {
-                    showBiometricDisabledAlert = true
-                }
-                showBiometricPrompt = false
-            }
-        )
-    }
-
-    if (showBiometricDisabledAlert) {
-        BiometricDisabledDialog {
-            showBiometricDisabledAlert = false
-        }
     }
 
     Column(
@@ -68,7 +42,7 @@ fun PinConfirm(
                     )
                 } else {
                     headline1_leah(
-                        text = viewModel.uiState.title,
+                        text = stringResource(R.string.Unlock_Passcode),
                         textAlign = TextAlign.Center
                     )
                 }
@@ -84,11 +58,7 @@ fun PinConfirm(
         PinNumpad(
             onNumberClick = { number -> viewModel.onKeyClick(number) },
             onDeleteClick = { viewModel.onDelete() },
-            showFingerScanner = viewModel.uiState.fingerScannerEnabled,
             showRandomizer = true,
-            showBiometricPrompt = {
-                showBiometricPrompt = true
-            },
             inputState = viewModel.uiState.inputState
         )
     }
