b6eb37ab2bc590d5acf1f12d4d3ea3ecdaded311
bakyt <abdrasulov@gmail.com>
2023-09-25 18:38:47 +0600

    Ask user to set pin on biometric enabling

--- app/src/main/java/io/horizontalsystems/bankwallet/core/NavController.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/NavController.kt
@@ -1,13 +1,17 @@
 package io.horizontalsystems.bankwallet.core
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.annotation.IdRes
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.pin.PinModule
+import io.horizontalsystems.bankwallet.modules.pin.SetPinFragment
 import io.horizontalsystems.bankwallet.modules.settings.terms.TermsFragment
 import io.horizontalsystems.core.getNavigationResult
+import java.util.UUID
 
 fun NavController.slideFromRight(@IdRes resId: Int, args: Bundle? = null) {
     val navOptions = NavOptions.Builder()
@@ -58,5 +62,66 @@ fun NavController.navigateWithTermsAccepted(action: () -> Unit) {
         slideFromBottom(R.id.termsFragment)
     } else {
         action.invoke()
+    }
+}
+
+fun NavController.ensurePinSet(action: () -> Unit) {
+    if (App.pinComponent.isPinSet) {
+        action.invoke()
+    } else {
+        slideFromRightForResult<SetPinFragment.Result>(R.id.setPinFragment) {
+            action.invoke()
+        }
+    }
+}
+
+fun <T: Parcelable> NavController.slideFromRightForResult(
+    @IdRes resId: Int,
+    args: Bundle? = null,
+    onResult: (T) -> Unit
+) {
+    val navOptions = NavOptions.Builder()
+        .setEnterAnim(R.anim.slide_from_right)
+        .setExitAnim(android.R.anim.fade_out)
+        .setPopEnterAnim(android.R.anim.fade_in)
+        .setPopExitAnim(R.anim.slide_to_right)
+        .build()
+
+    navigateForResult(resId, args, navOptions, onResult)
+}
+
+private fun <T : Parcelable> NavController.navigateForResult(
+    resId: Int,
+    args: Bundle?,
+    navOptions: NavOptions,
+    onResult: (T) -> Unit,
+) {
+    val key = UUID.randomUUID().toString()
+    getNavigationResultX(key, onResult)
+    val bundle = bundleOf("resultKey" to key)
+    args?.let { bundle.putAll(it) }
+    navigate(resId, bundle, navOptions)
+}
+
+private fun <T: Parcelable> NavController.getNavigationResultX(key: String, onResult: (T) -> Unit) {
+    currentBackStackEntry?.let { backStackEntry ->
+        backStackEntry.savedStateHandle.getLiveData<T>(key).observe(backStackEntry) {
+            onResult.invoke(it)
+
+            backStackEntry.savedStateHandle.remove<T>(key)
+        }
+    }
+}
+
+fun <T: Parcelable> NavController.setNavigationResultX(result: T, destinationId: Int? = null) {
+    val resultKey = currentBackStackEntry?.arguments?.getString("resultKey")
+
+    val backStackEntry = when (destinationId) {
+        null -> previousBackStackEntry
+        else -> currentBackStack.value.findLast { it.destination.id == destinationId }
+    }
+
+    resultKey?.let {
+        backStackEntry?.savedStateHandle?.set(resultKey, result)
     }
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/security/ui/PasscodeBlock.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/security/ui/PasscodeBlock.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.authorizedAction
+import io.horizontalsystems.bankwallet.core.ensurePinSet
 import io.horizontalsystems.bankwallet.core.slideFromRight
 import io.horizontalsystems.bankwallet.modules.settings.security.SecurityCenterCell
 import io.horizontalsystems.bankwallet.modules.settings.security.passcode.SecuritySettingsViewModel
@@ -128,7 +129,9 @@ fun PasscodeBlock(
                         checked = uiState.biometricsEnabled,
                         onCheckedChange = { enabled ->
                             if (enabled) {
-                                viewModel.enableBiometrics()
+                                navController.ensurePinSet {
+                                    viewModel.enableBiometrics()
+                                }
                             } else {
                                 viewModel.disableBiometrics()
                             }
