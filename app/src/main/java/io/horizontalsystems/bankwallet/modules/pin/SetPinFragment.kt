8f0b88705525dcd92483025cec2c16eece9c95ee
bakyt <abdrasulov@gmail.com>
2023-09-25 18:38:47 +0600

    Ask user to set pin on biometric enabling

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/SetPinFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/SetPinFragment.kt
@@ -1,24 +1,34 @@
 package io.horizontalsystems.bankwallet.modules.pin
 
+import android.os.Parcelable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
+import io.horizontalsystems.bankwallet.core.setNavigationResultX
 import io.horizontalsystems.bankwallet.modules.pin.ui.PinSet
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.core.findNavController
+import kotlinx.parcelize.Parcelize
 
 class SetPinFragment : BaseComposeFragment() {
 
     @Composable
     override fun GetContent() {
+        val navController = findNavController()
         ComposeAppTheme {
             PinSet(
                 title = stringResource(R.string.PinSet_Title),
                 description = stringResource(R.string.PinSet_Info),
-                dismissWithSuccess = { findNavController().popBackStack() },
-                onBackPress = { findNavController().popBackStack() }
+                dismissWithSuccess = {
+                    navController.setNavigationResultX(Result(true))
+                    navController.popBackStack()
+                },
+                onBackPress = { navController.popBackStack() }
             )
         }
     }
+
+    @Parcelize
+    data class Result(val success: Boolean) : Parcelable
 }
