ca2f4ca69da5e3068f954e58248aae8c587e6ded
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-10-04 17:42:19 +0600

    Update design for Coin description block

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/overview/ui/About.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/overview/ui/About.kt
@@ -1,17 +1,33 @@
 package io.horizontalsystems.bankwallet.modules.coin.overview.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
-import io.horizontalsystems.bankwallet.ui.compose.components.*
+import io.horizontalsystems.bankwallet.ui.compose.components.CellData2
+import io.horizontalsystems.bankwallet.ui.compose.components.CellSingleLineClear
+import io.horizontalsystems.bankwallet.ui.compose.components.DescriptionMarkdown
+import io.horizontalsystems.bankwallet.ui.compose.components.body_leah
+import io.horizontalsystems.bankwallet.ui.compose.components.subhead2_jacob
 
 @Preview
 @Composable
@@ -25,36 +41,44 @@ fun AboutPreview() {
 fun About(text: String) {
     Column {
         CellSingleLineClear(borderTop = true) {
-            body_leah(text = stringResource(id = R.string.CoinPage_About))
+            body_leah(text = stringResource(id = R.string.CoinPage_Overview))
         }
 
         var showReadMoreToggle by remember { mutableStateOf(false)}
         var expanded by remember { mutableStateOf(false)}
 
-        DescriptionMarkdown(
-            textMaxLines = 8,
-            toggleLines = 2,
-            text = text,
-            expanded = expanded
-        ) { overflow ->
-            showReadMoreToggle = overflow
-        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(ComposeAppTheme.colors.lawrence)
+        ) {
+            DescriptionMarkdown(
+                textMaxLines = 8,
+                toggleLines = 2,
+                text = text,
+                expanded = expanded
+            ) { overflow ->
+                showReadMoreToggle = overflow
+            }
 
-        if (showReadMoreToggle) {
-            CellData2 {
-                Row(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    val stringId = if (expanded) R.string.CoinPage_ReadLess else R.string.CoinPage_ReadMore
-                    subhead2_jacob(
-                        modifier = Modifier
-                            .clickable { expanded = !expanded }
-                            .padding(horizontal = 16.dp),
-                        text = stringResource(id = stringId),
-                        textAlign = TextAlign.End,
-                    )
+            if (showReadMoreToggle) {
+                CellData2 {
+                    Row(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalArrangement = Arrangement.End,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        val stringId = if (expanded) R.string.CoinPage_ReadLess else R.string.CoinPage_ReadMore
+                        subhead2_jacob(
+                            modifier = Modifier
+                                .clickable { expanded = !expanded }
+                                .padding(horizontal = 16.dp),
+                            text = stringResource(id = stringId),
+                            textAlign = TextAlign.End,
+                        )
+                    }
                 }
             }
         }
