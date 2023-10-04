991002a0fba32496a5e6e010174ed8494befb2e2
bakyt <abdrasulov@gmail.com>
2023-10-04 15:44:37 +0600

    Apply uiState template to PinSetViewModel

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/set/PinSetViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/set/PinSetViewModel.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.core.managers.UserManager
 import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.bankwallet.modules.pin.PinModule
 import io.horizontalsystems.bankwallet.modules.pin.set.PinSetModule.PinSetViewState
@@ -18,20 +17,24 @@ import kotlinx.coroutines.launch
 
 class PinSetViewModel(
     private val pinComponent: IPinComponent,
-    private val userManager: UserManager,
     private val forDuress: Boolean,
 ) : ViewModel() {
 
     private var enteredPin = ""
     private var submittedPin = ""
 
+    private var stage = Enter
+    private var finished = false
+    private var reverseSlideAnimation = false
+    private var error: String? = null
+
     var uiState by mutableStateOf(
         PinSetViewState(
-            stage = Enter,
+            stage = stage,
             enteredCount = enteredPin.length,
-            finished = false,
-            reverseSlideAnimation = false,
-            error = null,
+            finished = finished,
+            reverseSlideAnimation = reverseSlideAnimation,
+            error = error,
         )
     )
         private set
@@ -40,65 +43,70 @@ class PinSetViewModel(
     fun onDelete() {
         if (enteredPin.isNotEmpty()) {
             enteredPin = enteredPin.dropLast(1)
-            uiState = uiState.copy(
-                enteredCount = enteredPin.length
+            emitState()
+        }
+    }
+
+    private fun emitState(delayTime: Long = 0) {
+        viewModelScope.launch {
+            delay(delayTime)
+            uiState = PinSetViewState(
+                stage = stage,
+                enteredCount = enteredPin.length,
+                finished = finished,
+                reverseSlideAnimation = reverseSlideAnimation,
+                error = error,
             )
         }
     }
 
     fun finished() {
-        uiState = uiState.copy(finished = false)
+        finished = false
+        emitState()
     }
 
     fun onKeyClick(number: Int) {
-        if (enteredPin.length < PinModule.PIN_COUNT) {
+        if (enteredPin.length >= PinModule.PIN_COUNT) return
 
-            enteredPin += number.toString()
-            uiState = uiState.copy(
-                enteredCount = enteredPin.length,
-                error = null,
-                reverseSlideAnimation = false
-            )
+        error = null
+        reverseSlideAnimation = false
+        enteredPin += number.toString()
+        emitState()
 
-            if (enteredPin.length == PinModule.PIN_COUNT) {
-                if (uiState.stage == Enter) {
+        if (enteredPin.length != PinModule.PIN_COUNT) return
 
-                    if (pinComponent.isUnique(enteredPin, forDuress)) {
-                        submittedPin = enteredPin
+        when {
+            stage == Enter -> {
+                if (pinComponent.isUnique(enteredPin, forDuress)) {
+                    submittedPin = enteredPin
 
-                        viewModelScope.launch {
-                            delay(500)
-                            enteredPin = ""
-                            uiState = uiState.copy(
-                                stage = Confirm,
-                                enteredCount = enteredPin.length,
-                            )
+                    enteredPin = ""
+
+                    stage = Confirm
+                    emitState(500)
+                } else {
+                    enteredPin = ""
+
+                    error = Translator.getString(R.string.PinSet_ErrorPinInUse)
+                    emitState()
+                }
+            }
+            submittedPin.isNotEmpty() -> {
+                if (submittedPin == enteredPin) {
+                    try {
+                        if (forDuress) {
+                            pinComponent.setDuressPin(submittedPin)
+                        } else {
+                            pinComponent.setPin(submittedPin)
                         }
-                    } else {
-                        enteredPin = ""
-                        uiState = uiState.copy(
-                            enteredCount = enteredPin.length,
-                            error = Translator.getString(R.string.PinSet_ErrorPinInUse),
-                        )
+
+                        finished = true
+                        emitState()
+                    } catch (ex: Exception) {
+                        resetWithError(R.string.PinSet_ErrorFailedToSavePin)
                     }
-                } else if (submittedPin.isNotEmpty()) {
-                    if (submittedPin == enteredPin) {
-                        try {
-                            if (forDuress) {
-                                pinComponent.setDuressPin(submittedPin)
-                            } else {
-                                pinComponent.setPin(submittedPin)
-                            }
-                            uiState = uiState.copy(finished = true)
-                        } catch (ex: Exception) {
-                            resetWithError(R.string.PinSet_ErrorFailedToSavePin)
-                        }
-                    } else {
-                        viewModelScope.launch {
-                            delay(500)
-                            resetWithError(R.string.PinSet_ErrorPinsDontMatch)
-                        }
-                    }
+                } else {
+                    resetWithError(R.string.PinSet_ErrorPinsDontMatch)
                 }
             }
         }
@@ -107,12 +115,11 @@ class PinSetViewModel(
     private fun resetWithError(errorMessage: Int) {
         submittedPin = ""
         enteredPin = ""
-        uiState = uiState.copy(
-            stage = Enter,
-            enteredCount = enteredPin.length,
-            reverseSlideAnimation = true,
-            error = Translator.getString(errorMessage),
-        )
+
+        stage = Enter
+        reverseSlideAnimation = true
+        error = Translator.getString(errorMessage)
+        emitState(500)
     }
 
 }
