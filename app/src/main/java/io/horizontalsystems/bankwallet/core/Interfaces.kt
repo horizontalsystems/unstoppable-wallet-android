2b2f01925b494b6e9ee9f694a889265a9c896b71
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-10-11 16:38:05 +0600

    Add Zcash status info to AppStatus

--- app/src/main/java/io/horizontalsystems/bankwallet/core/Interfaces.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/Interfaces.kt
@@ -218,6 +218,15 @@ sealed class AdapterState {
     data class Syncing(val progress: Int? = null, val lastBlockDate: Date? = null) : AdapterState()
     data class SearchingTxs(val count: Int) : AdapterState()
     data class NotSynced(val error: Throwable) : AdapterState()
+
+    override fun toString(): String {
+        return when (this) {
+            is Synced -> "Synced"
+            is Syncing -> "Syncing ${progress?.let { "${it * 100}" } ?: ""} lastBlockDate: $lastBlockDate"
+            is SearchingTxs -> "SearchingTxs count: $count"
+            is NotSynced -> "NotSynced ${error.javaClass.simpleName} - message: ${error.message}"
+        }
+    }
 }
 
 interface IBinanceKitManager {
