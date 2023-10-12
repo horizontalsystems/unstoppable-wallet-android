eyovvalupe/Ethereum-wallet-payment

Add full backup restore

app/src/main/java/io/horizontalsystems/bankwallet/core/App.kt
@@ -80,6 +80,7 @@ import io.horizontalsystems.bankwallet.core.storage.EnabledWalletsStorage
 import io.horizontalsystems.bankwallet.core.storage.EvmSyncSourceStorage
 import io.horizontalsystems.bankwallet.core.storage.NftStorage
 import io.horizontalsystems.bankwallet.core.storage.RestoreSettingsStorage
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.BackupProvider
 import io.horizontalsystems.bankwallet.modules.balance.BalanceViewTypeManager
 import io.horizontalsystems.bankwallet.modules.chart.ChartIndicatorManager
 import io.horizontalsystems.bankwallet.modules.contacts.ContactsRepository
@@ -93,6 +94,9 @@ import io.horizontalsystems.bankwallet.modules.market.topplatforms.TopPlatformsR
 import io.horizontalsystems.bankwallet.modules.pin.PinComponent
 import io.horizontalsystems.bankwallet.modules.profeatures.ProFeaturesAuthorizationManager
 import io.horizontalsystems.bankwallet.modules.profeatures.storage.ProFeaturesStorage
+import io.horizontalsystems.bankwallet.modules.settings.appearance.AppIconService
+import io.horizontalsystems.bankwallet.modules.settings.appearance.LaunchScreenService
+import io.horizontalsystems.bankwallet.modules.theme.ThemeService
 import io.horizontalsystems.bankwallet.modules.theme.ThemeType
 import io.horizontalsystems.bankwallet.modules.walletconnect.storage.WC2SessionStorage
 import io.horizontalsystems.bankwallet.modules.walletconnect.version2.WC2Manager
@@ -183,6 +187,7 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         lateinit var cexProviderManager: CexProviderManager
         lateinit var cexAssetManager: CexAssetManager
         lateinit var chartIndicatorManager: ChartIndicatorManager
+        lateinit var backupProvider: BackupProvider
     }
 
     override fun onCreate() {
@@ -261,8 +266,6 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         solanaKitManager = SolanaKitManager(appConfigProvider, solanaRpcSourceManager, solanaWalletManager, backgroundManager)
 
         tronKitManager = TronKitManager(appConfigProvider, backgroundManager)
-
-        blockchainSettingsStorage = BlockchainSettingsStorage(appDatabase)
 
         wordsManager = WordsManager(Mnemonic())
         networkManager = NetworkManager()
@@ -387,6 +390,32 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         cexProviderManager = CexProviderManager(accountManager)
         cexAssetManager = CexAssetManager(marketKit, appDatabase.cexAssetsDao())
         chartIndicatorManager = ChartIndicatorManager(appDatabase.chartIndicatorSettingsDao(), localStorage)
+
+        backupProvider = BackupProvider(
+            localStorage = localStorage,
+            languageManager = languageManager,
+            walletStorage = enabledWalletsStorage,
+            settingsManager = restoreSettingsManager,
+            accountManager = accountManager,
+            accountFactory = accountFactory,
+            walletManager = walletManager,
+            restoreSettingsManager = restoreSettingsManager,
+            blockchainSettingsStorage = blockchainSettingsStorage,
+            evmBlockchainManager = evmBlockchainManager,
+            marketFavoritesManager = marketFavoritesManager,
+            balanceViewTypeManager = balanceViewTypeManager,
+            appIconService = AppIconService(localStorage),
+            themeService = ThemeService(localStorage),
+            chartIndicatorManager = chartIndicatorManager,
+            balanceHiddenManager = balanceHiddenManager,
+            baseTokenManager = baseTokenManager,
+            launchScreenService = LaunchScreenService(localStorage),
+            currencyManager = currencyManager,
+            btcBlockchainManager = btcBlockchainManager,
+            evmSyncSourceManager = evmSyncSourceManager,
+            solanaRpcSourceManager = solanaRpcSourceManager,
+            contactsRepository = contactsRepository
+        )
 
         startTasks()
     }

app/src/main/java/io/horizontalsystems/bankwallet/modules/restorelocal/RestoreLocalViewModel.kt
@@ -8,28 +8,21 @@ import androidx.lifecycle.viewModelScope
 import com.google.gson.GsonBuilder
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.IAccountFactory
-import io.horizontalsystems.bankwallet.core.IAccountManager
-import io.horizontalsystems.bankwallet.core.IWalletManager
-import io.horizontalsystems.bankwallet.core.managers.EncryptDecryptManager
-import io.horizontalsystems.bankwallet.core.managers.RestoreSettings
-import io.horizontalsystems.bankwallet.core.managers.RestoreSettingsManager
 import io.horizontalsystems.bankwallet.core.providers.Translator
-import io.horizontalsystems.bankwallet.entities.AccountOrigin
 import io.horizontalsystems.bankwallet.entities.AccountType
 import io.horizontalsystems.bankwallet.entities.DataState
-import io.horizontalsystems.bankwallet.entities.EnabledWallet
 import io.horizontalsystems.bankwallet.modules.backuplocal.BackupLocalModule
-import io.horizontalsystems.marketkit.models.TokenQuery
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.BackupProvider
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.FullBackup
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.RestoreException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class RestoreLocalViewModel(
     private val backupJsonString: String?,
-    private val accountManager: IAccountManager,
     private val accountFactory: IAccountFactory,
-    private val walletManager: IWalletManager,
-    private val restoreSettingsManager: RestoreSettingsManager,
+    private val backupProvider: BackupProvider,
     fileName: String?,
 ) : ViewModel() {
 
@@ -37,7 +30,7 @@ class RestoreLocalViewModel(
     private var passphraseState: DataState.Error? = null
     private var showButtonSpinner = false
     private var walletBackup: BackupLocalModule.WalletBackup? = null
-    private val encryptDecryptManager = EncryptDecryptManager()
+    private var fullBackup: FullBackup? = null
     private var parseError: Exception? = null
     private var accountType: AccountType? = null
     private var manualBackup = false
@@ -72,6 +65,13 @@ class RestoreLocalViewModel(
                     .disableHtmlEscaping()
                     .enableComplexMapKeySerialization()
                     .create()
+
+                fullBackup = try {
+                    gson.fromJson(backupJsonString, FullBackup::class.java)
+                } catch (ex: Exception) {
+                    null
+                }
+
                 walletBackup = gson.fromJson(backupJsonString, BackupLocalModule.WalletBackup::class.java)
                 manualBackup = walletBackup?.manualBackup ?: false
             } catch (e: Exception) {
@@ -88,32 +88,36 @@ class RestoreLocalViewModel(
     }
 
     fun onImportClick() {
-        val backup = walletBackup ?: return
+        when {
+            fullBackup != null -> {
+                fullBackup?.let { restoreFullBackup(it) }
+            }
+
+            walletBackup != null -> {
+                walletBackup?.let { restoreSingleWallet(it, accountName) }
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+    private fun restoreFullBackup(fullBackup: FullBackup) {
         showButtonSpinner = true
         syncState()
+
         viewModelScope.launch(Dispatchers.IO) {
-            val kdfParams = backup.crypto.kdfparams
-            val key = EncryptDecryptManager.getKey(passphrase, kdfParams) ?: return@launch
-            val enabledWallets = backup.enabledWallets
-            if (EncryptDecryptManager.passwordIsCorrect(backup.crypto.mac, backup.crypto.ciphertext, key)) {
-                val decrypted = encryptDecryptManager.decrypt(backup.crypto.ciphertext, key, backup.crypto.cipherparams.iv)
-                try {
-                    val type = BackupLocalModule.getAccountTypeFromData(backup.type, decrypted)
-                    if (type is AccountType.Cex) {
-                        restoreCexAccount(type)
-                        return@launch
-                    } else if (!enabledWallets.isNullOrEmpty()) {
-                        restoreWithWallets(type, enabledWallets)
-                        return@launch
-                    } else {
-                        accountType = type
-                    }
-                } catch (e: IllegalStateException) {
-                    parseError = e
-                }
-            } else {
+            try {
+                backupProvider.restore(fullBackup, passphrase)
+
+                restored = true
+            } catch (keyException: RestoreException.EncryptionKeyException) {
+                parseError = keyException
+            } catch (invalidPassword: RestoreException.InvalidPasswordException) {
                 passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
             }
+
             showButtonSpinner = false
             withContext(Dispatchers.Main) {
                 syncState()
@@ -121,45 +125,32 @@ class RestoreLocalViewModel(
         }
     }
 
-    private fun restoreWithWallets(
-        type: AccountType,
-        enabledWalletBackups: List<BackupLocalModule.EnabledWalletBackup>
-    ) {
-        val account = accountFactory.account(accountName, type, AccountOrigin.Restored, false, true)
-        accountManager.save(account)
+    @Throws
+    private fun restoreSingleWallet(backup: BackupLocalModule.WalletBackup, accountName: String) {
+        showButtonSpinner = true
+        syncState()
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val type = backupProvider.accountType(backup, passphrase)
+                if (type is AccountType.Cex) {
+                    backupProvider.restoreCexAccount(type, accountName)
 
-        val enabledWallets = enabledWalletBackups.map {
-            EnabledWallet(
-                tokenQueryId = it.tokenQueryId,
-                accountId = account.id,
-                coinName = it.coinName,
-                coinCode = it.coinCode,
-                coinDecimals = it.decimals
-            )
-        }
-        walletManager.saveEnabledWallets(enabledWallets)
-
-        enabledWalletBackups.forEach { backup ->
-            TokenQuery.fromId(backup.tokenQueryId)?.let { tokenQuery ->
-                if (!backup.settings.isNullOrEmpty()) {
-                    val restoreSettings = RestoreSettings()
-                    backup.settings.forEach { (restoreSettingType, value) ->
-                        restoreSettings[restoreSettingType] = value
-                    }
-                    restoreSettingsManager.save(restoreSettings, account, tokenQuery.blockchainType)
+                    restored = true
+                } else {
+                    accountType = type
                 }
+            } catch (keyException: RestoreException.EncryptionKeyException) {
+                parseError = keyException
+            } catch (invalidPassword: RestoreException.InvalidPasswordException) {
+                passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
+            } catch (e: IllegalStateException) {
+                parseError = e
+            }
+            showButtonSpinner = false
+            withContext(Dispatchers.Main) {
+                syncState()
             }
         }
-
-        restored = true
-        syncState()
-    }
-
-    private fun restoreCexAccount(accountType: AccountType) {
-        val account = accountFactory.account(accountName, accountType, AccountOrigin.Restored, true, true)
-        accountManager.save(account)
-        restored = true
-        syncState()
     }
 
     fun onSelectCoinsShown() {

app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/appearance/AppIconService.kt
@@ -1,5 +1,9 @@
 package io.horizontalsystems.bankwallet.modules.settings.appearance
 
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.util.Log
+import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.ILocalStorage
 import io.horizontalsystems.bankwallet.ui.compose.Select
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,10 +19,24 @@ class AppIconService(private val localStorage: ILocalStorage) {
     val optionsFlow = _optionsFlow.asStateFlow()
 
     fun setAppIcon(appIcon: AppIcon) {
+
+        Log.e("ee", "setAppIcon: $appIcon")
+
         localStorage.appIcon = appIcon
 
         _optionsFlow.update {
             Select(appIcon, appIcons)
+        }
+
+        val enabled = PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+        val disabled = PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+
+        AppIcon.values().forEach { item ->
+            App.instance.packageManager.setComponentEnabledSetting(
+                ComponentName(App.instance, item.launcherName),
+                if (appIcon == item) enabled else disabled,
+                PackageManager.DONT_KILL_APP
+            )
         }
     }
 }

horizontalsystems/unstoppable-wallet-android

app/src/main/java/cash/p/terminal/core/App.kt
@@ -80,6 +80,7 @@ import cash.p.terminal.core.storage.EnabledWalletsStorage
 import cash.p.terminal.core.storage.EvmSyncSourceStorage
 import cash.p.terminal.core.storage.NftStorage
 import cash.p.terminal.core.storage.RestoreSettingsStorage
+import cash.p.terminal.modules.backuplocal.fullbackup.BackupProvider
 import cash.p.terminal.modules.balance.BalanceViewTypeManager
 import cash.p.terminal.modules.chart.ChartIndicatorManager
 import cash.p.terminal.modules.contacts.ContactsRepository
@@ -93,6 +94,9 @@ import cash.p.terminal.modules.market.topplatforms.TopPlatformsRepository
 import cash.p.terminal.modules.pin.PinComponent
 import cash.p.terminal.modules.profeatures.ProFeaturesAuthorizationManager
 import cash.p.terminal.modules.profeatures.storage.ProFeaturesStorage
+import cash.p.terminal.modules.settings.appearance.AppIconService
+import cash.p.terminal.modules.settings.appearance.LaunchScreenService
+import cash.p.terminal.modules.theme.ThemeService
 import cash.p.terminal.modules.theme.ThemeType
 import cash.p.terminal.modules.walletconnect.storage.WC2SessionStorage
 import cash.p.terminal.modules.walletconnect.version2.WC2Manager
@@ -183,6 +187,7 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         lateinit var cexProviderManager: CexProviderManager
         lateinit var cexAssetManager: CexAssetManager
         lateinit var chartIndicatorManager: ChartIndicatorManager
+        lateinit var backupProvider: BackupProvider
     }
 
     override fun onCreate() {
@@ -261,8 +266,6 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         solanaKitManager = SolanaKitManager(appConfigProvider, solanaRpcSourceManager, solanaWalletManager, backgroundManager)
 
         tronKitManager = TronKitManager(appConfigProvider, backgroundManager)
-
-        blockchainSettingsStorage = BlockchainSettingsStorage(appDatabase)
 
         wordsManager = WordsManager(Mnemonic())
         networkManager = NetworkManager()
@@ -387,6 +390,32 @@ class App : CoreApp(), WorkConfiguration.Provider, ImageLoaderFactory {
         cexProviderManager = CexProviderManager(accountManager)
         cexAssetManager = CexAssetManager(marketKit, appDatabase.cexAssetsDao())
         chartIndicatorManager = ChartIndicatorManager(appDatabase.chartIndicatorSettingsDao(), localStorage)
+
+        backupProvider = BackupProvider(
+            localStorage = localStorage,
+            languageManager = languageManager,
+            walletStorage = enabledWalletsStorage,
+            settingsManager = restoreSettingsManager,
+            accountManager = accountManager,
+            accountFactory = accountFactory,
+            walletManager = walletManager,
+            restoreSettingsManager = restoreSettingsManager,
+            blockchainSettingsStorage = blockchainSettingsStorage,
+            evmBlockchainManager = evmBlockchainManager,
+            marketFavoritesManager = marketFavoritesManager,
+            balanceViewTypeManager = balanceViewTypeManager,
+            appIconService = AppIconService(localStorage),
+            themeService = ThemeService(localStorage),
+            chartIndicatorManager = chartIndicatorManager,
+            balanceHiddenManager = balanceHiddenManager,
+            baseTokenManager = baseTokenManager,
+            launchScreenService = LaunchScreenService(localStorage),
+            currencyManager = currencyManager,
+            btcBlockchainManager = btcBlockchainManager,
+            evmSyncSourceManager = evmSyncSourceManager,
+            solanaRpcSourceManager = solanaRpcSourceManager,
+            contactsRepository = contactsRepository
+        )
 
         startTasks()
     }

app/src/main/java/cash/p/terminal/modules/restorelocal/RestoreLocalViewModel.kt
@@ -8,28 +8,21 @@ import androidx.lifecycle.viewModelScope
 import com.google.gson.GsonBuilder
 import cash.p.terminal.R
 import cash.p.terminal.core.IAccountFactory
-import cash.p.terminal.core.IAccountManager
-import cash.p.terminal.core.IWalletManager
-import cash.p.terminal.core.managers.EncryptDecryptManager
-import cash.p.terminal.core.managers.RestoreSettings
-import cash.p.terminal.core.managers.RestoreSettingsManager
 import cash.p.terminal.core.providers.Translator
-import cash.p.terminal.entities.AccountOrigin
 import cash.p.terminal.entities.AccountType
 import cash.p.terminal.entities.DataState
-import cash.p.terminal.entities.EnabledWallet
 import cash.p.terminal.modules.backuplocal.BackupLocalModule
-import io.horizontalsystems.marketkit.models.TokenQuery
+import cash.p.terminal.modules.backuplocal.fullbackup.BackupProvider
+import cash.p.terminal.modules.backuplocal.fullbackup.FullBackup
+import cash.p.terminal.modules.backuplocal.fullbackup.RestoreException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class RestoreLocalViewModel(
     private val backupJsonString: String?,
-    private val accountManager: IAccountManager,
     private val accountFactory: IAccountFactory,
-    private val walletManager: IWalletManager,
-    private val restoreSettingsManager: RestoreSettingsManager,
+    private val backupProvider: BackupProvider,
     fileName: String?,
 ) : ViewModel() {
 
@@ -37,7 +30,7 @@ class RestoreLocalViewModel(
     private var passphraseState: DataState.Error? = null
     private var showButtonSpinner = false
     private var walletBackup: BackupLocalModule.WalletBackup? = null
-    private val encryptDecryptManager = EncryptDecryptManager()
+    private var fullBackup: FullBackup? = null
     private var parseError: Exception? = null
     private var accountType: AccountType? = null
     private var manualBackup = false
@@ -72,6 +65,13 @@ class RestoreLocalViewModel(
                     .disableHtmlEscaping()
                     .enableComplexMapKeySerialization()
                     .create()
+
+                fullBackup = try {
+                    gson.fromJson(backupJsonString, FullBackup::class.java)
+                } catch (ex: Exception) {
+                    null
+                }
+
                 walletBackup = gson.fromJson(backupJsonString, BackupLocalModule.WalletBackup::class.java)
                 manualBackup = walletBackup?.manualBackup ?: false
             } catch (e: Exception) {
@@ -88,32 +88,36 @@ class RestoreLocalViewModel(
     }
 
     fun onImportClick() {
-        val backup = walletBackup ?: return
+        when {
+            fullBackup != null -> {
+                fullBackup?.let { restoreFullBackup(it) }
+            }
+
+            walletBackup != null -> {
+                walletBackup?.let { restoreSingleWallet(it, accountName) }
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+    private fun restoreFullBackup(fullBackup: FullBackup) {
         showButtonSpinner = true
         syncState()
+
         viewModelScope.launch(Dispatchers.IO) {
-            val kdfParams = backup.crypto.kdfparams
-            val key = EncryptDecryptManager.getKey(passphrase, kdfParams) ?: return@launch
-            val enabledWallets = backup.enabledWallets
-            if (EncryptDecryptManager.passwordIsCorrect(backup.crypto.mac, backup.crypto.ciphertext, key)) {
-                val decrypted = encryptDecryptManager.decrypt(backup.crypto.ciphertext, key, backup.crypto.cipherparams.iv)
-                try {
-                    val type = BackupLocalModule.getAccountTypeFromData(backup.type, decrypted)
-                    if (type is AccountType.Cex) {
-                        restoreCexAccount(type)
-                        return@launch
-                    } else if (!enabledWallets.isNullOrEmpty()) {
-                        restoreWithWallets(type, enabledWallets)
-                        return@launch
-                    } else {
-                        accountType = type
-                    }
-                } catch (e: IllegalStateException) {
-                    parseError = e
-                }
-            } else {
+            try {
+                backupProvider.restore(fullBackup, passphrase)
+
+                restored = true
+            } catch (keyException: RestoreException.EncryptionKeyException) {
+                parseError = keyException
+            } catch (invalidPassword: RestoreException.InvalidPasswordException) {
                 passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
             }
+
             showButtonSpinner = false
             withContext(Dispatchers.Main) {
                 syncState()
@@ -121,45 +125,32 @@ class RestoreLocalViewModel(
         }
     }
 
-    private fun restoreWithWallets(
-        type: AccountType,
-        enabledWalletBackups: List<BackupLocalModule.EnabledWalletBackup>
-    ) {
-        val account = accountFactory.account(accountName, type, AccountOrigin.Restored, false, true)
-        accountManager.save(account)
+    @Throws
+    private fun restoreSingleWallet(backup: BackupLocalModule.WalletBackup, accountName: String) {
+        showButtonSpinner = true
+        syncState()
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val type = backupProvider.accountType(backup, passphrase)
+                if (type is AccountType.Cex) {
+                    backupProvider.restoreCexAccount(type, accountName)
 
-        val enabledWallets = enabledWalletBackups.map {
-            EnabledWallet(
-                tokenQueryId = it.tokenQueryId,
-                accountId = account.id,
-                coinName = it.coinName,
-                coinCode = it.coinCode,
-                coinDecimals = it.decimals
-            )
-        }
-        walletManager.saveEnabledWallets(enabledWallets)
-
-        enabledWalletBackups.forEach { backup ->
-            TokenQuery.fromId(backup.tokenQueryId)?.let { tokenQuery ->
-                if (!backup.settings.isNullOrEmpty()) {
-                    val restoreSettings = RestoreSettings()
-                    backup.settings.forEach { (restoreSettingType, value) ->
-                        restoreSettings[restoreSettingType] = value
-                    }
-                    restoreSettingsManager.save(restoreSettings, account, tokenQuery.blockchainType)
+                    restored = true
+                } else {
+                    accountType = type
                 }
+            } catch (keyException: RestoreException.EncryptionKeyException) {
+                parseError = keyException
+            } catch (invalidPassword: RestoreException.InvalidPasswordException) {
+                passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
+            } catch (e: IllegalStateException) {
+                parseError = e
+            }
+            showButtonSpinner = false
+            withContext(Dispatchers.Main) {
+                syncState()
             }
         }
-
-        restored = true
-        syncState()
-    }
-
-    private fun restoreCexAccount(accountType: AccountType) {
-        val account = accountFactory.account(accountName, accountType, AccountOrigin.Restored, true, true)
-        accountManager.save(account)
-        restored = true
-        syncState()
     }
 
     fun onSelectCoinsShown() {

app/src/main/java/cash/p/terminal/modules/settings/appearance/AppIconService.kt
@@ -1,5 +1,9 @@
 package cash.p.terminal.modules.settings.appearance
 
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.util.Log
+import cash.p.terminal.core.App
 import cash.p.terminal.core.ILocalStorage
 import cash.p.terminal.ui.compose.Select
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,10 +19,24 @@ class AppIconService(private val localStorage: ILocalStorage) {
     val optionsFlow = _optionsFlow.asStateFlow()
 
     fun setAppIcon(appIcon: AppIcon) {
+
+        Log.e("ee", "setAppIcon: $appIcon")
+
         localStorage.appIcon = appIcon
 
         _optionsFlow.update {
             Select(appIcon, appIcons)
+        }
+
+        val enabled = PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+        val disabled = PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+
+        AppIcon.values().forEach { item ->
+            App.instance.packageManager.setComponentEnabledSetting(
+                ComponentName(App.instance, item.launcherName),
+                if (appIcon == item) enabled else disabled,
+                PackageManager.DONT_KILL_APP
+            )
         }
     }
 }