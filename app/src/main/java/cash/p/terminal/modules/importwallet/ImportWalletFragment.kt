eyovvalupe/Ethereum-wallet-payment

Show contents of backup file when importing

app/src/main/java/io/horizontalsystems/bankwallet/modules/importwallet/ImportWalletFragment.kt
@@ -26,6 +26,7 @@ import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 import io.horizontalsystems.bankwallet.core.navigateWithTermsAccepted
 import io.horizontalsystems.bankwallet.core.slideFromBottom
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.BackupFileValidator
 import io.horizontalsystems.bankwallet.modules.backuplocal.password.BackupLocalPasswordViewModel.*
 import io.horizontalsystems.bankwallet.modules.contacts.screen.ConfirmationBottomSheet
 import io.horizontalsystems.bankwallet.modules.manageaccounts.ManageAccountsModule
@@ -73,11 +74,8 @@ private fun ImportWalletScreen(
                     inputStream.bufferedReader().use { br ->
                         val jsonString = br.readText()
                         //validate json format
-//                        val json = Gson().fromJson(jsonString, FullBackup::class.java)
-                        //Gson will set field as null if the json file doesn't have the matching field
-//                        if (json.version == null || json.crypto == null){
-//                            throw Exception("Invalid json format")
-//                        }
+                        BackupFileValidator().validate(jsonString)
+
                         navController.navigateWithTermsAccepted {
                             val fileName = context.getFileName(uriNonNull)
                             navController.slideFromBottom(
@@ -220,7 +218,7 @@ private fun ImportOption(
     }
 }
 
-fun Context.getFileName(uri: Uri): String? = when(uri.scheme) {
+fun Context.getFileName(uri: Uri): String? = when (uri.scheme) {
     ContentResolver.SCHEME_CONTENT -> getContentFileName(uri)
     else -> uri.path?.let(::File)?.name
 }

app/src/main/java/io/horizontalsystems/bankwallet/modules/restorelocal/RestoreLocalModule.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.ViewModelProvider
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.entities.AccountType
 import io.horizontalsystems.bankwallet.entities.DataState
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.BackupItems
 
 object RestoreLocalModule {
 
     class Factory(private val backupJsonString: String?, private val fileName: String?) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return RestoreLocalViewModel(backupJsonString,App.accountFactory, App.backupProvider, fileName) as T
+            return RestoreLocalViewModel(backupJsonString, App.accountFactory, App.backupProvider, fileName) as T
         }
     }
 
@@ -21,6 +22,8 @@ object RestoreLocalModule {
         val parseError: Exception?,
         val showSelectCoins: AccountType?,
         val manualBackup: Boolean,
-        val restored: Boolean
+        val restored: Boolean,
+        val backupItems: BackupItems?,
+        val showBackupItems: Boolean
     )
 }

app/src/main/java/io/horizontalsystems/bankwallet/modules/restorelocal/RestoreLocalViewModel.kt
@@ -11,11 +11,15 @@ import io.horizontalsystems.bankwallet.core.IAccountFactory
 import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.bankwallet.entities.AccountType
 import io.horizontalsystems.bankwallet.entities.DataState
-import io.horizontalsystems.bankwallet.modules.backuplocal.BackupLocalModule
+import io.horizontalsystems.bankwallet.modules.backuplocal.BackupLocalModule.WalletBackup
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.BackupItems
 import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.BackupProvider
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.DecryptedFullBackup
 import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.FullBackup
 import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.RestoreException
+import io.horizontalsystems.bankwallet.modules.restorelocal.RestoreLocalModule.UiState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -29,12 +33,16 @@ class RestoreLocalViewModel(
     private var passphrase = ""
     private var passphraseState: DataState.Error? = null
     private var showButtonSpinner = false
-    private var walletBackup: BackupLocalModule.WalletBackup? = null
+    private var walletBackup: WalletBackup? = null
     private var fullBackup: FullBackup? = null
     private var parseError: Exception? = null
     private var showSelectCoins: AccountType? = null
     private var manualBackup = false
     private var restored = false
+
+    private var decryptedFullBackup: DecryptedFullBackup? = null
+    private var backupItems: BackupItems? = null
+    private var showBackupItems = false
 
     val accountName by lazy {
         fileName?.let { name ->
@@ -47,13 +55,15 @@ class RestoreLocalViewModel(
     }
 
     var uiState by mutableStateOf(
-        RestoreLocalModule.UiState(
+        UiState(
             passphraseState = null,
             showButtonSpinner = showButtonSpinner,
             parseError = parseError,
             showSelectCoins = showSelectCoins,
             manualBackup = manualBackup,
-            restored = restored
+            restored = restored,
+            backupItems = backupItems,
+            showBackupItems = showBackupItems
         )
     )
         private set
@@ -74,7 +84,7 @@ class RestoreLocalViewModel(
                     null
                 }
 
-                walletBackup = gson.fromJson(backupJsonString, BackupLocalModule.WalletBackup::class.java)
+                walletBackup = gson.fromJson(backupJsonString, WalletBackup::class.java)
                 manualBackup = walletBackup?.manualBackup ?: false
             } catch (e: Exception) {
                 parseError = e
@@ -92,7 +102,7 @@ class RestoreLocalViewModel(
     fun onImportClick() {
         when {
             fullBackup != null -> {
-                fullBackup?.let { restoreFullBackup(it) }
+                fullBackup?.let { showFullBackupItems(it) }
             }
 
             walletBackup != null -> {
@@ -105,18 +115,50 @@ class RestoreLocalViewModel(
         }
     }
 
-    private fun restoreFullBackup(fullBackup: FullBackup) {
+    private fun showFullBackupItems(it: FullBackup): Job {
+        showButtonSpinner = true
+        syncState()
+
+        return viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val decrypted = backupProvider.decryptedFullBackup(it, passphrase)
+                backupItems = backupProvider.fullBackupItems(decrypted)
+                decryptedFullBackup = decrypted
+
+                showBackupItems = true
+            } catch (keyException: RestoreException.EncryptionKeyException) {
+                parseError = keyException
+            } catch (invalidPassword: RestoreException.InvalidPasswordException) {
+                passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
+            } catch (e: Exception) {
+                parseError = e
+            }
+
+            withContext(Dispatchers.Main) {
+                showButtonSpinner = false
+                syncState()
+            }
+        }
+    }
+
+    fun restoreFullBackup() {
+        decryptedFullBackup?.let { restoreFullBackup(it) }
+    }
+
+    private fun restoreFullBackup(decryptedFullBackup: DecryptedFullBackup) {
         showButtonSpinner = true
         syncState()
 
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                backupProvider.restoreFullBackup(fullBackup, passphrase)
+                backupProvider.restoreFullBackup(decryptedFullBackup, passphrase)
                 restored = true
             } catch (keyException: RestoreException.EncryptionKeyException) {
                 parseError = keyException
             } catch (invalidPassword: RestoreException.InvalidPasswordException) {
                 passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
+            } catch (e: Exception) {
+                parseError = e
             }
 
             showButtonSpinner = false
@@ -127,7 +169,7 @@ class RestoreLocalViewModel(
     }
 
     @Throws
-    private fun restoreSingleWallet(backup: BackupLocalModule.WalletBackup, accountName: String) {
+    private fun restoreSingleWallet(backup: WalletBackup, accountName: String) {
         showButtonSpinner = true
         syncState()
         viewModelScope.launch(Dispatchers.IO) {
@@ -146,7 +188,7 @@ class RestoreLocalViewModel(
                 parseError = keyException
             } catch (invalidPassword: RestoreException.InvalidPasswordException) {
                 passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
-            } catch (e: IllegalStateException) {
+            } catch (e: Exception) {
                 parseError = e
             }
             showButtonSpinner = false
@@ -161,14 +203,21 @@ class RestoreLocalViewModel(
         syncState()
     }
 
+    fun onBackupItemsShown() {
+        showBackupItems = false
+        syncState()
+    }
+
     private fun syncState() {
-        uiState = RestoreLocalModule.UiState(
+        uiState = UiState(
             passphraseState = passphraseState,
             showButtonSpinner = showButtonSpinner,
             parseError = parseError,
             showSelectCoins = showSelectCoins,
             manualBackup = manualBackup,
-            restored = restored
+            restored = restored,
+            backupItems = backupItems,
+            showBackupItems = showBackupItems
         )
     }
 

horizontalsystems/unstoppable-wallet-android

app/src/main/java/cash/p/terminal/modules/importwallet/ImportWalletFragment.kt
@@ -26,6 +26,7 @@ import cash.p.terminal.R
 import cash.p.terminal.core.BaseComposeFragment
 import cash.p.terminal.core.navigateWithTermsAccepted
 import cash.p.terminal.core.slideFromBottom
+import cash.p.terminal.modules.backuplocal.fullbackup.BackupFileValidator
 import cash.p.terminal.modules.backuplocal.password.BackupLocalPasswordViewModel.*
 import cash.p.terminal.modules.contacts.screen.ConfirmationBottomSheet
 import cash.p.terminal.modules.manageaccounts.ManageAccountsModule
@@ -73,11 +74,8 @@ private fun ImportWalletScreen(
                     inputStream.bufferedReader().use { br ->
                         val jsonString = br.readText()
                         //validate json format
-//                        val json = Gson().fromJson(jsonString, FullBackup::class.java)
-                        //Gson will set field as null if the json file doesn't have the matching field
-//                        if (json.version == null || json.crypto == null){
-//                            throw Exception("Invalid json format")
-//                        }
+                        BackupFileValidator().validate(jsonString)
+
                         navController.navigateWithTermsAccepted {
                             val fileName = context.getFileName(uriNonNull)
                             navController.slideFromBottom(
@@ -220,7 +218,7 @@ private fun ImportOption(
     }
 }
 
-fun Context.getFileName(uri: Uri): String? = when(uri.scheme) {
+fun Context.getFileName(uri: Uri): String? = when (uri.scheme) {
     ContentResolver.SCHEME_CONTENT -> getContentFileName(uri)
     else -> uri.path?.let(::File)?.name
 }

app/src/main/java/cash/p/terminal/modules/restorelocal/RestoreLocalModule.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.ViewModelProvider
 import cash.p.terminal.core.App
 import cash.p.terminal.entities.AccountType
 import cash.p.terminal.entities.DataState
+import cash.p.terminal.modules.backuplocal.fullbackup.BackupItems
 
 object RestoreLocalModule {
 
     class Factory(private val backupJsonString: String?, private val fileName: String?) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return RestoreLocalViewModel(backupJsonString,App.accountFactory, App.backupProvider, fileName) as T
+            return RestoreLocalViewModel(backupJsonString, App.accountFactory, App.backupProvider, fileName) as T
         }
     }
 
@@ -21,6 +22,8 @@ object RestoreLocalModule {
         val parseError: Exception?,
         val showSelectCoins: AccountType?,
         val manualBackup: Boolean,
-        val restored: Boolean
+        val restored: Boolean,
+        val backupItems: BackupItems?,
+        val showBackupItems: Boolean
     )
 }

app/src/main/java/io/horizontalsystems/bankwallet/modules/restorelocal/RestoreLocalViewModel.kt
@@ -11,11 +11,15 @@ import cash.p.terminal.core.IAccountFactory
 import cash.p.terminal.core.providers.Translator
 import cash.p.terminal.entities.AccountType
 import cash.p.terminal.entities.DataState
-import cash.p.terminal.modules.backuplocal.BackupLocalModule
+import cash.p.terminal.modules.backuplocal.BackupLocalModule.WalletBackup
+import cash.p.terminal.modules.backuplocal.fullbackup.BackupItems
 import cash.p.terminal.modules.backuplocal.fullbackup.BackupProvider
+import cash.p.terminal.modules.backuplocal.fullbackup.DecryptedFullBackup
 import cash.p.terminal.modules.backuplocal.fullbackup.FullBackup
 import cash.p.terminal.modules.backuplocal.fullbackup.RestoreException
+import cash.p.terminal.modules.restorelocal.RestoreLocalModule.UiState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -29,12 +33,16 @@ class RestoreLocalViewModel(
     private var passphrase = ""
     private var passphraseState: DataState.Error? = null
     private var showButtonSpinner = false
-    private var walletBackup: BackupLocalModule.WalletBackup? = null
+    private var walletBackup: WalletBackup? = null
     private var fullBackup: FullBackup? = null
     private var parseError: Exception? = null
     private var showSelectCoins: AccountType? = null
     private var manualBackup = false
     private var restored = false
+
+    private var decryptedFullBackup: DecryptedFullBackup? = null
+    private var backupItems: BackupItems? = null
+    private var showBackupItems = false
 
     val accountName by lazy {
         fileName?.let { name ->
@@ -47,13 +55,15 @@ class RestoreLocalViewModel(
     }
 
     var uiState by mutableStateOf(
-        RestoreLocalModule.UiState(
+        UiState(
             passphraseState = null,
             showButtonSpinner = showButtonSpinner,
             parseError = parseError,
             showSelectCoins = showSelectCoins,
             manualBackup = manualBackup,
-            restored = restored
+            restored = restored,
+            backupItems = backupItems,
+            showBackupItems = showBackupItems
         )
     )
         private set
@@ -74,7 +84,7 @@ class RestoreLocalViewModel(
                     null
                 }
 
-                walletBackup = gson.fromJson(backupJsonString, BackupLocalModule.WalletBackup::class.java)
+                walletBackup = gson.fromJson(backupJsonString, WalletBackup::class.java)
                 manualBackup = walletBackup?.manualBackup ?: false
             } catch (e: Exception) {
                 parseError = e
@@ -92,7 +102,7 @@ class RestoreLocalViewModel(
     fun onImportClick() {
         when {
             fullBackup != null -> {
-                fullBackup?.let { restoreFullBackup(it) }
+                fullBackup?.let { showFullBackupItems(it) }
             }
 
             walletBackup != null -> {
@@ -105,18 +115,50 @@ class RestoreLocalViewModel(
         }
     }
 
-    private fun restoreFullBackup(fullBackup: FullBackup) {
+    private fun showFullBackupItems(it: FullBackup): Job {
+        showButtonSpinner = true
+        syncState()
+
+        return viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val decrypted = backupProvider.decryptedFullBackup(it, passphrase)
+                backupItems = backupProvider.fullBackupItems(decrypted)
+                decryptedFullBackup = decrypted
+
+                showBackupItems = true
+            } catch (keyException: RestoreException.EncryptionKeyException) {
+                parseError = keyException
+            } catch (invalidPassword: RestoreException.InvalidPasswordException) {
+                passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
+            } catch (e: Exception) {
+                parseError = e
+            }
+
+            withContext(Dispatchers.Main) {
+                showButtonSpinner = false
+                syncState()
+            }
+        }
+    }
+
+    fun restoreFullBackup() {
+        decryptedFullBackup?.let { restoreFullBackup(it) }
+    }
+
+    private fun restoreFullBackup(decryptedFullBackup: DecryptedFullBackup) {
         showButtonSpinner = true
         syncState()
 
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                backupProvider.restoreFullBackup(fullBackup, passphrase)
+                backupProvider.restoreFullBackup(decryptedFullBackup, passphrase)
                 restored = true
             } catch (keyException: RestoreException.EncryptionKeyException) {
                 parseError = keyException
             } catch (invalidPassword: RestoreException.InvalidPasswordException) {
                 passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
+            } catch (e: Exception) {
+                parseError = e
             }
 
             showButtonSpinner = false
@@ -127,7 +169,7 @@ class RestoreLocalViewModel(
     }
 
     @Throws
-    private fun restoreSingleWallet(backup: BackupLocalModule.WalletBackup, accountName: String) {
+    private fun restoreSingleWallet(backup: WalletBackup, accountName: String) {
         showButtonSpinner = true
         syncState()
         viewModelScope.launch(Dispatchers.IO) {
@@ -146,7 +188,7 @@ class RestoreLocalViewModel(
                 parseError = keyException
             } catch (invalidPassword: RestoreException.InvalidPasswordException) {
                 passphraseState = DataState.Error(Exception(Translator.getString(R.string.ImportBackupFile_Error_InvalidPassword)))
-            } catch (e: IllegalStateException) {
+            } catch (e: Exception) {
                 parseError = e
             }
             showButtonSpinner = false
@@ -161,14 +203,21 @@ class RestoreLocalViewModel(
         syncState()
     }
 
+    fun onBackupItemsShown() {
+        showBackupItems = false
+        syncState()
+    }
+
     private fun syncState() {
-        uiState = RestoreLocalModule.UiState(
+        uiState = UiState(
             passphraseState = passphraseState,
             showButtonSpinner = showButtonSpinner,
             parseError = parseError,
             showSelectCoins = showSelectCoins,
             manualBackup = manualBackup,
-            restored = restored
+            restored = restored,
+            backupItems = backupItems,
+            showBackupItems = showBackupItems
         )
     }
 