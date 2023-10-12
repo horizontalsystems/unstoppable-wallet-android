eyovvalupe/Ethereum-wallet-payment

Initial implementation of App Backup module

app/src/main/java/io/horizontalsystems/bankwallet/modules/backuplocal/BackupLocalFragment.kt
@@ -1,6 +1,7 @@
 package io.horizontalsystems.bankwallet.modules.backuplocal
 
 import android.os.Bundle
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.core.os.bundleOf
 import androidx.navigation.NavController
@@ -9,6 +10,8 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 import io.horizontalsystems.bankwallet.core.composablePage
+import io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup.SelectBackupItemsScreen
+import io.horizontalsystems.bankwallet.modules.backuplocal.password.BackupType
 import io.horizontalsystems.bankwallet.modules.backuplocal.password.LocalBackupPasswordScreen
 import io.horizontalsystems.bankwallet.modules.backuplocal.terms.LocalBackupTermsScreen
 import io.horizontalsystems.core.findNavController
@@ -17,7 +20,12 @@ class BackupLocalFragment : BaseComposeFragment() {
 
     @Composable
     override fun GetContent() {
-        BackupLocalNavHost(findNavController(), requireArguments().getString(ACCOUNT_ID_KEY))
+        val accountId = arguments?.getString(ACCOUNT_ID_KEY)
+        if (accountId != null) {
+            SingleWalletBackupNavHost(findNavController(), accountId)
+        } else {
+            FullBackupNavHost(fragmentNavController = findNavController())
+        }
     }
 
     companion object {
@@ -29,20 +37,79 @@ class BackupLocalFragment : BaseComposeFragment() {
 }
 
 @Composable
-private fun BackupLocalNavHost(fragmentNavController: NavController, accountId: String?) {
+private fun FullBackupNavHost(fragmentNavController: NavController) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "select_backup_items",
+    ) {
+        composable("select_backup_items") {
+            SelectBackupItemsScreen(
+                onNextClick = { accountIdsList ->
+                    val accountIds = accountIdsList.joinToString(separator = ",")
+                    navController.navigate("terms_page/${accountIds}")
+                },
+                onBackClick = {
+                    fragmentNavController.popBackStack()
+                }
+            )
+        }
+
+        composablePage("terms_page/{accountIds}") { backStackEntry ->
+            val accountIds = backStackEntry.arguments?.getString("accountIds")
+            LocalBackupTermsScreen(
+                onTermsAccepted = {
+                    navController.navigate("password_page/${accountIds}")
+                },
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composablePage("password_page/{accountIds}") { backStackEntry ->
+            val accountIds = backStackEntry.arguments?.getString("accountIds")?.split(",") ?: listOf()
+            Log.e("eee", "password_page selectedWallets ${accountIds.size}: ${backStackEntry.arguments?.getString("accountIds")}")
+
+            LocalBackupPasswordScreen(
+                backupType = BackupType.FullBackup(accountIds),
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFinish = {
+                    fragmentNavController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun SingleWalletBackupNavHost(fragmentNavController: NavController, accountId: String) {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = "terms_page",
     ) {
         composable("terms_page") {
-            LocalBackupTermsScreen(fragmentNavController, navController)
+            LocalBackupTermsScreen(
+                onTermsAccepted = {
+                    navController.navigate("password_page")
+                },
+                onBackClick = {
+                    fragmentNavController.popBackStack()
+                }
+            )
         }
         composablePage("password_page") {
             LocalBackupPasswordScreen(
-                fragmentNavController,
-                navController,
-                accountId
+                backupType = BackupType.SingleWalletBackup(accountId),
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFinish = {
+                    fragmentNavController.popBackStack()
+                }
             )
         }
     }

app/src/main/java/io/horizontalsystems/bankwallet/modules/backuplocal/terms/BackupLocalTerms.kt
@@ -14,16 +14,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
-import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.bankwallet.ui.compose.components.CellUniversalLawrenceSection
+import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
 import io.horizontalsystems.bankwallet.ui.compose.components.HsCheckbox
-import io.horizontalsystems.bankwallet.ui.compose.components.MenuItem
 import io.horizontalsystems.bankwallet.ui.compose.components.RowUniversal
 import io.horizontalsystems.bankwallet.ui.compose.components.TextImportantWarning
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
@@ -31,8 +29,8 @@ import io.horizontalsystems.bankwallet.ui.compose.components.subhead2_leah
 
 @Composable
 fun LocalBackupTermsScreen(
-    fragmentNavController: NavController,
-    navController: NavController,
+    onTermsAccepted: ()-> Unit,
+    onBackClick: () -> Unit,
 ) {
     var termChecked by rememberSaveable { mutableStateOf(false) }
 
@@ -42,15 +40,9 @@ fun LocalBackupTermsScreen(
             topBar = {
                 AppBar(
                     title = stringResource(R.string.LocalBackup_Title),
-                    menuItems = listOf(
-                        MenuItem(
-                            title = TranslatableString.ResString(R.string.Button_Close),
-                            icon = R.drawable.ic_close,
-                            onClick = {
-                                fragmentNavController.popBackStack()
-                            }
-                        )
-                    )
+                    navigationIcon = {
+                        HsBackButton(onClick = onBackClick)
+                    },
                 )
             }
         ) {
@@ -80,9 +72,7 @@ fun LocalBackupTermsScreen(
                             .padding(start = 16.dp, end = 16.dp),
                         title = stringResource(R.string.Button_Continue),
                         enabled = termChecked,
-                        onClick = {
-                            navController.navigate("password_page")
-                        },
+                        onClick = onTermsAccepted,
                     )
                 }
             }

app/src/main/java/io/horizontalsystems/bankwallet/modules/backuplocal/fullbackup/SelectBackupItemsScreen.kt
@@ -1,0 +1,103 @@
+package io.horizontalsystems.bankwallet.modules.backuplocal.fullbackup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.entities.ViewState
+import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
+import io.horizontalsystems.bankwallet.ui.compose.components.CellUniversalLawrenceSection
+import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
+import io.horizontalsystems.bankwallet.ui.compose.components.HsCheckbox
+import io.horizontalsystems.bankwallet.ui.compose.components.RowUniversal
+import io.horizontalsystems.bankwallet.ui.compose.components.body_leah
+import io.horizontalsystems.bankwallet.ui.compose.components.subhead2_grey
+import io.horizontalsystems.bankwallet.ui.compose.components.subhead2_lucian
+
+@Composable
+fun SelectBackupItemsScreen(
+    onNextClick: (accountIds: List<String>) -> Unit,
+    onBackClick: () -> Unit
+) {
+    val viewModel = viewModel<SelectBackupItemsViewModel>(factory = SelectBackupItemsViewModel.Factory())
+    val uiState = viewModel.uiState
+
+    ComposeAppTheme {
+        Scaffold(
+            backgroundColor = ComposeAppTheme.colors.tyler,
+            topBar = {
+                AppBar(
+                    title = stringResource(R.string.BackupManager_BаckupFile),
+                    navigationIcon = {
+                        HsBackButton(onClick = onBackClick)
+                    },
+                )
+            },
+            bottomBar = {
+                ButtonsGroupWithShade {
+                    ButtonPrimaryYellow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 16.dp, end = 16.dp),
+                        title = stringResource(R.string.Button_Next),
+                        onClick = {
+                            onNextClick(viewModel.selectedWallets)
+                        }
+                    )
+                }
+            }
+        ) {
+            LazyColumn(modifier = Modifier.padding(it)) {
+
+                when (uiState.viewState) {
+                    ViewState.Success -> {
+                        item {
+                            CellUniversalLawrenceSection(uiState.wallets, showFrame = true) { wallet ->
+
+                                RowUniversal(
+                                    modifier = Modifier.padding(horizontal = 16.dp),
+                                    onClick = { viewModel.toggle(wallet) }
+                                ) {
+
+                                    Column(modifier = Modifier.weight(1f)) {
+                                        body_leah(text = wallet.name)
+                                        if (wallet.manualBackupRequired) {
+                                            subhead2_lucian(text = stringResource(id = R.string.BackupManager_ManualBackupRequired))
+                                        } else {
+                                            subhead2_grey(
+                                                text = wallet.type,
+                                                overflow = TextOverflow.Ellipsis,
+                                                maxLines = 1
+                                            )
+                                        }
+                                    }
+                                    HsCheckbox(
+                                        checked = wallet.selected,
+                                        onCheckedChange = {
+                                            viewModel.toggle(wallet)
+                                        },
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    is ViewState.Error,
+                    ViewState.Loading -> Unit
+                }
+
+            }
+        }
+    }
+}

horizontalsystems/unstoppable-wallet-android

app/src/main/java/cash/p/terminal/modules/backuplocal/BackupLocalFragment.kt
@@ -1,6 +1,7 @@
 package cash.p.terminal.modules.backuplocal
 
 import android.os.Bundle
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.core.os.bundleOf
 import androidx.navigation.NavController
@@ -9,6 +10,8 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import cash.p.terminal.core.BaseComposeFragment
 import cash.p.terminal.core.composablePage
+import cash.p.terminal.modules.backuplocal.fullbackup.SelectBackupItemsScreen
+import cash.p.terminal.modules.backuplocal.password.BackupType
 import cash.p.terminal.modules.backuplocal.password.LocalBackupPasswordScreen
 import cash.p.terminal.modules.backuplocal.terms.LocalBackupTermsScreen
 import io.horizontalsystems.core.findNavController
@@ -17,7 +20,12 @@ class BackupLocalFragment : BaseComposeFragment() {
 
     @Composable
     override fun GetContent() {
-        BackupLocalNavHost(findNavController(), requireArguments().getString(ACCOUNT_ID_KEY))
+        val accountId = arguments?.getString(ACCOUNT_ID_KEY)
+        if (accountId != null) {
+            SingleWalletBackupNavHost(findNavController(), accountId)
+        } else {
+            FullBackupNavHost(fragmentNavController = findNavController())
+        }
     }
 
     companion object {
@@ -29,20 +37,79 @@ class BackupLocalFragment : BaseComposeFragment() {
 }
 
 @Composable
-private fun BackupLocalNavHost(fragmentNavController: NavController, accountId: String?) {
+private fun FullBackupNavHost(fragmentNavController: NavController) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "select_backup_items",
+    ) {
+        composable("select_backup_items") {
+            SelectBackupItemsScreen(
+                onNextClick = { accountIdsList ->
+                    val accountIds = accountIdsList.joinToString(separator = ",")
+                    navController.navigate("terms_page/${accountIds}")
+                },
+                onBackClick = {
+                    fragmentNavController.popBackStack()
+                }
+            )
+        }
+
+        composablePage("terms_page/{accountIds}") { backStackEntry ->
+            val accountIds = backStackEntry.arguments?.getString("accountIds")
+            LocalBackupTermsScreen(
+                onTermsAccepted = {
+                    navController.navigate("password_page/${accountIds}")
+                },
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composablePage("password_page/{accountIds}") { backStackEntry ->
+            val accountIds = backStackEntry.arguments?.getString("accountIds")?.split(",") ?: listOf()
+            Log.e("eee", "password_page selectedWallets ${accountIds.size}: ${backStackEntry.arguments?.getString("accountIds")}")
+
+            LocalBackupPasswordScreen(
+                backupType = BackupType.FullBackup(accountIds),
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFinish = {
+                    fragmentNavController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun SingleWalletBackupNavHost(fragmentNavController: NavController, accountId: String) {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = "terms_page",
     ) {
         composable("terms_page") {
-            LocalBackupTermsScreen(fragmentNavController, navController)
+            LocalBackupTermsScreen(
+                onTermsAccepted = {
+                    navController.navigate("password_page")
+                },
+                onBackClick = {
+                    fragmentNavController.popBackStack()
+                }
+            )
         }
         composablePage("password_page") {
             LocalBackupPasswordScreen(
-                fragmentNavController,
-                navController,
-                accountId
+                backupType = BackupType.SingleWalletBackup(accountId),
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onFinish = {
+                    fragmentNavController.popBackStack()
+                }
             )
         }
     }

app/src/main/java/cash/p/terminal/modules/backuplocal/terms/BackupLocalTerms.kt
@@ -14,16 +14,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import cash.p.terminal.R
 import cash.p.terminal.modules.evmfee.ButtonsGroupWithShade
 import cash.p.terminal.ui.compose.ComposeAppTheme
-import cash.p.terminal.ui.compose.TranslatableString
 import cash.p.terminal.ui.compose.components.AppBar
 import cash.p.terminal.ui.compose.components.ButtonPrimaryYellow
 import cash.p.terminal.ui.compose.components.CellUniversalLawrenceSection
+import cash.p.terminal.ui.compose.components.HsBackButton
 import cash.p.terminal.ui.compose.components.HsCheckbox
-import cash.p.terminal.ui.compose.components.MenuItem
 import cash.p.terminal.ui.compose.components.RowUniversal
 import cash.p.terminal.ui.compose.components.TextImportantWarning
 import cash.p.terminal.ui.compose.components.VSpacer
@@ -31,8 +29,8 @@ import cash.p.terminal.ui.compose.components.subhead2_leah
 
 @Composable
 fun LocalBackupTermsScreen(
-    fragmentNavController: NavController,
-    navController: NavController,
+    onTermsAccepted: ()-> Unit,
+    onBackClick: () -> Unit,
 ) {
     var termChecked by rememberSaveable { mutableStateOf(false) }
 
@@ -42,15 +40,9 @@ fun LocalBackupTermsScreen(
             topBar = {
                 AppBar(
                     title = stringResource(R.string.LocalBackup_Title),
-                    menuItems = listOf(
-                        MenuItem(
-                            title = TranslatableString.ResString(R.string.Button_Close),
-                            icon = R.drawable.ic_close,
-                            onClick = {
-                                fragmentNavController.popBackStack()
-                            }
-                        )
-                    )
+                    navigationIcon = {
+                        HsBackButton(onClick = onBackClick)
+                    },
                 )
             }
         ) {
@@ -80,9 +72,7 @@ fun LocalBackupTermsScreen(
                             .padding(start = 16.dp, end = 16.dp),
                         title = stringResource(R.string.Button_Continue),
                         enabled = termChecked,
-                        onClick = {
-                            navController.navigate("password_page")
-                        },
+                        onClick = onTermsAccepted,
                     )
                 }
             }

app/src/main/java/io/horizontalsystems/bankwallet/modules/backuplocal/fullbackup/SelectBackupItemsScreen.kt
@@ -1,0 +1,103 @@
+package cash.p.terminal.modules.backuplocal.fullbackup
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import cash.p.terminal.R
+import cash.p.terminal.entities.ViewState
+import cash.p.terminal.modules.evmfee.ButtonsGroupWithShade
+import cash.p.terminal.ui.compose.ComposeAppTheme
+import cash.p.terminal.ui.compose.components.AppBar
+import cash.p.terminal.ui.compose.components.ButtonPrimaryYellow
+import cash.p.terminal.ui.compose.components.CellUniversalLawrenceSection
+import cash.p.terminal.ui.compose.components.HsBackButton
+import cash.p.terminal.ui.compose.components.HsCheckbox
+import cash.p.terminal.ui.compose.components.RowUniversal
+import cash.p.terminal.ui.compose.components.body_leah
+import cash.p.terminal.ui.compose.components.subhead2_grey
+import cash.p.terminal.ui.compose.components.subhead2_lucian
+
+@Composable
+fun SelectBackupItemsScreen(
+    onNextClick: (accountIds: List<String>) -> Unit,
+    onBackClick: () -> Unit
+) {
+    val viewModel = viewModel<SelectBackupItemsViewModel>(factory = SelectBackupItemsViewModel.Factory())
+    val uiState = viewModel.uiState
+
+    ComposeAppTheme {
+        Scaffold(
+            backgroundColor = ComposeAppTheme.colors.tyler,
+            topBar = {
+                AppBar(
+                    title = stringResource(R.string.BackupManager_BаckupFile),
+                    navigationIcon = {
+                        HsBackButton(onClick = onBackClick)
+                    },
+                )
+            },
+            bottomBar = {
+                ButtonsGroupWithShade {
+                    ButtonPrimaryYellow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 16.dp, end = 16.dp),
+                        title = stringResource(R.string.Button_Next),
+                        onClick = {
+                            onNextClick(viewModel.selectedWallets)
+                        }
+                    )
+                }
+            }
+        ) {
+            LazyColumn(modifier = Modifier.padding(it)) {
+
+                when (uiState.viewState) {
+                    ViewState.Success -> {
+                        item {
+                            CellUniversalLawrenceSection(uiState.wallets, showFrame = true) { wallet ->
+
+                                RowUniversal(
+                                    modifier = Modifier.padding(horizontal = 16.dp),
+                                    onClick = { viewModel.toggle(wallet) }
+                                ) {
+
+                                    Column(modifier = Modifier.weight(1f)) {
+                                        body_leah(text = wallet.name)
+                                        if (wallet.manualBackupRequired) {
+                                            subhead2_lucian(text = stringResource(id = R.string.BackupManager_ManualBackupRequired))
+                                        } else {
+                                            subhead2_grey(
+                                                text = wallet.type,
+                                                overflow = TextOverflow.Ellipsis,
+                                                maxLines = 1
+                                            )
+                                        }
+                                    }
+                                    HsCheckbox(
+                                        checked = wallet.selected,
+                                        onCheckedChange = {
+                                            viewModel.toggle(wallet)
+                                        },
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    is ViewState.Error,
+                    ViewState.Loading -> Unit
+                }
+
+            }
+        }
+    }
+}