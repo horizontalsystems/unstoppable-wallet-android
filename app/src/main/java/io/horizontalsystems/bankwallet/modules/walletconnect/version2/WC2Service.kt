5edc800273a38a4f77b7235bc2e94d98bb027080
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2023-10-26 12:30:10 +0600

    Handle custom WC pending requests topics

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/version2/WC2Service.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/version2/WC2Service.kt
@@ -19,7 +19,9 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.concurrent.CopyOnWriteArrayList
 
-class WC2Service : SignClient.WalletDelegate {
+class WC2Service(
+    private val sessionRequestFilterManager: SessionRequestFilterManager
+) : SignClient.WalletDelegate {
 
     private val TAG = "WC2Service"
 
@@ -215,6 +217,9 @@ class WC2Service : SignClient.WalletDelegate {
     }
 
     override fun onSessionRequest(sessionRequest: Sign.Model.SessionRequest, verifyContext: Sign.Model.VerifyContext) {
+        if (sessionRequestFilterManager.canHandle(sessionRequest)) {
+            return
+        }
         sessionsRequestReceivedSubject.onNext(sessionRequest)
         pendingRequestUpdatedSubject.onNext(Unit)
     }
