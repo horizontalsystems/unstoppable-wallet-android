0e177d2ffef7d31eefb232127b0ac02a35ad1dcb
bakyt <abdrasulov@gmail.com>
2023-11-30 12:17:03 +0600

    Move "leave some balance for fee" checking to validator

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/amount/AmountValidator.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/amount/AmountValidator.kt
@@ -1,8 +1,11 @@
 package io.horizontalsystems.bankwallet.modules.amount
 
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.HSCaution
 import io.horizontalsystems.bankwallet.modules.send.SendErrorInsufficientBalance
 import io.horizontalsystems.bankwallet.modules.send.SendErrorMaximumSendAmount
 import io.horizontalsystems.bankwallet.modules.send.SendErrorMinimumSendAmount
+import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import java.math.BigDecimal
 
 class AmountValidator {
@@ -13,6 +16,7 @@ class AmountValidator {
         availableBalance: BigDecimal,
         minimumSendAmount: BigDecimal? = null,
         maximumSendAmount: BigDecimal? = null,
+        leaveSomeBalanceForFee: Boolean = false
     ) = when {
         coinAmount == null -> null
         coinAmount == BigDecimal.ZERO -> null
@@ -24,6 +28,12 @@ class AmountValidator {
         }
         maximumSendAmount != null && coinAmount > maximumSendAmount -> {
             SendErrorMaximumSendAmount(maximumSendAmount)
+        }
+        leaveSomeBalanceForFee && coinAmount == availableBalance -> {
+            HSCaution(
+                TranslatableString.ResString(R.string.EthereumTransaction_Warning_CoinNeededForFee, coinCode),
+                HSCaution.Type.Warning
+            )
         }
         else -> null
     }
