fa0a724f5ac29dd5780e0da50674dd5be4d1062d
bakyt <abdrasulov@gmail.com>
2023-12-05 16:27:47 +0600

    Apply new method for passing input

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/ProChartFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/metricchart/ProChartFragment.kt
@@ -1,6 +1,7 @@
 package io.horizontalsystems.bankwallet.modules.metricchart
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,33 +12,18 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewmodel.compose.viewModel
 import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.getInputX
 import io.horizontalsystems.bankwallet.modules.chart.ChartViewModel
 import io.horizontalsystems.bankwallet.modules.coin.overview.ui.Chart
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
 import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
 import io.horizontalsystems.bankwallet.ui.extensions.BottomSheetHeader
+import kotlinx.parcelize.Parcelize
 
 class ProChartFragment : BaseComposableBottomSheetFragment() {
-
-    private val coinUid by lazy {
-        requireArguments().getString(coinUidKey) ?: ""
-    }
-
-    private val chartType by lazy {
-        val enumOrdinal = requireArguments().getInt(chartTypeKey, 0)
-        enumValues<ProChartModule.ChartType>()[enumOrdinal]
-    }
-
-    private val title by lazy {
-        requireArguments().getString(titleKey) ?: ""
-    }
-
-    private val chartViewModel by viewModels<ChartViewModel> {
-        ProChartModule.Factory(coinUid, chartType)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,11 +35,19 @@ class ProChartFragment : BaseComposableBottomSheetFragment() {
                 ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
             )
             setContent {
+                val input = requireArguments().getInputX<Input>()!!
+                val chartViewModel = viewModel<ChartViewModel>(
+                    factory = ProChartModule.Factory(
+                        input.coinUid,
+                        enumValues<ProChartModule.ChartType>()[input.chartType]
+                    )
+                )
+
                 ComposeAppTheme {
                     BottomSheetHeader(
                         iconPainter = painterResource(R.drawable.ic_chart_24),
                         iconTint = ColorFilter.tint(ComposeAppTheme.colors.jacob),
-                        title = title,
+                        title = input.title,
                         onCloseClick = { close() }
                     ) {
                         Chart(chartViewModel = chartViewModel)
@@ -64,11 +58,14 @@ class ProChartFragment : BaseComposableBottomSheetFragment() {
         }
     }
 
-    companion object {
-        private const val coinUidKey = "coinUidKey"
-        private const val chartTypeKey = "chartTypeKey"
-        private const val titleKey = "titleKey"
+    @Parcelize
+    data class Input(
+        val coinUid: String,
+        val title: String,
+        val chartType: Int,
+    ) : Parcelable
 
+    companion object {
         fun show(
             fragmentManager: FragmentManager,
             coinUid: String,
@@ -77,9 +74,7 @@ class ProChartFragment : BaseComposableBottomSheetFragment() {
         ) {
             val fragment = ProChartFragment()
             fragment.arguments = bundleOf(
-                coinUidKey to coinUid,
-                titleKey to title,
-                chartTypeKey to chartType.ordinal,
+                "input" to Input(coinUid, title, chartType.ordinal)
             )
             fragment.show(fragmentManager, "pro_chart_dialog")
         }
