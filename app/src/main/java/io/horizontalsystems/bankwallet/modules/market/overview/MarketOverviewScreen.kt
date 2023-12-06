e8d156783374cbed3266152f28c28bed4b474a74
bakyt <abdrasulov@gmail.com>
2023-12-06 12:02:18 +0600

    Show metric cards in one scrollable row

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/overview/MarketOverviewScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/overview/MarketOverviewScreen.kt
@@ -1,10 +1,8 @@
 package io.horizontalsystems.bankwallet.modules.market.overview
 
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -12,7 +10,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -67,11 +64,7 @@ fun MarketOverviewScreen(
                                 .fillMaxSize()
                                 .verticalScroll(scrollState)
                         ) {
-                            Box(
-                                modifier = Modifier.height(240.dp)
-                            ) {
-                                MetricChartsView(viewItem.marketMetrics, navController)
-                            }
+                            MetricChartsView(viewItem.marketMetrics, navController)
                             BoardsView(
                                 boards = viewItem.boards,
                                 navController = navController,
