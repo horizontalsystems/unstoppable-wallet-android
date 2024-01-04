5abf7ad2c14ef430711c70a104c992bbf9fdf998
bakyt <abdrasulov@gmail.com>
2024-01-04 15:05:28 +0600

    Pass base currency when fetching top pairs

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/toppairs/TopPairsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/toppairs/TopPairsViewModel.kt
@@ -7,16 +7,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.managers.CurrencyManager
 import io.horizontalsystems.bankwallet.core.managers.MarketKitWrapper
 import io.horizontalsystems.bankwallet.entities.ViewState
 import io.horizontalsystems.bankwallet.modules.market.overview.TopPairViewItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.asFlow
 import kotlinx.coroutines.rx2.await
 import kotlinx.coroutines.withContext
 
-class TopPairsViewModel(private val marketKit: MarketKitWrapper) : ViewModel() {
+class TopPairsViewModel(
+    private val marketKit: MarketKitWrapper,
+    private val currencyManager: CurrencyManager,
+) : ViewModel() {
     private var isRefreshing = false
     private var items = listOf<TopPairViewItem>()
     private var viewState: ViewState = ViewState.Loading
@@ -31,8 +36,12 @@ class TopPairsViewModel(private val marketKit: MarketKitWrapper) : ViewModel() {
         private set
 
     init {
-        viewState = ViewState.Loading
-        emitState()
+        viewModelScope.launch {
+            currencyManager.baseCurrencyUpdatedSignal.asFlow().collect {
+                fetchItems()
+                emitState()
+            }
+        }
 
         viewModelScope.launch {
             fetchItems()
@@ -42,9 +51,9 @@ class TopPairsViewModel(private val marketKit: MarketKitWrapper) : ViewModel() {
 
     private suspend fun fetchItems() = withContext(Dispatchers.Default) {
         try {
-            val topPairs = marketKit.topPairsSingle(1, 100).await()
+            val topPairs = marketKit.topPairsSingle(currencyManager.baseCurrency.code, 1, 100).await()
             items = topPairs.map {
-                TopPairViewItem.createFromTopPair(it)
+                TopPairViewItem.createFromTopPair(it, currencyManager.baseCurrency.symbol)
             }
             viewState = ViewState.Success
         } catch (e: Throwable) {
@@ -81,7 +90,7 @@ class TopPairsViewModel(private val marketKit: MarketKitWrapper) : ViewModel() {
     class Factory : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return TopPairsViewModel(App.marketKit) as T
+            return TopPairsViewModel(App.marketKit, App.currencyManager) as T
         }
     }
 
