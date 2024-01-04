894798738be600fbe19f0ad0ecbe341acd988272
bakyt <abdrasulov@gmail.com>
2024-01-04 14:45:14 +0600

    Replace loading/error fields with ViewState enum

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/toppairs/TopPairsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/toppairs/TopPairsFragment.kt
@@ -16,6 +16,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
+import io.horizontalsystems.bankwallet.entities.ViewState
 import io.horizontalsystems.bankwallet.modules.coin.overview.ui.Loading
 import io.horizontalsystems.bankwallet.modules.market.ImageSource
 import io.horizontalsystems.bankwallet.modules.market.overview.ui.TopPairItem
@@ -50,34 +51,38 @@ fun TopPairsScreen(navController: NavController) {
                 refreshing = uiState.isRefreshing,
                 onRefresh = viewModel::refresh
             ) {
-                Crossfade(uiState.loading, label = "") { loading ->
-                    if (loading) {
-                        Loading()
-                    } else if (uiState.error != null) {
-                        ListErrorView(
-                            stringResource(R.string.SyncError),
-                            viewModel::onErrorClick
-                        )
-                    } else {
-                        LazyColumn(
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            item {
-                                DescriptionCard(
-                                    stringResource(id = R.string.TopPairs_Title),
-                                    stringResource(id = R.string.TopPairs_Description),
-                                    ImageSource.Local(R.drawable.ic_token_pairs)
-                                )
-                            }
-                            itemsIndexed(uiState.items) { i, item ->
-                                TopPairItem(item, borderTop = i == 0, borderBottom = true) {
-                                    it.tradeUrl?.let {
-                                        LinkHelper.openLinkInAppBrowser(context, it)
+                Crossfade(uiState.viewState, label = "") { viewState ->
+                    when (viewState) {
+                        ViewState.Loading -> {
+                            Loading()
+                        }
+                        is ViewState.Error -> {
+                            ListErrorView(
+                                stringResource(R.string.SyncError),
+                                viewModel::onErrorClick
+                            )
+                        }
+                        ViewState.Success -> {
+                            LazyColumn(
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                item {
+                                    DescriptionCard(
+                                        stringResource(id = R.string.TopPairs_Title),
+                                        stringResource(id = R.string.TopPairs_Description),
+                                        ImageSource.Local(R.drawable.ic_token_pairs)
+                                    )
+                                }
+                                itemsIndexed(uiState.items) { i, item ->
+                                    TopPairItem(item, borderTop = i == 0, borderBottom = true) {
+                                        it.tradeUrl?.let {
+                                            LinkHelper.openLinkInAppBrowser(context, it)
+                                        }
                                     }
                                 }
-                            }
-                            item {
-                                VSpacer(height = 32.dp)
+                                item {
+                                    VSpacer(height = 32.dp)
+                                }
                             }
                         }
                     }
