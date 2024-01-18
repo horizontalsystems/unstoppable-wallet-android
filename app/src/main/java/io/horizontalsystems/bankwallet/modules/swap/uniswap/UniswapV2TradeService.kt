6d36668599e2fbbc1556fa6e9960ddce62db6dce
bakyt <abdrasulov@gmail.com>
2024-01-18 18:20:41 +0600

    Fetch fee

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/uniswap/UniswapV2TradeService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/uniswap/UniswapV2TradeService.kt
@@ -1,11 +1,21 @@
 package io.horizontalsystems.bankwallet.modules.swap.uniswap
 
 import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.ethereum.EvmCoinServiceFactory
 import io.horizontalsystems.bankwallet.entities.Address
+import io.horizontalsystems.bankwallet.modules.evmfee.EvmCommonGasDataService
+import io.horizontalsystems.bankwallet.modules.evmfee.EvmFeeService
+import io.horizontalsystems.bankwallet.modules.evmfee.Transaction
+import io.horizontalsystems.bankwallet.modules.evmfee.eip1559.Eip1559GasPriceService
+import io.horizontalsystems.bankwallet.modules.evmfee.legacy.LegacyGasPriceService
 import io.horizontalsystems.bankwallet.modules.swap.SwapMainModule.SwapResultState
 import io.horizontalsystems.bankwallet.modules.swap.SwapQuote
 import io.horizontalsystems.bankwallet.modules.swap.UniversalSwapTradeData
 import io.horizontalsystems.bankwallet.modules.swap.settings.uniswap.SwapTradeOptions
+import io.horizontalsystems.bankwallet.modules.swapxxx.ui.SwapDataField
+import io.horizontalsystems.bankwallet.modules.swapxxx.ui.SwapFeeField
+import io.horizontalsystems.ethereumkit.core.LegacyGasPriceProvider
+import io.horizontalsystems.ethereumkit.core.eip1559.Eip1559GasPriceProvider
 import io.horizontalsystems.ethereumkit.models.Chain
 import io.horizontalsystems.ethereumkit.models.RpcSource
 import io.horizontalsystems.ethereumkit.models.TransactionData
@@ -14,12 +24,15 @@ import io.horizontalsystems.marketkit.models.Token
 import io.horizontalsystems.marketkit.models.TokenType
 import io.horizontalsystems.uniswapkit.UniswapKit
 import io.horizontalsystems.uniswapkit.models.SwapData
+import io.horizontalsystems.uniswapkit.models.TradeData
 import io.horizontalsystems.uniswapkit.models.TradeOptions
 import io.reactivex.Single
 import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.rx2.asFlow
 import kotlinx.coroutines.rx2.await
 import java.math.BigDecimal
 
@@ -63,7 +76,74 @@ class UniswapV2TradeService : IUniswapTradeService {
         val swapData = swapDataSingle(tokenIn, tokenOut).await()
         val tradeData = uniswapKit.bestTradeExactIn(swapData, amountIn, tradeOptions.tradeOptions)
         val amountOut = tradeData.amountOut!!
-        return SwapQuote(amountOut, listOf())
+        val fields = mutableListOf<SwapDataField>()
+
+        val feeAmountData = App.evmBlockchainManager.getBaseToken(tokenIn.blockchainType)?.let { baseToken ->
+            getFeeData(tokenIn.blockchainType, tradeData)?.let { transaction ->
+                val coinServiceFactory = EvmCoinServiceFactory(
+                    baseToken,
+                    App.marketKit,
+                    App.currencyManager,
+                    App.coinManager
+                )
+
+                val coinService = coinServiceFactory.baseCoinService
+                coinService.amountData(
+                    transaction.gasData.estimatedFee,
+                    transaction.gasData.isSurcharged
+                )
+            }
+        }
+
+        feeAmountData?.let {
+            fields.add(SwapFeeField(feeAmountData))
+        }
+
+
+        return SwapQuote(amountOut, fields, feeAmountData)
+    }
+
+    private suspend fun getFeeData(
+        blockchainType: BlockchainType,
+        tradeData: TradeData,
+    ): Transaction? {
+        val evmKitWrapper = App.evmBlockchainManager
+            .getEvmKitManager(blockchainType)
+            .evmKitWrapper ?: return null
+
+        val evmKit = evmKitWrapper.evmKit
+        val gasPriceService = if (evmKit.chain.isEIP1559Supported) {
+            val gasPriceProvider = Eip1559GasPriceProvider(evmKit)
+            Eip1559GasPriceService(gasPriceProvider, evmKit)
+        } else {
+            val gasPriceProvider = LegacyGasPriceProvider(evmKit)
+            LegacyGasPriceService(gasPriceProvider)
+        }
+
+        val gasDataService = EvmCommonGasDataService.instance(
+            evmKitWrapper.evmKit,
+            evmKitWrapper.blockchainType
+        )
+
+        val chain = App.evmBlockchainManager.getChain(blockchainType)
+
+        val transactionData = uniswapKit.transactionData(
+            evmKitWrapper.evmKit.receiveAddress,
+            chain,
+            tradeData
+        )
+
+        val evmFeeService = EvmFeeService(
+            evmKitWrapper.evmKit,
+            gasPriceService,
+            gasDataService,
+            transactionData
+        )
+        val transactionDataState = evmFeeService.transactionStatusObservable
+            .asFlow()
+            .firstOrNull { !it.loading }
+
+        return transactionDataState?.dataOrNull
     }
 
     override fun updateSwapSettings(recipient: Address?, slippage: BigDecimal?, ttl: Long?) {
