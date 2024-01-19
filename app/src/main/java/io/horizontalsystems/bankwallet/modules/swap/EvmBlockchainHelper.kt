e2b139030ac710a8af542c3ff628e975c55213a7
bakyt <abdrasulov@gmail.com>
2024-01-19 14:43:43 +0600

    Extract EvmBlockchainHelper

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/EvmBlockchainHelper.kt
@@ -0,0 +1,73 @@
+package io.horizontalsystems.bankwallet.modules.swap
+
+import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.ethereum.EvmCoinServiceFactory
+import io.horizontalsystems.bankwallet.modules.evmfee.EvmCommonGasDataService
+import io.horizontalsystems.bankwallet.modules.evmfee.EvmFeeService
+import io.horizontalsystems.bankwallet.modules.evmfee.Transaction
+import io.horizontalsystems.bankwallet.modules.evmfee.eip1559.Eip1559GasPriceService
+import io.horizontalsystems.bankwallet.modules.evmfee.legacy.LegacyGasPriceService
+import io.horizontalsystems.bankwallet.modules.send.SendModule
+import io.horizontalsystems.ethereumkit.core.LegacyGasPriceProvider
+import io.horizontalsystems.ethereumkit.core.eip1559.Eip1559GasPriceProvider
+import io.horizontalsystems.ethereumkit.models.RpcSource
+import io.horizontalsystems.ethereumkit.models.TransactionData
+import io.horizontalsystems.marketkit.models.BlockchainType
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.rx2.asFlow
+
+class EvmBlockchainHelper(private val blockchainType: BlockchainType) {
+    private val evmKit = App.evmBlockchainManager
+        .getEvmKitManager(blockchainType)
+        .evmKitWrapper?.evmKit
+
+    val baseToken by lazy { App.evmBlockchainManager.getBaseToken(blockchainType) }
+    val receiveAddress by lazy { evmKit?.receiveAddress }
+    val chain by lazy { App.evmBlockchainManager.getChain(blockchainType) }
+
+    private suspend fun getFeeData(transactionData: TransactionData): Transaction? {
+        val evmKit = evmKit ?: return null
+
+        val gasPriceService = if (chain.isEIP1559Supported) {
+            val gasPriceProvider = Eip1559GasPriceProvider(evmKit)
+            Eip1559GasPriceService(gasPriceProvider, evmKit)
+        } else {
+            val gasPriceProvider = LegacyGasPriceProvider(evmKit)
+            LegacyGasPriceService(gasPriceProvider)
+        }
+
+        val gasDataService = EvmCommonGasDataService.instance(evmKit, blockchainType)
+        val evmFeeService = EvmFeeService(evmKit, gasPriceService, gasDataService, transactionData)
+
+        val transactionDataState = evmFeeService.transactionStatusObservable
+            .asFlow()
+            .firstOrNull { !it.loading }
+
+        return transactionDataState?.dataOrNull
+    }
+
+    fun getRpcSourceHttp(): RpcSource.Http {
+        val httpSyncSource = App.evmSyncSourceManager.getHttpSyncSource(blockchainType)
+        val httpRpcSource = httpSyncSource?.rpcSource as? RpcSource.Http
+            ?: throw IllegalStateException("No HTTP RPC Source for blockchain $blockchainType")
+        return httpRpcSource
+    }
+
+    suspend fun getFeeAmountData(transactionData: TransactionData): SendModule.AmountData? {
+        val baseToken = baseToken ?: return null
+        val feeData = getFeeData(transactionData) ?: return null
+
+        val coinServiceFactory = EvmCoinServiceFactory(
+            baseToken,
+            App.marketKit,
+            App.currencyManager,
+            App.coinManager
+        )
+
+        return coinServiceFactory.baseCoinService.amountData(
+            feeData.gasData.estimatedFee,
+            feeData.gasData.isSurcharged
+        )
+    }
+
+}
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/uniswap/UniswapV2TradeService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/uniswap/UniswapV2TradeService.kt
@@ -1,38 +1,25 @@
 package io.horizontalsystems.bankwallet.modules.swap.uniswap
 
-import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.bankwallet.core.ethereum.EvmCoinServiceFactory
 import io.horizontalsystems.bankwallet.entities.Address
-import io.horizontalsystems.bankwallet.modules.evmfee.EvmCommonGasDataService
-import io.horizontalsystems.bankwallet.modules.evmfee.EvmFeeService
-import io.horizontalsystems.bankwallet.modules.evmfee.Transaction
-import io.horizontalsystems.bankwallet.modules.evmfee.eip1559.Eip1559GasPriceService
-import io.horizontalsystems.bankwallet.modules.evmfee.legacy.LegacyGasPriceService
+import io.horizontalsystems.bankwallet.modules.swap.EvmBlockchainHelper
 import io.horizontalsystems.bankwallet.modules.swap.SwapMainModule.SwapResultState
 import io.horizontalsystems.bankwallet.modules.swap.SwapQuote
 import io.horizontalsystems.bankwallet.modules.swap.UniversalSwapTradeData
 import io.horizontalsystems.bankwallet.modules.swap.settings.uniswap.SwapTradeOptions
 import io.horizontalsystems.bankwallet.modules.swapxxx.ui.SwapDataField
 import io.horizontalsystems.bankwallet.modules.swapxxx.ui.SwapFeeField
-import io.horizontalsystems.ethereumkit.core.LegacyGasPriceProvider
-import io.horizontalsystems.ethereumkit.core.eip1559.Eip1559GasPriceProvider
 import io.horizontalsystems.ethereumkit.models.Chain
-import io.horizontalsystems.ethereumkit.models.RpcSource
 import io.horizontalsystems.ethereumkit.models.TransactionData
-import io.horizontalsystems.marketkit.models.BlockchainType
 import io.horizontalsystems.marketkit.models.Token
 import io.horizontalsystems.marketkit.models.TokenType
 import io.horizontalsystems.uniswapkit.UniswapKit
 import io.horizontalsystems.uniswapkit.models.SwapData
-import io.horizontalsystems.uniswapkit.models.TradeData
 import io.horizontalsystems.uniswapkit.models.TradeOptions
 import io.reactivex.Single
 import io.reactivex.disposables.Disposable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.rx2.asFlow
 import kotlinx.coroutines.rx2.await
 import java.math.BigDecimal
 import io.horizontalsystems.ethereumkit.models.Address as EthereumKitAddress
@@ -72,79 +59,30 @@ class UniswapV2TradeService : IUniswapTradeService {
     override suspend fun fetchQuote(
         tokenIn: Token,
         tokenOut: Token,
-        amountIn: BigDecimal
+        amountIn: BigDecimal,
     ): SwapQuote {
-        val swapData = swapDataSingle(tokenIn, tokenOut).await()
+        val evmBlockchainHelper = EvmBlockchainHelper(tokenIn.blockchainType)
+
+        val swapData = swapDataSingle(tokenIn, tokenOut, evmBlockchainHelper).await()
         val tradeData = uniswapKit.bestTradeExactIn(swapData, amountIn, tradeOptions.tradeOptions)
-        val amountOut = tradeData.amountOut!!
-        val fields = mutableListOf<SwapDataField>()
+        val transactionData = evmBlockchainHelper.receiveAddress?.let { receiveAddress ->
+            uniswapKit.transactionData(
+                receiveAddress,
+                evmBlockchainHelper.chain,
+                tradeData
+            )
+        }
+        val feeAmountData = transactionData?.let {
+            evmBlockchainHelper.getFeeAmountData(it)
+        }
 
-        val feeAmountData = App.evmBlockchainManager.getBaseToken(tokenIn.blockchainType)?.let { baseToken ->
-            getFeeData(tokenIn.blockchainType, tradeData)?.let { transaction ->
-                val coinServiceFactory = EvmCoinServiceFactory(
-                    baseToken,
-                    App.marketKit,
-                    App.currencyManager,
-                    App.coinManager
-                )
-
-                val coinService = coinServiceFactory.baseCoinService
-                coinService.amountData(
-                    transaction.gasData.estimatedFee,
-                    transaction.gasData.isSurcharged
-                )
+        val fields = buildList<SwapDataField> {
+            feeAmountData?.let {
+                add(SwapFeeField(feeAmountData))
             }
         }
 
-        feeAmountData?.let {
-            fields.add(SwapFeeField(feeAmountData))
-        }
-
-
-        return SwapQuote(amountOut, fields, feeAmountData)
-    }
-
-    private suspend fun getFeeData(
-        blockchainType: BlockchainType,
-        tradeData: TradeData,
-    ): Transaction? {
-        val evmKitWrapper = App.evmBlockchainManager
-            .getEvmKitManager(blockchainType)
-            .evmKitWrapper ?: return null
-
-        val evmKit = evmKitWrapper.evmKit
-        val gasPriceService = if (evmKit.chain.isEIP1559Supported) {
-            val gasPriceProvider = Eip1559GasPriceProvider(evmKit)
-            Eip1559GasPriceService(gasPriceProvider, evmKit)
-        } else {
-            val gasPriceProvider = LegacyGasPriceProvider(evmKit)
-            LegacyGasPriceService(gasPriceProvider)
-        }
-
-        val gasDataService = EvmCommonGasDataService.instance(
-            evmKitWrapper.evmKit,
-            evmKitWrapper.blockchainType
-        )
-
-        val chain = App.evmBlockchainManager.getChain(blockchainType)
-
-        val transactionData = uniswapKit.transactionData(
-            evmKitWrapper.evmKit.receiveAddress,
-            chain,
-            tradeData
-        )
-
-        val evmFeeService = EvmFeeService(
-            evmKitWrapper.evmKit,
-            gasPriceService,
-            gasDataService,
-            transactionData
-        )
-        val transactionDataState = evmFeeService.transactionStatusObservable
-            .asFlow()
-            .firstOrNull { !it.loading }
-
-        return transactionDataState?.dataOrNull
+        return SwapQuote(tradeData.amountOut!!, fields, feeAmountData)
     }
 
     override fun updateSwapSettings(recipient: Address?, slippage: BigDecimal?, ttl: Long?) {
@@ -166,18 +104,20 @@ class UniswapV2TradeService : IUniswapTradeService {
         swapDataDisposable = null
     }
 
-    private fun swapDataSingle(tokenIn: Token, tokenOut: Token): Single<SwapData> {
+    private fun swapDataSingle(
+        tokenIn: Token,
+        tokenOut: Token,
+        evmBlockchainHelper: EvmBlockchainHelper
+    ): Single<SwapData> {
         return try {
-            val blockchainType = tokenIn.blockchainType
-            val chain = App.evmBlockchainManager.getChain(blockchainType)
-            val httpSyncSource = App.evmSyncSourceManager.getHttpSyncSource(blockchainType)
-            val httpRpcSource = httpSyncSource?.rpcSource as? RpcSource.Http
-                ?: throw IllegalStateException("No HTTP RPC Source for blockchain $blockchainType")
+            val chain = evmBlockchainHelper.chain
 
-            val uniswapTokenIn = uniswapToken(tokenIn, chain)
-            val uniswapTokenOut = uniswapToken(tokenOut, chain)
-
-            uniswapKit.swapData(httpRpcSource, chain, uniswapTokenIn, uniswapTokenOut)
+            uniswapKit.swapData(
+                evmBlockchainHelper.getRpcSourceHttp(),
+                chain,
+                uniswapToken(tokenIn, chain),
+                uniswapToken(tokenOut, chain)
+            )
         } catch (error: Throwable) {
             Single.error(error)
         }
