f78859f3b9f3d4ab72f47798ab90e7cb132e628d
bakyt <abdrasulov@gmail.com>
2024-02-05 13:02:35 +0600

    Apply new CellUniversal in BlockchainSelectorScreen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/addtoken/blockchainselector/AddTokenBlockchainSelector.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/addtoken/blockchainselector/AddTokenBlockchainSelector.kt
@@ -1,32 +1,21 @@
 package io.horizontalsystems.bankwallet.modules.addtoken.blockchainselector
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import coil.compose.rememberAsyncImagePainter
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.core.imageUrl
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
-import io.horizontalsystems.bankwallet.ui.compose.components.CellBorderedRowUniversal
 import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
-import io.horizontalsystems.bankwallet.ui.compose.components.body_leah
 import io.horizontalsystems.bankwallet.ui.compose.components.cell.CellBlockchainChecked
 import io.horizontalsystems.bankwallet.ui.compose.components.cell.SectionUniversalLawrence
 import io.horizontalsystems.marketkit.models.Blockchain
@@ -74,48 +63,6 @@ fun AddTokenBlockchainSelectorScreen(
                 }
             }
             Spacer(Modifier.height(32.dp))
-        }
-    }
-}
-
-@Composable
-fun BlockchainCell(
-    item: Blockchain,
-    selected: Boolean,
-    onCheck: (Blockchain) -> Unit,
-    borderTop: Boolean
-) {
-    CellBorderedRowUniversal(
-        borderTop = borderTop,
-        modifier = Modifier
-            .clickable {
-                onCheck(item)
-            }
-            .fillMaxWidth(),
-        backgroundColor = ComposeAppTheme.colors.lawrence
-    ) {
-        Image(
-            painter = rememberAsyncImagePainter(
-                model = item.type.imageUrl,
-                error = painterResource(R.drawable.ic_platform_placeholder_32)
-            ),
-            contentDescription = null,
-            modifier = Modifier.size(32.dp)
-        )
-        body_leah(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .weight(1f),
-            text = item.name,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-        )
-        if (selected) {
-            Icon(
-                painter = painterResource(R.drawable.ic_checkmark_20),
-                tint = ComposeAppTheme.colors.jacob,
-                contentDescription = null,
-            )
         }
     }
 }
