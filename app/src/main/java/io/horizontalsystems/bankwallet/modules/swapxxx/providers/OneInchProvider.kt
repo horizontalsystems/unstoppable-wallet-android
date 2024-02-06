1a6bec75d5c1603936f22421513dcf9a0cb4eee5
bakyt <abdrasulov@gmail.com>
2024-02-06 11:22:37 +0600

    Get quote for new OneInchProvider

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swapxxx/providers/OneInchProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swapxxx/providers/OneInchProvider.kt
@@ -1,9 +1,19 @@
 package cash.p.terminal.modules.swapxxx.providers
 
 import cash.p.terminal.R
+import cash.p.terminal.core.App
+import cash.p.terminal.core.convertedError
+import cash.p.terminal.modules.swap.scaleUp
+import cash.p.terminal.modules.swapxxx.EvmBlockchainHelper
 import cash.p.terminal.modules.swapxxx.ISwapQuote
+import cash.p.terminal.modules.swapxxx.SwapQuoteOneInch
+import io.horizontalsystems.ethereumkit.models.Address
 import io.horizontalsystems.marketkit.models.BlockchainType
 import io.horizontalsystems.marketkit.models.Token
+import io.horizontalsystems.marketkit.models.TokenType
+import io.horizontalsystems.oneinchkit.OneInchKit
+import io.reactivex.Single
+import kotlinx.coroutines.rx2.await
 import java.math.BigDecimal
 
 object OneInchProvider : ISwapXxxProvider {
@@ -11,7 +21,10 @@ object OneInchProvider : ISwapXxxProvider {
     override val title = "1inch"
     override val url = "https://app.1inch.io/"
     override val icon = R.drawable.oneinch
-//    private val oneInchKitHelper by lazy { OneInchKitHelper(App.appConfigProvider.oneInchApiKey) }
+    private val oneInchKit by lazy { OneInchKit.getInstance(App.appConfigProvider.oneInchApiKey) }
+
+    // TODO take evmCoinAddress from oneInchKit
+    private val evmCoinAddress = Address("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
 
     override fun supports(blockchainType: BlockchainType) = when (blockchainType) {
         BlockchainType.Ethereum,
@@ -28,11 +41,24 @@ object OneInchProvider : ISwapXxxProvider {
     }
 
     override suspend fun fetchQuote(tokenIn: Token, tokenOut: Token, amountIn: BigDecimal): ISwapQuote {
-        TODO()
-//        val chain = App.evmBlockchainManager.getChain(tokenIn.blockchainType)
-//
-//        val quote = oneInchKitHelper.getQuoteAsync(chain, tokenIn, tokenOut, amountIn).await()
-//        val amountOut = quote.toTokenAmount.abs().toBigDecimal().movePointLeft(quote.toToken.decimals).stripTrailingZeros()
-//        return SwapQuoteOneInch(amountOut, listOf(), null, tokenIn.blockchainType)
+        val evmBlockchainHelper = EvmBlockchainHelper(tokenIn.blockchainType)
+
+        val quote = oneInchKit.getQuoteAsync(
+            chain = evmBlockchainHelper.chain,
+            fromToken = getTokenAddress(tokenIn),
+            toToken = getTokenAddress(tokenOut),
+            amount = amountIn.scaleUp(tokenIn.decimals)
+        ).onErrorResumeNext {
+            Single.error(it.convertedError)
+        }.await()
+
+        val amountOut = quote.toTokenAmount.abs().toBigDecimal().movePointLeft(quote.toToken.decimals).stripTrailingZeros()
+        return SwapQuoteOneInch(amountOut, listOf(), null, tokenIn.blockchainType)
+    }
+
+    private fun getTokenAddress(token: Token) = when (val tokenType = token.type) {
+        TokenType.Native -> evmCoinAddress
+        is TokenType.Eip20 -> Address(tokenType.address)
+        else -> throw IllegalStateException("Unsupported tokenType: $tokenType")
     }
 }
