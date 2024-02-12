45961636fb31bba20fa54c457f76f7b82eb726d3
bakyt <abdrasulov@gmail.com>
2024-02-12 16:42:49 +0600

    Add swap transaction settings empty screen

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swapxxx/settings/SwapTransactionSettingsFragment.kt
@@ -0,0 +1,24 @@
+package io.horizontalsystems.bankwallet.modules.swapxxx.settings
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import io.horizontalsystems.bankwallet.core.BaseComposeFragment
+import io.horizontalsystems.bankwallet.modules.swapxxx.SwapViewModel
+
+class SwapTransactionSettingsFragment : BaseComposeFragment() {
+    @Composable
+    override fun GetContent(navController: NavController) {
+        SwapTransactionSettingsScreen(navController)
+    }
+}
+
+@Composable
+fun SwapTransactionSettingsScreen(navController: NavController) {
+    val viewModel = viewModel<SwapViewModel>(
+        viewModelStoreOwner = navController.previousBackStackEntry!!,
+        factory = SwapViewModel.Factory()
+    )
+
+    viewModel.uiState.tokenIn
+}
