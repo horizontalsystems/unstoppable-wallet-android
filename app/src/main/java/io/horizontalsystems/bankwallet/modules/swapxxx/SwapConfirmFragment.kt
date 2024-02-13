1eddc7e9991182ac53bf83c661abb78086dc7de8
bakyt <abdrasulov@gmail.com>
2024-02-13 17:23:56 +0600

    Show swap data on confirmation screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swapxxx/SwapConfirmFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swapxxx/SwapConfirmFragment.kt
@@ -2,18 +2,39 @@ package cash.p.terminal.modules.swapxxx
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import cash.p.terminal.R
 import cash.p.terminal.core.BaseComposeFragment
+import cash.p.terminal.core.badge
+import cash.p.terminal.core.iconPlaceholder
+import cash.p.terminal.core.imageUrl
+import cash.p.terminal.entities.CoinValue
 import cash.p.terminal.ui.compose.ComposeAppTheme
-import cash.p.terminal.ui.compose.TranslatableString
 import cash.p.terminal.ui.compose.components.AppBar
-import cash.p.terminal.ui.compose.components.MenuItem
+import cash.p.terminal.ui.compose.components.CoinImage
+import cash.p.terminal.ui.compose.components.HFillSpacer
+import cash.p.terminal.ui.compose.components.HSpacer
+import cash.p.terminal.ui.compose.components.HsBackButton
+import cash.p.terminal.ui.compose.components.VSpacer
+import cash.p.terminal.ui.compose.components.caption_grey
+import cash.p.terminal.ui.compose.components.cell.CellUniversal
+import cash.p.terminal.ui.compose.components.cell.SectionUniversalLawrence
+import cash.p.terminal.ui.compose.components.subhead1_leah
+import cash.p.terminal.ui.compose.components.subhead2_grey
+import cash.p.terminal.ui.compose.components.subhead2_leah
+import io.horizontalsystems.marketkit.models.Token
+import java.math.BigDecimal
 
 class SwapConfirmFragment : BaseComposeFragment() {
     @Composable
@@ -30,23 +51,95 @@ fun SwapConfirmScreen(navController: NavController) {
     )
 
     val uiState = viewModel.uiState
+    val quote = uiState.quote ?: return
 
     Scaffold(
         topBar = {
             AppBar(
                 title = stringResource(R.string.Swap_Confirm_Title),
-                menuItems = listOf(
-                    MenuItem(
-                        title = TranslatableString.ResString(R.string.Button_Close),
-                        icon = R.drawable.ic_close,
-                        onClick = navController::popBackStack
-                    )
-                ),
+                navigationIcon = {
+                    HsBackButton(onClick = navController::popBackStack)
+                },
             )
         },
         backgroundColor = ComposeAppTheme.colors.tyler,
     ) {
-        Column(modifier = Modifier.padding(it)) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .verticalScroll(rememberScrollState())
+        ) {
+            VSpacer(height = 12.dp)
+            SectionUniversalLawrence {
+                TokenRow(quote.tokenIn, quote.amountIn, TokenRowType.In, false)
+                TokenRow(quote.tokenOut, quote.amountOut, TokenRowType.Out)
+            }
+            VSpacer(height = 16.dp)
+            SectionUniversalLawrence {
+                val swapPriceUIHelper = SwapPriceUIHelper(quote.tokenIn, quote.tokenOut, quote.amountIn, quote.amountOut)
+                SwapInfoRow(false, stringResource(id = R.string.Swap_Price), swapPriceUIHelper.priceStr)
+            }
+            VSpacer(height = 16.dp)
+            SectionUniversalLawrence {
+                SwapInfoRow(false, stringResource(id = R.string.FeeSettings_NetworkFee), "")
+            }
+            VSpacer(height = 32.dp)
+        }
+    }
+}
+
+@Composable
+private fun SwapInfoRow(borderTop: Boolean, title: String, value: String) {
+    CellUniversal(borderTop = borderTop) {
+        subhead2_grey(text = title)
+        HFillSpacer(minWidth = 8.dp)
+        subhead1_leah(text = value)
+    }
+}
+
+enum class TokenRowType {
+    In, Out;
+}
+
+@Composable
+private fun TokenRow(
+    token: Token,
+    amount: BigDecimal,
+    type: TokenRowType,
+    borderTop: Boolean = true,
+) {
+    CellUniversal(borderTop = borderTop) {
+        CoinImage(
+            iconUrl = token.coin.imageUrl,
+            placeholder = token.iconPlaceholder,
+            modifier = Modifier.size(32.dp)
+        )
+        HSpacer(width = 16.dp)
+        Column {
+            val title = when (type) {
+                TokenRowType.In -> stringResource(R.string.Send_Confirmation_YouSend)
+                TokenRowType.Out -> stringResource(R.string.Swap_ToAmountTitle)
+            }
+
+            subhead2_leah(text = title)
+            token.badge?.let {
+                VSpacer(height = 1.dp)
+                caption_grey(text = it)
+            }
+        }
+        HFillSpacer(minWidth = 8.dp)
+        Column(horizontalAlignment = Alignment.End) {
+            val color = when (type) {
+                TokenRowType.In -> ComposeAppTheme.colors.leah
+                TokenRowType.Out -> ComposeAppTheme.colors.remus
+            }
+            Text(
+                text = CoinValue(token, amount).getFormattedFull(),
+                style = ComposeAppTheme.typography.subhead1,
+                color = color,
+            )
+            VSpacer(height = 1.dp)
+            caption_grey(text = "$$$")
         }
     }
 }
