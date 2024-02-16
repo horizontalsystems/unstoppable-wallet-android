d8e65482b5849cb62749dd43fe9b421d60d72c86
bakyt <abdrasulov@gmail.com>
2024-02-16 17:34:11 +0600

    Add SwapConfirmViewModel

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swapxxx/SwapConfirmViewModel.kt
@@ -0,0 +1,119 @@
+package io.horizontalsystems.bankwallet.modules.swapxxx
+
+import android.os.CountDownTimer
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SwapConfirmViewModel(private var quote: SwapProviderQuote, private val settings: Map<String, Any?>) : ViewModel() {
+    private var expiresIn = quote.expireAt - System.currentTimeMillis()
+    private var expired = false
+    private var refreshing = false
+
+    var uiState by mutableStateOf(
+        SwapConfirmUiState(
+            quote = quote,
+            expiresIn = expiresIn,
+            expired = expired,
+            refreshing = refreshing
+        )
+    )
+        private set
+
+    private var expirationTimer: CountDownTimer? = null
+
+    init {
+        handleQuote(quote)
+    }
+
+    private fun runExpirationTimer(millisInFuture: Long, onTick: (Long) -> Unit, onFinish: () -> Unit) {
+        viewModelScope.launch {
+            expirationTimer?.cancel()
+            expirationTimer = object : CountDownTimer(millisInFuture, 1000) {
+                override fun onTick(millisUntilFinished: Long) = onTick.invoke(millisUntilFinished)
+                override fun onFinish() = onFinish.invoke()
+            }
+            expirationTimer?.start()
+        }
+    }
+
+    private suspend fun reFetchQuote(quote: SwapProviderQuote): SwapProviderQuote {
+        val tokenIn = quote.tokenIn
+        val tokenOut = quote.tokenOut
+        val amountIn = quote.amountIn
+
+        return SwapProviderQuote(
+            provider = quote.provider,
+            tokenIn = tokenIn,
+            tokenOut = tokenOut,
+            amountIn = amountIn,
+            swapQuote = quote.provider.fetchQuote(tokenIn, tokenOut, amountIn, settings)
+        )
+    }
+
+    private fun emitState() {
+        viewModelScope.launch {
+            uiState = SwapConfirmUiState(
+                quote = quote,
+                expiresIn = expiresIn,
+                expired = expired,
+                refreshing = refreshing
+            )
+        }
+    }
+
+    private fun handleQuote(quote: SwapProviderQuote) {
+        this.quote = quote
+
+        expiresIn = quote.expireAt - System.currentTimeMillis()
+        expired = false
+        emitState()
+
+        runExpirationTimer(
+            millisInFuture = expiresIn,
+            onTick = { millisUntilFinished ->
+                expiresIn = millisUntilFinished
+                emitState()
+            },
+            onFinish = {
+                expired = true
+                emitState()
+            }
+        )
+    }
+
+    override fun onCleared() {
+        expirationTimer?.cancel()
+    }
+
+    fun refresh() {
+        viewModelScope.launch(Dispatchers.Default) {
+            refreshing = true
+            emitState()
+
+            val newQuote = reFetchQuote(quote)
+            refreshing = false
+
+            handleQuote(newQuote)
+        }
+    }
+
+    companion object {
+        fun init(quote: SwapProviderQuote, settings: Map<String, Any?>): CreationExtras.() -> SwapConfirmViewModel = {
+            SwapConfirmViewModel(quote, settings)
+        }
+    }
+}
+
+
+data class SwapConfirmUiState(
+    val quote: SwapProviderQuote,
+    val expiresIn: Long,
+    val expired: Boolean,
+    val refreshing: Boolean,
+)
