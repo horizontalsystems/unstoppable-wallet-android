6d58ba5720f002bbdb4b9a2c5bf7e9b0493c80cf
Rafael <rafaelekol@gmail.com>
2024-02-27 18:09:46 +0600

    Center pin info text

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinTopBlock.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ui/PinTopBlock.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
@@ -51,7 +52,10 @@ fun PinTopBlock(
                     modifier = Modifier.weight(1f),
                     verticalArrangement = Arrangement.Bottom
                 ) {
-                    subhead2_grey(text = title)
+                    subhead2_grey(
+                        text = title,
+                        textAlign = TextAlign.Center
+                    )
                     Spacer(Modifier.height(16.dp))
                 }
 
