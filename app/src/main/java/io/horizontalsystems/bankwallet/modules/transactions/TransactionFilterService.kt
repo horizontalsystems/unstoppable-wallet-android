134de0bcb9352c4a9d3acef1b6157391a2f48ce4
bakyt <abdrasulov@gmail.com>
2024-02-27 19:32:06 +0600

    Reset contact when blockchain is changed
    If selected blockchain is not supported for contact filtering or the selected contact has no address for selected blockchain that the contact will be set to null

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionFilterService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionFilterService.kt
@@ -1,6 +1,7 @@
 package io.horizontalsystems.bankwallet.modules.transactions
 
 import io.horizontalsystems.bankwallet.entities.Wallet
+import io.horizontalsystems.bankwallet.modules.contacts.SelectContactViewModel
 import io.horizontalsystems.bankwallet.modules.contacts.model.Contact
 import io.horizontalsystems.marketkit.models.Blockchain
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -86,6 +87,8 @@ class TransactionFilterService {
         selectedBlockchain = blockchain
         selectedWallet = null
 
+        refreshContact()
+
         emitState()
     }
 
@@ -115,6 +118,17 @@ class TransactionFilterService {
             || selectedBlockchain != null
             || selectedTransactionType != FilterTransactionType.All
             || contact != null
+    }
+
+    private fun refreshContact() {
+        val tmpBlockchain = selectedBlockchain ?: return
+        val tmpContact = contact ?: return
+
+        if (!SelectContactViewModel.supportedBlockchainTypes.contains(tmpBlockchain.type)) {
+            contact = null
+        } else if (tmpContact.addresses.none { it.blockchain.type == tmpBlockchain.type }) {
+            contact = null
+        }
     }
 
     data class State(
