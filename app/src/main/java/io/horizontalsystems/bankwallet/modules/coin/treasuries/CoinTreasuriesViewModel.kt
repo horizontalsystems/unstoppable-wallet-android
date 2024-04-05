72b98fc76dc2002b59f492b5c447d725572c6956
bakyt <abdrasulov@gmail.com>
2024-04-05 13:16:49 +0600

    Replace rxObservable with coroutines

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/treasuries/CoinTreasuriesViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/treasuries/CoinTreasuriesViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.horizontalsystems.bankwallet.core.IAppNumberFormatter
 import io.horizontalsystems.bankwallet.core.logoUrl
-import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.entities.DataState
 import io.horizontalsystems.bankwallet.entities.ViewState
 import io.horizontalsystems.bankwallet.modules.coin.treasuries.CoinTreasuriesModule.CoinTreasuriesData
@@ -14,42 +13,43 @@ import io.horizontalsystems.bankwallet.modules.coin.treasuries.CoinTreasuriesMod
 import io.horizontalsystems.bankwallet.modules.coin.treasuries.CoinTreasuriesModule.TreasuryTypeFilter
 import io.horizontalsystems.bankwallet.ui.compose.Select
 import io.horizontalsystems.marketkit.models.CoinTreasury
-import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.rx2.asFlow
 
 class CoinTreasuriesViewModel(
     private val service: CoinTreasuriesService,
     private val numberFormatter: IAppNumberFormatter
 ) : ViewModel() {
-    private val disposables = CompositeDisposable()
-
     val viewStateLiveData = MutableLiveData<ViewState>(ViewState.Loading)
     val isRefreshingLiveData = MutableLiveData<Boolean>()
     val coinTreasuriesLiveData = MutableLiveData<CoinTreasuriesData>()
     val treasuryTypeSelectorDialogStateLiveData = MutableLiveData<SelectorDialogState>()
 
     init {
-        service.stateObservable
-            .subscribeIO({ state ->
-                when (state) {
-                    is DataState.Success -> {
-                        viewStateLiveData.postValue(ViewState.Success)
-
-                        syncCoinTreasuriesData(state.data)
-                    }
-                    is DataState.Error -> {
-                        viewStateLiveData.postValue(ViewState.Error(state.error))
-                    }
-                    DataState.Loading -> {}
+        viewModelScope.launch {
+            service.stateObservable.asFlow()
+                .catch {
+                    viewStateLiveData.postValue(ViewState.Error(it))
                 }
-            }, {
-                viewStateLiveData.postValue(ViewState.Error(it))
-            }).let {
-                disposables.add(it)
-            }
+                .collect { state ->
+                    handleServiceState(state)
+                }
+        }
 
         service.start()
+    }
+
+    private fun handleServiceState(state: DataState<List<CoinTreasury>>) {
+        state.dataOrNull?.let {
+            viewStateLiveData.postValue(ViewState.Success)
+            syncCoinTreasuriesData(it)
+        }
+
+        state.errorOrNull?.let {
+            viewStateLiveData.postValue(ViewState.Error(it))
+        }
     }
 
     fun refresh() {
@@ -80,7 +80,6 @@ class CoinTreasuriesViewModel(
     }
 
     override fun onCleared() {
-        disposables.clear()
         service.stop()
     }
 
