b217f40bef0dded607c1d9eddf2a40211136c073
Rafael <rafaelekol@gmail.com>
2024-04-12 12:23:05 +0600

    Remove rates from Coin Markets

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/coinmarkets/CoinMarketsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/coinmarkets/CoinMarketsViewModel.kt
@@ -8,12 +8,10 @@ import io.horizontalsystems.bankwallet.core.subscribeIO
 import io.horizontalsystems.bankwallet.entities.DataState
 import io.horizontalsystems.bankwallet.entities.ViewState
 import io.horizontalsystems.bankwallet.modules.coin.MarketTickerViewItem
-import io.horizontalsystems.bankwallet.modules.coin.coinmarkets.CoinMarketsModule.VolumeMenuType
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.reactivex.disposables.CompositeDisposable
 
 class CoinMarketsViewModel(private val service: CoinMarketsService) : ViewModel() {
-    val volumeMenu by service::volumeMenu
     val verifiedMenu by service::verifiedMenu
     val viewStateLiveData = MutableLiveData<ViewState>(ViewState.Loading)
     val viewItemsLiveData = MutableLiveData<List<MarketTickerViewItem>>()
@@ -45,21 +43,11 @@ class CoinMarketsViewModel(private val service: CoinMarketsService) : ViewModel(
             item.market,
             item.marketImageUrl,
             "${item.baseCoinCode}/${item.targetCoinCode}",
-            App.numberFormatter.formatCoinFull(item.rate, item.targetCoinCode, 8),
-            getVolume(item),
+            App.numberFormatter.formatFiatShort(item.volumeFiat, service.currency.symbol, service.currency.decimal),
+            App.numberFormatter.formatCoinShort(item.volumeToken, item.baseCoinCode, 8),
             item.tradeUrl,
             if (item.verified) TranslatableString.ResString(R.string.CoinPage_MarketsLabel_Verified) else null
         )
-    }
-
-    private fun getVolume(item: MarketTickerItem) = when (item.volumeType) {
-        is VolumeMenuType.Coin -> {
-            App.numberFormatter.formatCoinShort(item.volume, item.baseCoinCode, 8)
-        }
-        is VolumeMenuType.Currency -> {
-            val currency = service.currency
-            App.numberFormatter.formatFiatShort(item.volume, currency.symbol, currency.decimal)
-        }
     }
 
     override fun onCleared() {
@@ -75,7 +63,4 @@ class CoinMarketsViewModel(private val service: CoinMarketsService) : ViewModel(
         service.setVerifiedType(verifiedType)
     }
 
-    fun toggleVolumeType(volumeMenuType: VolumeMenuType) {
-        service.setVolumeType(volumeMenuType)
-    }
 }
