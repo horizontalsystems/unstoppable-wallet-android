cbffbde97f8a928f8ffde991a31d379ea7363776
Rafael <rafaelekol@gmail.com>
2024-05-08 12:28:32 +0600

    Update QR code generation

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/helpers/TextHelper.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/helpers/TextHelper.kt
@@ -3,12 +3,6 @@ package io.horizontalsystems.bankwallet.ui.helpers
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.graphics.Bitmap
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.EncodeHintType
-import com.google.zxing.MultiFormatWriter
-import com.google.zxing.WriterException
-import com.journeyapps.barcodescanner.BarcodeEncoder
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.IClipboardManager
 
@@ -29,21 +23,6 @@ object TextHelper : IClipboardManager {
                 null
             }
         } ?: ""
-    }
-
-    fun getQrCodeBitmap(address: String, size: Float = 150F): Bitmap? {
-        val multiFormatWriter = MultiFormatWriter()
-        return try {
-            val imageSize = LayoutHelper.dp(size, App.instance)
-            val hints = hashMapOf(EncodeHintType.MARGIN to 0, EncodeHintType.ERROR_CORRECTION to "H")
-            val bitMatrix = multiFormatWriter.encode(address, BarcodeFormat.QR_CODE, imageSize, imageSize, hints)
-            val barcodeEncoder = BarcodeEncoder()
-            val bitmap = barcodeEncoder.createBitmap(bitMatrix)
-            bitmap
-        } catch (e: WriterException) {
-            e.printStackTrace()
-            null
-        }
     }
 
     fun getCleanedUrl(link: String): String{
