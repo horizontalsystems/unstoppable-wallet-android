0fa9d9e22ef70bddd924172c87c017261e288f64
Rafael <rafaelekol@gmail.com>
2024-05-08 12:28:32 +0600

    Update QR code generation

--- app/src/main/java/cash/p/terminal/modules/receive/ui/ReceiveAddressScreen.kt
+++ app/src/main/java/cash/p/terminal/modules/receive/ui/ReceiveAddressScreen.kt
@@ -57,6 +57,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
+import io.github.alexzhirkevich.qrose.options.QrBallShape
+import io.github.alexzhirkevich.qrose.options.QrErrorCorrectionLevel
+import io.github.alexzhirkevich.qrose.options.QrFrameShape
+import io.github.alexzhirkevich.qrose.options.QrLogoPadding
+import io.github.alexzhirkevich.qrose.options.QrLogoShape
+import io.github.alexzhirkevich.qrose.options.QrPixelShape
+import io.github.alexzhirkevich.qrose.options.roundCorners
+import io.github.alexzhirkevich.qrose.rememberQrCodePainter
 import cash.p.terminal.R
 import cash.p.terminal.core.UsedAddress
 import cash.p.terminal.core.stats.StatEntity
@@ -163,7 +171,6 @@ fun ReceiveAddressScreen(
                             }
 
                             ViewState.Success -> {
-                                val qrCodeBitmap = TextHelper.getQrCodeBitmap(uiState.uri)
                                 Column(
                                     modifier = Modifier
                                         .weight(1f)
@@ -188,9 +195,15 @@ fun ReceiveAddressScreen(
                                                 .fillMaxWidth()
                                                 .clickable {
                                                     TextHelper.copyText(uiState.uri)
-                                                    HudHelper.showSuccessMessage(localView, R.string.Hud_Text_Copied)
+                                                    HudHelper.showSuccessMessage(
+                                                        localView,
+                                                        R.string.Hud_Text_Copied
+                                                    )
 
-                                                    stat(page = StatPage.Receive, event = StatEvent.Copy(StatEntity.ReceiveAddress))
+                                                    stat(
+                                                        page = StatPage.Receive,
+                                                        event = StatEvent.Copy(StatEntity.ReceiveAddress)
+                                                    )
                                                 },
                                             horizontalAlignment = Alignment.CenterHorizontally,
                                         ) {
@@ -202,32 +215,7 @@ fun ReceiveAddressScreen(
                                                     .size(224.dp),
                                                 contentAlignment = Alignment.Center
                                             ) {
-                                                qrCodeBitmap?.let { qrCode ->
-                                                    Image(
-                                                        modifier = Modifier
-                                                            .padding(8.dp)
-                                                            .fillMaxSize(),
-                                                        bitmap = qrCode.asImageBitmap(),
-                                                        contentScale = ContentScale.FillWidth,
-                                                        contentDescription = null
-                                                    )
-                                                    Box(
-                                                        modifier = Modifier
-                                                            .clip(RoundedCornerShape(8.dp))
-                                                            .background(ComposeAppTheme.colors.white)
-                                                            .size(64.dp),
-                                                        contentAlignment = Alignment.Center
-                                                    ) {
-                                                        Image(
-                                                            modifier = Modifier.size(48.dp),
-                                                            painter = adaptiveIconPainterResource(
-                                                                id = R.mipmap.launcher_main,
-                                                                fallbackDrawable = R.drawable.launcher_main_preview
-                                                            ),
-                                                            contentDescription = null
-                                                        )
-                                                    }
-                                                }
+                                                QrCodeImage(uiState.uri)
                                             }
                                             VSpacer(12.dp)
                                             subhead2_leah(
@@ -316,6 +304,39 @@ fun ReceiveAddressScreen(
             }
         }
     }
+}
+
+@Composable
+private fun QrCodeImage(address: String) {
+    val logoPainter: Painter =
+        adaptiveIconPainterResource(
+            id = R.mipmap.launcher_main,
+            fallbackDrawable = R.drawable.launcher_main_preview
+        )
+    val qrcodePainter: Painter =
+        rememberQrCodePainter(address) {
+            errorCorrectionLevel = QrErrorCorrectionLevel.Medium
+            logo {
+                painter = logoPainter
+                padding = QrLogoPadding.Natural(.3f)
+                shape = QrLogoShape.roundCorners(0.8f)
+                size = 0.2f
+            }
+
+            shapes {
+                ball = QrBallShape.roundCorners(.25f)
+                darkPixel = QrPixelShape.roundCorners()
+                frame = QrFrameShape.roundCorners(.25f)
+            }
+        }
+    Image(
+        painter = qrcodePainter,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        contentScale = ContentScale.FillWidth,
+        contentDescription = null
+    )
 }
 
 @Composable
