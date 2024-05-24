b0281dd399cc9d75799b2cccb3e092bf029ce246
Rafael <rafaelekol@gmail.com>
2024-05-24 15:29:03 +0600

    Update Markets data when base currency changed

--- app/src/main/java/io/horizontalsystems/bankwallet/core/managers/CurrencyManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/managers/CurrencyManager.kt
@@ -4,8 +4,20 @@ import io.horizontalsystems.bankwallet.core.ILocalStorage
 import io.horizontalsystems.bankwallet.core.providers.AppConfigProvider
 import io.horizontalsystems.bankwallet.entities.Currency
 import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class CurrencyManager(private val localStorage: ILocalStorage, private val appConfigProvider: AppConfigProvider) {
+
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    private val _baseCurrencyUpdatedFlow: MutableSharedFlow<Unit> = MutableSharedFlow()
+    val baseCurrencyUpdatedFlow: SharedFlow<Unit> = _baseCurrencyUpdatedFlow.asSharedFlow()
 
     var baseCurrency = getInitialCurrency()
         set(value) {
@@ -13,6 +25,9 @@ class CurrencyManager(private val localStorage: ILocalStorage, private val appCo
 
             localStorage.baseCurrencyCode = value.code
             baseCurrencyUpdatedSignal.onNext(Unit)
+            scope.launch {
+                _baseCurrencyUpdatedFlow.emit(Unit)
+            }
         }
 
     private val defaultCurrency: Currency
