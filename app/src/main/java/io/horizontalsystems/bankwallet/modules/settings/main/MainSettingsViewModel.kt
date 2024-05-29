a9d32aa159cef40d9890fee482ad5c32d75829c4
Rafael <rafaelekol@gmail.com>
2024-05-29 15:16:09 +0600

    Move currency and language to Main Settings
    - Refactor MainSettings ViewModel

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/main/MainSettingsViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/main/MainSettingsViewModel.kt
@@ -1,80 +1,152 @@
 package io.horizontalsystems.bankwallet.modules.settings.main
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.core.IAccountManager
+import io.horizontalsystems.bankwallet.core.IBackupManager
+import io.horizontalsystems.bankwallet.core.ITermsManager
+import io.horizontalsystems.bankwallet.core.ViewModelUiState
+import io.horizontalsystems.bankwallet.core.managers.CurrencyManager
+import io.horizontalsystems.bankwallet.core.managers.LanguageManager
+import io.horizontalsystems.bankwallet.core.providers.AppConfigProvider
+import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.bankwallet.modules.settings.main.MainSettingsModule.CounterType
 import io.horizontalsystems.bankwallet.modules.walletconnect.WCManager
+import io.horizontalsystems.bankwallet.modules.walletconnect.WCSessionManager
+import io.horizontalsystems.core.IPinComponent
+import io.horizontalsystems.core.ISystemInfoManager
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.rx2.asFlow
 
 class MainSettingsViewModel(
-    private val service: MainSettingsService,
-    val companyWebPage: String
-) : ViewModel() {
+    private val backupManager: IBackupManager,
+    private val systemInfoManager: ISystemInfoManager,
+    private val termsManager: ITermsManager,
+    private val pinComponent: IPinComponent,
+    private val wcSessionManager: WCSessionManager,
+    private val wcManager: WCManager,
+    private val accountManager: IAccountManager,
+    private val appConfigProvider: AppConfigProvider,
+    private val languageManager: LanguageManager,
+    private val currencyManager: CurrencyManager,
+) : ViewModelUiState<MainSettingUiState>() {
 
-    val manageWalletShowAlertLiveData = MutableLiveData(shouldShowAlertForManageWallet(service.allBackedUp, service.hasNonStandardAccount))
-    val securityCenterShowAlertLiveData = MutableLiveData(!service.isPinSet)
-    val aboutAppShowAlertLiveData = MutableLiveData(!service.termsAccepted)
-    val wcCounterLiveData = MutableLiveData<CounterType?>(null)
-    val appVersion by service::appVersion
-    val appWebPageLink by service::appWebPageLink
+    val appVersion: String
+        get() {
+            var appVersion = systemInfoManager.appVersion
+            if (Translator.getString(R.string.is_release) == "false") {
+                appVersion += " (${appConfigProvider.appBuild})"
+            }
 
-    private var wcSessionsCount = service.walletConnectSessionCount
+            return appVersion
+        }
+
+    val companyWebPage = appConfigProvider.companyWebPageLink
+
+    val walletConnectSupportState: WCManager.SupportState
+        get() = wcManager.getWalletConnectSupportState()
+
+    private val currentLanguageDisplayName: String
+        get() = languageManager.currentLanguageName
+
+    private val baseCurrencyCode: String
+        get() = currencyManager.baseCurrency.code
+
+    private val appWebPageLink = appConfigProvider.appWebPageLink
+    private val hasNonStandardAccount: Boolean
+        get() = accountManager.hasNonStandardAccount
+
+    private val allBackedUp: Boolean
+        get() = backupManager.allBackedUp
+
+    private val walletConnectSessionCount: Int
+        get() = wcSessionManager.sessions.count()
+
+    private val isPinSet: Boolean
+        get() = pinComponent.isPinSet
+
+
+    private var wcCounterType: CounterType? = null
+    private var wcSessionsCount = walletConnectSessionCount
     private var wcPendingRequestCount = 0
 
     init {
         viewModelScope.launch {
-            service.termsAcceptedFlow.collect {
-                aboutAppShowAlertLiveData.postValue(!it)
+            backupManager.allBackedUpFlowable.asFlow().collect {
+                emitState()
             }
         }
         viewModelScope.launch {
-            service.backedUpObservable.asFlow().collect {
-                manageWalletShowAlertLiveData.postValue(shouldShowAlertForManageWallet(it, service.hasNonStandardAccount))
-            }
-        }
-        viewModelScope.launch {
-            service.pinSetObservable.asFlow().collect {
-                securityCenterShowAlertLiveData.postValue(!it)
-            }
-        }
-        viewModelScope.launch {
-            service.walletConnectSessionCountObservable.asFlow().collect {
-                wcSessionsCount = it
+            wcSessionManager.sessionsFlow.collect {
+                wcSessionsCount = walletConnectSessionCount
                 syncCounter()
             }
         }
         viewModelScope.launch {
-            service.pendingRequestCountFlow.collect {
+            pinComponent.pinSetFlowable.asFlow().collect {
+                emitState()
+            }
+        }
+
+        viewModelScope.launch {
+            termsManager.termsAcceptedSignalFlow.collect {
+                emitState()
+            }
+        }
+
+        viewModelScope.launch {
+            wcSessionManager.pendingRequestCountFlow.collect {
                 wcPendingRequestCount = it
                 syncCounter()
             }
         }
+        viewModelScope.launch {
+            currencyManager.baseCurrencyUpdatedSignal.asFlow().collect {
+                emitState()
+            }
+        }
         syncCounter()
-        service.start()
     }
 
-    private fun shouldShowAlertForManageWallet(allBackedUp: Boolean, hasNonStandardAccount: Boolean): Boolean {
-        return !allBackedUp || hasNonStandardAccount
-    }
-    // ViewModel
-
-    override fun onCleared() {
-        service.stop()
-    }
-
-    fun getWalletConnectSupportState(): WCManager.SupportState {
-        return service.getWalletConnectSupportState()
+    override fun createState(): MainSettingUiState {
+        return MainSettingUiState(
+            currentLanguage = currentLanguageDisplayName,
+            baseCurrencyCode = baseCurrencyCode,
+            appWebPageLink = appWebPageLink,
+            hasNonStandardAccount = hasNonStandardAccount,
+            allBackedUp = allBackedUp,
+            pendingRequestCount = wcPendingRequestCount,
+            walletConnectSessionCount = wcSessionsCount,
+            manageWalletShowAlert = !allBackedUp || hasNonStandardAccount,
+            securityCenterShowAlert = !isPinSet,
+            aboutAppShowAlert = !termsManager.allTermsAccepted,
+            wcCounterType = wcCounterType
+        )
     }
 
     private fun syncCounter() {
         if (wcPendingRequestCount > 0) {
-            wcCounterLiveData.postValue(CounterType.PendingRequestCounter(wcPendingRequestCount))
+            wcCounterType = CounterType.PendingRequestCounter(wcPendingRequestCount)
         } else if (wcSessionsCount > 0) {
-            wcCounterLiveData.postValue(CounterType.SessionCounter(wcSessionsCount))
+            wcCounterType = CounterType.SessionCounter(wcSessionsCount)
         } else {
-            wcCounterLiveData.postValue(null)
+            wcCounterType = null
         }
+        emitState()
     }
 }
+
+data class MainSettingUiState(
+    val currentLanguage: String,
+    val baseCurrencyCode: String,
+    val appWebPageLink: String,
+    val hasNonStandardAccount: Boolean,
+    val allBackedUp: Boolean,
+    val pendingRequestCount: Int,
+    val walletConnectSessionCount: Int,
+    val manageWalletShowAlert: Boolean,
+    val securityCenterShowAlert: Boolean,
+    val aboutAppShowAlert: Boolean,
+    val wcCounterType: CounterType?
+)
