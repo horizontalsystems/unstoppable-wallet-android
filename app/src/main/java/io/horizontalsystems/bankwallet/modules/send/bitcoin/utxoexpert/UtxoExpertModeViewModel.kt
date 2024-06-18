38440cbb13ec64958defe3c1aeff913bd6434405
Rafael <rafaelekol@gmail.com>
2024-06-18 15:58:50 +0600

    Update UtxoExpertMode page

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/utxoexpert/UtxoExpertModeViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/bitcoin/utxoexpert/UtxoExpertModeViewModel.kt
@@ -7,9 +7,7 @@ import cash.z.ecc.android.sdk.ext.collectWith
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.ISendBitcoinAdapter
 import io.horizontalsystems.bankwallet.core.ViewModelUiState
-import io.horizontalsystems.bankwallet.core.shorten
 import io.horizontalsystems.bankwallet.core.toHexString
-import io.horizontalsystems.bankwallet.entities.Address
 import io.horizontalsystems.bankwallet.modules.xrate.XRateService
 import io.horizontalsystems.bitcoincore.storage.UnspentOutputInfo
 import io.horizontalsystems.core.helpers.DateHelper
@@ -20,30 +18,16 @@ import java.util.Date
 class UtxoExpertModeViewModel(
     private val adapter: ISendBitcoinAdapter,
     private val token: Token,
-    private val address: Address?,
-    private val memo: String?,
-    private val feeRate: Int?,
-    initialValue: BigDecimal?,
     initialCustomUnspentOutputs: List<UnspentOutputInfo>?,
     xRateService: XRateService,
 ) : ViewModelUiState<UtxoExpertModeModule.UiState>() {
 
-    private var value = initialValue ?: BigDecimal.ZERO
     private var unspentOutputViewItems = listOf<UtxoExpertModeModule.UnspentOutputViewItem>()
     private var selectedUnspentOutputs = listOf<String>()
     private var coinRate = xRateService.getRate(token.coin.uid)
-    private var sendToInfo = UtxoExpertModeModule.InfoItem(
-        subTitle = address?.hex?.shorten(),
-        value = App.numberFormatter.formatCoinFull(value, token.coin.code, token.decimals),
-        subValue = coinRate?.let { rate ->
-            rate.copy(value = value.times(rate.value)).getFormattedFull()
-        } ?: "",
-    )
-    private var changeInfo: UtxoExpertModeModule.InfoItem? = null
-    private var feeInfo = UtxoExpertModeModule.InfoItem(
-        subTitle = null,
-        value = null,
-        subValue = null,
+    private var availableBalanceInfo = UtxoExpertModeModule.InfoItem(
+        value = App.numberFormatter.formatCoinFull(BigDecimal.ZERO, token.coin.code, token.decimals),
+        subValue = "",
     )
 
     val customOutputs: List<UnspentOutputInfo>
@@ -57,73 +41,33 @@ class UtxoExpertModeViewModel(
             coinRate = it
             setUnspentOutputViewItems()
         }
+        setAvailableBalanceInfo()
         setUnspentOutputViewItems()
-        if (selectedUnspentOutputs.isNotEmpty()) {
-            updateFee()
-        }
         emitState()
     }
 
     override fun createState() = UtxoExpertModeModule.UiState(
-        sendToInfo = sendToInfo,
-        changeInfo = changeInfo,
-        feeInfo = feeInfo,
+        availableBalanceInfo = availableBalanceInfo,
         utxoItems = unspentOutputViewItems,
+        unselectAllIsEnabled = selectedUnspentOutputs.isNotEmpty(),
     )
 
     private fun getUnspentId(unspentOutputInfo: UnspentOutputInfo) = "${unspentOutputInfo.transactionHash.toHexString()}-${unspentOutputInfo.outputIndex}"
 
-    fun onUnspentOutputClicked(id: String) {
-        selectedUnspentOutputs = if (selectedUnspentOutputs.contains(id)) {
-            selectedUnspentOutputs.filter { it != id }
-        } else {
-            selectedUnspentOutputs + id
+    private fun setAvailableBalanceInfo() {
+        var totalCoinValue = BigDecimal.ZERO
+        adapter.unspentOutputs.map { utxo ->
+            val utxoId = getUnspentId(utxo)
+            if (selectedUnspentOutputs.isEmpty() || selectedUnspentOutputs.contains(utxoId)) {
+                totalCoinValue += utxo.value.toBigDecimal().movePointLeft(token.decimals)
+            }
         }
-        unspentOutputViewItems = unspentOutputViewItems.map { utxo ->
-            utxo.copy(selected = selectedUnspentOutputs.contains(utxo.id))
-        }
-        updateFee()
-        emitState()
-    }
-
-    private fun updateFee() {
-        val feeRateNonNull = feeRate ?: return
-        val bitcoinFeeInfo = adapter.bitcoinFeeInfo(
-            amount = value,
-            feeRate = feeRateNonNull,
-            address = address?.hex,
-            memo = memo,
-            unspentOutputs = customOutputs,
-            pluginData = null,
-        )
-        val fee = bitcoinFeeInfo?.fee
-        if (fee == null) {
-            feeInfo = feeInfo.copy(
-                value = null,
-                subValue = null,
-            )
-            changeInfo = null
-            return
-        }
-        feeInfo = feeInfo.copy(
-            value = App.numberFormatter.formatCoinFull(fee, token.coin.code, token.decimals),
+        availableBalanceInfo = availableBalanceInfo.copy(
+            value = App.numberFormatter.formatCoinFull(totalCoinValue, token.coin.code, token.decimals),
             subValue = coinRate?.let { rate ->
-                rate.copy(value = fee.times(rate.value)).getFormattedFull()
+                rate.copy(value = totalCoinValue.times(rate.value)).getFormattedFull()
             } ?: "",
         )
-        changeInfo = if (bitcoinFeeInfo.changeAddress != null && bitcoinFeeInfo.changeValue != null) {
-            val value = bitcoinFeeInfo.changeValue
-            UtxoExpertModeModule.InfoItem(
-                subTitle = bitcoinFeeInfo.changeAddress.stringValue.shorten(),
-                value = App.numberFormatter.formatCoinFull(value, token.coin.code, token.decimals),
-                subValue = coinRate?.let { rate ->
-                    rate.copy(value = value.times(rate.value)).getFormattedFull()
-                } ?: "",
-            )
-        } else {
-            null
-        }
-        emitState()
     }
 
     private fun setUnspentOutputViewItems() {
@@ -144,6 +88,37 @@ class UtxoExpertModeViewModel(
         }
     }
 
+    private fun updateUtxoSelectedState() {
+        unspentOutputViewItems = unspentOutputViewItems.map { utxo ->
+            utxo.copy(selected = selectedUnspentOutputs.contains(utxo.id))
+        }
+    }
+
+    fun onUnspentOutputClicked(id: String) {
+        selectedUnspentOutputs = if (selectedUnspentOutputs.contains(id)) {
+            selectedUnspentOutputs.filter { it != id }
+        } else {
+            selectedUnspentOutputs + id
+        }
+        updateUtxoSelectedState()
+        setAvailableBalanceInfo()
+        emitState()
+    }
+
+    fun unselectAll() {
+        selectedUnspentOutputs = listOf()
+        updateUtxoSelectedState()
+        setAvailableBalanceInfo()
+        emitState()
+    }
+
+    fun selectAll() {
+        selectedUnspentOutputs = unspentOutputViewItems.map { it.id }
+        updateUtxoSelectedState()
+        setAvailableBalanceInfo()
+        emitState()
+    }
+
 }
 
 object UtxoExpertModeModule {
@@ -152,20 +127,12 @@ object UtxoExpertModeModule {
     class Factory(
         private val adapter: ISendBitcoinAdapter,
         private val token: Token,
-        private val address: Address?,
-        private val memo: String?,
-        private val value: BigDecimal?,
-        private val feeRate: Int?,
         private val customUnspentOutputs: List<UnspentOutputInfo>?,
     ) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             return UtxoExpertModeViewModel(
                 adapter = adapter,
                 token = token,
-                address = address,
-                memo = memo,
-                feeRate = feeRate,
-                initialValue = value,
                 initialCustomUnspentOutputs = customUnspentOutputs,
                 xRateService = XRateService(App.marketKit, App.currencyManager.baseCurrency)
             ) as T
@@ -173,14 +140,12 @@ object UtxoExpertModeModule {
     }
 
     data class UiState(
-        val sendToInfo: InfoItem,
-        val changeInfo: InfoItem?,
-        val feeInfo: InfoItem,
+        val availableBalanceInfo: InfoItem,
         val utxoItems: List<UnspentOutputViewItem>,
+        val unselectAllIsEnabled: Boolean,
     )
 
     data class InfoItem(
-        val subTitle: String?,
         val value: String?,
         val subValue: String?,
     )
