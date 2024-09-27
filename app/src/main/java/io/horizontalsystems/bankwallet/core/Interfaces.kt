eeb1ea8efbfd3a8a3dd3ed4d80b488e3241ca06a
abdrasulov <abdrasulov@gmail.com>
2024-09-27 15:59:15 +0600

    Add info for ZCash pending balance

--- app/src/main/java/io/horizontalsystems/bankwallet/core/Interfaces.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/Interfaces.kt
@@ -300,9 +300,10 @@ interface IBalanceAdapter {
 data class BalanceData(
     val available: BigDecimal,
     val timeLocked: BigDecimal = BigDecimal.ZERO,
-    val notRelayed: BigDecimal = BigDecimal.ZERO
+    val notRelayed: BigDecimal = BigDecimal.ZERO,
+    val pending: BigDecimal = BigDecimal.ZERO,
 ) {
-    val total get() = available + timeLocked + notRelayed
+    val total get() = available + timeLocked + notRelayed + pending
 }
 
 interface IReceiveAdapter {
