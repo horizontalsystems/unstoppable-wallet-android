b467bef1489d5ee6de64dfa670a6652b8f40e978
abdrasulov <abdrasulov@gmail.com>
2024-12-17 18:33:48 +0600

    Add predefined subscriptions

--- subscriptions-core/src/main/java/io/horizontalsystems/subscriptions/core/Subscription.kt
+++ subscriptions-core/src/main/java/io/horizontalsystems/subscriptions/core/Subscription.kt
@@ -1,6 +1,11 @@
 package io.horizontalsystems.subscriptions.core
 
-data class Subscription(val id: String, val name: String, val description: String)
+data class Subscription(
+    val id: String,
+    val name: String,
+    val description: String,
+    val actions: List<IPaidAction>
+)
 
 data class BasePlan(
     val id: String,
--- subscriptions-core/src/main/java/io/horizontalsystems/subscriptions/core/SubscriptionService.kt
+++ subscriptions-core/src/main/java/io/horizontalsystems/subscriptions/core/SubscriptionService.kt
@@ -3,6 +3,8 @@ package io.horizontalsystems.subscriptions.core
 import android.app.Activity
 
 interface SubscriptionService {
+    var predefinedSubscriptions: List<Subscription>
+
     fun isActionAllowed(paidAction: IPaidAction): Boolean
     suspend fun getSubscriptions(): List<Subscription>
     suspend fun launchPurchaseFlow(subscriptionId: String, planId: String, activity: Activity): HSPurchase?
