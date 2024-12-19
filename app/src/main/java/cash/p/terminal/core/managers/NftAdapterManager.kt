b6a87615b84bb42f73b39aa4826ca74070a79281
Rafael <rafaelekol@gmail.com>
2024-12-19 12:30:33 +0600

    Disable adapters initialization for NFT

--- app/src/main/java/cash/p/terminal/core/managers/NftAdapterManager.kt
+++ app/src/main/java/cash/p/terminal/core/managers/NftAdapterManager.kt
@@ -28,7 +28,8 @@ class NftAdapterManager(
         coroutineScope.launch {
             walletManager.activeWalletsUpdatedObservable.asFlow()
                 .collect {
-                    initAdapters(it)
+                    //disable NFT adapters for now
+                    //initAdapters(it)
                 }
         }
     }
