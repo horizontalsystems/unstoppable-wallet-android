dab5291db355c662b9b95e0f75a41b2044da5fd9
Rafael <rafaelekol@gmail.com>
2025-01-23 15:22:07 +0600

    Apply UX changes to Sectors/Blockchains selectors in MarketFilters

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/filters/BlockchainsSelectorFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/filters/BlockchainsSelectorFragment.kt
@@ -3,6 +3,7 @@ package io.horizontalsystems.bankwallet.modules.market.filters
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -19,9 +20,12 @@ import androidx.navigation.NavController
 import androidx.navigation.navGraphViewModels
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
+import io.horizontalsystems.bankwallet.modules.evmfee.ButtonsGroupWithShade
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
-import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
+import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
+import io.horizontalsystems.bankwallet.ui.compose.components.MenuItem
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
 import io.horizontalsystems.bankwallet.ui.compose.components.body_grey
 import io.horizontalsystems.bankwallet.ui.compose.components.cell.CellBlockchainChecked
@@ -69,39 +73,73 @@ private fun FilterByBlockchainsScreen(
             AppBar(
                 title = stringResource(R.string.Market_Filter_Blockchains),
                 navigationIcon = {
-                    HsBackButton(onClick = { navController.popBackStack() })
-                }
+                    Icon(
+                        modifier = Modifier.padding(start = 24.dp),
+                        painter = painterResource(id = R.drawable.ic_blocks_24),
+                        contentDescription = null,
+                        tint = ComposeAppTheme.colors.jacob
+                    )
+                },
+                menuItems = listOf(
+                    MenuItem(
+                        title = TranslatableString.ResString(R.string.Button_Close),
+                        icon = R.drawable.ic_close,
+                        onClick = navController::popBackStack
+                    )
+                ),
             )
         },
         backgroundColor = ComposeAppTheme.colors.tyler,
     ) {
         Column(
-            modifier = Modifier
-                .padding(it)
-                .verticalScroll(rememberScrollState())
+            modifier = Modifier.padding(it)
         ) {
-            VSpacer(height = 12.dp)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                VSpacer(height = 12.dp)
 
-            SectionUniversalLawrence {
-                AnyCell(
-                    checked = uiState.selectedBlockchains.isEmpty(),
-                    onClick = { viewModel.anyBlockchains() }
-                )
-                uiState.blockchainOptions.forEach { item ->
-                    CellBlockchainChecked(
-                        blockchain = item.blockchain,
-                        checked = item.checked
-                    ) {
-                        if (item.checked) {
-                            viewModel.onBlockchainUncheck(item.blockchain)
-                        } else {
-                            viewModel.onBlockchainCheck(item.blockchain)
+                SectionUniversalLawrence {
+                    AnyCell(
+                        checked = uiState.selectedBlockchains.isEmpty(),
+                        onClick = { viewModel.anyBlockchains() }
+                    )
+                    uiState.blockchainOptions.forEach { item ->
+                        CellBlockchainChecked(
+                            blockchain = item.blockchain,
+                            checked = item.checked
+                        ) {
+                            if (item.checked) {
+                                viewModel.onBlockchainUncheck(item.blockchain)
+                            } else {
+                                viewModel.onBlockchainCheck(item.blockchain)
+                            }
                         }
                     }
                 }
-            }
 
-            VSpacer(height = 32.dp)
+                VSpacer(height = 32.dp)
+            }
+            ButtonsGroupWithShade {
+                ButtonPrimaryYellow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    title = if (uiState.selectedBlockchains.isEmpty()) {
+                        stringResource(R.string.Button_Done)
+                    } else {
+                        stringResource(
+                            R.string.Market_Filters_Select,
+                            uiState.selectedBlockchains.size
+                        )
+                    },
+                    onClick = {
+                        navController.popBackStack()
+                    },
+                )
+            }
         }
     }
 }
