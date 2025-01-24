53c0a062134c8e2cc5bba16ae991ba6f508c352c
abdrasulov <abdrasulov@gmail.com>
2025-01-24 17:19:07 +0600

    Fetch address list from ViewModel

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/send/address/EnterAddressFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/send/address/EnterAddressFragment.kt
@@ -82,39 +82,12 @@ fun EnterAddressScreen(navController: NavController, input: EnterAddressFragment
         factory = AddressParserModule.Factory(wallet.token, prefilledData?.amount)
     )
 
-    val onNext = {
-        navController.slideFromRight(
-            R.id.sendXFragment,
-            SendFragment.Input(
-                wallet = wallet,
-                sendEntryPointDestId = R.id.sendTokenSelectFragment,
-                title = input.title,
-                predefinedAddress = input.predefinedAddress,
-                prefilledAddressData = prefilledData,
-            )
-        )
-    }
-
     val uiState = viewModel.uiState
     val addressError = uiState.addressError
-    val recent: String? = "1EzZFZhopU4vBJKLiq5kUKphXZ4M22M1QjmEdfk"
     val addressErrorMessage: ErrorMessage? = ErrorMessage(
         title = stringResource(R.string.Send_Address_ErrorMessage_PhishingDetected),
         description = stringResource(R.string.Send_Address_ErrorMessage_PhishingDetected_Description)
     )
-    val contacts = listOf(
-        SContact("My Wallet", "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"),
-        SContact("TalgatETH", "0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5"),
-        SContact("Esso", "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"),
-        SContact("Escobar", "0x2B5AD5c4795c026514f8317c7a215E218DcCD6cF"),
-        SContact("Vitalik", "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B"),
-        SContact("Binance_1", "0x28C6c06298d514Db089934071355E5743bf21d60"),
-        SContact("Kraken_Hot", "0x2910543Af39abA0Cd09dBb2D50200b3E800A63D2"),
-        SContact("FTX_Exploiter", "0x59ABf3837Fa962d6853b4Cc0a19513AA031fd32b"),
-        SContact("Coinbase_2", "0x503828976D22510aad0201ac7EC88293211D23Da"),
-        SContact("Metamask_DEV", "0x9696f59E4d72E237BE84fFD425DCaD154Bf96976")
-    )
-
     Scaffold(
         backgroundColor = ComposeAppTheme.colors.tyler,
         topBar = {
@@ -148,7 +121,9 @@ fun EnterAddressScreen(navController: NavController, input: EnterAddressFragment
                     viewModel.onEnterAddress(it)
                 }
                 if (uiState.address == null) {
-                    AddressSuggestions(recent, contacts)
+                    AddressSuggestions(uiState.recentAddress, uiState.contacts) {
+                        viewModel.onEnterAddress(Address(it))
+                    }
                 } else {
                     AddressCheck(false, addressErrorMessage)
                 }
@@ -159,7 +134,18 @@ fun EnterAddressScreen(navController: NavController, input: EnterAddressFragment
                         .fillMaxWidth()
                         .padding(start = 16.dp, end = 16.dp),
                     title = stringResource(R.string.Button_Next),
-                    onClick = onNext,
+                    onClick = {
+                        navController.slideFromRight(
+                            R.id.sendXFragment,
+                            SendFragment.Input(
+                                wallet = wallet,
+                                sendEntryPointDestId = R.id.sendTokenSelectFragment,
+                                title = input.title,
+                                predefinedAddress = input.predefinedAddress,
+                                prefilledAddressData = prefilledData,
+                            )
+                        )
+                    },
                     enabled = uiState.canBeSendToAddress
                 )
             }
@@ -299,10 +285,7 @@ fun CheckLocked() {
 }
 
 @Composable
-fun AddressSuggestions(
-    recent: String?,
-    contacts: List<SContact>
-) {
+fun AddressSuggestions(recent: String?, contacts: List<SContact>, onClick: (String) -> Unit) {
     recent?.let { address ->
         SectionHeaderText(stringResource(R.string.Send_Address_Recent))
         Box(
@@ -316,7 +299,7 @@ fun AddressSuggestions(
                     RoundedCornerShape(12.dp)
                 )
                 .clickable {
-
+                    onClick.invoke(address)
                 }
                 .padding(horizontal = 16.dp, vertical = 12.dp)
         ) {
@@ -349,7 +332,7 @@ fun AddressSuggestions(
                     modifier = Modifier
                         .fillMaxWidth()
                         .clickable {
-
+                            onClick.invoke(contact.address)
                         }
                         .padding(horizontal = 16.dp, vertical = 12.dp)
                 ) {
