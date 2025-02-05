5eaa94c7d5d53a178571cf7d360b293416105cae
abdrasulov <abdrasulov@gmail.com>
2025-02-05 15:38:49 +0600

    Implement SubscriptionServiceFDroid

---
+++ subscriptions-fdroid/src/main/java/io/horizontalsystems/subscriptions/fdroid/SubscriptionServiceFDroid.kt
@@ -0,0 +1,43 @@
+package io.horizontalsystems.subscriptions.fdroid
+
+import android.app.Activity
+import android.content.Context
+import io.horizontalsystems.subscriptions.core.BasePlan
+import io.horizontalsystems.subscriptions.core.HSPurchase
+import io.horizontalsystems.subscriptions.core.IPaidAction
+import io.horizontalsystems.subscriptions.core.Subscription
+import io.horizontalsystems.subscriptions.core.SubscriptionService
+import io.horizontalsystems.subscriptions.core.UserSubscription
+import io.horizontalsystems.subscriptions.core.VIPSupport
+
+class SubscriptionServiceFDroid(context: Context) : SubscriptionService {
+    override var predefinedSubscriptions = listOf<Subscription>()
+
+    override fun isActionAllowed(paidAction: IPaidAction): Boolean {
+        return paidAction !is VIPSupport
+    }
+
+    override fun getActiveSubscriptions(): List<UserSubscription> {
+        return listOf()
+    }
+
+    override suspend fun getSubscriptions(): List<Subscription> {
+        return listOf()
+    }
+
+    override suspend fun launchPurchaseFlow(
+        subscriptionId: String,
+        offerToken: String,
+        activity: Activity,
+    ): HSPurchase? {
+        return null
+    }
+
+    override fun getBasePlans(subscriptionId: String): List<BasePlan> {
+        return listOf()
+    }
+
+    override suspend fun onResume() {
+
+    }
+}
