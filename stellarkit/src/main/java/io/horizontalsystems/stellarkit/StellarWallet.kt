11d6c5eedb0b10d74c499feb85d312a6674d0eaf
abdrasulov <abdrasulov@gmail.com>
2025-04-29 12:56:08 +0600

    Extract stellarkit to separate repo

--- stellarkit/src/main/java/io/horizontalsystems/stellarkit/StellarWallet.kt
+++
@@ -1,6 +0,0 @@
-package io.horizontalsystems.stellarkit
-
-sealed interface StellarWallet {
-    data class WatchOnly(val addressStr: String) : StellarWallet
-    data class Seed(val seed: ByteArray) : StellarWallet
-}
