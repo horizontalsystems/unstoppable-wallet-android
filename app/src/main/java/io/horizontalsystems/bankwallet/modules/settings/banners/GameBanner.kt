58ba3aac4475d85e394c119255265b0015f5ef67
Rafael <rafaelekol@gmail.com>
2025-05-13 11:57:37 +0600

    Add donate banner and refactor other banners

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/banners/GameBanner.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/banners/GameBanner.kt
@@ -1,14 +1,11 @@
-package io.horizontalsystems.bankwallet.modules.settings.gamebanner
+package io.horizontalsystems.bankwallet.modules.settings.banners
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +16,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,29 +25,27 @@ import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.ui.compose.Bright
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
-import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
-import io.horizontalsystems.bankwallet.ui.compose.components.headline1_jacob
+import io.horizontalsystems.bankwallet.ui.compose.YellowD
 
 @Composable
 fun GameBanner(onClick: () -> Unit) {
 
     Box(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+            .height(130.dp)
             .clip(RoundedCornerShape(16.dp))
-            .height(IntrinsicSize.Max)
+            .paint(
+                painter = painterResource(R.drawable.game_banner_bg),
+                contentScale = ContentScale.FillBounds
+            )
             .clickable(onClick = onClick)
     ) {
-        Image(
-            painter = painterResource(R.drawable.game_banner_bg),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-        )
 
         Row(
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .padding(horizontal = 16.dp),
         ) {
             Column(
@@ -56,20 +53,18 @@ fun GameBanner(onClick: () -> Unit) {
                     .fillMaxHeight()
                     .weight(0.5f)
                     .padding(vertical = 16.dp),
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                headline1_jacob(stringResource(R.string.SettingsBanner_Title))
-                VSpacer(20.dp)
-                Column(
-                    modifier = Modifier.defaultMinSize(minHeight = 60.dp),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = stringResource(R.string.SettingsBanner_GameDescription),
-                        style = ComposeAppTheme.typography.subhead,
-                        color = Bright,
-                    )
-                    VSpacer(4.dp)
-                }
+                Text(
+                    text = stringResource(R.string.SettingsBanner_Title),
+                    style = ComposeAppTheme.typography.headline1,
+                    color = YellowD,
+                )
+                Text(
+                    text = stringResource(R.string.SettingsBanner_GameDescription),
+                    style = ComposeAppTheme.typography.subhead,
+                    color = Bright,
+                )
             }
 
             Spacer(
@@ -83,7 +78,7 @@ fun GameBanner(onClick: () -> Unit) {
 
 @Preview
 @Composable
-fun PremiumBannerPreview() {
+fun Preview_PremiumBanner() {
     ComposeAppTheme {
         GameBanner(
             onClick = {},
