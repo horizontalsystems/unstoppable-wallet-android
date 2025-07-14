6f57cbde498d6c34f64d56791e74c8e4b6ffb673
Rafael <rafaelekol@gmail.com>
2025-07-14 15:59:09 +0600

    Update balance design for no account

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/main/MainFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/main/MainFragment.kt
@@ -15,15 +15,17 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BadgedBox
 import androidx.compose.material.Icon
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -65,6 +67,7 @@ import io.horizontalsystems.bankwallet.modules.transactions.TransactionsViewMode
 import io.horizontalsystems.bankwallet.modules.walletconnect.WCAccountTypeNotSupportedDialog
 import io.horizontalsystems.bankwallet.modules.walletconnect.WCManager.SupportState
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
+import io.horizontalsystems.bankwallet.ui.compose.Orange
 import io.horizontalsystems.bankwallet.ui.compose.components.BadgeText
 import io.horizontalsystems.bankwallet.ui.compose.components.HsBottomNavigation
 import io.horizontalsystems.bankwallet.ui.compose.components.HsBottomNavigationItem
@@ -80,8 +83,9 @@ class MainFragment : BaseComposeFragment() {
         val backStackEntry = navController.safeGetBackStackEntry(R.id.mainFragment)
 
         backStackEntry?.let {
-            val viewModel = ViewModelProvider(backStackEntry.viewModelStore,  TransactionsModule.Factory())
-                .get(TransactionsViewModel::class.java)
+            val viewModel =
+                ViewModelProvider(backStackEntry.viewModelStore, TransactionsModule.Factory())
+                    .get(TransactionsViewModel::class.java)
             MainScreenWithRootedDeviceCheck(
                 transactionsViewModel = viewModel,
                 navController = navController,
@@ -122,6 +126,7 @@ private fun MainScreenWithRootedDeviceCheck(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MainScreen(
     mainActivityViewModel: MainActivityViewModel,
@@ -142,12 +147,100 @@ private fun MainScreen(
     val pagerState = rememberPagerState(initialPage = selectedPage) { uiState.mainNavItems.size }
 
     val coroutineScope = rememberCoroutineScope()
-    val modalBottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
 
-    ModalBottomSheetLayout(
-        sheetState = modalBottomSheetState,
-        sheetBackgroundColor = ComposeAppTheme.colors.transparent,
-        sheetContent = {
+    val modalBottomSheetState = androidx.compose.material3.rememberModalBottomSheetState()
+    var isBottomSheetVisible by remember { mutableStateOf(false) }
+
+    Scaffold(
+        backgroundColor = ComposeAppTheme.colors.tyler,
+        bottomBar = {
+            Column {
+                if (uiState.torEnabled) {
+                    TorStatusView()
+                }
+                HsBottomNavigation(
+                    backgroundColor = ComposeAppTheme.colors.blade,
+                    elevation = 0.dp
+                ) {
+                    uiState.mainNavItems.forEach { item ->
+                        HsBottomNavigationItem(
+                            icon = {
+                                BadgedIcon(item.badge) {
+                                    Icon(
+                                        painter = painterResource(item.mainNavItem.iconRes),
+                                        contentDescription = stringResource(item.mainNavItem.titleRes)
+                                    )
+                                }
+                            },
+                            selected = item.selected,
+                            enabled = item.enabled,
+                            selectedContentColor = Orange,
+                            unselectedContentColor = if (item.enabled) ComposeAppTheme.colors.grey else ComposeAppTheme.colors.andy,
+                            onClick = {
+                                viewModel.onSelect(item.mainNavItem)
+
+                                stat(
+                                    page = StatPage.Main,
+                                    event = StatEvent.SwitchTab(item.mainNavItem.statTab)
+                                )
+                            },
+                            onLongClick = {
+                                if (item.mainNavItem == MainNavigation.Balance) {
+                                    coroutineScope.launch {
+                                        modalBottomSheetState.show()
+                                        isBottomSheetVisible = true
+
+                                        stat(
+                                            page = StatPage.Main,
+                                            event = StatEvent.Open(StatPage.SwitchWallet)
+                                        )
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+        }
+    ) {
+        BackHandler(enabled = modalBottomSheetState.isVisible) {
+            coroutineScope.launch {
+                modalBottomSheetState.hide()
+                isBottomSheetVisible = false
+            }
+        }
+        Column(modifier = Modifier.padding(it)) {
+            LaunchedEffect(key1 = selectedPage, block = {
+                pagerState.scrollToPage(selectedPage)
+            })
+
+            HorizontalPager(
+                modifier = Modifier.weight(1f),
+                state = pagerState,
+                userScrollEnabled = false,
+                verticalAlignment = Alignment.Top
+            ) { page ->
+                when (uiState.mainNavItems[page].mainNavItem) {
+                    MainNavigation.Market -> MarketScreen(fragmentNavController)
+                    MainNavigation.Balance -> BalanceScreen(fragmentNavController)
+                    MainNavigation.Transactions -> TransactionsScreen(
+                        fragmentNavController,
+                        transactionsViewModel
+                    )
+
+                    MainNavigation.Settings -> SettingsScreen(fragmentNavController)
+                }
+            }
+        }
+    }
+    if (isBottomSheetVisible) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                isBottomSheetVisible = false
+            },
+            sheetState = modalBottomSheetState,
+            containerColor = ComposeAppTheme.colors.transparent
+        ) {
             WalletSwitchBottomSheet(
                 wallets = viewModel.wallets,
                 watchingAddresses = viewModel.watchWallets,
@@ -155,6 +248,7 @@ private fun MainScreen(
                 onSelectListener = {
                     coroutineScope.launch {
                         modalBottomSheetState.hide()
+                        isBottomSheetVisible = false
                         viewModel.onSelect(it)
 
                         stat(
@@ -166,90 +260,10 @@ private fun MainScreen(
                 onCancelClick = {
                     coroutineScope.launch {
                         modalBottomSheetState.hide()
+                        isBottomSheetVisible = false
                     }
                 }
             )
-        },
-    ) {
-        Scaffold(
-            backgroundColor = ComposeAppTheme.colors.tyler,
-            bottomBar = {
-                Column {
-                    if (uiState.torEnabled) {
-                        TorStatusView()
-                    }
-                    HsBottomNavigation(
-                        backgroundColor = ComposeAppTheme.colors.blade,
-                        elevation = 0.dp
-                    ) {
-                        uiState.mainNavItems.forEach { item ->
-                            HsBottomNavigationItem(
-                                icon = {
-                                    BadgedIcon(item.badge) {
-                                        Icon(
-                                            painter = painterResource(item.mainNavItem.iconRes),
-                                            contentDescription = stringResource(item.mainNavItem.titleRes)
-                                        )
-                                    }
-                                },
-                                selected = item.selected,
-                                enabled = item.enabled,
-                                selectedContentColor = ComposeAppTheme.colors.jacob,
-                                unselectedContentColor = if (item.enabled) ComposeAppTheme.colors.grey else ComposeAppTheme.colors.andy,
-                                onClick = {
-                                    viewModel.onSelect(item.mainNavItem)
-
-                                    stat(
-                                        page = StatPage.Main,
-                                        event = StatEvent.SwitchTab(item.mainNavItem.statTab)
-                                    )
-                                },
-                                onLongClick = {
-                                    if (item.mainNavItem == MainNavigation.Balance) {
-                                        coroutineScope.launch {
-                                            modalBottomSheetState.show()
-
-                                            stat(
-                                                page = StatPage.Main,
-                                                event = StatEvent.Open(StatPage.SwitchWallet)
-                                            )
-                                        }
-                                    }
-                                }
-                            )
-                        }
-                    }
-                }
-            }
-        ) {
-            BackHandler(enabled = modalBottomSheetState.isVisible) {
-                coroutineScope.launch {
-                    modalBottomSheetState.hide()
-                }
-            }
-            Column(modifier = Modifier.padding(it)) {
-                LaunchedEffect(key1 = selectedPage, block = {
-                    pagerState.scrollToPage(selectedPage)
-                })
-
-                HorizontalPager(
-                    modifier = Modifier.weight(1f),
-                    state = pagerState,
-                    userScrollEnabled = false,
-                    verticalAlignment = Alignment.Top
-                ) { page ->
-                    when (uiState.mainNavItems[page].mainNavItem) {
-                        MainNavigation.Market -> MarketScreen(fragmentNavController)
-                        MainNavigation.Balance -> BalanceScreen(fragmentNavController)
-                        MainNavigation.Transactions -> TransactionsScreen(
-                            fragmentNavController,
-                            transactionsViewModel
-                        )
-
-                        MainNavigation.Settings -> SettingsScreen(fragmentNavController)
-                    }
-                }
-            }
         }
     }
 
