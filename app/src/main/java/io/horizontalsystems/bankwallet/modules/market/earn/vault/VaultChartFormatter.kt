7f67fc5e016cfc36274800d1b62edf453008ae83
Rafael <rafaelekol@gmail.com>
2025-08-08 11:55:22 +0600

    Customize chart for Vaults page

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/market/earn/vault/VaultChartFormatter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/market/earn/vault/VaultChartFormatter.kt
@@ -8,7 +8,13 @@ import java.math.BigDecimal
 class VaultChartFormatter : ChartModule.ChartNumberFormatter {
 
     override fun formatValue(currency: Currency, value: BigDecimal): String {
-        return App.numberFormatter.format(value, 0, 2, "", "%")
+        return App.numberFormatter.format(value, 0, 2, "APY ", "%")
     }
 
+    override fun formatMinMaxValue(
+        currency: Currency,
+        value: BigDecimal
+    ): String {
+        return App.numberFormatter.format(value, 0, 2, "", "%")
+    }
 }
