15971eb552220ba8520c8f2d417118cb98afbbb1
abdrasulov <abdrasulov@gmail.com>
2025-08-13 16:45:25 +0600

    Handle wc request stellar_signXDR

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/stellar/WCActionStellarSignXdr.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/stellar/WCActionStellarSignXdr.kt
@@ -1,33 +1,63 @@
 package io.horizontalsystems.bankwallet.modules.walletconnect.stellar
 
+import com.google.gson.GsonBuilder
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.modules.walletconnect.request.AbstractWCAction
+import io.horizontalsystems.bankwallet.modules.walletconnect.request.WCActionContentItem
 import io.horizontalsystems.bankwallet.modules.walletconnect.request.WCActionState
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
+import io.horizontalsystems.stellarkit.StellarKit
 import kotlinx.coroutines.CoroutineScope
 
 class WCActionStellarSignXdr(
-    private val params: String,
+    private val paramsJsonStr: String,
     private val peerName: String,
+    private val stellarKit: StellarKit,
 ) : AbstractWCAction() {
+
+    private val gson = GsonBuilder().create()
+    private val params = gson.fromJson(paramsJsonStr, Params::class.java)
 
     override fun getTitle(): TranslatableString {
         return TranslatableString.ResString(R.string.WalletConnect_SignMessageRequest_Title)
     }
 
     override fun getApproveButtonTitle(): TranslatableString {
-        TODO("Not yet implemented")
+        return TranslatableString.ResString(R.string.Button_Sign)
     }
 
     override suspend fun performAction(): String {
-        TODO("Not yet implemented")
+        return gson.toJson(mapOf("signedXDR" to stellarKit.signTransaction(params.xdr)))
     }
 
-    override fun start(coroutineScope: CoroutineScope) {
-        TODO("Not yet implemented")
-    }
+    override fun start(coroutineScope: CoroutineScope) = Unit
 
-    override fun createState(): WCActionState {
-        TODO("Not yet implemented")
-    }
+    override fun createState() = WCActionState(
+        runnable = true,
+        items = listOf(
+            WCActionContentItem.Section(
+                listOf(
+                    WCActionContentItem.SingleLine(
+                        TranslatableString.ResString(R.string.WalletConnect_SignMessageRequest_dApp),
+                        TranslatableString.PlainString(peerName)
+                    ),
+                    WCActionContentItem.SingleLine(
+                        TranslatableString.PlainString("Stellar"),
+                        null
+                    )
+                )
+            ),
+
+            WCActionContentItem.Section(
+                listOf(
+                    WCActionContentItem.Paragraph(
+                        TranslatableString.PlainString(params.xdr)
+                    )
+                ),
+                TranslatableString.PlainString("Transaction XDR")
+            )
+        )
+    )
+
+    data class Params(val xdr: String)
 }
