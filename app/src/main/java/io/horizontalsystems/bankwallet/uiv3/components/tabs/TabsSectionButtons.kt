70b4c1f800f445f9ae6d07b7cbde0897d024d7b3
abdrasulov <abdrasulov@gmail.com>
2025-08-27 11:49:45 +0600

    Implement TabsSectionButtons

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/uiv3/components/tabs/TabsSectionButtons.kt
@@ -0,0 +1,30 @@
+package io.horizontalsystems.bankwallet.uiv3.components.tabs
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TabsSectionButtons(
+    left: @Composable RowScope.() -> Unit = {},
+    right: @Composable RowScope.() -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+            left()
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+            right()
+        }
+    }
+}
