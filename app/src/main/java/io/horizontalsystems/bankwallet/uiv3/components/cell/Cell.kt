3f8e78797f4c6776811441f80b5d0d13ac8cc36c
abdrasulov <abdrasulov@gmail.com>
2025-08-28 16:21:33 +0600

    Limit width of the middle block

--- app/src/main/java/io/horizontalsystems/bankwallet/uiv3/components/cell/Cell.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/uiv3/components/cell/Cell.kt
@@ -1,24 +1,31 @@
 package io.horizontalsystems.bankwallet.uiv3.components.cell
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import io.horizontalsystems.bankwallet.ui.compose.components.HFillSpacer
+import io.horizontalsystems.bankwallet.R
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 
 @Composable
 private fun Cell(
     paddingValues: PaddingValues,
-    left: @Composable() (RowScope.() -> Unit)?,
-    middle: @Composable() (RowScope.() -> Unit),
-    right: @Composable() (RowScope.() -> Unit)?,
+    left: @Composable() (() -> Unit)?,
+    middle: @Composable() (() -> Unit),
+    right: @Composable() (() -> Unit)?,
     onClick: (() -> Unit)?,
 ) {
     var modifier = Modifier
@@ -33,18 +40,19 @@ private fun Cell(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        left?.invoke(this)
-        middle.invoke(this)
-        HFillSpacer(0.dp)
-        right?.invoke(this)
+        left?.invoke()
+        Box(modifier = Modifier.weight(1f)) {
+            middle.invoke()
+        }
+        right?.invoke()
     }
 }
 
 @Composable
 fun CellPrimary(
-    left: @Composable() (RowScope.() -> Unit)? = null,
-    middle: @Composable() (RowScope.() -> Unit),
-    right: @Composable() (RowScope.() -> Unit)? = null,
+    left: @Composable() (() -> Unit)? = null,
+    middle: @Composable() (() -> Unit),
+    right: @Composable() (() -> Unit)? = null,
     onClick: (() -> Unit)? = null
 ) {
     Cell(
@@ -58,9 +66,9 @@ fun CellPrimary(
 
 @Composable
 fun CellSecondary(
-    left: @Composable() (RowScope.() -> Unit)? = null,
-    middle: @Composable() (RowScope.() -> Unit),
-    right: @Composable() (RowScope.() -> Unit)? = null,
+    left: @Composable() (() -> Unit)? = null,
+    middle: @Composable() (() -> Unit),
+    right: @Composable() (() -> Unit)? = null,
     onClick: (() -> Unit)? = null
 ) {
     Cell(
@@ -70,4 +78,54 @@ fun CellSecondary(
         right = right,
         onClick = onClick
     )
+}
+
+@Preview
+@Composable
+fun Preview_CellPrimary() {
+    ComposeAppTheme {
+        Column {
+            CellPrimary(
+                left = {
+                    Image(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(R.drawable.ic_app_logo_72),
+                        contentDescription = null
+                    )
+                },
+                middle = {
+                    CellMiddleInfo(
+                        title = "The quick brown fox jumps over the lazy dog".hs
+                    )
+                },
+                right = {
+                    CellRightInfo(
+                        title = "Value".hs
+                    )
+                }
+            )
+
+            Divider()
+
+            CellPrimary(
+                left = {
+                    Image(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(R.drawable.ic_app_logo_72),
+                        contentDescription = null
+                    )
+                },
+                middle = {
+                    CellMiddleInfo(
+                        title = "Title".hs
+                    )
+                },
+                right = {
+                    CellRightInfo(
+                        title = "Value".hs
+                    )
+                }
+            )
+        }
+    }
 }
