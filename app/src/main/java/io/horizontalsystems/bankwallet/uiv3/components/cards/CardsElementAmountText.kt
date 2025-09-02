428896c6b090ed044c66bb60adda0a5ff3ab37da
abdrasulov <abdrasulov@gmail.com>
2025-09-02 13:44:18 +0600

    Update CardsElementAmountText

--- app/src/main/java/io/horizontalsystems/bankwallet/uiv3/components/cards/CardsElementAmountText.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/uiv3/components/cards/CardsElementAmountText.kt
@@ -1,9 +1,18 @@
 package io.horizontalsystems.bankwallet.uiv3.components.cards
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import io.horizontalsystems.bankwallet.ui.compose.components.DoubleText
-import io.horizontalsystems.bankwallet.uiv3.components.HSPreview
+import androidx.compose.ui.unit.dp
+import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 
 @Composable
 fun CardsElementAmountText(
@@ -13,19 +22,44 @@ fun CardsElementAmountText(
     onClickTitle: () -> Unit,
     onClickSubtitle: () -> Unit,
 ) {
-    DoubleText(
-        title,
-        body,
-        dimmed,
-        onClickTitle,
-        onClickSubtitle,
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(ComposeAppTheme.colors.tyler)
+            .padding(horizontal = 16.dp, vertical = 24.dp),
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = onClickTitle
+                ),
+            text = title,
+            style = ComposeAppTheme.typography.title2М,
+            color = if (dimmed) ComposeAppTheme.colors.grey else ComposeAppTheme.colors.leah,
+            maxLines = 1
+        )
+        Text(
+            modifier = Modifier
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = onClickSubtitle
+                ),
+            text = body,
+            style = ComposeAppTheme.typography.body,
+            color = if (dimmed) ComposeAppTheme.colors.andy else ComposeAppTheme.colors.grey,
+            maxLines = 1
+        )
+    }
 }
 
 @Preview
 @Composable
 fun Preview_CardsElementAmountText() {
-    HSPreview {
+    ComposeAppTheme {
         CardsElementAmountText(
             title = "\$1,289,231.60",
             body = "≈22.6057 BTC",
