ee45f091582fae5d811b99f1a38f0d2b2c674972
abdrasulov <abdrasulov@gmail.com>
2025-09-09 12:10:49 +0600

    Get Dash address statically

--- app/src/main/java/io/horizontalsystems/bankwallet/core/adapters/DashAdapter.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/adapters/DashAdapter.kt
@@ -85,6 +85,31 @@ class DashAdapter(
     companion object {
         private const val confirmationsThreshold = 1
 
+        fun firstAddress(accountType: AccountType): String {
+            when (accountType) {
+                is AccountType.Mnemonic -> {
+                    val address = DashKit.firstAddress(
+                        accountType.seed,
+                        NetworkType.MainNet
+                    )
+
+                    return address.stringValue
+                }
+                is AccountType.HdExtendedKey -> {
+                    val address = DashKit.firstAddress(
+                        accountType.hdExtendedKey,
+                        NetworkType.MainNet
+                    )
+
+                    return address.stringValue
+                }
+                is AccountType.BitcoinAddress -> {
+                    return accountType.address
+                }
+                else -> throw UnsupportedAccountException()
+            }
+        }
+
         private fun createKit(wallet: Wallet, syncMode: BitcoinCore.SyncMode): DashKit {
             val account = wallet.account
 
