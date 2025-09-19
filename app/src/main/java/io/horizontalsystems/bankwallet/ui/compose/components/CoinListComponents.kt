e56bf99bbbd904da47086d688d1a414b835f4af4
Rafael <rafaelekol@gmail.com>
2025-09-19 11:53:28 +0600

    Center error message

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/CoinListComponents.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/compose/components/CoinListComponents.kt
@@ -11,19 +11,23 @@ import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
@@ -319,11 +323,11 @@ fun ScreenMessageWithAction(
         modifier = Modifier
             .padding(paddingValues)
             .fillMaxSize()
-            .background(ComposeAppTheme.colors.lawrence)
+            .windowInsetsPadding(WindowInsets.ime)
             .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        VSpacer(64.dp)
         Box(
             modifier = Modifier.size(96.dp),
             contentAlignment = Alignment.Center
