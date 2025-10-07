6ae21f44d326e666d0b91e5f243b7b8d9b14c83e
Rafael <rafaelekol@gmail.com>
2025-10-07 15:29:50 +0600

    Refactor Receive screens with new UI components

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/ui/BalanceItems.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/balance/ui/BalanceItems.kt
@@ -298,7 +298,7 @@ fun BalanceItems(
                                 when (val receiveAllowedState =
                                     viewModel.getReceiveAllowedState()) {
                                     ReceiveAllowedState.Allowed -> {
-                                        navController.slideFromRight(R.id.receiveChooseCoinFragment)
+                                        navController.slideFromBottom(R.id.receiveChooseCoinFragment)
 
                                         stat(
                                             page = StatPage.Balance,
