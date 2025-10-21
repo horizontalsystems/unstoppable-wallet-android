82ce343fc59e8ae83761c6efeba5af7ca5604f0d
Rafael <rafaelekol@gmail.com>
2025-10-21 16:33:17 +0600

    Refactor Contacts screens to use HSScaffold

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/contacts/screen/AddressScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/contacts/screen/AddressScreen.kt
@@ -1,18 +1,19 @@
 package io.horizontalsystems.bankwallet.modules.contacts.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
-import androidx.compose.material.ModalBottomSheetLayout
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -24,18 +25,18 @@ import io.horizontalsystems.bankwallet.modules.contacts.model.ContactAddress
 import io.horizontalsystems.bankwallet.modules.contacts.viewmodel.AddressViewModel
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
-import io.horizontalsystems.bankwallet.ui.compose.components.AppBar
 import io.horizontalsystems.bankwallet.ui.compose.components.CellUniversalLawrenceSection
 import io.horizontalsystems.bankwallet.ui.compose.components.FormsInput
-import io.horizontalsystems.bankwallet.ui.compose.components.HsBackButton
 import io.horizontalsystems.bankwallet.ui.compose.components.MenuItem
 import io.horizontalsystems.bankwallet.ui.compose.components.RowUniversal
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
 import io.horizontalsystems.bankwallet.ui.compose.components.body_lucian
 import io.horizontalsystems.bankwallet.ui.compose.components.subhead1_leah
 import io.horizontalsystems.bankwallet.ui.compose.components.subhead2_grey
+import io.horizontalsystems.bankwallet.uiv3.components.HSScaffold
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddressScreen(
     viewModel: AddressViewModel,
@@ -46,106 +47,106 @@ fun AddressScreen(
 ) {
     val uiState = viewModel.uiState
 
-    val modalBottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
-    val coroutineScope = rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
-    ModalBottomSheetLayout(
-        sheetState = modalBottomSheetState,
-        sheetBackgroundColor = ComposeAppTheme.colors.transparent,
-        sheetContent = {
-            ConfirmationBottomSheet(
-                title = stringResource(R.string.Contacts_DeleteAddress),
-                text = stringResource(R.string.Contacts_DeleteAddress_Warning),
-                iconPainter = painterResource(R.drawable.ic_delete_20),
-                iconTint = ColorFilter.tint(ComposeAppTheme.colors.lucian),
-                confirmText = stringResource(R.string.Button_Delete),
-                cautionType = Caution.Type.Error,
-                cancelText = stringResource(R.string.Button_Cancel),
-                onConfirm = {
-                    uiState.editingAddress?.let { onDelete(it) }
-                },
-                onClose = {
-                    coroutineScope.launch { modalBottomSheetState.hide() }
+    HSScaffold(
+        title = uiState.headerTitle.getString(),
+        onBack = onNavigateToBack,
+        menuItems = listOf(
+            MenuItem(
+                title = TranslatableString.ResString(R.string.Button_Done),
+                enabled = uiState.doneEnabled,
+                tint = ComposeAppTheme.colors.jacob,
+                onClick = {
+                    uiState.addressState?.dataOrNull?.let {
+                        onDone(ContactAddress(uiState.blockchain, it.hex))
+                    }
                 }
             )
-        }
+        )
     ) {
-        Scaffold(
-            backgroundColor = ComposeAppTheme.colors.tyler,
-            topBar = {
-                AppBar(
-                    title = uiState.headerTitle.getString(),
-                    navigationIcon = {
-                        HsBackButton(onNavigateToBack)
-                    },
-                    menuItems = listOf(
-                        MenuItem(
-                            title = TranslatableString.ResString(R.string.Button_Done),
-                            enabled = uiState.doneEnabled,
-                            tint = ComposeAppTheme.colors.jacob,
-                            onClick = {
-                                uiState.addressState?.dataOrNull?.let {
-                                    onDone(ContactAddress(uiState.blockchain, it.hex))
-                                }
-                            }
+        Column(modifier = Modifier.fillMaxSize()) {
+            VSpacer(12.dp)
+
+            CellUniversalLawrenceSection(
+                listOf {
+                    RowUniversal(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        onClick = if (uiState.canChangeBlockchain) onNavigateToBlockchainSelector else null
+                    ) {
+                        subhead2_grey(
+                            text = stringResource(R.string.AddToken_Blockchain),
+                            modifier = Modifier.weight(1f)
                         )
-                    )
-                )
-            }
-        ) { paddingValues ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(paddingValues)
-            ) {
-                Spacer(modifier = Modifier.height(12.dp))
+                        subhead1_leah(
+                            text = uiState.blockchain.name,
+                            modifier = Modifier.padding(horizontal = 8.dp)
+                        )
+                        if (uiState.canChangeBlockchain) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_down_arrow_20),
+                                contentDescription = null,
+                                tint = ComposeAppTheme.colors.grey
+                            )
+                        }
+                    }
+                }
+            )
 
-                CellUniversalLawrenceSection(
-                    listOf {
-                        RowUniversal(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp),
-                            onClick = if (uiState.canChangeBlockchain) onNavigateToBlockchainSelector else null
-                        ) {
-                            subhead2_grey(
-                                text = stringResource(R.string.AddToken_Blockchain),
-                                modifier = Modifier.weight(1f)
-                            )
-                            subhead1_leah(
-                                text = uiState.blockchain.name,
-                                modifier = Modifier.padding(horizontal = 8.dp)
-                            )
-                            if (uiState.canChangeBlockchain) {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.ic_down_arrow_20),
-                                    contentDescription = null,
-                                    tint = ComposeAppTheme.colors.grey
-                                )
+            VSpacer(32.dp)
+
+            FormsInput(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                initial = uiState.address,
+                hint = stringResource(R.string.Contacts_AddressHint),
+                state = uiState.addressState,
+                qrScannerEnabled = true,
+            ) {
+                viewModel.onEnterAddress(it)
+            }
+            if (uiState.showDelete) {
+                VSpacer(32.dp)
+                DeleteAddressButton {
+                    showBottomSheet = true
+                }
+            }
+        }
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState,
+                containerColor = ComposeAppTheme.colors.transparent
+            ) {
+                ConfirmationBottomSheet(
+                    title = stringResource(R.string.Contacts_DeleteAddress),
+                    text = stringResource(R.string.Contacts_DeleteAddress_Warning),
+                    iconPainter = painterResource(R.drawable.ic_delete_20),
+                    iconTint = ColorFilter.tint(ComposeAppTheme.colors.lucian),
+                    confirmText = stringResource(R.string.Button_Delete),
+                    cautionType = Caution.Type.Error,
+                    cancelText = stringResource(R.string.Button_Cancel),
+                    onConfirm = {
+                        uiState.editingAddress?.let { onDelete(it) }
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
+                        }
+                    },
+                    onClose = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
                             }
                         }
                     }
                 )
-
-                VSpacer(32.dp)
-
-                FormsInput(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    initial = uiState.address,
-                    hint = stringResource(R.string.Contacts_AddressHint),
-                    state = uiState.addressState,
-                    qrScannerEnabled = true,
-                ) {
-                    viewModel.onEnterAddress(it)
-                }
-                if (uiState.showDelete) {
-                    VSpacer(32.dp)
-                    DeleteAddressButton {
-                        coroutineScope.launch {
-                            modalBottomSheetState.show()
-                        }
-                    }
-                }
             }
         }
     }
