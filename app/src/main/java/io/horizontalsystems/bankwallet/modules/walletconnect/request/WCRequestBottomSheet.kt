65e1476557ee4131e40e6618e11057d397adaf4b
Rafael <rafaelekol@gmail.com>
2025-12-24 16:25:37 +0600

    Show DefenseSystem message in SwapConfirm

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/request/WCRequestBottomSheet.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/request/WCRequestBottomSheet.kt
@@ -7,11 +7,8 @@ import android.view.ViewGroup
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -19,17 +16,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.painterResource
@@ -46,17 +38,14 @@ import io.horizontalsystems.bankwallet.core.imageUrl
 import io.horizontalsystems.bankwallet.core.slideFromBottom
 import io.horizontalsystems.bankwallet.modules.premium.DefenseSystemFeatureDialog
 import io.horizontalsystems.bankwallet.modules.premium.PremiumFeature
-import io.horizontalsystems.bankwallet.modules.walletconnect.session.DefenseSystemState
 import io.horizontalsystems.bankwallet.modules.walletconnect.session.WCSessionButtonStates
 import io.horizontalsystems.bankwallet.modules.walletconnect.session.WCSessionModule
 import io.horizontalsystems.bankwallet.modules.walletconnect.session.WCSessionViewModel
 import io.horizontalsystems.bankwallet.modules.walletconnect.session.WCWhiteListState
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
-import io.horizontalsystems.bankwallet.ui.compose.components.HSpacer
 import io.horizontalsystems.bankwallet.ui.compose.components.HsImage
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
 import io.horizontalsystems.bankwallet.ui.compose.components.headline1_leah
-import io.horizontalsystems.bankwallet.ui.compose.components.headline2_leah
 import io.horizontalsystems.bankwallet.ui.compose.components.subhead_grey
 import io.horizontalsystems.bankwallet.ui.extensions.BaseComposableBottomSheetFragment
 import io.horizontalsystems.bankwallet.ui.helpers.TextHelper
@@ -70,6 +59,8 @@ import io.horizontalsystems.bankwallet.uiv3.components.controls.ButtonSize
 import io.horizontalsystems.bankwallet.uiv3.components.controls.ButtonVariant
 import io.horizontalsystems.bankwallet.uiv3.components.controls.HSButton
 import io.horizontalsystems.bankwallet.uiv3.components.info.TextBlock
+import io.horizontalsystems.bankwallet.uiv3.components.message.DefenseSystemMessage
+import io.horizontalsystems.bankwallet.uiv3.components.message.DefenseSystemState
 import io.horizontalsystems.core.findNavController
 import io.horizontalsystems.marketkit.models.BlockchainType
 
@@ -180,7 +171,7 @@ fun WCRequestScreen(
                 text = stringResource(R.string.WalletConnect_ConnectWarning),
             )
             VSpacer(16.dp)
-            DefenseSystemMessage(
+            WCDefenseSystemMessage(
                 activated = uiState.hasSubscription,
                 whiteListState = uiState.whiteListState,
                 onActivateClick = {
@@ -298,7 +289,7 @@ fun IconsFromUrls(
 }
 
 @Composable
-fun DefenseSystemMessage(
+private fun WCDefenseSystemMessage(
     activated: Boolean,
     whiteListState: WCWhiteListState,
     onActivateClick: () -> Unit = {},
@@ -307,141 +298,37 @@ fun DefenseSystemMessage(
         !activated -> DefenseSystemState.WARNING
         whiteListState == WCWhiteListState.NotInWhiteList -> DefenseSystemState.DANGER
         whiteListState == WCWhiteListState.InWhiteList -> DefenseSystemState.SAFE
-        whiteListState == WCWhiteListState.InProgress -> DefenseSystemState.CHECKING
-        else -> DefenseSystemState.CHECKING
+        whiteListState == WCWhiteListState.InProgress -> DefenseSystemState.IDLE
+        else -> DefenseSystemState.IDLE
     }
 
-    val clickableModifier = when (state) {
-        DefenseSystemState.WARNING -> Modifier.clickable(
-            onClick = { onActivateClick.invoke() },
-            interactionSource = remember { MutableInteractionSource() },
-            indication = null,
-        )
-
-        else -> Modifier
+    val title: Int = when (state) {
+        DefenseSystemState.WARNING -> R.string.WalletConnect_Attention
+        DefenseSystemState.DANGER -> R.string.WalletConnect_Danger
+        DefenseSystemState.SAFE -> R.string.WalletConnect_Safe
+        DefenseSystemState.IDLE -> R.string.WalletConnect_Checking
     }
 
-    Column(
-        modifier = Modifier.padding(horizontal = 16.dp)
-    ) {
-        // Message bubble
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(16.dp))
-                .background(state.bubbleColor)
-                .then(clickableModifier)
-                .padding(horizontal = 24.dp, vertical = 16.dp)
-        ) {
-            Column {
-                if (state == DefenseSystemState.CHECKING) {
-                    headline2_leah(
-                        text = stringResource(R.string.WalletConnect_Checking)
-                    )
-                    VSpacer(30.dp)
-                } else {
-                    val titleIcon: Int? = when (state) {
-                        DefenseSystemState.WARNING -> R.drawable.warning_filled_24
-                        DefenseSystemState.DANGER -> R.drawable.warning_filled_24
-                        DefenseSystemState.SAFE -> R.drawable.shield_check_filled_24
-                        DefenseSystemState.CHECKING -> null
-                    }
-                    val title: Int? = when (state) {
-                        DefenseSystemState.WARNING -> R.string.WalletConnect_Attention
-                        DefenseSystemState.DANGER -> R.string.WalletConnect_Danger
-                        DefenseSystemState.SAFE -> R.string.WalletConnect_Safe
-                        DefenseSystemState.CHECKING -> null
-                    }
-                    if (titleIcon != null && title != null) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier.padding(bottom = 8.dp)
-                        ) {
-                            Icon(
-                                painter = painterResource(titleIcon),
-                                contentDescription = null,
-                                tint = when (state) {
-                                    DefenseSystemState.SAFE,
-                                    DefenseSystemState.WARNING -> Color.Black
-
-                                    DefenseSystemState.DANGER -> Color.White
-                                    else -> Color.Black
-                                },
-                                modifier = Modifier.size(20.dp)
-                            )
-                            HSpacer(8.dp)
-                            Text(
-                                text = stringResource(title),
-                                style = ComposeAppTheme.typography.headline2,
-                                color = when (state) {
-                                    DefenseSystemState.WARNING,
-                                    DefenseSystemState.SAFE -> Color.Black
-
-                                    DefenseSystemState.DANGER -> Color.White
-                                    else -> Color.Black
-                                }
-                            )
-                        }
-                    }
-                    // Message Text
-                    Text(
-                        text = when (state) {
-                            DefenseSystemState.WARNING -> stringResource(R.string.WalletConnect_DefenseMessage_Warning)
-                            DefenseSystemState.DANGER -> stringResource(R.string.WalletConnect_DefenseMessage_Danger)
-                            DefenseSystemState.SAFE -> stringResource(R.string.WalletConnect_DefenseMessage_Safe)
-                            DefenseSystemState.CHECKING -> ""
-                        },
-                        style = ComposeAppTheme.typography.subheadR,
-                        color = when (state) {
-                            DefenseSystemState.WARNING,
-                            DefenseSystemState.SAFE,
-                            DefenseSystemState.CHECKING -> Color.Black
-
-                            DefenseSystemState.DANGER -> Color.White
-                        }
-                    )
-
-                    if (state == DefenseSystemState.WARNING) {
-                        VSpacer(12.dp)
-                        Row(modifier = Modifier.align(Alignment.End)) {
-                            Text(
-                                text = "Activate",
-                                style = ComposeAppTheme.typography.subheadSB,
-                                color = Color.Black
-                            )
-                            HSpacer(8.dp)
-                            Icon(
-                                painter = painterResource(R.drawable.arrow_m_right_24),
-                                contentDescription = null,
-                                tint = Color.Black,
-                                modifier = Modifier.size(20.dp)
-                            )
-                        }
-                    }
-                }
-            }
-        }
-        // Speech bubble tail
-        Box(
-            modifier = Modifier
-                .offset(x = 48.dp, y = (-8).dp)
-                .size(16.dp)
-                .rotate(45f)
-                .background(state.bubbleColor)
-        )
+    val content: Int? = when (state) {
+        DefenseSystemState.WARNING -> R.string.WalletConnect_DefenseMessage_Warning
+        DefenseSystemState.DANGER -> R.string.WalletConnect_DefenseMessage_Danger
+        DefenseSystemState.SAFE -> R.string.WalletConnect_DefenseMessage_Safe
+        DefenseSystemState.IDLE -> null
     }
 
-    CellSecondary(
-        left = {
-            Image(
-                painter = painterResource(R.drawable.ic_defense_shield_20),
-                contentDescription = null,
-            )
-        },
-        middle = {
-            CellMiddleInfo(
-                subtitle = stringResource(R.string.Premium_DefenseSystem).hs
-            )
-        },
+    val icon = when (state) {
+        DefenseSystemState.WARNING -> R.drawable.warning_filled_24
+        DefenseSystemState.DANGER -> R.drawable.warning_filled_24
+        DefenseSystemState.SAFE -> R.drawable.shield_check_filled_24
+        DefenseSystemState.IDLE -> null
+    }
+
+    DefenseSystemMessage(
+        state = state,
+        title = stringResource(title),
+        content = content?.let { stringResource(it) },
+        icon = icon,
+        actionText = stringResource(R.string.Button_Activate),
+        onClick = if (state == DefenseSystemState.WARNING) onActivateClick else null
     )
 }
