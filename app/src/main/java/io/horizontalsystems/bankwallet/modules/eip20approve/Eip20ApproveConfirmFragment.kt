3854adfaa3d7699dee99116fb9b5a4794ae9d2da
Rafael <rafaelekol@gmail.com>
2025-12-31 16:00:42 +0600

    Update eip20 approve confirm screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/eip20approve/Eip20ApproveConfirmFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/eip20approve/Eip20ApproveConfirmFragment.kt
@@ -1,7 +1,12 @@
 package io.horizontalsystems.bankwallet.modules.eip20approve
 
 import android.os.Parcelable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,35 +14,45 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
+import io.horizontalsystems.bankwallet.core.badge
 import io.horizontalsystems.bankwallet.core.setNavigationResultX
 import io.horizontalsystems.bankwallet.core.slideFromRight
+import io.horizontalsystems.bankwallet.entities.CoinValue
+import io.horizontalsystems.bankwallet.entities.Currency
+import io.horizontalsystems.bankwallet.entities.CurrencyValue
 import io.horizontalsystems.bankwallet.modules.confirm.ConfirmTransactionScreen
 import io.horizontalsystems.bankwallet.modules.eip20approve.AllowanceMode.OnlyRequired
 import io.horizontalsystems.bankwallet.modules.eip20approve.AllowanceMode.Unlimited
 import io.horizontalsystems.bankwallet.modules.evmfee.Cautions
-import io.horizontalsystems.bankwallet.modules.multiswap.TokenRow
-import io.horizontalsystems.bankwallet.modules.multiswap.TokenRowUnlimited
-import io.horizontalsystems.bankwallet.modules.multiswap.ui.DataFieldFee
+import io.horizontalsystems.bankwallet.modules.multiswap.ui.DataFieldFeeTemplate
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryDefault
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
-import io.horizontalsystems.bankwallet.ui.compose.components.TransactionInfoAddressCell
-import io.horizontalsystems.bankwallet.ui.compose.components.TransactionInfoContactCell
+import io.horizontalsystems.bankwallet.ui.compose.components.CoinImage
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
-import io.horizontalsystems.bankwallet.ui.compose.components.cell.SectionUniversalLawrence
-import io.horizontalsystems.bankwallet.uiv3.components.BoxBordered
+import io.horizontalsystems.bankwallet.ui.helpers.TextHelper
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellMiddleInfo
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellPrimary
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellRightControlsButtonText
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellRightInfo
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellSecondary
+import io.horizontalsystems.bankwallet.uiv3.components.cell.hs
 import io.horizontalsystems.core.SnackbarDuration
 import io.horizontalsystems.core.helpers.HudHelper
+import io.horizontalsystems.marketkit.models.Token
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
+import java.math.BigDecimal
 
 class Eip20ApproveConfirmFragment : BaseComposeFragment() {
     @Composable
@@ -58,6 +73,7 @@ fun Eip20ApproveConfirmScreen(navController: NavController) {
         viewModelStoreOwner = viewModelStoreOwner,
     )
 
+    val view = LocalView.current
     val uiState = viewModel.uiState
 
     ConfirmTransactionScreen(
@@ -79,7 +95,11 @@ fun Eip20ApproveConfirmScreen(navController: NavController) {
                 onClick = {
                     coroutineScope.launch {
                         buttonEnabled = false
-                        HudHelper.showInProcessMessage(view, R.string.Swap_Approving, SnackbarDuration.INDEFINITE)
+                        HudHelper.showInProcessMessage(
+                            view,
+                            R.string.Swap_Approving,
+                            SnackbarDuration.INDEFINITE
+                        )
 
                         val result = try {
                             viewModel.approve()
@@ -109,52 +129,45 @@ fun Eip20ApproveConfirmScreen(navController: NavController) {
             )
         }
     ) {
-        SectionUniversalLawrence {
-            when (uiState.allowanceMode) {
-                OnlyRequired -> {
-                    TokenRow(
-                        token = uiState.token,
-                        amount = uiState.requiredAllowance,
-                        fiatAmount = uiState.fiatAmount,
-                        currency = uiState.currency,
-                        borderTop = false,
-                        title = stringResource(R.string.Approve_YouApprove),
-                        amountColor = ComposeAppTheme.colors.leah
-                    )
-                }
-                Unlimited -> {
-                    TokenRowUnlimited(
-                        token = uiState.token,
-                        borderTop = false,
-                        title = stringResource(R.string.Approve_YouApprove),
-                        amountColor = ComposeAppTheme.colors.leah
-                    )
-                }
-            }
-
-            BoxBordered(top = true) {
-                TransactionInfoAddressCell(
-                    title = stringResource(R.string.Approve_Spender),
-                    value = uiState.spenderAddress,
-                    showAdd = uiState.contact == null,
-                    blockchainType = uiState.token.blockchainType,
-                    navController = navController
+        when (uiState.allowanceMode) {
+            OnlyRequired -> {
+                ConfirmTokenSection(
+                    token = uiState.token,
+                    amount = uiState.requiredAllowance,
+                    fiatAmount = uiState.fiatAmount,
+                    currency = uiState.currency,
                 )
             }
 
-            uiState.contact?.let {
-                BoxBordered(top = true) {
-                    TransactionInfoContactCell(it.name)
-                }
+            Unlimited -> {
+                ConfirmTokenUnlimitedSection(uiState.token)
             }
         }
 
-        VSpacer(height = 16.dp)
-        SectionUniversalLawrence {
-            DataFieldFee(
-                navController,
-                uiState.networkFee?.primary?.getFormattedPlain() ?: "---",
-                uiState.networkFee?.secondary?.getFormattedPlain() ?: "---"
+        VSpacer(16.dp)
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(ComposeAppTheme.colors.lawrence)
+                .padding(vertical = 8.dp)
+        ) {
+            SpenderCell(
+                address = uiState.spenderAddress,
+                contact = uiState.contact?.name,
+                onCopyClick = {
+                    TextHelper.copyText(uiState.spenderAddress)
+                    HudHelper.showSuccessMessage(view, R.string.Hud_Text_Copied)
+                }
+            )
+
+            DataFieldFeeTemplate(
+                navController = navController,
+                primary = uiState.networkFee?.primary?.getFormattedPlain() ?: "---",
+                secondary = uiState.networkFee?.secondary?.getFormattedPlain(),
+                title = stringResource(id = R.string.FeeSettings_NetworkFee),
+                infoText = stringResource(id = R.string.FeeSettings_NetworkFee_Info)
             )
         }
 
@@ -162,4 +175,97 @@ fun Eip20ApproveConfirmScreen(navController: NavController) {
             Cautions(cautions = uiState.cautions)
         }
     }
+}
+
+@Composable
+private fun ConfirmTokenUnlimitedSection(token: Token) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(ComposeAppTheme.colors.lawrence)
+    ) {
+        CellPrimary(
+            left = {
+                CoinImage(
+                    coin = token.coin,
+                    modifier = Modifier.size(32.dp)
+                )
+            },
+            middle = {
+                CellMiddleInfo(
+                    subtitle = token.coin.code.hs(color = ComposeAppTheme.colors.leah),
+                    description = (token.badge ?: stringResource(id = R.string.CoinPlatforms_Native)).hs
+                )
+            },
+            right = {
+                CellRightInfo(
+                    titleSubheadSb = stringResource(R.string.Swap_Approve_Unlimited).hs,
+                )
+            }
+        )
+    }
+}
+
+@Composable
+private fun ConfirmTokenSection(
+    token: Token,
+    amount: BigDecimal,
+    fiatAmount: BigDecimal?,
+    currency: Currency,
+) {
+    val coinAmount = CoinValue(token, amount).getFormattedFull()
+    val fiatAmountFormatted =
+        fiatAmount?.let { CurrencyValue(currency, fiatAmount).getFormattedFull() }
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(ComposeAppTheme.colors.lawrence)
+    ) {
+        CellPrimary(
+            left = {
+                CoinImage(
+                    coin = token.coin,
+                    modifier = Modifier.size(32.dp)
+                )
+            },
+            middle = {
+                CellMiddleInfo(
+                    subtitle = token.coin.code.hs(color = ComposeAppTheme.colors.leah),
+                    description = (token.badge ?: stringResource(id = R.string.CoinPlatforms_Native)).hs
+                )
+            },
+            right = {
+                CellRightInfo(
+                    titleSubheadSb = coinAmount.hs,
+                    description = fiatAmountFormatted?.hs
+                )
+            }
+        )
+    }
+}
+
+@Composable
+private fun SpenderCell(
+    address: String,
+    contact: String?,
+    onCopyClick: () -> Unit
+) {
+    CellSecondary(
+        middle = {
+            CellMiddleInfo(
+                eyebrow = stringResource(R.string.Approve_Spender).hs,
+            )
+        },
+        right = {
+            CellRightControlsButtonText(
+                text = (contact ?: address).hs,
+                icon = painterResource(id = R.drawable.copy_filled_24),
+                iconTint = ComposeAppTheme.colors.leah,
+                onIconClick = onCopyClick
+            )
+        }
+    )
 }
