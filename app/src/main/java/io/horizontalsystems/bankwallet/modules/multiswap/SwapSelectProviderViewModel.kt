33ed215d16976c267f3cb0e13b9a766756bd573f
abdrasulov <abdrasulov@gmail.com>
2026-01-12 15:41:42 +0600

    Implement price impact calculations for quotes list

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/SwapSelectProviderViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/SwapSelectProviderViewModel.kt
@@ -6,29 +6,46 @@ import androidx.lifecycle.viewModelScope
 import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.ViewModelUiState
 import io.horizontalsystems.bankwallet.entities.CurrencyValue
-import io.horizontalsystems.marketkit.models.Token
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.rx2.asFlow
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 class SwapSelectProviderViewModel(
     private val quotes: List<SwapProviderQuote>,
     private val quote: SwapProviderQuote?
 ) : ViewModelUiState<SwapSelectProviderUiState>() {
+    private val normalPriceImpact = BigDecimal(1)
+    private val warningPriceImpact = BigDecimal(6)
+    private val highPriceImpact = BigDecimal(11)
+    private val forbiddenPriceImpact = BigDecimal(50)
+
     private val currencyManager = App.currencyManager
     private val marketKit = App.marketKit
 
     private val currency = currencyManager.baseCurrency
-    private var token: Token = quotes.first().tokenOut
-    private var rate: BigDecimal? = marketKit.coinPrice(token.coin.uid, currency.code)?.value
+    private var tokenIn = quotes.first().tokenIn
+    private var amountIn = quotes.first().amountIn
+    private var tokenOut = quotes.first().tokenOut
+    private var rateTokenIn: BigDecimal? = marketKit.coinPrice(tokenIn.coin.uid, currency.code)?.value
+    private var rateTokenOut: BigDecimal? = marketKit.coinPrice(tokenOut.coin.uid, currency.code)?.value
     private var quoteViewItems = getViewItems(quotes.sorted())
 
     init {
         viewModelScope.launch {
-            marketKit.coinPriceObservable("swap-providers", token.coin.uid, currency.code)
+            marketKit.coinPriceObservable("swap-providers", tokenIn.coin.uid, currency.code)
                 .asFlow()
                 .collect {
-                    rate = it.value
+                    rateTokenIn = it.value
+                    quoteViewItems = getViewItems(quotes.sorted())
+                    emitState()
+                }
+        }
+        viewModelScope.launch {
+            marketKit.coinPriceObservable("swap-providers", tokenOut.coin.uid, currency.code)
+                .asFlow()
+                .collect {
+                    rateTokenOut = it.value
                     quoteViewItems = getViewItems(quotes.sorted())
                     emitState()
                 }
@@ -39,37 +56,69 @@ class SwapSelectProviderViewModel(
         return this.sortedByDescending { it.amountOut }
     }
 
-    private fun getViewItems(quotes: List<SwapProviderQuote>) =
-        quotes.map { quote ->
-            val fiatAmount = getFiatValue(quote.amountOut)?.getFormattedFull()
+    private fun getViewItems(quotes: List<SwapProviderQuote>): List<QuoteViewItem> {
+        val fiatAmountIn = getFiatValue(amountIn, rateTokenIn)
+
+        return quotes.map { quote ->
+            val fiatAmountOut = getFiatValue(quote.amountOut, rateTokenOut)
             val tokenAmount = App.numberFormatter.formatCoinFull(
                 quote.amountOut,
                 quote.tokenOut.coin.code,
                 quote.tokenOut.decimals
             )
+            val priceImpactData = getPriceImpactData(fiatAmountOut?.value, fiatAmountIn?.value)
             QuoteViewItem(
-                quote,
-                fiatAmount,
-                tokenAmount,
-                getPriceImpactData(quote)
-                )
+                quote = quote,
+                fiatAmount = fiatAmountOut?.getFormattedFull(),
+                tokenAmount = tokenAmount,
+                priceImpactData = priceImpactData
+            )
+        }
+    }
+
+    private fun getPriceImpactData(amountOut: BigDecimal?, amountIn: BigDecimal?): PriceImpactData? {
+        var priceImpact = calculateDiff(amountOut, amountIn)
+        val priceImpactAbs = priceImpact?.abs()
+
+        var priceImpactLevel: PriceImpactLevel?
+
+        if (priceImpactAbs == null || priceImpactAbs < normalPriceImpact) {
+            priceImpact = null
+            priceImpactLevel = null
+        } else {
+            priceImpactLevel = when {
+                priceImpactAbs < warningPriceImpact -> PriceImpactLevel.Normal
+                priceImpactAbs < highPriceImpact -> PriceImpactLevel.Warning
+                priceImpactAbs < forbiddenPriceImpact -> PriceImpactLevel.High
+                else -> PriceImpactLevel.Forbidden
+            }
         }
 
-    private fun getPriceImpactData(quote: SwapProviderQuote): PriceImpactData? {
-        return PriceImpactData(20.toBigDecimal(), PriceImpactLevel.Warning) //todo implement price impact calculation
+        return priceImpact?.let {
+            PriceImpactData(it, priceImpactLevel)
+        }
     }
+
+    private fun calculateDiff(amountOut: BigDecimal?, amountIn: BigDecimal?): BigDecimal? {
+        if (amountOut == null || amountIn == null || amountIn.compareTo(BigDecimal.ZERO) == 0) return null
+
+        return (amountOut - amountIn)
+            .divide(amountIn, RoundingMode.DOWN)
+            .times(BigDecimal("100"))
+            .setScale(2, RoundingMode.DOWN)
+            .stripTrailingZeros()
+    }
+
 
     override fun createState() = SwapSelectProviderUiState(
         quoteViewItems = quoteViewItems,
         selectedQuote = quote
     )
 
-    private fun getFiatValue(amount: BigDecimal?): CurrencyValue? {
-        return amount?.let {
-            rate?.multiply(it)
-        }?.let { fiatBalance ->
-            CurrencyValue(currency, fiatBalance)
-        }
+    private fun getFiatValue(amount: BigDecimal?, rate: BigDecimal?): CurrencyValue? {
+        if (amount == null || rate == null) return null
+
+        return CurrencyValue(currency, amount.multiply(rate))
     }
 
     class Factory(private val quotes: List<SwapProviderQuote>, private val quote: SwapProviderQuote?) : ViewModelProvider.Factory {
