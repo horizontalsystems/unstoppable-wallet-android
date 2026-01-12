521681cfef602f08cd2d4942023dc4bbcf6a6b98
abdrasulov <abdrasulov@gmail.com>
2026-01-12 15:56:27 +0600

    Make use of PriceImpactCalculator

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/PriceImpactService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/PriceImpactService.kt
@@ -5,14 +5,8 @@ import io.horizontalsystems.bankwallet.core.HSCaution
 import io.horizontalsystems.bankwallet.core.ServiceState
 import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import java.math.BigDecimal
-import java.math.RoundingMode
 
 class PriceImpactService : ServiceState<PriceImpactService.State>() {
-    private val normalPriceImpact = BigDecimal(1)
-    private val warningPriceImpact = BigDecimal(6)
-    private val highPriceImpact = BigDecimal(11)
-    private val forbiddenPriceImpact = BigDecimal(50)
-
     private var fiatAmountIn: BigDecimal? = null
     private var fiatAmountOut: BigDecimal? = null
     private var fiatPriceImpact: BigDecimal? = null
@@ -34,54 +28,30 @@ class PriceImpactService : ServiceState<PriceImpactService.State>() {
     }
 
     private fun refreshFiatPriceImpact() {
-        val fiatAmountIn = fiatAmountIn
-        val fiatAmountOut = fiatAmountOut
+        val priceImpactData = PriceImpactCalculator.getPriceImpactData(fiatAmountOut, fiatAmountIn)
 
-        val fiatPriceImpact = calculateDiff(fiatAmountOut, fiatAmountIn)
-        val fiatPriceImpactAbs = fiatPriceImpact?.abs()
+        fiatPriceImpact = priceImpactData?.priceImpact
+        fiatPriceImpactLevel = priceImpactData?.priceImpactLevel
 
-        if (fiatPriceImpactAbs == null || fiatPriceImpactAbs < normalPriceImpact) {
-            this.fiatPriceImpact = null
-            fiatPriceImpactLevel = null
-        } else {
-            this.fiatPriceImpact = fiatPriceImpact
-            fiatPriceImpactLevel = when {
-                fiatPriceImpactAbs < warningPriceImpact -> PriceImpactLevel.Normal
-                fiatPriceImpactAbs < highPriceImpact -> PriceImpactLevel.Warning
-                fiatPriceImpactAbs < forbiddenPriceImpact -> PriceImpactLevel.High
-                else -> PriceImpactLevel.Forbidden
+        fiatPriceImpactCaution = when (fiatPriceImpactLevel) {
+            PriceImpactLevel.Forbidden -> {
+                HSCaution(
+                    s = TranslatableString.ResString(R.string.Swap_PriceImpact),
+                    type = HSCaution.Type.Error,
+                    description = TranslatableString.ResString(R.string.Swap_PriceImpactTooHigh, providerTitle ?: "")
+                )
             }
-
-            fiatPriceImpactCaution = when (fiatPriceImpactLevel) {
-                PriceImpactLevel.Forbidden -> {
-                    HSCaution(
-                        s = TranslatableString.ResString(R.string.Swap_PriceImpact),
-                        type = HSCaution.Type.Error,
-                        description = TranslatableString.ResString(R.string.Swap_PriceImpactTooHigh, providerTitle ?: "")
-                    )
-                }
-                PriceImpactLevel.Warning -> {
-                    HSCaution(
-                        s = TranslatableString.ResString(R.string.Swap_PriceImpact),
-                        type = HSCaution.Type.Warning,
-                        description = TranslatableString.ResString(R.string.Swap_PriceImpactWarning)
-                    )
-                }
-                else -> {
-                    null
-                }
+            PriceImpactLevel.Warning -> {
+                HSCaution(
+                    s = TranslatableString.ResString(R.string.Swap_PriceImpact),
+                    type = HSCaution.Type.Warning,
+                    description = TranslatableString.ResString(R.string.Swap_PriceImpactWarning)
+                )
+            }
+            else -> {
+                null
             }
         }
-    }
-
-    private fun calculateDiff(amountOut: BigDecimal?, amountIn: BigDecimal?): BigDecimal? {
-        if (amountOut == null || amountIn == null || amountIn.compareTo(BigDecimal.ZERO) == 0) return null
-
-        return (amountOut - amountIn)
-            .divide(amountIn, RoundingMode.DOWN)
-            .times(BigDecimal("100"))
-            .setScale(2, RoundingMode.DOWN)
-            .stripTrailingZeros()
     }
 
     fun setFiatAmountIn(fiatAmountIn: BigDecimal?) {
