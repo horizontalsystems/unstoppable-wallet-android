27b2eaf31e470898edc0b797eb179544bed2a039
abdrasulov <abdrasulov@gmail.com>
2026-01-13 12:38:56 +0600

    Pass recipient to viewModel on change

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/sendtransaction/AbstractSendTransactionService.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/sendtransaction/AbstractSendTransactionService.kt
@@ -30,8 +30,6 @@ abstract class AbstractSendTransactionService(val hasSettings: Boolean, val hasN
     open fun GetNonceSettingsContent(navController: NavController) = Unit
     @Composable
     open fun GetSlippageSettingsContent(navController: NavController) = Unit
-    @Composable
-    open fun GetRecipientSettingsContent(navController: NavController) = Unit
     abstract suspend fun sendTransaction(mevProtectionEnabled: Boolean = false): SendTransactionResult
 
     fun refreshUuid() {
