16cf753bebfb95a0a93f09c0844108c49d13e09d
Rafael <rafaelekol@gmail.com>
2026-01-22 15:20:59 +0600

    Fix badge spacing in Swap token select page

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/SelectSwapCoinDialogScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/SelectSwapCoinDialogScreen.kt
@@ -3,37 +3,27 @@ package io.horizontalsystems.bankwallet.modules.multiswap
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.bankwallet.core.alternativeImageUrl
 import io.horizontalsystems.bankwallet.core.badge
-import io.horizontalsystems.bankwallet.core.iconPlaceholder
-import io.horizontalsystems.bankwallet.core.imageUrl
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
-import io.horizontalsystems.bankwallet.ui.compose.components.B2
-import io.horizontalsystems.bankwallet.ui.compose.components.Badge
-import io.horizontalsystems.bankwallet.ui.compose.components.D1
-import io.horizontalsystems.bankwallet.ui.compose.components.HSpacer
+import io.horizontalsystems.bankwallet.ui.compose.components.CoinImage
 import io.horizontalsystems.bankwallet.ui.compose.components.HsDivider
-import io.horizontalsystems.bankwallet.ui.compose.components.HsImage
-import io.horizontalsystems.bankwallet.ui.compose.components.MultitextM1
-import io.horizontalsystems.bankwallet.ui.compose.components.RowUniversal
 import io.horizontalsystems.bankwallet.ui.compose.components.SearchBar
-import io.horizontalsystems.bankwallet.ui.compose.components.SectionUniversalItem
 import io.horizontalsystems.bankwallet.ui.compose.components.VSpacer
+import io.horizontalsystems.bankwallet.uiv3.components.BoxBordered
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellMiddleInfo
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellPrimary
+import io.horizontalsystems.bankwallet.uiv3.components.cell.CellRightInfo
+import io.horizontalsystems.bankwallet.uiv3.components.cell.hs
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -57,63 +47,44 @@ fun SelectSwapCoinDialogScreen(
                 HsDivider()
             }
             items(coinBalanceItems) { coinItem ->
-                SectionUniversalItem(borderBottom = true) {
-                    RowUniversal(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp),
-                        onClick = {
-                            onClickItem.invoke(coinItem)
-                        }
-                    ) {
-                        HsImage(
-                            url = coinItem.token.coin.imageUrl,
-                            alternativeUrl = coinItem.token.coin.alternativeImageUrl,
-                            placeholder = coinItem.token.iconPlaceholder,
-                            modifier = Modifier.size(32.dp)
-                        )
-                        Spacer(modifier = Modifier.size(16.dp))
-                        MultitextM1(
-                            modifier = Modifier.weight(1f),
-                            title = {
-                                Row {
-                                    B2(text = coinItem.token.coin.code)
-                                    coinItem.token.badge?.let {
-                                        Badge(text = it)
-                                    }
-                                }
-                            },
-                            subtitle = { D1(text = coinItem.token.coin.name) }
-                        )
-                        HSpacer(8.dp)
-                        MultitextM1(
-                            title = {
-                                coinItem.balance?.let {
-                                    App.numberFormatter.formatCoinShort(
-                                        it,
-                                        coinItem.token.coin.code,
-                                        8
-                                    )
-                                }?.let {
-                                    B2(text = it)
-                                }
-                            },
-                            subtitle = {
-                                coinItem.fiatBalanceValue?.let { fiatBalanceValue ->
+                BoxBordered(
+                    bottom = true
+                ) {
+                    CellPrimary(
+                        left = {
+                            CoinImage(
+                                coin = coinItem.token.coin,
+                                modifier = Modifier.size(32.dp)
+                            )
+                        },
+                        middle = {
+                            CellMiddleInfo(
+                                title = coinItem.token.coin.code.hs,
+                                badge = coinItem.token.badge?.hs,
+                                subtitle = coinItem.token.coin.name.hs,
+                            )
+                        },
+                        right = {
+                            CellRightInfo(
+                                title =
+                                    coinItem.balance?.let {
+                                        App.numberFormatter.formatCoinShort(
+                                            it,
+                                            coinItem.token.coin.code,
+                                            8
+                                        ).hs
+                                    } ?: "".hs,
+                                subtitle = coinItem.fiatBalanceValue?.let { fiatBalanceValue ->
                                     App.numberFormatter.formatFiatShort(
                                         fiatBalanceValue.value,
                                         fiatBalanceValue.currency.symbol,
                                         2
-                                    )
-                                }?.let {
-                                    D1(
-                                        modifier = Modifier.align(Alignment.End),
-                                        text = it
-                                    )
-                                }
-                            }
-                        )
-                    }
+                                    ).hs
+                                } ?: "".hs
+                            )
+                        },
+                        onClick = { onClickItem.invoke(coinItem) },
+                    )
                 }
             }
             item {
