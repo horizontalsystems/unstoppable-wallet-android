89664c0e4730f14f2c824bc17cd185fddc0d9f65
abdrasulov <abdrasulov@gmail.com>
2026-01-22 15:47:04 +0600

    Update swap providers data

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/providers/AllBridgeProvider.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/providers/AllBridgeProvider.kt
@@ -32,7 +32,7 @@ object AllBridgeProvider : IMultiSwapProvider {
     override val title = "AllBridge"
     override val icon = R.drawable.swap_provider_allbridge
     override val type = SwapProviderType.DEX
-    override val aml = true
+    override val aml = false
     private val feePaymentMethod = FeePaymentMethod.StableCoin
 
     private val proxies = mapOf(
