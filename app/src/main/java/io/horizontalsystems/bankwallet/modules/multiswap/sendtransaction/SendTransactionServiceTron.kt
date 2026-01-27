c9e3f29aa561faa5d0775f87aaf1f16c75941b65
abdrasulov <abdrasulov@gmail.com>
2026-01-27 14:18:48 +0600

    Add validation for trx balance

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/sendtransaction/SendTransactionServiceTron.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/sendtransaction/SendTransactionServiceTron.kt
@@ -1,27 +1,39 @@
 package io.horizontalsystems.bankwallet.modules.multiswap.sendtransaction
 
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
+import io.horizontalsystems.bankwallet.core.HSCaution
+import io.horizontalsystems.bankwallet.core.HSCaution.Type
 import io.horizontalsystems.bankwallet.core.ISendTronAdapter
+import io.horizontalsystems.bankwallet.core.ethereum.CautionViewItem
 import io.horizontalsystems.bankwallet.core.isNative
+import io.horizontalsystems.bankwallet.core.providers.Translator
 import io.horizontalsystems.bankwallet.entities.CoinValue
 import io.horizontalsystems.bankwallet.modules.amount.AmountValidator
 import io.horizontalsystems.bankwallet.modules.amount.SendAmountService
 import io.horizontalsystems.bankwallet.modules.send.SendModule
 import io.horizontalsystems.bankwallet.modules.send.tron.SendTronAddressService
 import io.horizontalsystems.bankwallet.modules.send.tron.SendTronFeeService
+import io.horizontalsystems.bankwallet.ui.compose.TranslatableString
 import io.horizontalsystems.marketkit.models.BlockchainType
 import io.horizontalsystems.marketkit.models.Token
 import io.horizontalsystems.marketkit.models.TokenQuery
 import io.horizontalsystems.marketkit.models.TokenType
+import io.horizontalsystems.tronkit.models.Contract
+import io.horizontalsystems.tronkit.models.TransferContract
+import io.horizontalsystems.tronkit.models.TriggerSmartContract
+import io.horizontalsystems.tronkit.network.CreatedTransaction
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import java.math.BigDecimal
+import java.math.BigInteger
 import java.math.RoundingMode
 
 class SendTransactionServiceTron(token: Token) : AbstractSendTransactionService(false, false) {
     override val sendTransactionSettingsFlow = MutableStateFlow(SendTransactionSettings.Tron())
     private val adapter = App.adapterManager.getAdapterForToken<ISendTronAdapter>(token)!!
-    private val feeToken = App.coinManager.getToken(TokenQuery(BlockchainType.Tron, TokenType.Native)) ?: throw IllegalArgumentException()
+    private val nativeToken = App.coinManager.getToken(TokenQuery(BlockchainType.Tron, TokenType.Native)) ?: throw IllegalArgumentException()
 
     private val amountService = SendAmountService(
         AmountValidator(),
@@ -30,15 +42,17 @@ class SendTransactionServiceTron(token: Token) : AbstractSendTransactionService(
         token.type.isNative,
     )
     private val addressService = SendTronAddressService(adapter, token)
-    private val feeService = SendTronFeeService(adapter, feeToken)
+    private val feeService = SendTronFeeService(adapter, nativeToken)
 
     private var amountState = amountService.stateFlow.value
     private var addressState = addressService.stateFlow.value
     private var feeState = feeService.stateFlow.value
 
     private var networkFee: SendModule.AmountData? = null
+    private var cautions: List<CautionViewItem> = listOf()
     private var sendTransactionData: SendTransactionData.Tron? = null
     private var loading = true
+    private var nativeTokenAmount: BigDecimal? = null
 
     override fun start(coroutineScope: CoroutineScope) {
         coroutineScope.launch {
@@ -62,7 +76,25 @@ class SendTransactionServiceTron(token: Token) : AbstractSendTransactionService(
         feeState = state
 
         networkFee = feeState.fee?.let {
-            getAmountData(CoinValue(feeToken, it))
+            getAmountData(CoinValue(nativeToken, it))
+        }
+
+        cautions = buildList {
+            val total = (nativeTokenAmount ?: BigDecimal.ZERO) + (feeState.fee ?: BigDecimal.ZERO)
+            if (adapter.trxBalanceData.available < total) {
+                add(
+                    HSCaution(
+                        s = TranslatableString.PlainString(Translator.getString(R.string.EthereumTransaction_Error_InsufficientBalance_Title)),
+                        type = Type.Error,
+                        description = TranslatableString.PlainString(
+                            Translator.getString(
+                                R.string.EthereumTransaction_Error_InsufficientBalanceForFee,
+                                nativeToken.coin.code
+                            )
+                        )
+                    ).toCautionViewItem()
+                )
+            }
         }
 
         emitState()
@@ -93,15 +125,13 @@ class SendTransactionServiceTron(token: Token) : AbstractSendTransactionService(
 
         if (data is SendTransactionData.Tron.WithContract) {
             feeService.setContract(data.contract)
+            nativeTokenAmount = extractTrxSun(data.contract)?.toBigDecimal(nativeToken.decimals)
         } else if (data is SendTransactionData.Tron.WithCreateTransaction) {
             feeService.setCreatedTransaction(data.transaction)
+            nativeTokenAmount = extractTrxSun(data.transaction).toBigDecimal(nativeToken.decimals)
         }
 
         emitState()
-
-//        amountService.setAmount()
-
-//        addressService.setAddress()
     }
 
     override suspend fun sendTransaction(mevProtectionEnabled: Boolean): SendTransactionResult {
@@ -113,7 +143,7 @@ class SendTransactionServiceTron(token: Token) : AbstractSendTransactionService(
                 adapter.send(tmpSendTransactionData.transaction)
             }
             null -> {
-                adapter.send(amountState.amount!!, addressState.tronAddress!!, feeState.feeLimit)
+                throw IllegalStateException("Not supported")
             }
         }
 
@@ -123,9 +153,48 @@ class SendTransactionServiceTron(token: Token) : AbstractSendTransactionService(
     override fun createState() = SendTransactionServiceState(
         uuid = uuid,
         networkFee = networkFee,
-        cautions = listOf(),
-        sendable = sendTransactionData != null || (amountState.canBeSend && feeState.canBeSend && addressState.canBeSend),
+        cautions = cautions,
+        sendable = feeState.canBeSend && cautions.none {
+            it.type == CautionViewItem.Type.Error
+        },
         loading = loading,
         fields = listOf(),
     )
+
+    fun extractTrxSun(contract: Contract) = when (contract) {
+        is TransferContract -> contract.amount
+        is TriggerSmartContract -> contract.callValue
+        else -> null
+    }
+
+    fun extractTrxSun(tx: CreatedTransaction): BigInteger {
+        val rawData = tx.raw_data
+        val contracts = rawData.contract
+
+        var totalSun = BigInteger.ZERO
+
+        for (contract in contracts) {
+            val type = contract.type
+
+            val parameter = contract.parameter
+            val value = parameter.value
+
+            when (type) {
+                "TransferContract" -> {
+                    value.amount?.let {
+                        totalSun += it
+                    }
+                }
+
+                "TriggerSmartContract",
+                "CreateSmartContract" -> {
+                    value.call_value?.let {
+                        totalSun += it
+                    }
+                }
+            }
+        }
+
+        return totalSun
+    }
 }
