c9aac95d3488ad16e9506807f8aa44dec55f8186
abdrasulov <abdrasulov@gmail.com>
2026-02-05 18:12:36 +0600

    Add entry point for Nav3

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/nav3/Nav3Fragment.kt
@@ -0,0 +1,15 @@
+package io.horizontalsystems.bankwallet.modules.nav3
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import io.horizontalsystems.bankwallet.core.BaseComposeFragment
+
+class Nav3Fragment : BaseComposeFragment() {
+
+    @Composable
+    override fun GetContent(navController: NavController) {
+
+
+    }
+
+}
