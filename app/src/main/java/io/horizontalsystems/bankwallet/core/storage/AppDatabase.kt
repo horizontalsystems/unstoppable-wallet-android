e7b6caad7d1251ca093720e47a282fdc9ef2f52b
chyngyz <chyngyz.omurov@gmail.com>
2026-02-25 17:07:30 +0600

    Merge migrations and downgrade database version to 72

--- app/src/main/java/io/horizontalsystems/bankwallet/core/storage/AppDatabase.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/storage/AppDatabase.kt
@@ -46,7 +46,6 @@ import io.horizontalsystems.bankwallet.core.storage.migrations.Migration_68_69
 import io.horizontalsystems.bankwallet.core.storage.migrations.Migration_69_70
 import io.horizontalsystems.bankwallet.core.storage.migrations.Migration_70_71
 import io.horizontalsystems.bankwallet.core.storage.migrations.Migration_71_72
-import io.horizontalsystems.bankwallet.core.storage.migrations.Migration_72_73
 import io.horizontalsystems.bankwallet.entities.ActiveAccount
 import io.horizontalsystems.bankwallet.entities.BlockchainSettingRecord
 import io.horizontalsystems.bankwallet.entities.EnabledWallet
@@ -78,7 +77,7 @@ import io.horizontalsystems.bankwallet.modules.profeatures.storage.ProFeaturesSe
 import io.horizontalsystems.bankwallet.modules.walletconnect.storage.WCSessionDao
 import io.horizontalsystems.bankwallet.modules.walletconnect.storage.WalletConnectV2Session
 
-@Database(version = 73, exportSchema = false, entities = [
+@Database(version = 72, exportSchema = false, entities = [
     EnabledWallet::class,
     EnabledWalletCache::class,
     AccountRecord::class,
@@ -193,7 +192,6 @@ abstract class AppDatabase : RoomDatabase() {
                             Migration_69_70,
                             Migration_70_71,
                             Migration_71_72,
-                            Migration_72_73,
                     )
                     .build()
         }
