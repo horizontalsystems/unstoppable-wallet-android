3f5583f8a9b7fb0aec42a852fc06e8e3096d29b8
abdrasulov <abdrasulov@gmail.com>
2026-02-27 11:16:01 +0600

    Navigate via Nav3

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/contacts/Mode.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/contacts/Mode.kt
@@ -1,16 +1,30 @@
 package io.horizontalsystems.bankwallet.modules.contacts
 
 import android.os.Parcelable
+import io.horizontalsystems.bankwallet.serializers.BlockchainTypeSerializer
 import io.horizontalsystems.marketkit.models.BlockchainType
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
 sealed class Mode : Parcelable {
     @Parcelize
+    @Serializable
     object Full : Mode()
 
     @Parcelize
-    class AddAddressToExistingContact(val blockchainType: BlockchainType, val address: String) : Mode()
+    @Serializable
+    class AddAddressToExistingContact(
+        @Serializable(with = BlockchainTypeSerializer::class)
+        val blockchainType: BlockchainType,
+        val address: String
+    ) : Mode()
 
     @Parcelize
-    class AddAddressToNewContact(val blockchainType: BlockchainType, val address: String) : Mode()
+    @Serializable
+    class AddAddressToNewContact(
+        @Serializable(with = BlockchainTypeSerializer::class)
+        val blockchainType: BlockchainType,
+        val address: String
+    ) : Mode()
 }
