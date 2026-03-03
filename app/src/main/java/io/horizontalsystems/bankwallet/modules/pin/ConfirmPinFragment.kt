daf773ae2a4ec71767b0a9130cee88a9a73ea36a
abdrasulov <abdrasulov@gmail.com>
2026-03-03 14:57:23 +0600

    Implement nav3 screens

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ConfirmPinFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/ConfirmPinFragment.kt
@@ -3,29 +3,48 @@ package io.horizontalsystems.bankwallet.modules.pin
 import android.os.Parcelable
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
-import io.horizontalsystems.bankwallet.core.setNavigationResultX
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
+import io.horizontalsystems.bankwallet.modules.nav3.ResultEventBus
 import io.horizontalsystems.bankwallet.modules.pin.ui.PinConfirm
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object ConfirmPinScreen : HSScreen()
+data object ConfirmPinScreen : HSScreen() {
+    @Composable
+    override fun GetContent(
+        backStack: NavBackStack<HSScreen>,
+        resultBus: ResultEventBus
+    ) {
+        PinConfirm(
+            onSuccess = {
+                resultBus.sendResult(result = Result(true))
+                backStack.removeLastOrNull()
+            },
+            onCancel = {
+                backStack.removeLastOrNull()
+            }
+        )
+    }
+
+    data class Result(val success: Boolean)
+}
 
 class ConfirmPinFragment : BaseComposeFragment(screenshotEnabled = false) {
 
     @Composable
     override fun GetContent(navController: NavController) {
-        PinConfirm(
-            onSuccess = {
-                navController.setNavigationResultX(Result(true))
-                navController.popBackStack()
-            },
-            onCancel = {
-                navController.popBackStack()
-            }
-        )
+//        PinConfirm(
+//            onSuccess = {
+//                navController.setNavigationResultX(Result(true))
+//                navController.popBackStack()
+//            },
+//            onCancel = {
+//                navController.popBackStack()
+//            }
+//        )
     }
 
     @Parcelize
