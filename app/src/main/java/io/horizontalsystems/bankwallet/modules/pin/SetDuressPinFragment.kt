f14546f7d0865a41f79a5dd03ec4a62961b6de86
abdrasulov <abdrasulov@gmail.com>
2026-03-03 14:18:35 +0600

    Implement nav3 screens

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/SetDuressPinFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/SetDuressPinFragment.kt
@@ -6,24 +6,25 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
-import io.horizontalsystems.bankwallet.core.getInput
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
+import io.horizontalsystems.bankwallet.modules.nav3.ResultEventBus
 import io.horizontalsystems.bankwallet.modules.pin.ui.PinSet
 import io.horizontalsystems.core.helpers.HudHelper
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object SetDuressPinScreen : HSScreen()
-
-class SetDuressPinFragment : BaseComposeFragment(screenshotEnabled = false) {
-
+data class SetDuressPinScreen(val accountIds: List<String>) : HSScreen() {
     @Composable
-    override fun GetContent(navController: NavController) {
+    override fun GetContent(
+        backStack: NavBackStack<HSScreen>,
+        resultBus: ResultEventBus
+    ) {
         val viewModel = viewModel<SetDuressPinViewModel>(
-            factory = SetDuressPinViewModel.Factory(navController.getInput())
+            factory = SetDuressPinViewModel.Factory(accountIds)
         )
         val view = LocalView.current
         PinSet(
@@ -32,11 +33,34 @@ class SetDuressPinFragment : BaseComposeFragment(screenshotEnabled = false) {
             dismissWithSuccess = {
                 viewModel.onDuressPinSet()
                 HudHelper.showSuccessMessage(view, R.string.Hud_Text_Created)
-                navController.popBackStack(R.id.setDuressPinIntroFragment, true)
+//                TODO("xxx nav3")
+//                navController.popBackStack(R.id.setDuressPinIntroFragment, true)
             },
-            onBackPress = { navController.popBackStack() },
+            onBackPress = { backStack.removeLastOrNull() },
             forDuress = true
         )
+    }
+}
+
+class SetDuressPinFragment : BaseComposeFragment(screenshotEnabled = false) {
+
+    @Composable
+    override fun GetContent(navController: NavController) {
+//        val viewModel = viewModel<SetDuressPinViewModel>(
+//            factory = SetDuressPinViewModel.Factory(navController.getInput())
+//        )
+//        val view = LocalView.current
+//        PinSet(
+//            title = stringResource(id = R.string.SetDuressPin_Title),
+//            description = stringResource(id = R.string.SetDuressPin_Description),
+//            dismissWithSuccess = {
+//                viewModel.onDuressPinSet()
+//                HudHelper.showSuccessMessage(view, R.string.Hud_Text_Created)
+//                navController.popBackStack(R.id.setDuressPinIntroFragment, true)
+//            },
+//            onBackPress = { navController.popBackStack() },
+//            forDuress = true
+//        )
     }
 
     @Parcelize
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/SetDuressPinViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/pin/SetDuressPinViewModel.kt
@@ -6,21 +6,20 @@ import io.horizontalsystems.bankwallet.core.App
 import io.horizontalsystems.bankwallet.core.managers.UserManager
 
 class SetDuressPinViewModel(
-    private val input: SetDuressPinFragment.Input?,
     private val userManager: UserManager,
+    private val accountIds: List<String>?,
 ) : ViewModel() {
 
     fun onDuressPinSet() {
-        val accountIds = input?.accountIds
         if (!accountIds.isNullOrEmpty()) {
             userManager.allowAccountsForDuress(accountIds)
         }
     }
 
-    class Factory(private val input: SetDuressPinFragment.Input?) : ViewModelProvider.Factory {
+    class Factory(val accountIds: List<String>?) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return SetDuressPinViewModel(input, App.userManager) as T
+            return SetDuressPinViewModel(App.userManager, accountIds) as T
         }
     }
 
