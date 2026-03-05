2652af482e08f28181848ec1cf6cad41e1153558
abdrasulov <abdrasulov@gmail.com>
2026-03-05 15:03:26 +0600

    Implement Nav3 screens

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/eip20approve/Eip20ApproveTransactionSettingsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/eip20approve/Eip20ApproveTransactionSettingsFragment.kt
@@ -1,34 +1,40 @@
 package io.horizontalsystems.bankwallet.modules.eip20approve
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import io.horizontalsystems.bankwallet.R
+import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
+import io.horizontalsystems.bankwallet.modules.nav3.ResultEventBus
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object Eip20ApproveTransactionSettingsScreen : HSScreen()
+data object Eip20ApproveTransactionSettingsScreen : HSScreen(
+    parentScreenClass = Eip20ApproveScreen::class
+) {
+    @Composable
+    override fun GetContent(
+        backStack: NavBackStack<HSScreen>,
+        resultBus: ResultEventBus
+    ) {
+        Eip20ApproveTransactionSettingsScreen(backStack)
+    }
+}
 
 class Eip20ApproveTransactionSettingsFragment : BaseComposeFragment() {
     @Composable
     override fun GetContent(navController: NavController) {
-        Eip20ApproveTransactionSettingsScreen(navController)
+//        Eip20ApproveTransactionSettingsScreen(navController)
     }
 }
 
 @Composable
-fun Eip20ApproveTransactionSettingsScreen(navController: NavController) {
-    val viewModelStoreOwner = remember(navController.currentBackStackEntry) {
-        navController.getBackStackEntry(R.id.eip20ApproveFragment)
-    }
-    val viewModel = viewModel<Eip20ApproveViewModel>(
-        viewModelStoreOwner = viewModelStoreOwner,
-    )
+fun Eip20ApproveTransactionSettingsScreen(navController: NavBackStack<HSScreen>) {
+    val viewModel = viewModel<Eip20ApproveViewModel>()
 
     val sendTransactionService = viewModel.sendTransactionService
 
-    sendTransactionService.GetSettingsContent(navController)
+//    TODO("xxx nav3")
+//    sendTransactionService.GetSettingsContent(navController)
 }
--- app/src/main/java/io/horizontalsystems/bankwallet/modules/eip20revoke/Eip20RevokeTransactionSettingsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/eip20revoke/Eip20RevokeTransactionSettingsFragment.kt
@@ -1,35 +1,40 @@
 package io.horizontalsystems.bankwallet.modules.eip20revoke
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import io.horizontalsystems.bankwallet.R
+import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
+import io.horizontalsystems.bankwallet.modules.nav3.ResultEventBus
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object Eip20RevokeTransactionSettingsScreen : HSScreen()
+data object Eip20RevokeTransactionSettingsScreen : HSScreen(
+    parentScreenClass = Eip20RevokeConfirmScreen::class
+) {
+    @Composable
+    override fun GetContent(
+        backStack: NavBackStack<HSScreen>,
+        resultBus: ResultEventBus
+    ) {
+        Eip20RevokeTransactionSettingsScreen(backStack)
+    }
+}
 
 class Eip20RevokeTransactionSettingsFragment : BaseComposeFragment() {
     @Composable
     override fun GetContent(navController: NavController) {
-        Eip20RevokeTransactionSettingsScreen(navController)
+//        Eip20RevokeTransactionSettingsScreen(navController)
     }
 }
 
 @Composable
-fun Eip20RevokeTransactionSettingsScreen(navController: NavController) {
-    val viewModelStoreOwner = remember(navController.currentBackStackEntry) {
-        navController.getBackStackEntry(R.id.eip20RevokeConfirmFragment)
-    }
-
-    val viewModel = viewModel<Eip20RevokeConfirmViewModel>(
-        viewModelStoreOwner = viewModelStoreOwner,
-    )
+fun Eip20RevokeTransactionSettingsScreen(navController: NavBackStack<HSScreen>) {
+    val viewModel = viewModel<Eip20RevokeConfirmViewModel>()
 
     val sendTransactionService = viewModel.sendTransactionService
 
-    sendTransactionService.GetSettingsContent(navController)
+//    TODO("xxx nav3")
+//    sendTransactionService.GetSettingsContent(navController)
 }
