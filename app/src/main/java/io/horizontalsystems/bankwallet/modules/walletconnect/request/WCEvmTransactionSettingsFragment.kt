1c41aed5e17a537f1ae18e6c263e8511c19c6320
abdrasulov <abdrasulov@gmail.com>
2026-03-05 13:34:02 +0600

    Implement Nav3 screens

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/request/WCEvmTransactionSettingsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/walletconnect/request/WCEvmTransactionSettingsFragment.kt
@@ -1,36 +1,41 @@
 package io.horizontalsystems.bankwallet.modules.walletconnect.request
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import io.horizontalsystems.bankwallet.R
+import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
+import io.horizontalsystems.bankwallet.modules.nav3.ResultEventBus
 import io.horizontalsystems.bankwallet.modules.walletconnect.request.sendtransaction.WCSendEthereumTransactionRequestViewModel
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object WCEvmTransactionSettingsScreen : HSScreen()
+data object WCEvmTransactionSettingsScreen : HSScreen(
+    parentScreenClass = WCRequestScreen::class
+) {
+    @Composable
+    override fun GetContent(
+        backStack: NavBackStack<HSScreen>,
+        resultBus: ResultEventBus
+    ) {
+        WCEvmTransactionSettingsScreen(backStack)
+    }
+}
 
 class WCEvmTransactionSettingsFragment : BaseComposeFragment() {
     @Composable
     override fun GetContent(navController: NavController) {
-        WCEvmTransactionSettingsScreen(navController)
+//        WCEvmTransactionSettingsScreen(navController)
     }
 }
 
 @Composable
-fun WCEvmTransactionSettingsScreen(navController: NavController) {
-    val viewModelStoreOwner = remember(navController.currentBackStackEntry) {
-        navController.getBackStackEntry(R.id.wcRequestFragment)
-    }
-
-    val viewModel = viewModel<WCSendEthereumTransactionRequestViewModel>(
-        viewModelStoreOwner = viewModelStoreOwner,
-    )
+fun WCEvmTransactionSettingsScreen(navController: NavBackStack<HSScreen>) {
+    val viewModel = viewModel<WCSendEthereumTransactionRequestViewModel>()
 
     val sendTransactionService = viewModel.sendTransactionService
 
-    sendTransactionService.GetSettingsContent(navController)
+//    TODO("xxx nav3")
+//    sendTransactionService.GetSettingsContent(navController)
 }
