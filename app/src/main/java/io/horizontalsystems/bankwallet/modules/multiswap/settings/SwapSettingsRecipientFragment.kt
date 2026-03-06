0ef62eed5883335e2d571fd95849404669bbfa43
abdrasulov <abdrasulov@gmail.com>
2026-03-06 16:48:34 +0600

    Implement nav3 screens

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/settings/SwapSettingsRecipientFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/multiswap/settings/SwapSettingsRecipientFragment.kt
@@ -2,33 +2,50 @@ package io.horizontalsystems.bankwallet.modules.multiswap.settings
 
 import android.os.Parcelable
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import androidx.navigation3.runtime.NavBackStack
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 import io.horizontalsystems.bankwallet.entities.Address
+import io.horizontalsystems.bankwallet.modules.enteraddress.EnterAddressScreen
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
+import io.horizontalsystems.bankwallet.modules.nav3.ResultEventBus
+import io.horizontalsystems.bankwallet.serializers.TokenSerializer
 import io.horizontalsystems.marketkit.models.Token
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Serializable
-data object SwapSettingsRecipientScreen : HSScreen()
+data class SwapSettingsRecipientScreen(
+    @Serializable(with = TokenSerializer::class)
+    val token: Token,
+    val recipient: Address?
+) : HSScreen() {
+    @Composable
+    override fun GetContent(
+        backStack: NavBackStack<HSScreen>,
+        resultBus: ResultEventBus
+    ) {
+        EnterAddressScreen(
+            backStack = backStack,
+            token = token,
+            title = stringResource(R.string.SendEvmSettings_SetRecipient),
+            buttonTitle = stringResource(R.string.Button_Apply),
+            allowNull = true,
+            initialAddress = recipient?.hex
+        ) { address, _ ->
+            resultBus.sendResult(result = Result(address))
+            backStack.removeLastOrNull()
+        }
+    }
+
+    data class Result(val address: Address?)
+}
 
 class SwapSettingsRecipientFragment : BaseComposeFragment() {
     @Composable
     override fun GetContent(navController: NavController) {
-//        withInput<Input>(navController) { input ->
-//            EnterAddressScreen(
-//                navController = navController,
-//                token = input.token,
-//                title = stringResource(SendEvmSettings_SetRecipient),
-//                buttonTitle = stringResource(Button_Apply),
-//                allowNull = true,
-//                initialAddress = input.recipient?.hex
-//            ) { address, _ ->
-//                navController.setNavigationResultX<Result>(Result(address))
-//                navController.popBackStack()
-//            }
-//        }
     }
 
     @Parcelize
