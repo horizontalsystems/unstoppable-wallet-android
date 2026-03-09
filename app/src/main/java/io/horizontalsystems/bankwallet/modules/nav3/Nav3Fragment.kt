11155f1c795a5b1fd147896ac69b1316241c29ee
abdrasulov <abdrasulov@gmail.com>
2026-03-09 12:18:38 +0600

    Add shortcut for getting vm by screen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/nav3/Nav3Fragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/nav3/Nav3Fragment.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSerializable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation3.runtime.NavBackStack
@@ -224,4 +225,13 @@ fun NavBackStack<HSScreen>.removeLastUntil(klass: KClass<out HSScreen>, inclusiv
             removeAt(index)
         }
     }
+}
+
+@Composable
+inline fun <reified VM : ViewModel> NavBackStack<HSScreen>.viewModelForScreen(klass: KClass<out HSScreen>) : VM {
+    val hSScreen = checkNotNull(findLast { it::class == klass })
+
+    return viewModel(
+        viewModelStoreOwner = rememberChildViewModelStoreOwner(hSScreen.contentKey()),
+    )
 }
