5164407b5e6525786edd013b40e4f12c60ed778b
abdrasulov <abdrasulov@gmail.com>
2026-03-19 17:47:30 +0600

    Implement MainScreen

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/nav3/MainScreen.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/nav3/MainScreen.kt
@@ -1,6 +1,11 @@
 package io.horizontalsystems.bankwallet.modules.nav3
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import io.horizontalsystems.bankwallet.modules.main.MainActivityViewModel
+import io.horizontalsystems.bankwallet.modules.main.MainScreenWithRootedDeviceCheck
+import io.horizontalsystems.bankwallet.modules.transactions.TransactionsModule
+import io.horizontalsystems.bankwallet.modules.transactions.TransactionsViewModel
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -8,7 +13,16 @@ data object MainScreen : HSScreen() {
     // TODO("Nav3: need to find other solution. There should not be mainActivityViewModel")
     lateinit var mainActivityViewModel: MainActivityViewModel
 
-//    @Composable
+    @Composable
+    override fun GetContent(navController: NavController) {
+        MainScreenWithRootedDeviceCheck(
+            transactionsViewModel = viewModel<TransactionsViewModel>(factory = TransactionsModule.Factory()),
+            navController = navController,
+            mainActivityViewModel = mainActivityViewModel,
+        )
+    }
+
+    //    @Composable
 //    override fun GetContent(backStack: NavBackStack<HSScreen>) {
 //        MainScreenWithRootedDeviceCheck(
 //            mainActivityViewModel = mainActivityViewModel,
