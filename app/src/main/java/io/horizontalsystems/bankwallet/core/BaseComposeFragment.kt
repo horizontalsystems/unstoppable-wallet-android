8d3df2ff2dc717154c02ff5b7073ccf7be63261f
abdrasulov <abdrasulov@gmail.com>
2026-03-25 11:34:09 +0600

    Remove redundant

--- app/src/main/java/io/horizontalsystems/bankwallet/core/BaseComposeFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/BaseComposeFragment.kt
@@ -1,20 +1,7 @@
 package io.horizontalsystems.bankwallet.core
 
-import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
-import androidx.annotation.LayoutRes
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
-import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 
 abstract class BaseComposeFragment(
     screenshotEnabled: Boolean = true
@@ -22,56 +9,5 @@ abstract class BaseComposeFragment(
 
     val childFragmentManager: FragmentManager
         get() = TODO()
-
-}
-
-abstract class BaseComposeFragmentX(
-    @LayoutRes layoutResId: Int = 0,
-    private val screenshotEnabled: Boolean = true
-) : Fragment(layoutResId) {
-
-    final override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setViewCompositionStrategy(
-                ViewCompositionStrategy.DisposeOnLifecycleDestroyed(viewLifecycleOwner)
-            )
-
-            setContent {
-                ComposeAppTheme {
-//                    GetContent(findNavController())
-                }
-            }
-        }
-    }
-
-    @Composable
-    abstract fun GetContent(navController: NavBackStack<HSScreen>)
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("AAA", "Fragment: ${this.javaClass.simpleName}")
-        if (screenshotEnabled) {
-            allowScreenshot()
-        } else {
-            disallowScreenshot()
-        }
-    }
-
-    override fun onPause() {
-        disallowScreenshot()
-        super.onPause()
-    }
-
-    private fun allowScreenshot() {
-        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
-    }
-
-    private fun disallowScreenshot() {
-        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
-    }
 
 }
