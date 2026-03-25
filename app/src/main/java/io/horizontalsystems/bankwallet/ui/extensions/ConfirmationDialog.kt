19345e342d047902f15805070b70fe2c81484a0e
abdrasulov <abdrasulov@gmail.com>
2026-03-25 15:30:42 +0600

    Convert ConfirmationDialog to inline bottom sheet

--- app/src/main/java/io/horizontalsystems/bankwallet/ui/extensions/ConfirmationDialog.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/ui/extensions/ConfirmationDialog.kt
@@ -1,6 +1,5 @@
 package io.horizontalsystems.bankwallet.ui.extensions
 
-import android.content.DialogInterface
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,118 +9,63 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.fragment.app.FragmentManager
-import androidx.navigation3.runtime.NavBackStack
 import io.horizontalsystems.bankwallet.R
-import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryTransparent
 import io.horizontalsystems.bankwallet.ui.compose.components.ButtonPrimaryYellow
 import io.horizontalsystems.bankwallet.ui.compose.components.TextImportantWarning
 
-class ConfirmationDialog(
-    private val listener: Listener,
-    private val title: String,
-    private val icon: Int?,
-    private val warningTitle: String?,
-    private val warningText: String?,
-    private val actionButtonTitle: String?,
-    private val transparentButtonTitle: String?,
-) : BaseComposableBottomSheetFragment() {
+@Composable
+fun ConfirmationDialog(
+    title: String,
+    icon: Int?,
+    warningTitle: String?,
+    warningText: String?,
+    actionButtonTitle: String?,
+    transparentButtonTitle: String?,
+    onClose: () -> Unit,
+    actionButtonXxx: () -> Unit,
+    transparentButtonXxx: () -> Unit
+) {
+    BottomSheetHeader(
+        iconPainter = painterResource(icon ?: R.drawable.ic_attention_24),
+        iconTint = ColorFilter.tint(ComposeAppTheme.colors.jacob),
+        title = title,
+        onCloseClick = onClose
+    ) {
 
-    interface Listener {
-        fun onActionButtonClick() {}
-        fun onTransparentButtonClick() {}
-        fun onCancelButtonClick() {}
-    }
-
-    override fun onCancel(dialog: DialogInterface) {
-        super.onCancel(dialog)
-        listener.onCancelButtonClick()
-    }
-
-    override fun close() {
-        super.close()
-        listener.onCancelButtonClick()
-    }
-
-    @Composable
-    override fun GetContent(navController: NavBackStack<HSScreen>) {
-        BottomScreen(navController)
-    }
-
-    @Composable
-    private fun BottomScreen(navController: NavBackStack<HSScreen>) {
-        BottomSheetHeader(
-            iconPainter = painterResource(icon ?: R.drawable.ic_attention_24),
-            iconTint = ColorFilter.tint(ComposeAppTheme.colors.jacob),
-            title = title,
-            onCloseClick = { navController.removeLastOrNull() }
-        ) {
-
-            warningText?.let {
-                TextImportantWarning(
-                    title = warningTitle,
-                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-                    text = it
-                )
-                Spacer(Modifier.height(8.dp))
-            }
-            actionButtonTitle?.let {
-                ButtonPrimaryYellow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 24.dp, top = 12.dp, end = 24.dp),
-                    title = actionButtonTitle,
-                    onClick = {
-                        listener.onActionButtonClick()
-                        navController.removeLastOrNull()
-                    }
-                )
-            }
-            transparentButtonTitle?.let {
-                ButtonPrimaryTransparent(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 24.dp, top = 12.dp, end = 24.dp),
-                    title = transparentButtonTitle,
-                    onClick = {
-                        listener.onTransparentButtonClick()
-                        navController.removeLastOrNull()
-                    }
-                )
-            }
-            Spacer(Modifier.height(32.dp))
-        }
-    }
-
-    companion object {
-
-        fun show(
-            icon: Int? = null,
-            title: String,
-            warningTitle: String? = null,
-            warningText: String?,
-            actionButtonTitle: String? = "",
-            transparentButtonTitle: String? = "",
-            fragmentManager: FragmentManager,
-            listener: Listener,
-        ) {
-
-            val fragment = ConfirmationDialog(
-                listener,
-                title,
-                icon,
-                warningTitle,
-                warningText,
-                actionButtonTitle,
-                transparentButtonTitle,
+        warningText?.let {
+            TextImportantWarning(
+                title = warningTitle,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
+                text = it
             )
-            val transaction = fragmentManager.beginTransaction()
-
-//            TODO("xxx nav3")
-//            transaction.add(fragment, "bottom_coin_settings_alert_dialog")
-//            transaction.commitAllowingStateLoss()
+            Spacer(Modifier.height(8.dp))
         }
+        actionButtonTitle?.let {
+            ButtonPrimaryYellow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, top = 12.dp, end = 24.dp),
+                title = actionButtonTitle,
+                onClick = {
+                    actionButtonXxx()
+                    onClose()
+                }
+            )
+        }
+        transparentButtonTitle?.let {
+            ButtonPrimaryTransparent(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp, top = 12.dp, end = 24.dp),
+                title = transparentButtonTitle,
+                onClick = {
+                    transparentButtonXxx()
+                    onClose()
+                }
+            )
+        }
+        Spacer(Modifier.height(32.dp))
     }
 }
