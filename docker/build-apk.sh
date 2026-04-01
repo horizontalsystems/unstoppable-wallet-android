4781d218aae5e0d7f8ec40ab3e34f7f35cdf0f3e
abdrasulov <abdrasulov@gmail.com>
2026-04-01 15:50:25 +0600

    Use tmp dir when building apk

--- docker/build-apk.sh
+++ docker/build-apk.sh
@@ -22,31 +22,29 @@ fi
 ####################################
 DOCKER_IMAGE="hsdao/android-release-build:java-17-update"
 GIT_REPO="https://github.com/horizontalsystems/unstoppable-wallet-android"
-WORK_DIR="$PWD/app"
+WORK_DIR="$(mktemp -d)"
 TAG=$1
 
 KEYSTORE=$2
 KEYSTORE_FILENAME="$(basename -- $KEYSTORE)"
 KEYSTORE_PASSWORD=$3
-BUILT_APK_FILE="app/app/build/outputs/apk/base/release/app-base-release.apk"
+BUILT_APK_FILE="${WORK_DIR}/app/build/outputs/apk/base/release/app-base-release.apk"
 ####################################
 
 function init() {
-    echo "Removing folder:${WORK_DIR}  ... "
-
-    rm -Rf $WORK_DIR
+    echo "Work dir: ${WORK_DIR}"
 }
 
 function gitClone () {
     echo "Trying to clone version ${TAG} ..."
-    git clone --quiet --branch "${TAG}" --depth 1 ${GIT_REPO} app || exit 1
+    git clone --quiet --branch "${TAG}" --depth 1 ${GIT_REPO} "${WORK_DIR}" || exit 1
 }
 
 function buildApk () {
     echo "Starting docker container  ${DOCKER_IMAGE} ..."
     echo "Building apk file ..."
 
-    docker run -it --volume $PWD/app:/mnt --workdir /mnt ${DOCKER_IMAGE} bash -x -c \
+    docker run -it --volume "${WORK_DIR}:/mnt" --workdir /mnt ${DOCKER_IMAGE} bash -x -c \
       './gradlew clean :app:assembleBaseRelease'
 
 }
@@ -54,17 +52,17 @@ function buildApk () {
 function signApk () {
     echo "Signing apk with  ${KEYSTORE}  ... "
 
-    cp $KEYSTORE $PWD
+    cp $KEYSTORE "${WORK_DIR}"
 
-    response=$(docker run -it --volume $PWD:/mnt --workdir /mnt ${DOCKER_IMAGE} bash -x -c \
+    response=$(docker run -it --volume "${WORK_DIR}:/mnt" --workdir /mnt ${DOCKER_IMAGE} bash -x -c \
       "apksigner sign --ks ${KEYSTORE_FILENAME} --ks-pass pass:${KEYSTORE_PASSWORD} ${BUILT_APK_FILE}")
 
     echo $response
-    rm "${PWD}/${KEYSTORE_FILENAME}"
+    rm "${WORK_DIR}/${KEYSTORE_FILENAME}"
 }
 
 function end () {
-    cp $BUILT_APK_FILE "${PWD}/app-release-signed.apk"
+    cp "${BUILT_APK_FILE}" "${PWD}/app-release-signed.apk"
 }
 
 #############################################
