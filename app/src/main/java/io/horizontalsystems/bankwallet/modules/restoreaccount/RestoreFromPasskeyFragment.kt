85fee949a131d52a4a0c6684651acca33b2354fb
abdrasulov <abdrasulov@gmail.com>
2026-05-18 12:53:48 +0600

    Refactor RestoreFromPasskeyFragment

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/restoreaccount/RestoreFromPasskeyFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/restoreaccount/RestoreFromPasskeyFragment.kt
@@ -1,23 +1,35 @@
 package io.horizontalsystems.bankwallet.modules.restoreaccount
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.horizontalsystems.bankwallet.core.stats.StatPage
+import io.horizontalsystems.bankwallet.modules.manageaccounts.ManageAccountsModule
 import io.horizontalsystems.bankwallet.modules.nav3.HSNavigation
 import io.horizontalsystems.bankwallet.modules.nav3.HSScreen
-import io.horizontalsystems.bankwallet.modules.nav3.ResultEffect
-import io.horizontalsystems.bankwallet.modules.restoreaccount.restoreblockchains.ManageWalletsScreen
 import io.horizontalsystems.bankwallet.serializers.HSScreenKClassSerializer
 import kotlinx.serialization.Serializable
-import java.util.UUID
 import kotlin.reflect.KClass
 
 @Serializable
 data class RestoreFromPasskeyFragment(val input: Input) : HSScreen(screenshotEnabled = false) {
     @Composable
     override fun GetContent(navController: HSNavigation) {
-        RestoreFromPasskeyNavHost(navController, input)
+        val viewModel = viewModel<RestoreFromPasskeyViewModel>(
+            factory = RestoreFromPasskeyViewModel.Factory()
+        )
+
+        val accountType = remember { viewModel.getAccountType(input.entropy) }
+        val accountName = remember { viewModel.getAccountName(input.accountName) }
+
+        restore_select_coins(
+            input = ManageAccountsModule.Input(input.popOffOnSuccess, input.popOffInclusive),
+            accountType = accountType,
+            accountName = accountName,
+            manualBackup = true,
+            fileBackup = false,
+            statPage = StatPage.ImportWalletFromPasskey
+        ).GetContent(navController)
     }
 
     @Serializable
@@ -26,51 +38,5 @@ data class RestoreFromPasskeyFragment(val input: Input) : HSScreen(screenshotEna
         val popOffInclusive: Boolean,
         val entropy: ByteArray,
         val accountName: String?
-    )
-}
-
-@Composable
-private fun RestoreFromPasskeyNavHost(
-    navController: HSNavigation,
-    input: RestoreFromPasskeyFragment.Input,
-) {
-    val viewModel = viewModel<RestoreFromPasskeyViewModel>(
-        factory = RestoreFromPasskeyViewModel.Factory()
-    )
-
-    val mainViewModel: RestoreViewModel = viewModel {
-        val accountType = viewModel.getAccountType(input.entropy)
-        val accountName = viewModel.getAccountName(input.accountName)
-
-        RestoreViewModel(
-            accountType = accountType,
-            accountName = accountName,
-            manualBackup = true,
-            fileBackup = false,
-            statPage = StatPage.ImportWalletFromPasskey
-        )
-    }
-
-    val uuid = rememberSaveable { UUID.randomUUID().toString() }
-    ResultEffect<RestoreBirthdayHeightPage.Result>(resultKeyUuid = uuid) {
-        val config = it.config
-        if (config != null) {
-            mainViewModel.setBirthdayHeightConfig2(config)
-        } else {
-            mainViewModel.cancelBirthdayHeightConfig = true
-        }
-    }
-
-    ManageWalletsScreen(
-        mainViewModel = mainViewModel,
-        openBirthdayHeightConfigure = { token ->
-            val screen = RestoreBirthdayHeightPage(token.blockchainType)
-            screen.resultKey = uuid
-            navController.add(screen)
-        },
-        onBackClick = { navController.removeLastOrNull() },
-        onFinish = {
-            navController.removeLastUntil(input.popOffOnSuccess, input.popOffInclusive)
-        }
     )
 }
