27044a94b4f76c64aab99fb8c328788d0ea28889
Oleg Leonov <pomni.o.nas@gmail.com>
2026-05-19 10:39:44 +0700

    - Add lock state observation to cancel NFC session on app lock - Fix scan error for new firmware cards

--- feature-tangem/src/test/java/cash/p/terminal/tangem/di/FeatureTangemModuleTest.kt
+++ feature-tangem/src/test/java/cash/p/terminal/tangem/di/FeatureTangemModuleTest.kt
@@ -2,6 +2,7 @@ package cash.p.terminal.tangem.di
 
 import cash.p.terminal.tangem.domain.sdk.CardSdkProvider
 import io.horizontalsystems.core.BackgroundManager
+import io.horizontalsystems.core.IPinComponent
 import io.mockk.mockk
 import org.junit.Test
 import org.koin.dsl.koinApplication
@@ -14,7 +15,10 @@ class FeatureTangemModuleTest {
     fun featureTangemModule_resolvesCardSdkProvider() {
         val koin = koinApplication {
             modules(
-                module { single<BackgroundManager> { mockk(relaxed = true) } },
+                module {
+                    single<BackgroundManager> { mockk(relaxed = true) }
+                    single<IPinComponent> { mockk(relaxed = true) }
+                },
                 featureTangemModule,
             )
         }.koin
