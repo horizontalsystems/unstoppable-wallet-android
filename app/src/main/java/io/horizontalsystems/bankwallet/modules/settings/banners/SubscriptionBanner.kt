31b1bc2dc354b2c62b407190c8cafc68e10df4e6
abdrasulov <abdrasulov@gmail.com>
2026-05-25 23:41:52 +0600

    Migrate SubscriptionBanner, BackupRequiredAlert to Hilt

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/banners/SubscriptionBanner.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/settings/banners/SubscriptionBanner.kt
@@ -22,8 +22,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.ViewModelUiState
 import io.horizontalsystems.bankwallet.ui.compose.Bright
@@ -35,7 +37,7 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun SubscriptionBanner(onClick: () -> Unit) {
-    val viewModel = viewModel<SubscriptionBannerViewModel>()
+    val viewModel = hiltViewModel<SubscriptionBannerViewModel>()
     SubscriptionBannerView(
         onClick = onClick,
         hasFreeTrial = viewModel.uiState.hasFreeTrial
@@ -113,7 +115,8 @@ fun Preview_SubscriptionBanner() {
     }
 }
 
-class SubscriptionBannerViewModel : ViewModelUiState<SubscriptionBannerUiState>() {
+@HiltViewModel
+class SubscriptionBannerViewModel @Inject constructor() : ViewModelUiState<SubscriptionBannerUiState>() {
     private var hasFreeTrial = false
 
     init {
