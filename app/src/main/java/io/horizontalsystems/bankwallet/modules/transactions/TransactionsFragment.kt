6a69bdead22353522f92b790cc2c390d8ccbaae1
Rafael <rafaelekol@gmail.com>
2026-06-02 12:48:48 +0600

    Fix viewModel creation with nav context

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionsFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/transactions/TransactionsFragment.kt
@@ -1,14 +1,18 @@
 package io.horizontalsystems.bankwallet.modules.transactions
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.navGraphViewModels
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.BaseComposeFragment
 
 class TransactionsFragment : BaseComposeFragment() {
+    private val viewModel: TransactionsViewModel by navGraphViewModels(R.id.mainFragment) {
+        TransactionsModule.Factory()
+    }
+
     @Composable
     override fun GetContent(navController: NavController) {
-        val viewModel = viewModel<TransactionsViewModel>(factory = TransactionsModule.Factory())
         TransactionsScreen(navController, viewModel)
     }
 }
