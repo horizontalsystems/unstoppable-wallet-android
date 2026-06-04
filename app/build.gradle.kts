b4260b19fc92f927abf44e5feaa5b00d31a7442a
chyngyz <chyngyz.omurov@gmail.com>
2026-06-04 17:59:34 +0600

    Bump version to 0.48.5 (169)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.horizontalsystems.bankwallet"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.compileSdk.get().toInt()
-        versionCode = 168
-        versionName = "0.48.4"
+        versionCode = 169
+        versionName = "0.48.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resourceConfigurations += listOf("de", "es", "en", "fa", "fr", "ko", "pt", "pt-rBR", "ru", "tr", "zh")
