0f61cea06bc8bf7f1446d5692d71d014befe2251
chyngyz <chyngyz.omurov@gmail.com>
2026-07-22 11:46:34 +0600

    Update version data (0.49.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "io.horizontalsystems.bankwallet"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.compileSdk.get().toInt()
-        versionCode = 173
-        versionName = "0.49.2"
+        versionCode = 174
+        versionName = "0.49.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         resourceConfigurations += listOf("de", "es", "en", "fa", "fr", "ko", "pt", "pt-rBR", "ru", "tr", "zh")
